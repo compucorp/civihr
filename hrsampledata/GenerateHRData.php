4ab34d90fdef1e4b68fe448b9fc541e658bdb5ec
Ravish Nair <ravish.nair@webaccess.co.in>
2013-06-04 13:08:45 +0530

    -- CRM-12762 removed unwanted comments

--- hrsampledata/GenerateHRData.php
+++ hrsampledata/GenerateHRData.php
@@ -34,57 +34,7 @@
  */
 
 /**
- * This class generates data for the schema located in Contact.sql
- *
- * each public method generates data for the concerned table.
- * so for example the addContactDomain method generates and adds
- * data to the contact_domain table
- *
- * Data generation is a bit tricky since the data generated
- * randomly in one table could be used as a FKEY in another
- * table.
- *
- * In order to ensure that a randomly generated FKEY matches
- * a field in the referened table, the field in the referenced
- * table is always generated linearly.
- *
- *
- *
- *
- * Some numbers
- *
- * Domain ID's - 1 to NUM_DOMAIN
- *
- * Context - 3/domain
- *
- * Contact - 1 to NUM_CONTACT
- *           75% - Individual
- *           10% - Household
- *           10% - Organization
- *
- *           Contact to Domain distribution should be equal.
- *
- *
- * Contact Individual = 1 to 0.8*NUM_CONTACT
- *
- * Contact Household = 0.8*NUM_CONTACT to 0.9*NUM_CONTACT
- *
- * Contact Organization = 0.9*NUM_CONTACT to NUM_CONTACT
- *
- * Assumption is that each household contains 4 individuals
- *
- */
-
-/**
- *
- * Note: implication of using of mt_srand(1) in constructor
- * The data generated will be done in a consistent manner
- * so as to give the same data during each run (but this
- * would involve populating the entire db at one go - since
- * mt_srand(1) is in the constructor, if one needs to be able
- * to get consistent random numbers then the mt_srand(1) shld
- * be in each function that adds data to each table.
- *
+ * This class generates sample data for the CiviHR extension from sample_data.xml file
  */
 
 // autoload
@@ -948,12 +898,9 @@ class GenerateHRData {
 }
 
 
-echo ("Starting data generation on " . date("F dS h:i:s A") . "\n");
 $obj1 = new GenerateHRData();
 $obj1->initID();
 $obj1->generate('Contact');
 $obj1->generate('Individual');
 $obj1->generate('Organization');
 $obj1->generate('Note');
-
-echo ("Ending data generation on " . date("F dS h:i:s A") . "\n");
