d71efe7f471aebbbdc5ed6c3a7fab4be35c50d11
Tim Otten <totten@civicrm.org>
2013-06-15 12:26:26 +0200

    CRM-12808 - hrmed - Display profile in tab

--- hrmed/hrmed.php
+++ hrmed/hrmed.php
@@ -68,3 +68,30 @@ function hrmed_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 function hrmed_civicrm_managed(&$entities) {
   return _hrmed_civix_civicrm_managed($entities);
 }
+
+/**
+ * Implementation of hook_civicrm_tabs
+ */
+function hrmed_civicrm_tabs(&$tabs, $contactID) {
+  $cgid = hrmed_getCustomGroupId();
+  foreach ($tabs as $k => $v) {
+    if ($v['id'] == "custom_{$cgid}") {
+      $tabs[$k]['url'] = CRM_Utils_System::url('civicrm/profile/edit', array(
+        'reset' => 1,
+        'gid' => hrmed_getUFGroupID(),
+        'id' => $contactID,
+        'snippet' => 1,
+      ));
+    }
+  }
+}
+
+function hrmed_getCustomGroupId() {
+  $groups = CRM_Core_PseudoConstant::get('CRM_Core_BAO_CustomField', 'custom_group_id', array('labelColumn' => 'name'));
+  return array_search('Medical_Disability', $groups);
+}
+
+function hrmed_getUFGroupID() {
+  $groups = CRM_Core_PseudoConstant::get('CRM_Core_BAO_UFField', 'uf_group_id', array('labelColumn' => 'name'));
+  return array_search('hrmed_tab', $groups);
+}
