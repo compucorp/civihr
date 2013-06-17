d3ac83a19499aa7ad07889d46fb5020cec2b4346
Deepak Srivastava <deepak.srivastava@webaccess.co.in>
2013-06-17 13:41:38 +0530

    CRM-12806

--- hrui/hrui.php
+++ hrui/hrui.php
@@ -30,6 +30,33 @@ function hrui_civicrm_xmlMenu(&$files) {
  * Implementation of hook_civicrm_install
  */
 function hrui_civicrm_install() {
+  $domainID = CRM_Core_Config::domainID();
+  $options  = CRM_Core_OptionGroup::values('contact_view_options', TRUE, FALSE);
+  $tabsToUnset = array($options['Activities'], $options['Tags']);
+
+  // get tab options from DB
+  $params = array(
+    'version'   => 3,
+    'domain_id' => $domainID,
+    'return'    => 'contact_view_options',
+  );
+  $result   = civicrm_api('setting', 'get', $params);
+  $options  = array_flip($result['values'][$domainID]['contact_view_options']);
+  
+  // unset activity & tag tab options
+  foreach ($tabsToUnset as $key) {
+    unset($options[$key]);
+  }
+  $options = array_keys($options);
+  
+  // set modified options in the DB
+  $params = array(
+    'version'   => 3,
+    'domain_id' => $domainID,
+    'contact_view_options' => $options,
+  );
+  $result = civicrm_api('setting', 'create', $params);
+
   return _hrui_civix_civicrm_install();
 }
 
@@ -37,6 +64,33 @@ function hrui_civicrm_install() {
  * Implementation of hook_civicrm_uninstall
  */
 function hrui_civicrm_uninstall() {
+  $domainID = CRM_Core_Config::domainID();
+  $options  = CRM_Core_OptionGroup::values('contact_view_options', TRUE, FALSE);
+  $tabsToSet = array($options['Activities'], $options['Tags']);
+  
+  // get tab options from DB
+  $params = array(
+    'version'   => 3,
+    'domain_id' => $domainID,
+    'return'    => 'contact_view_options',
+  );
+  $result   = civicrm_api('setting', 'get', $params);
+  $options  = array_flip($result['values'][$domainID]['contact_view_options']);
+  
+  // set activity & tag tab options
+  foreach ($tabsToSet as $key) {
+    $options[$key] = 1;
+  }
+  $options = array_keys($options);
+  
+  // set modified options in the DB
+  $params = array(
+    'version'   => 3,
+    'domain_id' => $domainID,
+    'contact_view_options' => $options,
+  );
+  $result = civicrm_api('setting', 'create', $params);
+
   return _hrui_civix_civicrm_uninstall();
 }
 
