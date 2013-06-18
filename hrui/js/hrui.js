5f1fd6747d07f2875d32d0e903d43610065650d3
Deepak Srivastava <deepak.srivastava@webaccess.co.in>
2013-06-18 19:12:38 +0530

    CRM-12807

---
+++ hrui/js/hrui.js
@@ -0,0 +1,5 @@
+// js to hide current employer and job title
+cj(document).ready(function($) {
+  cj('.crm-contact-current_employer').parent().hide();
+  cj('.crm-contact-job_title').parent().hide();
+});
