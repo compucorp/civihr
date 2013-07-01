ec765ed40227511e57f9dae9f1e6c16e5e2b5ee6
Deepak Srivastava <deepak.srivastava@webaccess.co.in>
2013-07-01 17:27:37 +0530

    CRM-12877

--- hrreport/CRM/HRReport/Form/Contact/HRDetail.mgd.php
+++ hrreport/CRM/HRReport/Form/Contact/HRDetail.mgd.php
@@ -4,18 +4,36 @@
 // database as appropriate. For more details, see "hook_civicrm_managed" at:
 // http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
 return array (
-  0 =>
   array (
-    'name' => 'CiviHR Contact Detail Report',
+    'name' => 'CiviHR Contact Detail Report Template',
     'entity' => 'ReportTemplate',
     'params' =>
     array (
       'version' => 3,
       'label' => 'CiviHR Contact Detail Report',
-      'description' => 'Report has basic contact information with applicant details',
+      'description' => 'HR Report showing drilled down details at individual level. ',
       'class_name' => 'CRM_HRReport_Form_Contact_HRDetail',
       'report_url' => 'civihr/detail',
       'component' => '',
+    ),
+  ),
+  array (
+    'name'   => 'CiviHR Contact Detail Report',
+    'entity' => 'ReportInstance',
+    'params' =>
+    array (
+      'version' => 3,
+      'title'   => 'CiviHR Contact Detail Report',
+      'description' => 'HR Report showing drilled down details at individual level. ',
+      'report_id'   => 'civihr/detail',
+      'fields' => array(
+        'id'  => 1,
+        'sort_name' => 1,
+        'email'     => 1,
+        'custom_14' => 1,//FIXME: custom name - this is sth we need to find better alternative for
+        'state_province_id' => 1,
+        'country_id' => 1,
+      ),
     ),
   ),
 );
