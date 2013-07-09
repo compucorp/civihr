1df990209c557197a5d91790582521e6b25b58f7
Tim Otten <totten@civicrm.org>
2013-07-08 17:25:04 -0700

    CRM-12932 - Tree.View - Generate dummy tree

--- hrjob/js/jobtabapp/tree/tree_views.js
+++ hrjob/js/jobtabapp/tree/tree_views.js
@@ -1,5 +1,43 @@
 CRM.HRApp.module('JobTabApp.Tree', function(Tree, HRApp, Backbone, Marionette, $, _){
   Tree.View = Marionette.ItemView.extend({
-    template: '#hrjob-tree-template'
+    template: '#hrjob-tree-template',
+    onRender: function() {
+      var jsTreeData = {
+        data: "Jobs",
+        state: 'open',
+        children: [
+          {
+            data: 'Break Dancer',
+            state: 'open',
+            children: [
+              {data: 'Hours'},
+              {data: 'Pay'},
+              {data: 'Pension'}
+            ]
+          },
+          {
+            data: 'Violinist',
+            state: 'open',
+            children: [
+              {data: 'Hours'},
+              {data: 'Pay'},
+              {data: 'Pension'}
+            ]
+          }
+        ]
+      };
+      this.$el.jstree({
+        'json_data': {
+          data: jsTreeData
+        },
+        'themes': {
+          "theme": 'classic',
+          "dots": false,
+          "icons": false,
+          "url": CRM.config.resourceBase + 'packages/jquery/plugins/jstree/themes/classic/style.css'
+        },
+        'plugins': ['themes', 'json_data', 'ui', 'search']
+      });
+    }
   });
 });
