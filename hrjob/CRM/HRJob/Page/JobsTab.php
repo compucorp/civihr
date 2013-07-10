a3ee40630c463173bbba5a03a6b8cc676dd60f4d
Tim Otten <totten@civicrm.org>
2013-07-10 15:00:52 -0700

    CRM-12923 - Remove dead code/comments

--- hrjob/CRM/HRJob/Page/JobsTab.php
+++ hrjob/CRM/HRJob/Page/JobsTab.php
@@ -7,9 +7,6 @@ class CRM_HRJob_Page_JobsTab extends CRM_Core_Page {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(ts('JobsTab'));
 
-    // Example: Assign a variable for use in a template
-    $this->assign('currentTime', date('Y-m-d H:i:s'));
-
     self::registerScripts();
     parent::run();
   }
@@ -37,8 +34,6 @@ class CRM_HRJob_Page_JobsTab extends CRM_Core_Page {
       ->addScriptFile('civicrm', 'packages/backbone/underscore.js', 110, 'html-header', FALSE)
       ->addScriptFile('civicrm', 'packages/backbone/backbone.js', 120, 'html-header')
       ->addScriptFile('civicrm', 'packages/backbone/backbone.marionette.js', 125, 'html-header', FALSE)
-      //->addScriptFile('civicrm', 'packages/jquery/plugins/jstree/jquery.jstree.js', 0, 'html-header', FALSE)
-      //->addStyleFile('civicrm', 'packages/jquery/plugins/jstree/themes/default/style.css', 0, 'html-header')
       ->addStyleFile('org.civicrm.hrjob', 'css/hrjob.css', 140, 'html-header')
       ->addScriptFile('org.civicrm.hrjob', 'js/hrapp.js', 150, 'html-header')
       ->addScriptFile('org.civicrm.hrjob', 'js/entities/hrjob.js', 155, 'html-header')
@@ -56,24 +51,6 @@ class CRM_HRJob_Page_JobsTab extends CRM_Core_Page {
         ->addScriptFile('org.civicrm.hrjob', "js/jobtabapp/$module/edit_views.js", 160, 'html-header')
         ;
     }
-
-/*
-      ->addScriptFile('org.civicrm.hrjob', 'js/jobtabapp/general/edit_controller.js', 160, 'html-header')
-      ->addScriptFile('org.civicrm.hrjob', 'js/jobtabapp/general/edit_views.js', 160, 'html-header')
-      ->addScriptFile('org.civicrm.hrjob', 'js/jobtabapp/role/edit_controller.js', 160, 'html-header')
-      ->addScriptFile('org.civicrm.hrjob', 'js/jobtabapp/role/edit_views.js', 160, 'html-header')
-      ;
-    /*
-      ->addScriptFile('civicrm', 'js/crm.backbone.js', 150)
-      ->addScriptFile('civicrm', 'js/model/crm.schema-mapped.js', 200)
-      ->addScriptFile('civicrm', 'js/model/crm.uf.js', 200)
-      ->addScriptFile('civicrm', 'js/model/crm.designer.js', 200)
-      ->addScriptFile('civicrm', 'js/model/crm.profile-selector.js', 200)
-      ->addScriptFile('civicrm', 'js/view/crm.designer.js', 200)
-      ->addScriptFile('civicrm', 'js/view/crm.profile-selector.js', 200)
-      ->addScriptFile('civicrm', 'js/jquery/jquery.crmProfileSelector.js', 250)
-      ->addScriptFile('civicrm', 'js/crm.designerapp.js', 250)
-    */
 
     $templateDir = CRM_Extension_System::singleton()->getMapper()->keyToBasePath('org.civicrm.hrjob') . '/templates/';
     $region = CRM_Core_Region::instance('page-header');
