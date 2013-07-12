8dfb0cf2a31afb5f287e08ff54d26711de2f99a8
Ravish Nair <ravish.nair@webaccess.co.in>
2013-07-12 16:40:42 +0530

    -- CRM-12914 made email addresses clickable in search results

---
+++ hrstaffdir/js/hrstaffdir.js
@@ -0,0 +1,12 @@
+//js to make email addresses clickable in search results
+cj(function($) {
+  if ($('tr:has(td.crm-email)').length > 0) {
+    $(".crm-search-results td.crm-email").each(function() {
+      if ($(this).text()) {
+        var email = $(this).text();
+        var clickableEmail = '<a href="mailto:' + email + '">' + email + '</a>';
+        $(this).html(clickableEmail);
+      }
+    });
+  }
+});
