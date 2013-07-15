560c16139fb8b06be3616d08bb30fb86e1040a9e
Ravish Nair <ravish.nair@webaccess.co.in>
2013-07-15 18:54:03 +0530

    -- CRM-12863 Hide profile titles in HR tabs

--- hrmed/hrmed.php
+++ hrmed/hrmed.php
@@ -85,6 +85,7 @@ function hrmed_civicrm_tabs(&$tabs, $contactID) {
       ));
     }
   }
+  CRM_Core_Resources::singleton()->addStyleFile('org.civicrm.hrmed', 'css/hrmed.css');
 }
 
 function hrmed_getCustomGroupId() {
