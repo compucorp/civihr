1d5b80e7ed351c4051c7223e63f721656fea036b
Tim Otten <totten@civicrm.org>
2013-07-23 10:40:00 -0700

    CRM-12943 - Fix rendering of boolean checkboxes

--- hrjob/js/common/mbind.js
+++ hrjob/js/common/mbind.js
@@ -1,6 +1,28 @@
 CRM.HRApp.module('Common', function(Common, HRApp, Backbone, Marionette, $, _) {
 
   /**
+   * Converter for checkbox/boolean fields
+   *
+   * @param direction
+   * @param value
+   * @return {*}
+   */
+  Common.convertCheckbox = function(direction, value) {
+    switch (direction) {
+      case 'ModelToView':
+        if (value == "0" || value == 0) return false;
+        if (value == "1" || value == 1) return true;
+        return value;
+        break;
+      case 'ViewToModel':
+        return value ? 1 : 0;
+        break;
+      default:
+        throw "Invalid direction"
+    }
+  };
+
+  /**
    * Marionette-ModelBinding helper
    *
    * Register a modelBinder for use with a Marionette view. The ModelBinder will
@@ -17,12 +39,25 @@ CRM.HRApp.module('Common', function(Common, HRApp, Backbone, Marionette, $, _) {
   Common.mbind = function(view) {
     var modelBinder = new Backbone.ModelBinder();
 
+    // ModelBinder - Some bindings to use in all our views
+    var booleanFieldRE = /^is_/;
+    var onBindingCreate = function(bindings) {
+      _.each(bindings, function(value, key){
+        if (booleanFieldRE.test(key)) {
+          value.converter = Common.convertCheckbox;
+        }
+      });
+    };
+
+    // View - onRender listener
     var onRender = function() {
       var bindings = Backbone.ModelBinder.createDefaultBindings(this.el, view.bindingAttribute || 'name');
+      onBindingCreate(bindings); // Apply some changes to bindings across all our views
       view.triggerMethod.call(view, 'binding:create', bindings);
       modelBinder.bind(this.model, this.el, bindings);
     };
 
+    // View - onClose listener
     var onClose = function() {
       modelBinder.unbind();
     };
