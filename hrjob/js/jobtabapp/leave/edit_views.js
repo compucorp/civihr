0d720e42c67ef709f5505c4ece9ad0053410dae5
Tim Otten <totten@civicrm.org>
2013-07-27 20:21:15 -0700

    CRM-12923, CRM-12943 - jobtabapp/leave - Rough draft of CRUD

--- hrjob/js/jobtabapp/leave/edit_views.js
+++ hrjob/js/jobtabapp/leave/edit_views.js
@@ -1,4 +1,4 @@
-CRM.HRApp.module('JobTabApp.Leave', function(Leave, HRApp, Backbone, Marionette, $, _){
+CRM.HRApp.module('JobTabApp.Leave', function(Leave, HRApp, Backbone, Marionette, $, _) {
   Leave.RowView = Marionette.ItemView.extend({
     tagName: 'tr',
     template: '#hrjob-leave-template',
@@ -7,6 +7,16 @@ CRM.HRApp.module('JobTabApp.Leave', function(Leave, HRApp, Backbone, Marionette,
         'RenderUtil': CRM.HRApp.RenderUtil,
         'FieldOptions': CRM.FieldOptions.HRJobLeave
       };
+    },
+    initialize: function() {
+      CRM.HRApp.Common.mbind(this);
+    },
+    onBindingCreate: function(bindings) {
+      var suffix = '_' + this.model.get('leave_type');
+      _.each(['leave_type', 'leave_amount'], function(field){
+        bindings[field] = bindings[field + suffix];
+        delete bindings[field + suffix];
+      });
     }
   });
 
@@ -19,6 +29,41 @@ CRM.HRApp.module('JobTabApp.Leave', function(Leave, HRApp, Backbone, Marionette,
         'RenderUtil': CRM.HRApp.RenderUtil,
         'FieldOptions': CRM.FieldOptions.HRJobLeave
       };
+    },
+    events: {
+      'click .standard-save': 'doSave',
+      'click .standard-reset': 'doReset'
+    },
+    doSave: function() {
+      var view = this;
+      HRApp.trigger('ui:block', ts('Saving'));
+      view.collection.save({
+        success: function() {
+          HRApp.trigger('ui:unblock');
+          CRM.alert('Saved');
+          view.triggerMethod('standard:save', view, view.model);
+        },
+        error: function() {
+          HRApp.trigger('ui:unblock');
+          // Note: CRM.Backbone.sync displays API errors with CRM.alert
+        }
+      });
+    },
+    doReset: function() {
+      var view = this;
+      HRApp.trigger('ui:block', ts('Loading'));
+      this.collection.fetch({
+        reset: true,
+        success: function() {
+          HRApp.trigger('ui:unblock');
+          CRM.alert('Reset');
+          view.triggerMethod('standard:reset', view, view.model);
+        },
+        error: function() {
+          HRApp.trigger('ui:unblock');
+          // Note: CRM.Backbone.sync displays API errors with CRM.alert
+        }
+      });
     }
   });
 });
