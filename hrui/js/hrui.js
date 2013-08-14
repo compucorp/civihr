c51e4fc69c265af38ad14a604bf3d11e2b82a449
vivekarora <vivek11112@gmail.com>
2013-08-14 14:23:40 +0530

    CRM-13176-Enhance styling of 'View Contact' tabs to indicate empty/non-empty tags- corrected formatting

--- hrui/js/hrui.js
+++ hrui/js/hrui.js
@@ -1,24 +1,24 @@
 // js to hide current employer and job title from contact view screen
 cj(document).ready(function($) {
-    cj('.crm-contact-current_employer').parent('div.crm-summary-row').hide();
-    cj('.crm-contact-job_title').parent('div.crm-summary-row').hide();
+  cj('.crm-contact-current_employer').parent('div.crm-summary-row').hide();
+  cj('.crm-contact-job_title').parent('div.crm-summary-row').hide();
 
-    //Enhance styling of "View Contact" tabs to indicate empty/non-empty tags
-    cj('div#mainTabContainer ul').find('li').each(function(n){
-        if (cj(this).find('em').html()==0) {
-            cj(this).addClass("disabled");
-        }
-    });
+  //Enhance styling of "View Contact" tabs to indicate empty/non-empty tags
+  cj('div#mainTabContainer ul').find('li').each(function(n){
+    if(cj(this).find('em').html()==0){
+      cj(this).addClass("disabled");
+    }
+  });
     
-    //rename "Summary" tab to "Contact Details"
-    $('#tab_summary a').text('Contact Details');
-    
+  //rename "Summary" tab to "Contact Details"
+  $('#tab_summary a').text('Contact Details');
+  
 });
 // for inline edit
 cj(document).ajaxSuccess(function() {
-    cj('#current_employer').parent('div.crm-content').parent('div.crm-summary-row').hide();
-    cj('#job_title').parent('div.crm-content').parent('div.crm-summary-row').children('div.crm-label').children('label[for="job_title"]').parent().hide();
-    cj('#job_title').parent('div.crm-content').hide();;
+  cj('#current_employer').parent('div.crm-content').parent('div.crm-summary-row').hide();
+  cj('#job_title').parent('div.crm-content').parent('div.crm-summary-row').children('div.crm-label').children('label[for="job_title"]').parent().hide();
+  cj('#job_title').parent('div.crm-content').hide();;
 });
 // for contact edit screen
 cj(document).ready(function($) {
