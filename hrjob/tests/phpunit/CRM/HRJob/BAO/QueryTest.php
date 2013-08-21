8f960160ad21a88f31b31a0d0fae0b395c581f6e
Ravish Nair <ravish.nair@webaccess.co.in>
2013-08-21 17:30:08 +0530

    -- CRM-13080 implemented unit test for HRJob advanced search fields

---
+++ hrjob/tests/phpunit/CRM/HRJob/BAO/QueryTest.php
@@ -0,0 +1,67 @@
+<?php
+require_once 'CiviTest/CiviUnitTestCase.php';
+
+/**
+ *  Include dataProvider for tests
+ */
+class CRM_HRJob_BAO_QueryTest extends CiviUnitTestCase {
+  function get_info() {
+    return array(
+      'name' => 'HRJob BAO Query',
+      'description' => 'Test all HRJob_BAO_Query methods.',
+      'group' => 'CiviCRM BAO Query Tests',
+    );
+  }
+
+  public function dataProvider() {
+    return new CRM_HRJob_BAO_QueryTestDataProvider;
+  }
+
+  function setUp() {
+    parent::setUp();
+  }
+
+  function tearDown() {
+    $tablesToTruncate = array(
+      'civicrm_hrjob',
+      'civicrm_hrjob_health',
+      'civicrm_hrjob_hour',
+      'civicrm_hrjob_leave',
+      'civicrm_hrjob_pay',
+      'civicrm_hrjob_pension',
+      'civicrm_hrjob_role',
+      'civicrm_contact',
+     );
+    $this->quickCleanup($tablesToTruncate);
+  }
+
+  /**
+   *  Test CRM_Contact_BAO_Query::searchQuery()
+   *  @dataProvider dataProvider
+   */
+  function testSearch($fv, $count, $ids, $full) {
+    $op = new PHPUnit_Extensions_Database_Operation_Insert();
+    $op->execute($this->_dbconn,
+      new PHPUnit_Extensions_Database_DataSet_FlatXMLDataSet(
+        dirname(__FILE__) . '/queryDataset.xml'
+      )
+    );
+
+    $params = CRM_Contact_BAO_Query::convertFormValues($fv);
+    $obj    = new CRM_Contact_BAO_Query($params);
+
+    $obj->_useGroupBy = TRUE;
+
+    $dao    = $obj->searchQuery();
+
+    $contacts = array();
+    while ($dao->fetch()) {
+      $contacts[] = $dao->contact_id;
+    }
+
+    sort($contacts, SORT_NUMERIC);
+
+    $this->assertEquals($ids, $contacts, 'In line ' . __LINE__);
+  }
+}
+
---
+++ hrjob/tests/phpunit/CRM/HRJob/BAO/QueryTestDataProvider.php
@@ -0,0 +1,252 @@
+<?php
+/**
+ *  Provide data to the CRM_HRJob_BAO_QueryTest class
+ *
+ *  @package CiviCRM
+ */
+class CRM_HRJob_BAO_QueryTestDataProvider implements Iterator {
+
+  /**
+   *  @var integer
+   */
+  private $i = 0;
+
+  /**
+   *  @var mixed[]
+   *  This dataset describes various form values and what contact
+   *  IDs should be selected when the form values are applied to the
+   *  database in dataset.xml
+   */
+  private $dataset = array(
+    array(
+      'fv' => array(
+        'hrjob_position' => 'Fundraiser',
+        'hrjob_title' => 'Copy-editor'
+      ),
+      'id' => array(
+        '9','10',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_level_type' => 'Senior Manager',
+        'hrjob_period_type' => 'Permanent',
+      ),
+      'id' => array(
+        '9',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_is_healthcare' => 1,
+      ),
+      'id' => array(
+        '9', '11',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_is_healthcare' => 1,
+        'hrjob_contract_type' => "Apprentice",
+        'hrjob_title' => "Chief Nomenclature Officer",
+      ),
+      'id' => array(
+        '11',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_contract_type' => "Apprentice",
+      ),
+      'id' => array(
+        '10', '11',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_is_enrolled' => 1,
+        'hrjob_contract_type' => "Apprentice",
+      ),
+      'id' => array(
+        '11',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_pay_grade' => "Paid",
+      ),
+      'id' => array(
+        '9', '10', '11',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_is_enrolled' => 1,
+      ),
+      'id' => array(
+        '9', '11',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_is_enrolled' => 0,
+      ),
+      'id' => array(
+        '10',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_is_enrolled' => 0,
+        'hrjob_pay_grade' => "Paid",
+      ),
+      'id' => array(
+        '10',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_hours_amount_low' => 20,
+        'hrjob_hours_amount_high' => 100,
+      ),
+      'id' => array(
+        '9', '10', '11',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_period_start_date_low' => "01/24/2010",
+        'hrjob_period_start_date_high' => "04/24/2010",
+        'hrjob_period_end_date_low' => "07/27/2013",
+        'hrjob_period_end_date_high' => "09/27/2013",
+      ),
+      'id' => array(
+        '9', '11',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_hours_fte_low' => 1,
+        'hrjob_hours_fte_high' => 10,
+      ),
+      'id' => array(
+        '9', '10', '11',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_hours_type' => "casual",
+      ),
+      'id' => array(
+        '9', '10',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_hours_unit' => "Week",
+      ),
+      'id' => array(
+        '9', '10', '11',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_hours_unit' => "Week",
+        'hrjob_hours_type' => "part",
+      ),
+      'id' => array(
+        '11',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_hours_unit' => "Week",
+        'hrjob_hours_type' => "casual",
+        'hrjob_hours_fte_low' => 2,
+        'hrjob_hours_fte_high' => 20,
+      ),
+      'id' => array(
+        '9', '10',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_period_start_date_low' => "01/24/2010",
+        'hrjob_period_start_date_high' => "04/24/2010",
+        'hrjob_period_end_date_low' => "07/27/2013",
+        'hrjob_period_end_date_high' => "09/27/2013",
+        'hrjob_is_enrolled' => 1,
+        'hrjob_pay_grade' => "Paid",
+        'hrjob_is_healthcare' => 1,
+      ),
+      'id' => array(
+        '9', '11',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_level_type' => 'Junior Staff',
+        'hrjob_period_type' => 'Temporary',
+        'hrjob_hours_type' => "casual",
+        'hrjob_pay_grade' => "Paid",
+      ),
+      'id' => array(
+        '10',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_pay_grade' => "Unpaid",
+        'hrjob_contract_type' => "Intern",
+      ),
+      'id' => array(
+        '9',
+      ),
+    ),
+    array(
+      'fv' => array(
+        'hrjob_pay_grade' => "Unpaid",
+        'hrjob_position' => "Volunteer Manager",
+        'hrjob_title' => "Research Assistant",
+      ),
+      'id' => array(
+        '9',
+      ),
+    ),
+  );
+
+  public function _construct() {
+    $this->i = 0;
+  }
+
+  public function rewind() {
+    $this->i = 0;
+  }
+
+  public function current() {
+    $count = count($this->dataset[$this->i]['id']);
+    $ids   = $this->dataset[$this->i]['id'];
+    $full  = array();
+    foreach ($this->dataset[$this->i]['id'] as $key => $value) {
+      $full[] = array(
+        'contact_id' => $value,
+        'contact_type' => 'Individual',
+        'sort_name' => "Test Contact $value",
+      );
+    }
+    return array($this->dataset[$this->i]['fv'], $count, $ids, $full);
+  }
+
+  public function key() {
+    return $this->i;
+  }
+
+  public function next() {
+    $this->i++;
+  }
+
+  public function valid() {
+    return isset($this->dataset[$this->i]);
+  }
+}
+
