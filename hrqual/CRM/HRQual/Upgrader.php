51ae0bdc4b21168f46cd1db734caaaa25f61cff7
Nileema <nileema@nileema.(none)>
2013-11-06 13:00:00 +0530

    -- CRM-13624 Display paper clip icon for file field on View contact screen

--- hrqual/CRM/HRQual/Upgrader.php
+++ hrqual/CRM/HRQual/Upgrader.php
@@ -138,4 +138,22 @@ class CRM_HRQual_Upgrader extends CRM_HRQual_Upgrader_Base {
     return TRUE;
   } // */
 
+  public function upgrade_4415() {
+    $this->ctx->log->info('Planning update 4415'); // PEAR Log interface
+    $groups = CRM_Core_PseudoConstant::get('CRM_Core_BAO_UFField', 'uf_group_id', array('labelColumn' => 'name'));
+    $gid = array_search('hrqual_tab', $groups);
+    $params = array(
+      'action' => 'submit',
+      'profile_id' => $gid,
+    );
+    $result = civicrm_api3('profile', 'getfields', $params);
+    if($result['is_error'] == 0 ) {
+      foreach($result['values'] as $key => $value) {
+        if(isset($value['html_type']) && $value['html_type'] == "File") {
+          CRM_Core_DAO::executeQuery("UPDATE civicrm_uf_field SET is_multi_summary = 1 WHERE civicrm_uf_field.uf_group_id = {$gid} AND civicrm_uf_field.field_name = '{$key}'");
+        }
+      }
+    }
+    return TRUE;
+  }
 }
