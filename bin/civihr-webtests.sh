16ffd5225fd69856bf6635c88f3a9b6f8a582311
Nileema <nileema@nileema.(none)>
2013-11-21 18:41:32 +0530

    -- HR-186 Modification to shell scripts

--- bin/civihr-webtests.sh
+++ bin/civihr-webtests.sh
@@ -10,12 +10,29 @@ hrqual \
 hrstaffdir \
 hrvisa \
 )
+CONF=`dirname $0`/setup.conf
+if [ -f "$CONF" ]; then
+  source "$CONF"
+fi
 ##################################
+set -x
 
 for var in "${ENTITY_EXTS[@]}"
   do
-    cd ../${var}
-    set -ex
-      civix test WebTest_AllTests
-    set +ex
+    pushd $CIVIHRDIR/${var}
+      set +e
+      pushd "$CIVISOURCEDIR/tools"
+        ./scripts/phpunit \
+          --include-path "$CIVIHRDIR/${var}/tests/phpunit" \
+          --tap \
+          --log-junit "$CIVISOURCEDIR/build/junit-${var}-CRM_AllTests.xml" \
+          WebTest_AllTests
+      popd
+      [ $? ] && HASERROR=1
+      set -e
+    popd    
 done
+
+if [ -n $HASERROR ]; then
+  exit 1
+fi
