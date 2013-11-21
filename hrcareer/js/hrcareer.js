d4bd361a49fecdc7f5ec1261cb628474f4a8ae4f
Dakshayani Chinchwadkar <dakshayani@dakshayani.(none)>
2013-11-21 15:45:13 +0530

    -- HR-8 for help icon

---
+++ hrcareer/js/hrcareer.js
@@ -0,0 +1,13 @@
+// Copyright CiviCRM LLC 2013. See http://civicrm.org/licensing
+cj(function($) {
+  // add helpicon for conitions
+  $(document).ajaxSuccess(function() {
+    if($(this).find("div#profile-dialog").length) {
+      var accessName = $('[data-crm-custom="Career:End_Date"]').attr('name');
+      if($('div#editrow-' + accessName + ' a.helpicon').length == 0) {
+        var helpIcon = $( "<span class ='crm-container'><a class='helpicon' onclick='CRM.help(\"\", {\"id\":\"access-enddate\",\"file\":\"CRM\/HRCareer\/Page\/helptext\"}); return false;'></a></span>" );
+        $('div#editrow-' + accessName +' div label').append(helpIcon);
+      }
+    }
+  });
+});
