3cf757d7e7330995d4530e330b1c86521e489841
Nileema <nileema@nileema.(none)>
2013-11-23 19:15:10 +0530

    -- HR-186 Modifcition to romove hrvisa CRM test

--- bin/civihr-crm-api-tests.sh
+++ bin/civihr-crm-api-tests.sh
@@ -58,7 +58,7 @@ fi
 runTest hrreport org.civicrm.hrjob,org.civicrm.hrreport CRM_AllTests
 runTest hrjob org.civicrm.hrjob api_v3_AllTests
 runTest hrjob org.civicrm.hrjob CRM_AllTests
-runTest hrvisa org.civicrm.hrjob,org.civicrm.hrvisa CRM_AllTests
+#runTest hrvisa org.civicrm.hrjob,org.civicrm.hrvisa CRM_AllTests
 
 if [ -f "${DUMP}" ] ; then
   rm "${DUMP}"
