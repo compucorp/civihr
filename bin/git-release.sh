dd77984e7928214254379a54f662513640dbb062
Nileema <nileema@nileema.(none)>
2013-12-04 12:04:23 +0530

    -- Modifications to Script for performing CiviHR release

--- bin/git-release.sh
+++ bin/git-release.sh
@@ -19,19 +19,20 @@ hrui \
 hrvisa \
 )
 
-set -x
+set -ex
 version="$1"
 releaseDate="$2"
 if [ ! -n "$version" -o ! -n "$releaseDate" ]; then
+  echo "Please specify CiviHR Version and Release Date eg 'bash ./bin/git-release.sh <version> <yyyy-mm-dd>'"
   exit 1
 fi
 
 for var in "${ENTITY_EXTS[@]}"
-  do
-    CONF="${var}/info.xml"
-    sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${version}\2|" "$CONF"
-    sed -i "s|\(<releaseDate>\)[^<>]*\(</releaseDate>\)|\1${releaseDate}\2|" "$CONF"
-    fileName="$fileName ${var}"/info.xml
+do
+  CONF="${var}/info.xml"
+  sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${version}\2|" "$CONF"
+  sed -i "s|\(<releaseDate>\)[^<>]*\(</releaseDate>\)|\1${releaseDate}\2|" "$CONF"
+  fileName="$fileName ${var}"/info.xml
 done
-git commit -m 'Updation of CiviHR Version and Release date' $fileName
+git commit -m 'Update CiviHR Version and Release Date' $fileName
 git tag -a ${version} -m "HR version ${version}"
