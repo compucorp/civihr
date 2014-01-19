456794429aeb4ad2c34013e748df13cd7ce2a354
Tim Otten <totten@civicrm.org>
2014-01-18 18:07:04 -0800

    HRAbsenceType - Restore code which creates two activity types. Autoformat.

--- hrabsence/CRM/HRAbsence/BAO/HRAbsenceType.php
+++ hrabsence/CRM/HRAbsence/BAO/HRAbsenceType.php
@@ -28,38 +28,55 @@
 class CRM_HRAbsence_BAO_HRAbsenceType extends CRM_HRAbsence_DAO_HRAbsenceType {
 
   public static function create($params) {
-  	$entityName = 'HRAbsenceType';
+    $entityName = 'HRAbsenceType';
     $hook = empty($params['id']) ? 'create' : 'edit';
 
     if (is_numeric(CRM_Utils_Array::value('is_primary', $params)) || empty($params['id'])) {
       CRM_Core_BAO_Block::handlePrimary($params, get_class());
     }
     if (!array_key_exists('name', $params) && !array_key_exists('id', $params)) {
-    	$params['name'] = CRM_Utils_String::munge($params['title']);
+      $params['name'] = CRM_Utils_String::munge($params['title']);
     }
-
-    $result = civicrm_api3('activity_type', 'get', array());
-    if (!CRM_Utils_Array::value('is_error', $result)) {
-      $weight = count($result["values"]);
-      $activityTypeId = array_search($params["name"], $result["values"]);
-      if(!$activityTypeId) {
-        $weight = $weight+1;
-        $paramsCreate = array(
-          'weight' => $weight,
-          'label' => $params["name"],
-          'filter' => 0,
-          'is_active' => 1,
-          'is_optgroup' => 0,
-          'is_default' => 0,
-        );
-        $resultCreateActivityType = civicrm_api3('activity_type', 'create', $paramsCreate);
-        $activityTypeId = $resultCreateActivityType["id"];
+    if (CRM_Utils_Array::value('allow_debits', $params, TRUE)) {
+      $result = civicrm_api3('activity_type', 'get', array());
+      if (!CRM_Utils_Array::value('is_error', $result)) {
+        $weight = count($result["values"]);
+        $activityTypeId = array_search($params["name"], $result["values"]);
+        if (!$activityTypeId) {
+          $weight = $weight + 1;
+          $paramsCreate = array(
+            'weight' => $weight,
+            'label' => $params["name"],
+            'filter' => 0,
+            'is_active' => 1,
+            'is_optgroup' => 0,
+            'is_default' => 0,
+          );
+          $resultCreateActivityType = civicrm_api3('activity_type', 'create', $paramsCreate);
+          $activityTypeId = $resultCreateActivityType["id"];
+        }
+        $params["debit_activity_type_id"] = $activityTypeId;
       }
-      if(CRM_Utils_Array::value('allow_debits', $params, TRUE)) {
-        $params["debit_activity_type_id"] = $activityTypeId;        
-      }
-      if(CRM_Utils_Array::value('allow_credits', $params)) {
-        $params["credit_activity_type_id"] = $activityTypeId;        
+    }
+    if (CRM_Utils_Array::value('allow_credits', $params)) {
+      $result = civicrm_api3('activity_type', 'get', array());
+      if (!CRM_Utils_Array::value('is_error', $result)) {
+        $weight = count($result["values"]);
+        $activityTypeId = array_search($params["name"], $result["values"]);
+        if (!$activityTypeId) {
+          $weight = $weight + 1;
+          $paramsCreate = array(
+            'weight' => $weight,
+            'label' => $params["name"],
+            'filter' => 0,
+            'is_active' => 1,
+            'is_optgroup' => 0,
+            'is_default' => 0,
+          );
+          $resultCreateActivityType = civicrm_api3('activity_type', 'create', $paramsCreate);
+          $activityTypeId = $resultCreateActivityType["id"];
+        }
+        $params["credit_activity_type_id"] = $activityTypeId;
       }
     }
     CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
@@ -82,5 +99,7 @@ class CRM_HRAbsence_BAO_HRAbsenceType extends CRM_HRAbsence_DAO_HRAbsenceType {
     $dao = new CRM_HRAbsence_DAO_HRAbsenceType();
     $dao->copyValues($params);
     return $dao->count();
-  }  
+  }
+
+
 }
