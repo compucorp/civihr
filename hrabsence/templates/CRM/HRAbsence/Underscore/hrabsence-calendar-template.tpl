93a2a44afd063d231a253a5f2b06dcf1fb877d79
Tim Otten <totten@civicrm.org>
2014-01-27 08:28:24 +0000

    CalendarView - Display columns (day-of-month), rows (month-in-period), and cells (day-of-week)

--- hrabsence/templates/CRM/HRAbsence/Underscore/hrabsence-calendar-template.tpl
+++ hrabsence/templates/CRM/HRAbsence/Underscore/hrabsence-calendar-template.tpl
@@ -1,6 +1,33 @@
 {literal}
 <script id="hrabsence-calendar-template" type="text/template">
-  TODO: Calendar view for <span class="activity-count"></span> item(s).
+
+  <table class="hrabsence-calendar">
+    <thead>
+      <tr>
+        <th>{/literal}{ts}Month{/ts}{literal}</th>
+        <% for (var i = 1; i<= 31; i++) { %>
+        <th><%= i %></th>
+        <% } %>
+      </tr>
+    </thead>
+    <tbody>
+      <% _.each(active_period_ids, function(periodId) { %>
+        <%
+        var month = CRM.HRAbsenceApp.moment(periods[periodId].start_date).date(1);
+        var end = CRM.HRAbsenceApp.moment(periods[periodId].end_date);
+        for (; !month.isAfter(end); month.add(1, 'months')) {
+        %>
+        <tr>
+          <td><%- month.format('MMM YYYY') %></td>
+          <% for (var i = 1; i<= 31; i++) { %>
+            <% var date = month.clone().date(i); %>
+          <td><%= date.format('dd') %></td>
+          <% } %>
+        </tr>
+        <% } %>
+      <% }); %>
+    </tbody>
+  </table>
 
   <% var legendPos =0, legendCount = _.size(CRM.absenceApp.legend); %>
   <table class="hrabsence-legend">
--- hrabsence/tests/qunit/hrabsenceapp/test-calendar.js
+++ hrabsence/tests/qunit/hrabsenceapp/test-calendar.js
@@ -6,6 +6,9 @@ module('CRM.HRAbsenceApp.Calendar.CalendarView', {
 
 test("Check Legend", function() {
   CRM.HRAbsenceApp.contentRegion.show(new CRM.HRAbsenceApp.Calendar.CalendarView({
+    criteria: new CRM.HRAbsenceApp.Models.AbsenceCriteria({
+      period_id: [3]
+    }),
     collection: new CRM.HRAbsenceApp.Models.AbsenceCollection(CRM.fixtures.get(['vacationFeb2013', 'toilFeb2013']))
   }));
   var $el = CRM.HRAbsenceApp.contentRegion.$el;
@@ -13,4 +16,37 @@ test("Check Legend", function() {
   assertLike(cj($el.find('.hrabsence-bg-0-debit ~ td:first')), 'Vacation');
   assertLike(cj($el.find('.hrabsence-bg-1-debit ~ td:first')), 'TOIL');
   assertLike(cj($el.find('.hrabsence-bg-1-credit ~ td:first')), 'TOIL (Credit)');
+});
+
+test("Check Rows - Single Period", function() {
+  CRM.HRAbsenceApp.contentRegion.show(new CRM.HRAbsenceApp.Calendar.CalendarView({
+    criteria: new CRM.HRAbsenceApp.Models.AbsenceCriteria({
+      period_id: [3]
+    }),
+    collection: new CRM.HRAbsenceApp.Models.AbsenceCollection(CRM.fixtures.get(['vacationFeb2013', 'toilFeb2013']))
+  }));
+  var $el = CRM.HRAbsenceApp.contentRegion.$el;
+
+  equal(cj('table.hrabsence-calendar tr').length, 1+12); // header + FY2013
+  assertLike(cj('table.hrabsence-calendar tr:nth-child(1) td:first').text(), 'Apr 2013');
+  assertLike(cj('table.hrabsence-calendar tr:nth-child(2) td:first').text(), 'May 2013');
+  assertLike(cj('table.hrabsence-calendar tr:nth-child(12) td:first').text(), 'Mar 2014');
+});
+
+test("Check Rows - Two Periods", function() {
+  CRM.HRAbsenceApp.contentRegion.show(new CRM.HRAbsenceApp.Calendar.CalendarView({
+    criteria: new CRM.HRAbsenceApp.Models.AbsenceCriteria({
+      period_id: [2,3]
+    }),
+    collection: new CRM.HRAbsenceApp.Models.AbsenceCollection(CRM.fixtures.get(['vacationFeb2013', 'toilFeb2013']))
+  }));
+  var $el = CRM.HRAbsenceApp.contentRegion.$el;
+
+  equal(cj('table.hrabsence-calendar tr').length, 1+12+12); // header + FY2012 + FY2013
+  assertLike(cj('table.hrabsence-calendar tr:nth-child(1) td:first').text(), 'Apr 2012');
+  assertLike(cj('table.hrabsence-calendar tr:nth-child(2) td:first').text(), 'May 2012');
+  assertLike(cj('table.hrabsence-calendar tr:nth-child(12) td:first').text(), 'Mar 2013');
+  assertLike(cj('table.hrabsence-calendar tr:nth-child(13) td:first').text(), 'Apr 2013');
+  assertLike(cj('table.hrabsence-calendar tr:nth-child(14) td:first').text(), 'May 2013');
+  assertLike(cj('table.hrabsence-calendar tr:nth-child(24) td:first').text(), 'Mar 2014');
 });
