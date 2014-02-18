300abee1286b0d65a24e116782c06307c782ac44
Poonam <poonam@poonam.(none)>
2014-02-18 19:56:16 +0530

    --HR-271  Absence-request screen shows weird vertical bar

--- hrabsence/CRM/HRAbsence/Form/AbsenceRequest.php
+++ hrabsence/CRM/HRAbsence/Form/AbsenceRequest.php
@@ -348,10 +348,10 @@ class CRM_HRAbsence_Form_AbsenceRequest extends CRM_Core_Form {
       $days = (strtotime($dateTo)- strtotime($dateFrom))/24/3600;
       $days = $days + 1;
     }
-    if (!$dateFrom) {
+    if (isset($dateFrom) && !$dateFrom) {
       $errors['start_date'] = ts('From date is required.');
     }
-    if (!$dateTo) {
+    if (isset($dateTo) && !$dateTo) {
       $errors['end_date'] = ts('End date is required.');
     }
     if (strtotime(isset($fields['start_date_display'])) && strtotime(isset($fields['end_date_display'])) && strtotime(isset($fields['start_date_display'])) > strtotime(isset($fields['end_date_display']))) {
