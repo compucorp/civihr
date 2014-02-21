bcf46d6af81cb2d003b79adabf230f4138d1077b
Tim Otten <totten@civicrm.org>
2014-02-21 01:21:11 -0800

    HR-216 - jobtabapp/pay - Hide/display message based on whether the lock is enabled

--- hrjob/js/jobtabapp/pay/edit_views.js
+++ hrjob/js/jobtabapp/pay/edit_views.js
@@ -13,23 +13,29 @@ CRM.HRApp.module('JobTabApp.Pay', function(Pay, HRApp, Backbone, Marionette, $, 
       'change:pay_grade': 'togglePayGrade'
     }),
     events: _.extend({}, HRApp.Common.Views.StandardForm.prototype.events,{
+      'change [name=pay_is_auto_est]': 'toggleLockMessage',
       'click .pay_annualized_est_edit': 'doAnnualizedEstEdit'
     }),
     onRender: function() {
       HRApp.Common.Views.StandardForm.prototype.onRender.apply(this, arguments);
-      var view = this;
       if (this.model.get('pay_grade') == 'paid') {
         this.$('.hrjob-needs-pay_grade').show();
       } else {
         this.$('.hrjob-needs-pay_grade').hide();
       }
+      this.$('.pay_annualized_est_expl').text(ts('(Estimates will be updated based on pay-rate, FTE, and system settings.)'));
+      // FIXME: figure out which field in this.options.settings is applicable and output it
       this.$('[name=pay_is_auto_est]').lockButton({
         lockedText: ts('Automatic estimate'),
         unlockedText: ts('Manual estimate'),
         for: this.$('[name=pay_annualized_est]'),
-        value: this.model.get('pay_annualized_est') // FIXME
+        value: this.model.get('pay_annualized_est') // FIXME use callback to calculate automatically
       });
-      this.$('.pay_annualized_est_expl').text(ts('(TODO: Explanatory message)'));
+      this.toggleLockMessage();
+    },
+    toggleLockMessage: function() {
+      var locked = this.$('[name=pay_is_auto_est]').lockButton('isLocked');
+      this.$('.pay_annualized_est_expl').parent().css('visibility', locked ? 'visible' : 'hidden');
     },
     togglePayGrade: function() {
       var view = this;
