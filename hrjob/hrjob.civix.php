9c993d8e3a8754c4b666db89472531314f2147dd
Tim Otten <totten@civicrm.org>
2014-02-24 17:26:15 -0800

    HR-287 - Fix bug in generating list of settings folders

--- hrjob/hrjob.civix.php
+++ hrjob/hrjob.civix.php
@@ -267,10 +267,6 @@ function _hrjob_civix_insert_navigation_menu(&$menu, $path, $item, $parentId = N
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
  */
 function _hrjob_civix_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  static $configured = FALSE;
-  if ($configured) return;
-  $configured = TRUE;
-
   $settingsDir = __DIR__ . DIRECTORY_SEPARATOR . 'settings';
   if(is_dir($settingsDir) && !in_array($settingsDir, $metaDataFolders)) {
     $metaDataFolders[] = $settingsDir;
