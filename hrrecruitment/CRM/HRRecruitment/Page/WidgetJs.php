72160ab1543e40ea978592eaf0f9f9f187a4551c
Nileema <nileema@nileema.(none)>
2014-03-24 20:02:29 +0530

    --HR-296 Public Listing of Vacancies

---
+++ hrrecruitment/CRM/HRRecruitment/Page/WidgetJs.php
@@ -0,0 +1,155 @@
+<?php
+/*
+  +--------------------------------------------------------------------+
+  | CiviHR version 1.3                                                 |
+  +--------------------------------------------------------------------+
+  | Copyright CiviCRM LLC (c) 2004-2013                                |
+  +--------------------------------------------------------------------+
+  | This file is a part of CiviCRM.                                    |
+  |                                                                    |
+  | CiviCRM is free software; you can copy, modify, and distribute it  |
+  | under the terms of the GNU Affero General Public License           |
+  | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+  |                                                                    |
+  | CiviCRM is distributed in the hope that it will be useful, but     |
+  | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+  | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+  | See the GNU Affero General Public License for more details.        |
+  |                                                                    |
+  | You should have received a copy of the GNU Affero General Public   |
+  | License and the CiviCRM Licensing Exception along                  |
+  | with this program; if not, contact CiviCRM LLC                     |
+  | at info[AT]civicrm[DOT]org. If you have questions about the        |
+  | GNU Affero General Public License or the licensing of CiviCRM,     |
+  | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+  +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2013
+ * $Id$
+ *
+ */
+
+/**
+ * Widget for displaying list of vacancies
+ */
+class CRM_HRRecruitment_Page_WidgetJs extends CRM_Core_Page {
+  function run() {
+    CRM_Utils_System::setTitle(ts('Public Vacancy List'));
+    global $civicrm_root;
+    global $base_url;
+
+    //fetch listing
+    $row = self::vacancyListDisplay();
+    $output = '';
+    $output .= '(function() {var cj = jQuery.noConflict();';
+    $output .= file_get_contents("{$civicrm_root}packages/backbone/lodash.compat.min.js");
+
+    //Add js file
+    $templateDir = array("{$civicrm_root}packages/jquery/", "{$civicrm_root}packages/jquery/jquery-ui/js/");
+    foreach ($templateDir as $templateDir) {
+      $files = (array) glob($templateDir . '*.js', GLOB_BRACE);
+      foreach ($files as $file) {
+        $output .= file_get_contents($file);
+      }
+    }
+
+    //Add css files
+    $scan = scandir("{$civicrm_root}packages/jquery/jquery-ui/css/theme/");    
+    foreach ($scan as $file) {
+    if (!is_dir("{$civicrm_root}packages/jquery/jquery-ui/css/theme/{$file}")) {
+      $output .= 	"var link = document.createElement('link');
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = '{$base_url}/sites/all/modules/civicrm/packages/jquery/jquery-ui/css/theme/{$file}';
+        link.media = 'all';
+        document.getElementsByTagName('head')[0].appendChild(link);";
+      }
+    }
+
+    //Add template 
+    $output .= "var $ = jQuery.noConflict();var c_ = _.noConflict();c_.templateSettings.variable = 'rc';";
+    $output .= CRM_Core_Smarty::singleton()->fetch('CRM/HRRecruitment/Page/WidgetJs.tpl');
+    $output .= "document.write('<div id=\'vacancyPublicList\'></div>');
+      var div = document.getElementById('vacancyPublicList');
+      div.innerHTML = listTemplate($row);
+      document.write('<div id=\'vacancyListDialog\'></div>');";
+
+    //Dialog box to display vacancy information
+    $output .= '$(".hr-job-position-link").bind("click",function(e){
+      e.preventDefault();
+      var infourl = $(this).attr("href") + "&callback=?";
+      var ht = "";
+	    $.getJSON(infourl).done(function(result) {
+        c_.templateSettings.variable = \'rc\';
+        $("#vacancyListDialog").html(infoTemplate(result));
+        $("#vacancyListDialog").dialog({
+          modal: true,
+          width: 600,
+          title: result.position,
+          buttons: {
+            "Apply Now": function() { window.location = result.apply; },
+            "Close": function() { $(this).dialog("close"); }
+          }
+        });
+      });
+    });';
+
+    $output .= '})();';
+    header('Content-type: text/javascript');
+    echo $output;
+    exit();
+  }
+
+  public static function vacancyListDisplay() {
+    global $base_url;
+    $vacancies = civicrm_api3('HRVacancy','get', array());
+    foreach ($vacancies['values'] as $vacancyKey => $vacancyVal) {
+      $row[$vacancyVal['id']]['id'] = $vacancyVal['id'];
+      $row[$vacancyVal['id']]['position'] = ts($vacancyVal['position']);
+      $row[$vacancyVal['id']]['positionLink'] = "{$base_url}/civicrm/vacancy/info?id={$vacancyVal['id']}";
+      $row[$vacancyVal['id']]['location'] = $vacancyVal['location'];
+      $row[$vacancyVal['id']]['salary'] = $vacancyVal['salary'];
+      $row[$vacancyVal['id']]['startDate'] = CRM_Utils_Date::customFormat($vacancyVal['start_date']);
+      $row[$vacancyVal['id']]['endDate'] = CRM_Utils_Date::customFormat($vacancyVal['end_date']);
+      $row[$vacancyVal['id']]['apply'] = "{$base_url}/civicrm/vacancy/apply?id={$vacancyVal['id']}";
+    }
+    if (!empty($_GET['callback'])) {
+      echo $_GET['callback'] .'('. json_encode($row) .')';
+    }
+    else {
+      return json_encode($row);
+    }
+    CRM_Utils_System::civiExit();
+  }
+
+  public static function vacancyInfo($vacancyId = NULL) {
+    if (!$_GET['id'] && !$vacancyId) {
+      return;
+    }
+    global $base_url;
+    $vacancyId = $_GET['id'] ? $_GET['id'] : $vacancyId;
+    $vacancy = civicrm_api3('HRVacancy','get', array('id'=>$vacancyId));
+    foreach ($vacancy['values'] as $vacancyKey => $vacancyVal) {
+      CRM_Utils_System::setTitle(ts("{$vacancyVal['position']}"));
+      $row['position'] = $vacancyVal['position'];
+      $row['id'] = $vacancyVal['id'];
+      $row['salary'] = $vacancyVal['salary'];
+      $row['location'] = $vacancyVal['location'];
+      $row['description'] = ts($vacancyVal['description']);
+      $row['benefits'] = $vacancyVal['benefits'];
+      $row['requirements'] = $vacancyVal['requirements'];
+      $row['apply'] = "{$base_url}/civicrm/vacancy/apply?id={$vacancyVal['id']}";
+    }
+    if (!empty($_GET['callback'])) {
+      echo $_GET['callback'] .'('. json_encode($row) .')';
+    }
+    else {
+      return json_encode($row);
+    }
+    CRM_Utils_System::civiExit();
+  }
+}
---
+++ hrrecruitment/templates/CRM/HRRecruitment/Page/VacancyList.tpl
@@ -0,0 +1,92 @@
+{*
+ +--------------------------------------------------------------------+
+ | CiviHR version 1.3                                                 |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2013                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*}
+{if $list}
+  <div class="view-hrvacancies">
+    {if $rows}
+      {strip}
+        <div class="description">
+          {ts}Click on the job position for more information.{/ts}
+        </div>
+        <table class="selector">
+          <tr class="columnheader">
+            <th>{ts}Job Position{/ts}</th>
+            <th>{ts}Location{/ts}</th>
+            <th>{ts}Salary{/ts}</th>
+            <th>{ts}Application Dates{/ts}</th>
+            <th></th>
+          </tr>
+          {foreach from=$rows item=row}
+            <tr id='rowid{$row.id}' class=" crm-hrvacancy-id_{$row.id} {cycle values="odd-row,even-row"}">
+              <td class="crm-job_position"><a class="hr-job-position-link" href="{crmURL p="civicrm/vacancy" q="reset=1&id=`$row.id`"}">{$row.position}</a></td>
+              <td class="crm-location">{$row.location}</td>
+              <td class="crm-salary">{$row.salary}</td>
+              <td class="crm-application_dates">{$row.startDate|crmDate} - {$row.endDate|crmDate}</td>
+              <td><a href="{crmURL p="civicrm/vacancy/apply" q="reset=1&id=`$row.id`"}">{ts}Apply Now{/ts}</a></td>
+            </tr>
+          {/foreach}
+        </table>
+      {/strip}
+    {else}
+      <div class="messages status no-popup">
+        <div class="icon inform-icon"></div>&nbsp;
+          {ts}There are no Job Vacanies.{/ts}
+        </div>
+    {/if}
+  </div>
+  {literal}
+  <script type="text/javascript">
+    cj(function($) {
+      $('.hr-job-position-link').live('click', function(e) {
+        var url = $(this).attr('href');
+        CRM.loadPage(url);
+        e.preventDefault();
+        $('.hr-job-info-close').live('click', function(e) {
+          e.preventDefault();
+          $(this).closest('div[role=dialog]').remove();
+        });
+      });
+    });
+  </script>
+  {/literal}
+{/if}
+{if $info}
+  <div class="view-hrvacancy-info">
+    {if $rows}
+      {strip}
+        <h3>{$rows.position}</h3>
+         <table class="vacancy-popup">
+           <tr><td>Salary:</td><td>{$rows.salary}</td></tr>
+           <tr><td>location:</td><td>{$rows.location}</td></tr>
+           <tr><td>description:</td><td>{$rows.description}</td></tr>
+           <tr><td>benefits:</td><td>{$rows.benefits}</td></tr>
+           <tr><td>requirements:</td><td>{$rows.requirements}</td></tr>
+         </table>
+         <a href="{crmURL p="civicrm/vacancy/apply" q="reset=1&id=`$rows.id`"}" class="button"><span>{ts}Apply Now{/ts}</a>
+         <a href="" class="button hr-job-info-close"><span>{ts}Close{/ts}</a>
+       {/strip}
+     {/if}
+  </div>
+{/if}
