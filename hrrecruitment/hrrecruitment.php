d3597ef30501a016ac0692416829dc88cc6fb19f
Divya Jose <divya@divya.(none)>
2014-07-08 15:55:54 +0530

    --HR-363 feedback - changes to rename Application to job vacancy

--- hrrecruitment/hrrecruitment.php
+++ hrrecruitment/hrrecruitment.php
@@ -451,8 +451,8 @@ function hrrecruitment_civicrm_buildForm($formName, &$form) {
   //To hide application case type from add assignment form
   if ($formName == 'CRM_Case_Form_Case') {
     $form->_caseType = CRM_Case_PseudoConstant::caseType();
-    $appValue = array_search('Application', $form->_caseType);
-    unset($form->_caseType[$appValue]);
+    $appValue = civicrm_api3('CaseType', 'getsingle', array('return' => "id", 'name' => "Application"));
+    unset($form->_caseType[$appValue['id']]);
     $form->add('select', 'case_type_id', ts('Assignment Type'), $form->_caseType, TRUE );
   }
 
