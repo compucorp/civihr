123783d11041412d2e20b6293b93b1c2a9a03dd1
Poonam <poonam@poonam.(none)>
2014-07-23 13:13:45 +0530

    --HR-356 changes related to replacement of text.

--- hrreport/js/hrreport.js
+++ hrreport/js/hrreport.js
@@ -8,6 +8,11 @@
     $('#Case #row_1 .crm-report-templateList-title a strong, #Case #row_10 .crm-report-templateList-title a strong').html('Assignment Summary Report');
     $('#Case #row_2 .crm-report-templateList-title a strong, #Case #row_11 .crm-report-templateList-title a strong').html('Assignment Time Spent Report');
     $('#Case #row_4 .crm-report-templateList-title a strong, #Case #row_13 .crm-report-templateList-title a strong').html('Assignment Detail Report');
+    //change description text
+    $('#Case #row_1 .crm-report-templateList-description').html('Provides a summary of assignments and their duration by date range, status, staff member and / or assignment role.');
+    $('#Case #row_2 .crm-report-templateList-description').html('Aggregates time spent on assignment and / or non-assignment activities by activity type and contact.');
+    $('#Case #row_3 .crm-report-templateList-description').html('Demographic breakdown for assignment contacts (and or non-assignment contacts) in your database. Includes custom contact fields.');
+    $('#Case #row_4 .crm-report-templateList-description').html('Assignment Details');
     //change text on anchor link
     $('.icon').parent('span:contains("New Case Report")').html('<div class="icon add-icon"></div>New Assignment Report');
     //change information text
