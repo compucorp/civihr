654d66d07e30f33bdeb1444b173ba383e9c34498
Mateusz Cieplinski <mateusz@coldrun.pl>
2015-08-31 12:33:40 +0200

    PCHR-381

--- hrjobcontract/js/controllers/contractList.js
+++ hrjobcontract/js/controllers/contractList.js
@@ -112,7 +112,7 @@ define(['controllers/controllers',
                 var modalInstance,
                     options = {
                         targetDomEl: $rootElement.find('div').eq(0),
-                        templateUrl: settings.pathApp+'views/modalForm.html?v=1111',
+                        templateUrl: settings.pathApp+'views/modalForm.html?v=2222',
                         size: 'lg',
                         controller: 'ModalContractNewCtrl',
                         resolve: {
