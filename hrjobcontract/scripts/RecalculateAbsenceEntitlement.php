e0f3f3ba0629e6d3194ca6287ae22b622032109e
coldrunKacper <kacper@coldrun.pl>
2015-09-11 16:04:44 +0200

    PHP CLI script for recalculating Absence Entitlements by existing Job Contracts.

---
+++ hrjobcontract/scripts/RecalculateAbsenceEntitlement.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Script started.\n";
+
+$timeStart = time();
+$jobContracts = CRM_Core_DAO::executeQuery('SELECT * FROM civicrm_hrjobcontract WHERE deleted = 0 ORDER BY id ASC');
+$i = 0;
+while ($jobContracts->fetch()) {
+    echo "Recalculating Absence Entitlement by Job Contract #{$jobContracts->id}...";
+    CRM_Hrjobcontract_BAO_HRJobLeave::recalculateAbsenceEntitlement($jobContracts->id);
+    echo "OK.\n";
+    $i++;
+}
+$timeEnd = time();
+
+echo "{$i} Job Contracts processed in " . ($timeEnd - $timeStart) . " seconds.\n";
+echo "Finished.\n";
