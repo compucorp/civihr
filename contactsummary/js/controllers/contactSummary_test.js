cf28d758e9e054cdafed105bbd615da54fcc88d2
Robin Mitra <robinmitra1@gmail.com>
2015-09-25 17:05:38 +0100

    Update spec for ContactSummaryCtrl to make use of the new mocks.

--- contactsummary/js/controllers/contactSummary_test.js
+++ contactsummary/js/controllers/contactSummary_test.js
@@ -1,25 +1,61 @@
-define(['angularMocks', 'app', 'controllers/contactSummary', 'services/keyDetails'], function () {
+define([
+  'angularMocks',
+  'app',
+  'mocks/module',
+  'mocks/constants',
+  'mocks/services',
+  'controllers/contactSummary'
+], function () {
   'use strict';
 
   describe('ContactSummaryCtrl', function () {
-    var ctrl, ctrlConstructor, log;
+    var ctrlConstructor;
+    var keyDetailsServiceMock, keyDatesServiceMock, settingsMock;
 
-    beforeEach(module('contactsummary'));
+    beforeEach(module('contactsummary', 'contactsummaryMocks'));
 
-    beforeEach(inject(function ($controller, $log) {
-      ctrlConstructor = $controller;
-      log = $log;
-      ctrl = ctrlConstructor('ContactSummaryCtrl');
+    beforeEach(module(function ($provide) {
+      $provide.factory('KeyDetailsService', function () {
+        return keyDetailsServiceMock;
+      });
+      $provide.factory('KeyDatesService', function () {
+        return keyDatesServiceMock;
+      });
+      $provide.value('settings', function () {
+        return settingsMock;
+      });
     }));
 
-    it('should call "get" on KeyDetailsService', inject(function (KeyDetailsService) {
-      var spyGet = spyOn(KeyDetailsService, 'get');
-      ctrlConstructor('ContactSummaryCtrl', {$log: log, KeyDetailsService: KeyDetailsService});
-      expect(spyGet).toHaveBeenCalled();
+    beforeEach(function () {
+      inject(function ($injector) {
+        // Instantiating injector isn't allowed before calls to 'module()'. Due to this limitation, we're returning
+        // references to mock variables above, and injecting the actual mocks into them here, since they would be
+        // lazily loaded anyway.
+        keyDetailsServiceMock = $injector.get('KeyDetailsServiceMock');
+        keyDatesServiceMock = $injector.get('KeyDatesServiceMock');
+        settingsMock = $injector.get('settingsMock');
+      });
+    });
+
+    beforeEach(inject(function (_$controller_) {
+      ctrlConstructor = _$controller_;
+    }));
+
+    it('should call "get" on KeyDetailsService', inject(function () {
+      ctrlConstructor('ContactSummaryCtrl');
+      expect(keyDetailsServiceMock.get).toHaveBeenCalled();
     }));
 
     it('should have key details', function () {
+      keyDetailsServiceMock.get.and.returnValue({});
+      var ctrl = ctrlConstructor('ContactSummaryCtrl');
       expect(ctrl.keyDetails).toBeDefined();
+    });
+
+    it('should have key dates', function () {
+      keyDatesServiceMock.get.and.returnValue([]);
+      var ctrl = ctrlConstructor('ContactSummaryCtrl');
+      expect(ctrl.keyDates).toBeDefined();
     });
   });
 });
