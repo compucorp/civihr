da54afd582016a8b06c793c082fd55079a2b4a2e
Robin Mitra <robinmitra1@gmail.com>
2015-10-07 16:20:26 +0100

    Add base model service.

---
+++ contactsummary/js/services/model.js
@@ -0,0 +1,69 @@
+define(['services/services'], function (services) {
+  'use strict';
+
+  /**
+   * Base service to extend.
+   *
+   * This will have common functionality to avoid duplication of functionality between services.
+   *
+   * @param {ItemService} Item
+   * @constructor
+   */
+  function ModelService(Item) {
+
+    ////////////////////
+    // Public Members //
+    ////////////////////
+
+    /**
+     * @ngdoc service
+     * @name ModelService
+     * @type {Object}
+     */
+    var factory = {};
+
+    /**
+     * @ngdoc method
+     * @name createInstance
+     * @methodOf ModelService
+     * @returns {ModelService|Object}
+     * @constructs
+     */
+    factory.createInstance = function () {
+      data = Item.create();
+      return Object.create(this);
+    };
+
+    /**
+     * @ngdoc method
+     * @name getData
+     * @methodOf ModelService
+     * @returns {Object}
+     */
+    factory.getData = function () {
+      return data.get();
+    };
+
+    /**
+     * @ngdoc method
+     * @name setDataKey
+     * @methodOf ModelService
+     * @param key
+     * @param value
+     */
+    factory.setDataKey = function (key, value) {
+      data.setKey(key, value);
+    };
+
+    /////////////////////
+    // Private Members //
+    /////////////////////
+
+    /**
+     * @type {ItemService}
+     */
+    var data;
+  }
+
+  services.factory('ModelService', ['ItemService', ModelService]);
+});
