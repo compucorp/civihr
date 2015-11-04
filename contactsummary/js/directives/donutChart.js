d7bbaed4de7c9ebe5f18d1e5128d439b43061027
Alessandro Verdura <alessandro@compucorp.co.uk>
2015-11-04 11:49:16 +0100

    PCHR-530: Refactor donutChart.js

--- contactsummary/js/directives/donutChart.js
+++ contactsummary/js/directives/donutChart.js
@@ -1,93 +1,89 @@
 define(['directives/directives', 'd3'], function (directives, d3) {
-  'use strict';
+    'use strict';
 
-  directives.directive('csDonutChart', ['$log', function ($log) {
-    $log.debug('Directive: csDonutChart');
-
-    var directive = {};
-
-    directive.scope = {
-      radius: '@',
-      thickness: '@',
-      items: '=',
-      itemKey: '@',
-      ready: '='
-    };
-
-    directive.restrict = 'AE';
-
-    directive.link = function (scope, elem, attrs, ctrl) {
-      var unbindWatch = scope.$watch(function () {
-        return scope.ready;
-      }, function (newValue, oldValue) {
-        console.log(oldValue, newValue);
-        if (newValue === true) {
-          ctrl.drawChart();
-          unbindWatch();
-        }
-      });
-    };
-
-    directive.controllerAs = 'CsDonutChartCtrl';
-
-    directive.controller = ['$scope', '$element', function ($scope, $element) {
-      this.drawChart = function () {
-        init($scope);
-
-        constructChart(
-          constructSvg($element[0], $scope.width, $scope.height),
-          constructArc($scope.radius - $scope.thickness, $scope.radius),
-          constructData($scope.items, $scope.itemKey));
-      };
-    }];
-
-    /////////////////////
-    // Private Members //
-    /////////////////////
-
-    function init($scope) {
-      $scope.radius = $scope.radius || 60;
-      $scope.width = $scope.height = $scope.radius * 2;
-      $scope.thickness = $scope.thickness || 15;
+    function init($element) {
+        this.height = this.width = $element[0].clientWidth;
+        this.radius = this.width / 2 || 60;
+        this.thickness = this.thickness || 15;
     }
 
-    function constructArc(innerRadius, outerRadius) {
-      return d3.svg.arc()
-        .innerRadius(innerRadius)
-        .outerRadius(outerRadius);
-    }
-
-    function constructSvg(element, width, height) {
-      return d3.select(element).append('svg')
-        .attr('width', width)
-        .attr('height', height)
-        .append('g')
-        .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-    }
-
-    function constructData(items, key) {
-      console.log('Key is', key);
-      var pieLayout = d3.layout.pie()
-        .sort(null)
-        .value(function (d) {
-          return d.value[key];
-        });
-
-      return pieLayout(d3.entries(items));
+    function constructArc() {
+        return d3.svg.arc()
+          .innerRadius(this.radius - this.thickness)
+          .outerRadius(this.radius);
     }
 
     function constructChart(svg, arc, data) {
-      var color = d3.scale.category20();
+        var color = d3.scale.category20();
 
-      return svg.selectAll('path')
-        .data(data)
-        .enter().append('path')
-        .attr('fill', function (d, i) {
-          return color(i);
-        })
-        .attr('d', arc);
+        return svg.selectAll('path')
+          .data(data)
+          .enter().append('path')
+          .attr('fill', function (d, i) {
+              return color(i);
+          })
+          .attr('d', arc);
     }
 
-    return directive;
-  }]);
+    function constructData() {
+        console.log('Key is', this.itemKey);
+
+        var pieLayout = d3.layout.pie()
+          .sort(null)
+          .value(function (d) {
+              return d.value[this.itemKey];
+          }.bind(this));
+
+        return pieLayout(d3.entries(this.items));
+    }
+
+    function constructSvg($element) {
+        return d3.select($element).append('svg')
+          .attr('width', this.width)
+          .attr('height', this.height)
+          .append('g')
+          .attr('transform', 'translate(' + this.width / 2 + ',' + this.height / 2 + ')');
+    }
+
+
+    directives.directive('csDonutChart', ['$log', function ($log) {
+        $log.debug('Directive: csDonutChart');
+
+        return {
+            controllerAs: 'CsDonutChartCtrl',
+            restrict: 'AE',
+            scope: {
+                radius: '@',
+                thickness: '@',
+                items: '=',
+                itemKey: '@',
+                ready: '='
+            },
+            controller: ['$scope', '$element', function ($scope, $element) {
+                this.drawChart = function () {
+                    // angular.extend is necessary cause `bindToController` is
+                    // available only in angular > 1.3
+                    init.call(angular.extend(this, $scope), $element);
+
+                    constructChart(
+                        constructSvg.call(this, $element[0]),
+                        constructArc.call(this),
+                        constructData.call(this)
+                    );
+                };
+            }],
+            link: function (scope, elem, attrs, ctrl) {
+                var unbindWatch = scope.$watch(function () {
+                    return scope.ready;
+                }, function (newValue, oldValue) {
+                    console.log(oldValue, newValue);
+
+                    if (newValue === true) {
+                        ctrl.drawChart();
+                        unbindWatch();
+                    }
+              });
+            }
+        };
+    }]);
 });
