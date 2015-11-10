efbc9d97559cbe2c5e7d3eb57c500a3b9d217e4d
Alessandro Verdura <alessandro@compucorp.co.uk>
2015-11-10 10:11:43 +0100

    PCHR-540: Fix indentation

--- contactsummary/js/services/contactDetails.js
+++ contactsummary/js/services/contactDetails.js
@@ -1,83 +1,84 @@
 define(['services/services', 'moment', 'services/model', 'services/api', 'lodash'], function (services, moment) {
-  'use strict';
-
-  /**
-   * @param Api
-   * @param {ModelService} Model
-   * @param settings
-   * @param $q
-   * @param $log
-   * @returns {*|Object|ModelService}
-   * @constructor
-   */
-  function ContactDetailsService(Api, Model, settings, $q, $log) {
-    $log.debug('Service: ContactDetailsService');
-
-    ////////////////////
-    // Public Members //
-    ////////////////////
+    'use strict';
 
     /**
-     * @ngdoc service
-     * @name ContactDetailsService
+     * @param Api
+     * @param {ModelService} Model
+     * @param settings
+     * @param $q
+     * @param $log
+     * @returns {*|Object|ModelService}
+     * @constructor
      */
-    var factory = Model.createInstance();
+    function ContactDetailsService(Api, Model, settings, $q, $log) {
+        $log.debug('Service: ContactDetailsService');
 
-    /**
-     * @ngdoc method
-     * @name get
-     * @methodOf ContactDetailsService
-     * @this ContactDetailsService
-     * @returns {*}
-     */
-    factory.get = function () {
-      /** @type {(ContactDetailsService|ModelService)} */
-      var self = this;
-      var deferred = $q.defer();
+        ////////////////////
+        // Public Members //
+        ////////////////////
 
-      init().then(function () {
-        deferred.resolve(self.getData());
-      });
+        /**
+         * @ngdoc service
+         * @name ContactDetailsService
+         */
+        var factory = Model.createInstance();
 
-      return deferred.promise;
-    };
+        /**
+         * @ngdoc method
+         * @name get
+         * @methodOf ContactDetailsService
+         * @this ContactDetailsService
+         * @returns {*}
+         */
+        factory.get = function () {
+            /** @type {(ContactDetailsService|ModelService)} */
+            var self = this;
+            var deferred = $q.defer();
 
-    /////////////////////
-    // Private Members //
-    /////////////////////
+            init().then(function () {
+                deferred.resolve(self.getData());
+            });
 
-    function init() {
-      var deferred = $q.defer();
+            return deferred.promise;
+        };
 
-      if (_.isEmpty(factory.getData())) {
-        var contactId = settings.contactId;
+        /////////////////////
+        // Private Members //
+        /////////////////////
 
-        Api.get('Contact', {contact_id: contactId, return: 'birth_date'})
-          .then(function (response) {
-            if (response.values.length === 0) throw new Error('Contact with ID ' + contactId + ' not found');
+        function init() {
+            var deferred = $q.defer();
 
-            var dob = response.values[0].birth_date,
-              age = moment(moment(dob, 'YYYY-MM-DD')).fromNow(true);
+            if (_.isEmpty(factory.getData())) {
+                var contactId = settings.contactId;
 
-            factory.setDataKey('id', contactId);
-            factory.setDataKey('dateOfBirth', dob);
-            factory.setDataKey('age', age);
+                Api.get('Contact', {contact_id: contactId, return: 'birth_date'})
+                    .then(function (response) {
+                        if (response.values.length === 0) {
+                            throw new Error('Contact with ID ' + contactId + ' not found');
+                        }
 
-            deferred.resolve();
-          })
-          .catch(function (response) {
-            deferred.reject(response);
-          });
-      } else {
-        deferred.resolve();
-      }
+                        var dob = response.values[0].birth_date,
+                            age = moment(moment(dob, 'YYYY-MM-DD')).fromNow(true);
 
-      return deferred.promise;
+                        factory.setDataKey('id', contactId);
+                        factory.setDataKey('dateOfBirth', dob);
+                        factory.setDataKey('age', age);
+
+                        deferred.resolve();
+                    })
+                    .catch(function (response) {
+                        deferred.reject(response);
+                    });
+            } else {
+                deferred.resolve();
+            }
+
+            return deferred.promise;
+        }
+
+        return factory;
     }
 
-
-    return factory;
-  }
-
-  services.factory('ContactDetailsService', ['ApiService', 'ModelService', 'settings', '$q', '$log', ContactDetailsService]);
+    services.factory('ContactDetailsService', ['ApiService', 'ModelService', 'settings', '$q', '$log', ContactDetailsService]);
 });
