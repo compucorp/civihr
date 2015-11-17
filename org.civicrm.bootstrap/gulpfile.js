e0bd6fe82b86a35d379ddc4ebb00019392c2b094
Alessandro Verdura <alessandro@compucorp.co.uk>
2015-11-17 16:16:28 +0100

    PCHR-530: Amend extensions usage

--- org.civicrm.bootstrap/gulpfile.js
+++ org.civicrm.bootstrap/gulpfile.js
@@ -1,25 +1,13 @@
 var gulp = require('gulp');
-var bulk = require('gulp-sass-bulk-import');
-var include = require("gulp-include");
-var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
   gulp.src('scss/*.scss')
-    .pipe(include())
-    .pipe(bulk())
     .pipe(sass({
         outputStyle: 'compressed',
         includePaths: ['vendor/']
     }).on('error', sass.logError))
-    .pipe(rename(function (path) {
-        path.basename = 'bootstrap-' + path.basename;
-    }))
     .pipe(gulp.dest('css/'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('scss/**/*.scss', ['sass']);
-});
-
-gulp.task('default', ['sass', 'watch']);;
+gulp.task('default', ['sass']);;
