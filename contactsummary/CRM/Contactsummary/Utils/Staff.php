1478c0a52dc65a3b5aa4f8e4fc7a7eb43e50b7c1
Robin Mitra <robinmitra1@gmail.com>
2015-11-27 12:38:57 +0000

    PCHR-624: Correct the SQL for getting total number of staff while calculating average sickness.

--- contactsummary/CRM/Contactsummary/Utils/Staff.php
+++ contactsummary/CRM/Contactsummary/Utils/Staff.php
@@ -20,14 +20,6 @@ class CRM_Contactsummary_Utils_Staff {
     INNER JOIN civicrm_hrjobcontract_details jcd
     ON (jcr.id = jcd.jobcontract_revision_id AND (jcd.period_end_date >= NOW() OR jcd.period_end_date IS NULL))
 
-    INNER JOIN civicrm_relationship civicrm_relationship ON c.id = civicrm_relationship.contact_id_a
-
-    INNER JOIN civicrm_contact c_b
-    ON civicrm_relationship.contact_id_b = c_b.id
-
-    INNER JOIN civicrm_relationship c_b_r
-    ON (c_b.id = c_b_r.contact_id_a AND (c_b_r.is_active <> 0 OR c_b_r.relationship_type_id = 18))
-
     WHERE c.contact_type = 'Individual'";
 
     $total = 0;
