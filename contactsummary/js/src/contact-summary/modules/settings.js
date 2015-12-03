cfdc78f2b322f04d870a45951e7e93632a7df5e8
Alessandro Verdura <alessandro@compucorp.co.uk>
2015-12-03 11:32:44 +0100

    PCHR-574: Refactor contact-summary app folder structure

---
+++ contactsummary/js/src/contact-summary/modules/settings.js
@@ -0,0 +1,15 @@
+define([
+    'common/angular'
+], function (angular) {
+    'use strict';
+
+    return angular.module('contactsummary.settings', []).constant('settings', {
+        classNamePrefix: 'contactSummary-',
+        contactId: decodeURIComponent((new RegExp('[?|&]cid=([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null,
+        debug: true,
+        pathApp: '',
+        pathRest: CRM.url('civicrm/ajax/rest'),
+        pathBaseUrl: CRM.vars.contactsummary.baseURL + '/',
+        pathTpl: 'views/'
+    });
+})
---
+++ contactsummary/js/src/contact-summary/services/contact.js
@@ -0,0 +1,97 @@
+define([
+    'common/lodash',
+    'contact-summary/modules/services',
+    'contact-summary/services/model',
+    'contact-summary/services/leave',
+    'contact-summary/services/contactDetails',
+    'contact-summary/services/contract'
+], function (_, services) {
+    'use strict';
+
+    /**
+     * @param {ModelService} Model
+     * @param ContactDetails
+     * @param {LeaveService} Leave
+     * @param {ContractService} Contract
+     * @param $q
+     * @param $log
+     * @returns {Object}
+     * @constructor
+     */
+    function ContactService($log, $q, Model, ContactDetails, Leave, Contract) {
+        $log.debug('Service: ContactService');
+
+        ////////////////////
+        // Public Members //
+        ////////////////////
+
+        /**
+         * @ngdoc service
+         * @name ContactService
+         */
+        var factory = Model.createInstance();
+
+        /**
+         * @ngdoc method
+         * @name get
+         * @methodOf ContactService
+         * @returns {*}
+         */
+        factory.get = function () {
+            /** @type {(ContactService|ModelService)} */
+            var self = this;
+
+            return init().then(function () {
+                return self.getData();
+            });
+        };
+
+        return factory;
+
+        /////////////////////
+        // Private Members //
+        /////////////////////
+
+        function init() {
+            var deferred = $q.defer();
+
+            if (_.isEmpty(factory.getData())) {
+                initContactDetails()
+                    .then(initContract)
+                    .then(initLeave)
+                    .then(function () {
+                        deferred.resolve();
+                    });
+            } else {
+                deferred.resolve();
+            }
+
+            return deferred.promise;
+        }
+
+        function initContactDetails() {
+            return ContactDetails.get()
+                .then(function (response) {
+                    factory.setDataKey('id', response.id);
+                    factory.setDataKey('dateOfBirth', response.dateOfBirth);
+                    factory.setDataKey('age', response.age);
+                });
+        }
+
+        function initContract() {
+            return Contract.get()
+                .then(function (response) {
+                    factory.setDataKey('contract', response);
+                });
+        }
+
+        function initLeave() {
+            return Leave.get()
+                .then(function (response) {
+                    factory.setDataKey('leave', response);
+                });
+        }
+    }
+
+    services.factory('ContactService', ['$log', '$q', 'ModelService', 'ContactDetailsService', 'LeaveService', 'ContractService', ContactService]);
+});
