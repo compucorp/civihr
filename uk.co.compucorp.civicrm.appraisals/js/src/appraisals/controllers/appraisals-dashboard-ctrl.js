83fccc2406a8f2b02032d45a554c7f36edbc24b0
Alessandro Verdura <alessandro@compucorp.co.uk>
2015-12-09 15:32:47 +0100

    PCHR-576: Implement crm-grades-chart with mock data

--- uk.co.compucorp.civicrm.appraisals/js/src/appraisals/controllers/appraisals-dashboard-ctrl.js
+++ uk.co.compucorp.civicrm.appraisals/js/src/appraisals/controllers/appraisals-dashboard-ctrl.js
@@ -6,7 +6,13 @@ define([
         $log.debug('AppraisalsDashboardCtrl');
 
         return {
-            filtersCollapsed: true
+            filtersCollapsed: true,
+            data: [ // mock data
+                { label: 1, value: 17 },
+                { label: 2, value: 74 },
+                { label: 3, value: 90 },
+                { label: 4, value: 30 }
+            ]
         }
     }]);
 });
