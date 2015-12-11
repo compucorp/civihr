1de41be175b9fbeee400ae593177c32598f3e1d3
Alessandro Verdura <alessandro@compucorp.co.uk>
2015-12-11 11:33:05 +0100

    PCHR-576: Get cycles list from api + status filter

--- uk.co.compucorp.civicrm.appraisals/js/src/appraisals/controllers/appraisals-dashboard-ctrl.js
+++ uk.co.compucorp.civicrm.appraisals/js/src/appraisals/controllers/appraisals-dashboard-ctrl.js
@@ -1,7 +1,8 @@
 define([
+    'common/angular',
     'appraisals/modules/controllers',
     'appraisals/models/appraisal-cycle'
-], function (controllers) {
+], function (angular, controllers) {
     'use strict';
 
     controllers.controller('AppraisalsDashboardCtrl',
@@ -10,14 +11,55 @@ define([
             $log.debug('AppraisalsDashboardCtrl');
 
             var vm = {};
+
+            vm.activeFilters = [
+                { label: 'active', value: true },
+                { label: 'inactive', value: false },
+                { label: 'all', value: null }
+            ];
+
+            vm.cycles = [];
             vm.chartData = [];
             vm.filtersCollapsed = true;
+            vm.filters = { active: vm.activeFilters[0].value };
+
             vm.activeCycles = activeCycles;
             vm.statusOverview = statusOverview;
             vm.statuses = statuses;
             vm.types = types;
 
+            /**
+             * Changes the status filter to a new (valid) value
+             *
+             * @param {string} newValue
+             */
+            vm.changeActiveFilter = function (newValue) {
+                newValue = vm.activeFilters.filter(function (filter) {
+                    return filter.label === newValue;
+                })[0];
+
+                if (typeof newValue !== 'undefined' && vm.filters.active !== newValue.value) {
+                    vm.filters.active = newValue.value;
+                    requestCycles();
+                }
+            };
+
             init();
+
+            /**
+             * Processes the selected appraisal cycle filters
+             *
+             * @return {object} a filters structure the model can use
+             */
+            function filters() {
+                var filters = angular.copy(vm.filters);
+
+                if (filters.active === null) {
+                    delete filters.active;
+                }
+
+                return filters;
+            }
 
             /**
              * Initialization code
@@ -25,6 +67,17 @@ define([
             function init() {
                 AppraisalCycle.grades().then(function (grades) {
                     vm.chartData = grades;
+                });
+
+                requestCycles();
+            }
+
+            /**
+             * Requests the model to return the list of cycles
+             */
+            function requestCycles() {
+                AppraisalCycle.all(filters()).then(function (cycles) {
+                    vm.cycles = cycles;
                 });
             }
 
--- uk.co.compucorp.civicrm.appraisals/js/test/controllers/appraisals-dashboard-ctrl_test.js
+++ uk.co.compucorp.civicrm.appraisals/js/test/controllers/appraisals-dashboard-ctrl_test.js
@@ -15,40 +15,134 @@ define([
             $controller = _$controller_;
             $scope = $rootScope.$new();
             AppraisalCycle = _AppraisalCycle_;
+
+            spyOn(AppraisalCycle, 'all').and.callFake(function () {
+                return { then: function () {} };
+            });
         }));
 
         describe('init', function () {
-            beforeEach(function () {
-                spyOn(AppraisalCycle, 'grades').and.callFake(function () {
-                    return { then: function () {} };
+            describe('general initial state', function () {
+                beforeEach(function () {
+                    initController();
                 });
 
+                it('is initialized', function () {
+                    expect($log.debug).toHaveBeenCalled();
+                });
+
+                it('stores on scope the data passed by ui-router', function () {
+                    expect(ctrl.activeCycles).toBeDefined();
+                    expect(ctrl.statusOverview).toBeDefined();
+                    expect(ctrl.statuses).toBeDefined();
+                    expect(ctrl.types).toBeDefined();
+                });
+
+                it('has the filters form collapsed', function () {
+                    expect(ctrl.filtersCollapsed).toBe(true);
+                });
+
+                it('has the cycle active filter set to "active"', function () {
+                    expect(ctrl.filters.active).toBe(true);
+                });
+            });
+
+            describe('grades chart data', function () {
+                beforeEach(function () {
+                    spyOn(AppraisalCycle, 'grades').and.callFake(function () {
+                        return { then: function () {} };
+                    });
+
+                    initController();
+                });
+
+                it('starts as an empty array', function () {
+                    expect(ctrl.chartData).toEqual([]);
+                });
+
+                it('is requested on init', function () {
+                    expect(AppraisalCycle.grades).toHaveBeenCalled();
+                });
+            });
+
+            describe('cycles list', function () {
+                beforeEach(function () {
+                    initController();
+                });
+
+                it('has appraisal cycles list as an empty array', function () {
+                    expect(ctrl.cycles).toEqual([]);
+                });
+
+                it('is requested on init, only the active cycles', function () {
+                    expect(AppraisalCycle.all).toHaveBeenCalledWith({ active: true });
+                });
+            });
+        });
+
+        describe('after init', function () {
+            beforeEach(function () {
                 initController();
             });
 
-            it('is initialized', function () {
-                expect($log.debug).toHaveBeenCalled();
-            });
+            describe('active filter', function () {
+                beforeEach(function () {
+                    AppraisalCycle.all.calls.reset();
+                });
 
-            it('stores on scope the data passed by ui-router', function () {
-                expect(ctrl.activeCycles).toBeDefined();
-                expect(ctrl.statusOverview).toBeDefined();
-                expect(ctrl.statuses).toBeDefined();
-                expect(ctrl.types).toBeDefined();
-            });
+                describe('when changing to a valid value', function () {
+                    beforeEach(function () {
+                        ctrl.changeActiveFilter('inactive');
+                    });
 
-            it('has the filters form collapsed', function () {
-                expect(ctrl.filtersCollapsed).toBe(true);
-            });
+                    it('sets the filter to the new value', function () {
+                        expect(ctrl.filters.active).toBe(false);
+                    });
 
-            it('has chartData as an empty array', function () {
-                expect(ctrl.chartData).toEqual([]);
-            });
+                    it('makes a new request to the api', function () {
+                        expect(AppraisalCycle.all).toHaveBeenCalledWith({ active: false });
+                    });
 
-            it('requests the grades data', function () {
-                expect(AppraisalCycle.grades).toHaveBeenCalled();
+                    describe('when changing to "all"', function () {
+                        beforeEach(function () {
+                            AppraisalCycle.all.calls.reset();
+                            ctrl.changeActiveFilter('all');
+                        });
+
+                        it('removes the `active` property from `filters`', function () {
+                            expect(AppraisalCycle.all).toHaveBeenCalledWith({});
+                        });
+                    });
+                });
+
+                describe('when changing to a invalid value', function () {
+                    beforeEach(function () {
+                        ctrl.changeActiveFilter('foo');
+                    });
+
+                    it('keep the old value set', function () {
+                        expect(ctrl.filters.active).toBe(true);
+                    });
+
+                    it('does not make a new request to the api', function () {
+                        expect(AppraisalCycle.all).not.toHaveBeenCalled();
+                    });
+                });
+
+                describe('when changing to the same value already set', function () {
+                    beforeEach(function () {
+                        ctrl.changeActiveFilter('inactive');
+                        AppraisalCycle.all.calls.reset();
+
+                        ctrl.changeActiveFilter('inactive');
+                    });
+
+                    it('does not make a new request to the api', function () {
+                        expect(AppraisalCycle.all).not.toHaveBeenCalled();
+                    });
+                })
             });
-        });
+        })
 
         /**
          * Initializes the controllers with its dependencies injected
