53ecb6459efc602e60ac34d73df17e0fe430ab80
Krzysztof Kałamarski <kkalamarski@gmail.com>
2015-12-17 17:04:20 +0100

    PCHR-607: Fix templates for angular-date decorated directives

--- org.civicrm.reqangular/src/common/angular-date/dist/angular-date.js
+++ org.civicrm.reqangular/src/common/angular-date/dist/angular-date.js
@@ -379,13 +379,9 @@ angular.module("templates/day.html", []).run(["$templateCache", function($templa
     "    <thead>\n" +
     "    <tr>\n" +
     "        <th>\n" +
-    "            <style>\n" +
-    "                .text-muted{\n" +
-    "                    color: #666666;\n" +
-    "                }\n" +
-    "            </style>\n" +
-    "            <button type=\"button\" class=\"btn btn-default btn-sm pull-left\" ng-click=\"move(-1)\" tabindex=\"-1\"><i\n" +
-    "                    class=\"glyphicon glyphicon-chevron-left\"></i></button>\n" +
+    "            <button type=\"button\" class=\"btn btn-default btn-sm pull-left\" ng-click=\"move(-1)\" tabindex=\"-1\">\n" +
+    "                <strong> < </strong>\n" +
+    "            </button>\n" +
     "        </th>\n" +
     "        <th colspan=\"{{5 + showWeeks}}\">\n" +
     "            <button id=\"{{uniqueId}}-title\" role=\"heading\" aria-live=\"assertive\" aria-atomic=\"true\" type=\"button\"\n" +
@@ -393,21 +389,25 @@ angular.module("templates/day.html", []).run(["$templateCache", function($templa
     "            </button>\n" +
     "        </th>\n" +
     "        <th>\n" +
-    "            <button type=\"button\" class=\"btn btn-default btn-sm pull-right\" ng-click=\"move(1)\" tabindex=\"-1\"><i\n" +
-    "                    class=\"glyphicon glyphicon-chevron-right\"></i></button>\n" +
+    "            <button type=\"button\" class=\"btn btn-default btn-sm pull-right\" ng-click=\"move(1)\" tabindex=\"-1\"><strong> > </strong></button>\n" +
     "        </th>\n" +
     "    </tr>\n" +
     "\n" +
-    "    <tr>\n" +
+    "    <tr style=\"border-top: 1px solid #DDD\">\n" +
     "        <th ng-show=\"showWeeks\" class=\"text-center\"></th>\n" +
     "        <th ng-repeat=\"label in labels track by $index\" class=\"text-center\">\n" +
-    "            <small aria-label=\"{{label.full}}\">{{label.abbr}}</small>\n" +
+    "            <small aria-label=\"{{label.full}}\">{{label.abbr[0]}}</small>\n" +
     "        </th>\n" +
     "    </tr>\n" +
     "\n" +
     "    </thead>\n" +
     "\n" +
     "    <tbody>\n" +
+    "    <style>\n" +
+    "        tr td.text-center span.out-of-scope{\n" +
+    "            color: #CCCCCC;\n" +
+    "        }\n" +
+    "    </style>\n" +
     "    <tr ng-repeat=\"row in rows track by $index\">\n" +
     "        <td ng-show=\"showWeeks\" class=\"text-center h6\"><em>{{ weekNumbers[$index] }}</em></td>\n" +
     "        <td ng-repeat=\"dt in row track by dt.date\" class=\"text-center\" role=\"gridcell\" id=\"{{dt.uid}}\"\n" +
@@ -422,8 +422,8 @@ angular.module("templates/day.html", []).run(["$templateCache", function($templa
     "                    ng-click=\"select(dt.date)\"\n" +
     "                    ng-disabled=\"dt.disabled\"\n" +
     "                    tabindex=\"-1\">\n" +
-    "\n" +
-    "                <span ng-class=\"{'text-muted': dt.secondary, 'text-info': dt.current}\">{{dt.label}}</span>\n" +
+    "<!--'text-muted': dt.secondary, -->\n" +
+    "                <span ng-class=\"{'out-of-scope': dt.secondary, 'text-info': dt.current}\">{{dt.label}}</span>\n" +
     "            </button>\n" +
     "        </td>\n" +
     "    </tr>\n" +
