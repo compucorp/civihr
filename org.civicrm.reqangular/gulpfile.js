5fcc4dd32fab12b6523dc0aebee6deb6d18dc606
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-01-28 16:57:10 +0100

    PCHR-782: Implement gulp-angular-templatecache module

--- org.civicrm.reqangular/gulpfile.js
+++ org.civicrm.reqangular/gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
+var replace = require('gulp-replace');
+var templateCache = require('gulp-angular-templatecache');
+
+
+gulp.task('cache-templates', function (cb) {
+    gulp.src('src/common/templates/**/*.html')
+        .pipe(templateCache({
+            moduleSystem: 'RequireJS'
+        }))
+        .pipe(replace("['angular']", "['common/angular']"))
+        .pipe(replace('module("templates")', 'module("common.templates", [])'))
+        .pipe(gulp.dest('src/common/modules'));
+
+    cb();
+});
 
 gulp.task('requirejs-bundle', function (done) {
     exec('r.js -o build.js', function (_, stdout, stderr) {
@@ -9,6 +24,7 @@ gulp.task('requirejs-bundle', function (done) {
 
 gulp.task('watch', function () {
     gulp.watch('src/**/*.js', ['requirejs-bundle']);
+    gulp.watch('src/common/templates/**/*.html', ['cache-templates', 'requirejs-bundle']);
 });
 
-gulp.task('default', ['requirejs-bundle', 'watch']);
+gulp.task('default', ['cache-templates', 'requirejs-bundle', 'watch']);
