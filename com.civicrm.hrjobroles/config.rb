b4e2bc1fbff46612d22aba2a6be5b80ecf016d99
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-02-02 11:48:58 +0100

    PCHR-834: Change sass/ folder into scss/

--- com.civicrm.hrjobroles/config.rb
+++ com.civicrm.hrjobroles/config.rb
@@ -5,7 +5,7 @@ require 'bootstrap-sass'
 # Set this to the root of your project when deployed:
 http_path = ""
 css_dir = "css"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
 
@@ -16,7 +16,7 @@ javascripts_dir = "js"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
