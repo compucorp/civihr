2b587a4982c0cb05ca4a398196fae8c53a2f4b61
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-02-03 08:35:50 +0100

    PCHR-835: Delete commented old code in job roles

--- com.civicrm.hrjobroles/js/build.js
+++ com.civicrm.hrjobroles/js/build.js
@@ -1,23 +1,3 @@
-// ({
-//     baseUrl : "js",
-//     name: "hrjobroles-main",
-//     out: "dist/hrjobroles-main.js",
-//     removeCombined: true,
-//     paths: {
-//         angularEditable: 'vendor/angular/xeditable.min',
-//         angularFilter: 'vendor/angular/angular-filter.min',
-//         requireLib:  'empty:'
-//     },
-//     deps: [
-//         'app',
-//         'controllers/HRJobRolesController',
-//         'services/HRJobRolesService',
-//         'directives/example',
-//         'requireLib'
-//     ],
-//     wrap: true
-// })
-
 ({
     baseUrl : 'src',
     out: 'dist/job-roles.min.js',
