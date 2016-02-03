09477bb940b7e87a21e2a29dbf79eac4e7ca9147
Krzysztof Kałamarski <kkalamarski@gmail.com>
2016-02-03 14:12:58 +0200

    PCHR-796: Setting up test env. for common dependencies.

---
+++ org.civicrm.reqangular/src/tests/karma.conf.js
@@ -0,0 +1,54 @@
+module.exports = function (config) {
+    var civicrmPath = '../../../../../../';
+    var civihrPath = 'tools/extensions/civihr/';
+
+    config.set({
+        basePath: civicrmPath,
+        browsers: ['Chrome'],
+        frameworks: ['jasmine'],
+        files: [
+            // the global dependencies
+            'packages/jquery/jquery-1.11.1.js',
+            'packages/jquery/jquery-ui/jquery-ui.js',
+            'packages/backbone/lodash.compat.js',
+            'packages/jquery/plugins/jquery.mousewheel.js',
+            'packages/jquery/plugins/select2/select2.js',
+            'packages/jquery/plugins/jquery.blockUI.js',
+            'js/Common.js',
+
+            // manual loading of requirejs as to avoid interference with the global dependencies above
+            civihrPath + 'uk.co.compucorp.civicrm.appraisals/node_modules/requirejs/require.js',
+            civihrPath + 'uk.co.compucorp.civicrm.appraisals/node_modules/karma-requirejs/lib/adapter.js',
+
+            // all the common/ dependencies
+            civihrPath + 'org.civicrm.reqangular/dist/reqangular.min.js',
+
+            // the application modules
+            { pattern: civihrPath + 'uk.co.compucorp.civicrm.appraisals/js/src/appraisals/**/*.js', included: false },
+
+            // the mocked components files
+            { pattern: civihrPath + 'uk.co.compucorp.civicrm.appraisals/js/test/mocks/**/*.js', included: false },
+
+            // the test files
+            { pattern: civihrPath + 'uk.co.compucorp.civicrm.appraisals/js/test/**/*_test.js', included: false },
+
+            // angular templates
+            civihrPath + 'uk.co.compucorp.civicrm.appraisals/views/**/*.html',
+
+            // the requireJS config file that bootstraps the whole test suite
+            civihrPath + 'uk.co.compucorp.civicrm.appraisals/js/test/test-main.js'
+        ],
+        exclude: [
+            civihrPath + 'uk.co.compucorp.civicrm.appraisals/js/src/appraisals.js'
+        ],
+        // Used to transform angular templates in JS strings
+        preprocessors: (function (obj) {
+            obj[civihrPath + 'uk.co.compucorp.civicrm.appraisals/views/**/*.html'] = ['ng-html2js'];
+            return obj;
+        })({}),
+        ngHtml2JsPreprocessor: {
+            prependPrefix: '/base/',
+            moduleName: 'appraisals.templates'
+        }
+    });
+};
---
+++ org.civicrm.reqangular/src/tests/test-main.js
@@ -0,0 +1,26 @@
+var TEST_REGEXP = /(spec|test)\.js$/i;
+var allTestFiles = [];
+var extPath = '/base/tools/extensions/civihr/uk.co.compucorp.civicrm.appraisals';
+var mocksPath = extPath + '/js/test/mocks';
+var srcPath = extPath + '/js/src/appraisals';
+
+Object.keys(window.__karma__.files).forEach(function(file) {
+    if (TEST_REGEXP.test(file)) {
+        allTestFiles.push(file);
+    }
+});
+
+require.config({
+    deps: allTestFiles,
+    waitSeconds: 60,
+    paths: {
+        'appraisals': srcPath,
+        'mocks': mocksPath
+    },
+    callback: function () {
+        // Simple hack to provide value to CRM.vars.appraisals.baseURL
+        CRM.vars = { appraisals: { baseURL: extPath } };
+
+        window.__karma__.start();
+    }
+});
