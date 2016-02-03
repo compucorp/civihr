7021aaab5823e919b123bb38ee3c2bed9c4487ea
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-02-03 09:25:28 +0100

    PCHR-835: Refactor app structure

--- hrjobcontract/js/filters/capitalize.js
+++
@@ -1,9 +0,0 @@
-define(['filters/filters'], function(filters){
-    filters.filter('capitalize',['$log',function($log){
-        $log.debug('Filter: capitalize');
-
-        return function(input) {
-            return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
-        }
-    }]);
-});
--- hrjobcontract/js/filters/formatAmount.js
+++
@@ -1,9 +0,0 @@
-define(['filters/filters'], function(filters){
-    filters.filter('formatAmount',['$log',function($log){
-        $log.debug('Filter: formatAmount');
-
-        return function(input) {
-            return input && input.indexOf('.') === -1 ? input+'.00' : input
-        }
-    }]);
-});
