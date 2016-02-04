768157d8be635e06f1b6cf9e82b8a432270e8c42
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-02-04 17:51:52 +0100

    PCHR-839: Move bootstrap code in app.js

--- hrjobcontract/js/src/job-contract/app.js
+++ hrjobcontract/js/src/job-contract/app.js
@@ -34,7 +34,7 @@ define([
 ], function (angular) {
     'use strict';
 
-    return angular.module('hrjc', [
+    angular.module('hrjc', [
         'ngAnimate',
         'ngRoute',
         'ngResource',
@@ -46,5 +46,74 @@ define([
         'hrjc.directives',
         'hrjc.filters',
         'hrjc.services'
+    ])
+    .constant('settings', {
+        classNamePrefix: 'hrjc-',
+        contactId: CRM.jobContractTabApp.contactId,
+        debug: +CRM.debug,
+        pathApp: CRM.jobContractTabApp.path,
+        pathFile: CRM.url('civicrm/hrjobcontract/file/'),
+        pathReport: CRM.url('civicrm/report/hrjobcontract/summary'),
+        pathRest: CRM.url('civicrm/ajax/rest'),
+        pathTpl: CRM.jobContractTabApp.path + 'views/',
+        CRM: {
+            options: CRM.FieldOptions || {},
+            defaultCurrency: CRM.jobContractTabApp.defaultCurrency,
+            apiTsFmt: 'YYYY-MM-DD HH:mm:ss',
+            fields: CRM.jobContractTabApp.fields,
+            maxFileSize: CRM.jobContractTabApp.maxFileSize
+        }
+    })
+    .config(['settings','$routeProvider','$resourceProvider','$logProvider','$httpProvider','datepickerConfig','uiSelectConfig',
+        function (settings, $routeProvider, $resourceProvider, $logProvider, $httpProvider, datepickerConfig, uiSelectConfig) {
+            $logProvider.debugEnabled(settings.debug);
+
+            $routeProvider.
+                when('/', {
+                    controller: 'ContractListCtrl',
+                    templateUrl: settings.pathApp+'views/contractList.html?v='+(new Date().getTime()),
+                    resolve: {
+                        contractList: ['ContractService',function(ContractService){
+                            return ContractService.get()
+                        }]
+                    }
+                }
+            ).otherwise({redirectTo:'/'});
+
+            $resourceProvider.defaults.stripTrailingSlashes = false;
+
+            $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
+            uiSelectConfig.theme = 'bootstrap';
+
+            datepickerConfig.showWeeks = false;
+        }
+    ])
+    .run(['settings','$rootScope','$q', '$log', 'ContractService', 'ContractDetailsService', 'ContractHourService',
+        'ContractPayService', 'ContractLeaveService', 'ContractHealthService', 'ContractPensionService',
+        function(settings, $rootScope, $q, $log, ContractService, ContractDetailsService, ContractHourService, ContractPayService,
+                 ContractLeaveService, ContractHealthService, ContractPensionService){
+            $log.debug('app.run');
+
+            $rootScope.pathTpl = settings.pathTpl;
+            $rootScope.prefix = settings.classNamePrefix;
+
+            $q.all({
+                contract: ContractService.getRevisionOptions(),
+                details: ContractDetailsService.getOptions(),
+                hour: ContractHourService.getOptions(),
+                pay: ContractPayService.getOptions(),
+                leave: ContractLeaveService.getOptions(),
+                health: ContractHealthService.getOptions(),
+                pension: ContractPensionService.getOptions()
+            }).then(function(results){
+                results.pay.pay_is_auto_est = ['No','Yes'];
+                results.pension.is_enrolled = ['No','Yes','Opted out'];
+
+                $log.debug('OPTIONS:');
+                $log.debug(results);
+                $rootScope.options = results;
+            });
+        }
     ]);
 });
