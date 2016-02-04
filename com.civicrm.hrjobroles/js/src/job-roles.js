b9181903e12e30dc044104c05adbecafb1920e07
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-02-04 17:31:18 +0100

    PCHR-839: Move dependencies requests and bootstrap code in app.js

--- com.civicrm.hrjobroles/js/src/job-roles.js
+++ com.civicrm.hrjobroles/js/src/job-roles.js
@@ -10,58 +10,8 @@
         }
     });
 
-    require([
-        'job-roles/app',
-        'job-roles/controllers/hr-job-roles-controller',
-        'job-roles/services/hr-job-roles-service',
-        'job-roles/directives/example'
-    ], function (app) {
+    require(['job-roles/app'], function (app) {
         'use strict';
-
-        app.constant('settings', {
-            classNamePrefix: 'hrjobroles-',
-            contactId: decodeURIComponent((new RegExp('[?|&]cid=([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null,
-            debug: true,
-            pathApp: '',
-            pathRest: CRM.url('civicrm/ajax/rest'),
-            pathBaseUrl: CRM.vars.hrjobroles.baseURL + '/',
-            pathTpl: 'views/',
-            pathIncludeTpl: 'views/include/'
-        });
-
-        app.config(['settings','$routeProvider','$resourceProvider','$httpProvider','$logProvider',
-            function(settings, $routeProvider, $resourceProvider, $httpProvider, $logProvider){
-                $logProvider.debugEnabled(settings.debug);
-
-                $routeProvider.
-                    when('/', {
-                        templateUrl: settings.pathBaseUrl + settings.pathTpl + 'mainTemplate.html?v=1',
-                        resolve: {}
-                    }).
-                    otherwise({redirectTo:'/'});
-
-                $resourceProvider.defaults.stripTrailingSlashes = false;
-
-                $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-            }
-        ]);
-
-        app.run(['settings','$rootScope','$q', '$log', 'editableOptions',
-            function(settings, $rootScope, $q, $log, editableOptions){
-                $log.debug('app.run');
-
-                // Set bootstrap 3 as default theme
-                editableOptions.theme = 'bs3';
-
-                // Pass the values from our settings
-                $rootScope.contactId = settings.contactId;
-                $rootScope.pathBaseUrl = settings.pathBaseUrl;
-                $rootScope.pathTpl = settings.pathTpl;
-                $rootScope.pathIncludeTpl = settings.pathIncludeTpl;
-                $rootScope.prefix = settings.classNamePrefix;
-
-            }
-        ]);
 
         document.addEventListener('hrjobrolesLoad', function(){
             angular.bootstrap(document.getElementById('hrjobroles'), ['hrjobroles']);
