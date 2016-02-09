d347d5a57191d16c800afba780f0936b82436ff4
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-02-09 13:44:33 +0100

    PCHR-567: Grades panel

--- uk.co.compucorp.civicrm.appraisals/js/src/appraisals/controllers/appraisal-cycle-ctrl.js
+++ uk.co.compucorp.civicrm.appraisals/js/src/appraisals/controllers/appraisal-cycle-ctrl.js
@@ -8,7 +8,17 @@ define([
         function ($log) {
             $log.debug('AppraisalCycleCtrl');
 
-            return {};
+            var vm = {};
+
+            // dummy data
+            vm.grades = [
+                { label: "Value #1", value: 10 },
+                { label: "Value #2", value: 20 },
+                { label: "Value #3", value: 30 },
+                { label: "Value #4", value: 40 }
+            ];
+
+            return vm;
         }
     ]);
 });
