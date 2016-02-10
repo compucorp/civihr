ac48714f10d88b49e8ef8b7ff145781125aae0a6
Krzysztof Kałamarski <kkalamarski@gmail.com>
2016-02-10 11:05:49 +0200

    PCHR-797: Job Contracts

--- hrjobcontract/js/src/job-contract/app.js
+++ hrjobcontract/js/src/job-contract/app.js
@@ -30,90 +30,100 @@ define([
     'job-contract/services/services',
     'job-contract/services/contract',
     'job-contract/vendor/angular-select',
-    'job-contract/vendor/job-summary'
+    'job-contract/vendor/job-summary',
+    'common/services/angular-date/date-format',
+    'common/modules/routers/compu-ng-route',
+    'common/directives/angular-date/date-input'
 ], function (angular) {
     'use strict';
 
     angular.module('hrjc', [
-        'ngAnimate',
-        'ngRoute',
-        'ngResource',
-        'ui.bootstrap',
-        'ui.select',
-        'angular-date',
-        'angularFileUpload',
-        'hrjc.controllers',
-        'hrjc.directives',
-        'hrjc.filters',
-        'hrjc.services'
-    ])
-    .constant('settings', {
-        classNamePrefix: 'hrjc-',
-        contactId: CRM.jobContractTabApp.contactId,
-        debug: +CRM.debug,
-        pathApp: CRM.jobContractTabApp.path,
-        pathFile: CRM.url('civicrm/hrjobcontract/file/'),
-        pathReport: CRM.url('civicrm/report/hrjobcontract/summary'),
-        pathRest: CRM.url('civicrm/ajax/rest'),
-        pathTpl: CRM.jobContractTabApp.path + 'views/',
-        CRM: {
-            options: CRM.FieldOptions || {},
-            defaultCurrency: CRM.jobContractTabApp.defaultCurrency,
-            apiTsFmt: 'YYYY-MM-DD HH:mm:ss',
-            fields: CRM.jobContractTabApp.fields,
-            maxFileSize: CRM.jobContractTabApp.maxFileSize
-        }
-    })
-    .config(['settings','$routeProvider','$resourceProvider','$logProvider','$httpProvider','datepickerConfig','uiSelectConfig',
-        function (settings, $routeProvider, $resourceProvider, $logProvider, $httpProvider, datepickerConfig, uiSelectConfig) {
-            $logProvider.debugEnabled(settings.debug);
+            'ngAnimate',
+            'compuNgRoute',
+            'ngResource',
+            'ui.bootstrap',
+            'ui.select',
+            'angular-date',
+            'common.angularDate',
+            'angularFileUpload',
+            'hrjc.controllers',
+            'hrjc.directives',
+            'hrjc.filters',
+            'hrjc.services'
+        ])
+        .constant('settings', {
+            classNamePrefix: 'hrjc-',
+            contactId: CRM.jobContractTabApp.contactId,
+            debug: +CRM.debug,
+            pathApp: CRM.jobContractTabApp.path,
+            pathFile: CRM.url('civicrm/hrjobcontract/file/'),
+            pathReport: CRM.url('civicrm/report/hrjobcontract/summary'),
+            pathRest: CRM.url('civicrm/ajax/rest'),
+            pathTpl: CRM.jobContractTabApp.path + 'views/',
+            CRM: {
+                options: CRM.FieldOptions || {},
+                defaultCurrency: CRM.jobContractTabApp.defaultCurrency,
+                apiTsFmt: 'YYYY-MM-DD HH:mm:ss',
+                fields: CRM.jobContractTabApp.fields,
+                maxFileSize: CRM.jobContractTabApp.maxFileSize
+            }
+        })
+        .config(['settings', '$routeProvider', '$resourceProvider', '$logProvider', '$httpProvider', 'datepickerConfig', 'uiSelectConfig',
+            function (settings, $routeProvider, $resourceProvider, $logProvider, $httpProvider, datepickerConfig, uiSelectConfig) {
+                $logProvider.debugEnabled(settings.debug);
 
-            $routeProvider.
-                when('/', {
-                    controller: 'ContractListCtrl',
-                    templateUrl: settings.pathApp+'views/contractList.html?v='+(new Date().getTime()),
-                    resolve: {
-                        contractList: ['ContractService',function(ContractService){
-                            return ContractService.get()
+                $routeProvider
+                    .resolveForAll({
+                        format: ['DateFormat', function(DateFormat){
+                            return DateFormat.getDateFormat();
                         }]
-                    }
-                }
-            ).otherwise({redirectTo:'/'});
+                    })
+                    .when('/', {
+                            controller: 'ContractListCtrl',
+                            templateUrl: settings.pathApp + 'views/contractList.html?v=' + (new Date().getTime()),
+                            resolve: {
+                                contractList: ['ContractService', function (ContractService) {
+                                    return ContractService.get()
+                                }]
+                            }
+                        }
+                    )
+                    .otherwise({ redirectTo: '/' });
 
-            $resourceProvider.defaults.stripTrailingSlashes = false;
+                $resourceProvider.defaults.stripTrailingSlashes = false;
 
-            $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+                $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 
-            uiSelectConfig.theme = 'bootstrap';
+                uiSelectConfig.theme = 'bootstrap';
 
-            datepickerConfig.showWeeks = false;
-        }
-    ])
-    .run(['settings','$rootScope','$q', '$log', 'ContractService', 'ContractDetailsService', 'ContractHourService',
-        'ContractPayService', 'ContractLeaveService', 'ContractHealthService', 'ContractPensionService',
-        function(settings, $rootScope, $q, $log, ContractService, ContractDetailsService, ContractHourService, ContractPayService,
-                 ContractLeaveService, ContractHealthService, ContractPensionService){
-            $log.debug('app.run');
+                datepickerConfig.showWeeks = false;
+            }
+        ])
+        .run(['settings', '$rootScope', '$q', '$log', 'ContractService', 'ContractDetailsService', 'ContractHourService',
+            'ContractPayService', 'ContractLeaveService', 'ContractHealthService', 'ContractPensionService',
+            function (settings, $rootScope, $q, $log, ContractService, ContractDetailsService, ContractHourService, ContractPayService,
+                      ContractLeaveService, ContractHealthService, ContractPensionService) {
+                $log.debug('app.run');
 
-            $rootScope.pathTpl = settings.pathTpl;
-            $rootScope.prefix = settings.classNamePrefix;
+                $rootScope.pathTpl = settings.pathTpl;
+                $rootScope.prefix = settings.classNamePrefix;
 
-            $q.all({
-                contract: ContractService.getRevisionOptions(),
-                details: ContractDetailsService.getOptions(),
-                hour: ContractHourService.getOptions(),
-                pay: ContractPayService.getOptions(),
-                leave: ContractLeaveService.getOptions(),
-                health: ContractHealthService.getOptions(),
-                pension: ContractPensionService.getOptions()
-            }).then(function(results){
-                results.pay.pay_is_auto_est = ['No','Yes'];
-                results.pension.is_enrolled = ['No','Yes','Opted out'];
+                $q.all({
+                    contract: ContractService.getRevisionOptions(),
+                    details: ContractDetailsService.getOptions(),
+                    hour: ContractHourService.getOptions(),
+                    pay: ContractPayService.getOptions(),
+                    leave: ContractLeaveService.getOptions(),
+                    health: ContractHealthService.getOptions(),
+                    pension: ContractPensionService.getOptions()
+                }).then(function (results) {
+                    results.pay.pay_is_auto_est = ['No', 'Yes'];
+                    results.pension.is_enrolled = ['No', 'Yes', 'Opted out'];
 
-                $log.debug('OPTIONS:');
-                $log.debug(results);
-                $rootScope.options = results;
-            });
-        }
-    ]);
+                    $log.debug('OPTIONS:');
+                    $log.debug(results);
+                    $rootScope.options = results;
+                });
+            }
+        ]);
 });
