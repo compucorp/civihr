719d68071e03ccf14749b590044bafce837708f9
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-02-21 18:49:46 +0100

    PCHR-578: Amend mocked appraisals structure

--- org.civicrm.reqangular/test/mocks/services/api/appraisal-mock.js
+++ org.civicrm.reqangular/test/mocks/services/api/appraisal-mock.js
@@ -54,8 +54,6 @@ define([
                         {
                             id: '3451',
                             appraisal_cycle_id: '1',
-                            contact_id: '201',
-                            manager_id: '201',
                             self_appraisal_due: '2016-01-01',
                             manager_appraisal_due: '2016-02-02',
                             grade_due: '2016-03-03',
@@ -65,29 +63,41 @@ define([
                             status_id: '2',
                             original_id: '3451',
                             created_date: '2015-01-01',
-                            is_current: '1'
+                            is_current: '1',
+                            contact: {
+                                id: '201',
+                                display_name: 'Contact #1'
+                            },
+                            manager: {
+                                id: '301',
+                                display_name: 'Manager #1'
+                            }
                         },
                         {
                             id: '3452',
                             appraisal_cycle_id: '2',
-                            contact_id: '202',
-                            manager_id: '202',
                             self_appraisal_due: '2016-02-02',
                             manager_appraisal_due: '2016-03-03',
                             grade_due: '2016-04-04',
                             due_changed: '0',
                             meeting_completed: '0',
                             approved_by_employee: '0',
-                            status_id: '1',
+                            status_id: '3',
                             original_id: '3452',
                             created_date: '2015-02-02',
-                            is_current: '0'
+                            is_current: '1',
+                            contact: {
+                                id: '202',
+                                display_name: 'Contact #2'
+                            },
+                            manager: {
+                                id: '302',
+                                display_name: 'Manager #2'
+                            }
                         },
                         {
                             id: '3453',
                             appraisal_cycle_id: '3',
-                            contact_id: '203',
-                            manager_id: '203',
                             self_appraisal_due: '2016-03-03',
                             manager_appraisal_due: '2016-04-04',
                             grade_due: '2016-05-05',
@@ -97,13 +107,19 @@ define([
                             status_id: '2',
                             original_id: '3453',
                             created_date: '2015-03-03',
-                            is_current: '1'
+                            is_current: '1',
+                            contact: {
+                                id: '203',
+                                display_name: 'Contact #3'
+                            },
+                            manager: {
+                                id: '303',
+                                display_name: 'Manager #3'
+                            }
                         },
                         {
                             id: '3454',
                             appraisal_cycle_id: '4',
-                            contact_id: '204',
-                            manager_id: '204',
                             self_appraisal_due: '2016-04-04',
                             manager_appraisal_due: '2016-05-05',
                             grade_due: '2016-06-06',
@@ -113,13 +129,19 @@ define([
                             status_id: '3',
                             original_id: '3454',
                             created_date: '2015-04-04',
-                            is_current: '1'
+                            is_current: '1',
+                            contact: {
+                                id: '204',
+                                display_name: 'Contact #4'
+                            },
+                            manager: {
+                                id: '304',
+                                display_name: 'Manager #4'
+                            }
                         },
                         {
                             id: '3455',
                             appraisal_cycle_id: '5',
-                            contact_id: '205',
-                            manager_id: '205',
                             self_appraisal_due: '2016-05-05',
                             manager_appraisal_due: '2016-06-06',
                             grade_due: '2016-07-07',
@@ -129,13 +151,19 @@ define([
                             status_id: '1',
                             original_id: '3455',
                             created_date: '2015-05-05',
-                            is_current: '1'
+                            is_current: '1',
+                            contact: {
+                                id: '205',
+                                display_name: 'Contact #5'
+                            },
+                            manager: {
+                                id: '305',
+                                display_name: 'Manager #5'
+                            }
                         },
                         {
                             id: '3456',
                             appraisal_cycle_id: '6',
-                            contact_id: '206',
-                            manager_id: '206',
                             self_appraisal_due: '2016-06-06',
                             manager_appraisal_due: '2016-07-07',
                             grade_due: '2016-08-08',
@@ -143,15 +171,65 @@ define([
                             meeting_completed: '0',
                             approved_by_employee: '0',
                             status_id: '1',
+                            original_id: '3456',
+                            created_date: '2015-06-06',
+                            is_current: '0',
+                            contact: {
+                                id: '206',
+                                display_name: 'Contact #6'
+                            },
+                            manager: {
+                                id: '306',
+                                display_name: 'Manager #6'
+                            }
+                        },
+                        {
+                            id: '3457',
+                            appraisal_cycle_id: '7',
+                            self_appraisal_due: '2016-07-07',
+                            manager_appraisal_due: '2016-08-08',
+                            grade_due: '2016-09-09',
+                            due_changed: '0',
+                            meeting_completed: '0',
+                            approved_by_employee: '0',
+                            status_id: '1',
+                            original_id: '3457',
+                            created_date: '2015-07-07',
+                            is_current: '0',
+                            contact: {
+                                id: '207',
+                                display_name: 'Contact #7'
+                            },
+                            manager: {
+                                id: '307',
+                                display_name: 'Manager #7'
+                            }
+                        },
+                        {
+                            id: '3458',
+                            appraisal_cycle_id: '8',
+                            self_appraisal_due: '2016-08-08',
+                            manager_appraisal_due: '2016-09-09',
+                            grade_due: '2016-10-10',
+                            due_changed: '0',
+                            meeting_completed: '0',
+                            approved_by_employee: '0',
+                            status_id: '1',
                             original_id: '3458',
                             created_date: '2015-08-08',
-                            is_current: '0'
+                            is_current: '0',
+                            contact: {
+                                id: '208',
+                                display_name: 'Contact #8'
+                            },
+                            manager: {
+                                id: '308',
+                                display_name: 'Manager #8'
+                            }
                         },
                         {
                             id: '3459',
                             appraisal_cycle_id: '9',
-                            contact_id: '209',
-                            manager_id: '209',
                             self_appraisal_due: '2016-09-09',
                             manager_appraisal_due: '2016-10-10',
                             grade_due: '2016-11-11',
@@ -161,13 +239,19 @@ define([
                             status_id: '3',
                             original_id: '3459',
                             created_date: '2015-09-09',
-                            is_current: '1'
+                            is_current: '1',
+                            contact: {
+                                id: '209',
+                                display_name: 'Contact #9'
+                            },
+                            manager: {
+                                id: '309',
+                                display_name: 'Manager #9'
+                            }
                         },
                         {
                             id: '3460',
                             appraisal_cycle_id: '10',
-                            contact_id: '210',
-                            manager_id: '210',
                             self_appraisal_due: '2016-10-10',
                             manager_appraisal_due: '2016-11-11',
                             grade_due: '2016-12-12',
@@ -177,7 +261,15 @@ define([
                             status_id: '1',
                             original_id: '3460',
                             created_date: '2015-10-10',
-                            is_current: '1'
+                            is_current: '1',
+                            contact: {
+                                id: '213',
+                                display_name: 'Contact #10'
+                            },
+                            manager: {
+                                id: '313',
+                                display_name: 'Manager #10'
+                            }
                         },
                     ]
                 }
