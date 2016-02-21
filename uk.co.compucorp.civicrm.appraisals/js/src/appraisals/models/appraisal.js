6dd95e6ea8cda937351ec314650729364cf8a403
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-02-21 17:26:27 +0100

    PCHR-578: Implement Model in Appraisal

--- uk.co.compucorp.civicrm.appraisals/js/src/appraisals/models/appraisal.js
+++ uk.co.compucorp.civicrm.appraisals/js/src/appraisals/models/appraisal.js
@@ -1,42 +1,46 @@
 define([
     'appraisals/modules/models',
+    'common/models/model',
     'common/services/api/appraisal'
 ], function (models) {
     'use strict';
 
-    models.factory('Appraisal', ['api.appraisal', 'AppraisalInstance', function (appraisalAPI, instance) {
+    models.factory('Appraisal', [
+        'Model', 'api.appraisal', 'AppraisalInstance',
+        function (Model, appraisalAPI, instance) {
 
-        return {
+            return Model.extend({
 
-            /**
-             * Returns a list of appraisals, each converted to a model instance
-             *
-             * @param {object} filters - Values the full list should be filtered by
-             * @param {object} pagination
-             *   `page` for the current page, `size` for number of items per page
-             * @return {Promise}
-             */
-            all: function (filters, pagination) {
-                return appraisalAPI.all(filters, pagination).then(function (response) {
-                    response.list = response.list.map(function (appraisal) {
+                /**
+                 * Returns a list of appraisals, each converted to a model instance
+                 *
+                 * @param {object} filters - Values the full list should be filtered by
+                 * @param {object} pagination
+                 *   `page` for the current page, `size` for number of items per page
+                 * @return {Promise}
+                 */
+                all: function (filters, pagination) {
+                    return appraisalAPI.all(this.processFilters(filters), pagination).then(function (response) {
+                        response.list = response.list.map(function (appraisal) {
+                            return instance.init(appraisal, true);
+                        });
+
+                        return response;
+                    });
+                },
+
+                /**
+                 * Finds an appraisal by id
+                 *
+                 * @param {string} id
+                 * @return {Promise} - Resolves with found appraisail
+                 */
+                find: function (id) {
+                    return appraisalAPI.find(id).then(function (appraisal) {
                         return instance.init(appraisal, true);
                     });
-
-                    return response;
-                });
-            },
-
-            /**
-             * Finds an appraisal by id
-             *
-             * @param {string} id
-             * @return {Promise} - Resolves with found appraisail
-             */
-            find: function (id) {
-                return appraisalAPI.find(id).then(function (appraisal) {
-                    return instance.init(appraisal, true);
-                });
-            }
-        };
-    }]);
+                }
+            });
+        }
+    ]);
 })
