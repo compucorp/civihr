b4835ef2f48ba32b156cb5022ae815dda327bce2
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-02-23 14:06:19 +0100

    PCHR-578: Add validation of Edit Dates form

--- uk.co.compucorp.civicrm.appraisals/js/src/appraisals/controllers/modals/edit-dates-modal-ctrl.js
+++ uk.co.compucorp.civicrm.appraisals/js/src/appraisals/controllers/modals/edit-dates-modal-ctrl.js
@@ -10,17 +10,25 @@ define([
             $log.debug('EditDatesModalCtrl');
 
             var vm = Object.create($controller('BasicModalCtrl', {
-                $modalInstance: $modalInstance,
-                $scope: $scope
+                $modalInstance: $modalInstance
             }));
 
             vm.cycle = angular.copy($scope.cycle);
+            vm.formErrors = {};
+            vm.formSubmitted = false;
 
             /**
              * Submits the form
              */
             vm.submit = function () {
+                vm.formSubmitted = true;
+
                 formatDates();
+
+                if (!vm.form.$valid) {
+                    vm.formErrors = formErrors();
+                    return;
+                }
 
                 vm.cycle.update().then(function () {
                     $rootScope.$emit('AppraisalCycle::edit', vm.cycle);
@@ -38,6 +46,37 @@ define([
                 Object.keys(vm.cycle.dueDates()).forEach(function (key) {
                     vm.cycle[key] = $filter('date')(vm.cycle[key], 'dd/MM/yyyy');
                 });
+            }
+
+            /**
+             * Extracts from the AngularJS form object all the current errors
+             * for each field
+             *
+             * @return {object} contains the errors grouped by field:
+             *   {
+             *     field_1: { error_1: true, error_2: true },
+             *     // ..
+             *     field_N: { error_1: true }
+             *   }
+             */
+            function formErrors() {
+                return _(vm.form)
+                    // filters out internal properties of the form object
+                    .omit(function (value, key) {
+                        return _.startsWith(key, '$');
+                    })
+                    // creates an object which groups the errors by field
+                    .transform(function (result, value, key) {
+                        result[key] = _.omit(value.$error, function (value) {
+                            return !value
+                        });
+                        return result;
+                    }, {})
+                    // filters out fields with no errors
+                    .omit(function (value) {
+                        return _.isEmpty(value);
+                    })
+                    .value()
             }
 
             return vm;
--- uk.co.compucorp.civicrm.appraisals/js/test/controllers/modals/edit-dates-modal-ctrl_test.js
+++ uk.co.compucorp.civicrm.appraisals/js/test/controllers/modals/edit-dates-modal-ctrl_test.js
@@ -7,11 +7,11 @@ define([
     'use strict';
 
     describe('EditDatesModalCtrl', function () {
-        var $filter, $modalInstance, $provide, $rootScope, appraisalCycleAPIMock,
-            cycle, ctrl;
+        var $compile, $controller, $filter, $modalInstance, $provide, $rootScope,
+        $templateCache, $scope, appraisalCycleAPIMock, cycle, ctrl;
 
         beforeEach(function () {
-            module('appraisals', 'common.mocks', function (_$provide_) {
+            module('common.mocks', 'appraisals', 'appraisals.templates', function (_$provide_) {
                 $provide = _$provide_;
             });
             // Override api.appraisal-cycle with the mocked version
@@ -24,23 +24,23 @@ define([
             ]);
         });
         beforeEach(inject([
-            '$controller', '$rootScope', 'AppraisalCycleInstance',
-            function ($controller, _$rootScope_, AppraisalCycleInstance) {
-                $rootScope = _$rootScope_;
+            '$compile', '$controller', '$filter', '$rootScope', '$templateCache',
+            'AppraisalCycleInstance',
+            function (_$compile_, _$controller_, _$filter_, _$rootScope_, _$templateCache_, AppraisalCycleInstance) {
                 cycle = AppraisalCycleInstance.init(
                     appraisalCycleAPIMock.mockedCycles().list[2]
                 );
 
-                createSpies();
+                $compile = _$compile_;
+                $controller = _$controller_;
+                $templateCache = _$templateCache_;
+                $rootScope = _$rootScope_;
 
-                ctrl = $controller('EditDatesModalCtrl', {
-                    $filter: $filter,
-                    $modalInstance: $modalInstance,
-                    $scope: (function (scope) {
-                        scope.cycle = cycle;
-                        return scope;
-                    })($rootScope.$new())
-                });
+                initSpies(_$filter_);
+                initController();
+                initForm();
+
+                spyOn(ctrl.cycle, 'update').and.callThrough();
             }
         ]));
 
@@ -50,24 +50,83 @@ define([
             });
         });
 
-        describe('cycle instance', function () {
+        describe('init()', function () {
+            it('marks the form as not submitted', function () {
+                expect(ctrl.formSubmitted).toBe(false);
+            });
+
+            it('has an empty list of form errors', function () {
+                expect(ctrl.formErrors).toEqual({});
+            });
+
             it('contains the cycle in its scope', function () {
                 expect(ctrl.cycle).toBeDefined();
                 expect(ctrl.cycle.id).toBe(cycle.id);
             });
 
-            it('makes a copy of the cycle, not working directly on it', function () {
+            it('has made a copy of the cycle, not working directly on it', function () {
                 expect(ctrl.cycle).not.toBe(cycle);
+            });
+        });
+
+        describe('form validation', function () {
+            var validData = {
+                cycle_self_appraisal_due: '01/01/2001',
+                cycle_manager_appraisal_due: '02/02/2002',
+                cycle_grade_due: '03/03/2003',
+            };
+
+            describe('valid data', function () {
+                beforeEach(function () {
+                    submitFormWith(validData);
+                });
+
+                it('submits the form when validation is passed', function () {
+                    expect(ctrl.form.$valid).toBe(true);
+                });
+            });
+
+            describe('mandatory fields', function () {
+                beforeEach(function () {
+                    submitFormWith(_.omit(validData, ['cycle_self_appraisal_due', 'cycle_grade_due']));
+                });
+
+                it('must be present', function () {
+                    expect(ctrl.form.$valid).toBe(false);
+                    expect(ctrl.form.cycle_self_appraisal_due.$error.required).toBe(true);
+                    expect(ctrl.form.cycle_grade_due.$error.required).toBe(true);
+                    expect(ctrl.cycle.update).not.toHaveBeenCalled();
+                });
+            });
+        });
+
+        describe('form errors', function () {
+            var invalidData = {
+                cycle_self_appraisal_due: '01/01/2001'
+            };
+
+            beforeEach(function () {
+                submitFormWith(invalidData);
+            });
+
+            it('returns the list of fields, each with its own errors', function () {
+                expect(ctrl.formErrors).toEqual({
+                    cycle_grade_due: { required: true },
+                    cycle_manager_appraisal_due: { required: true }
+                });
             });
         });
 
         describe('submit()', function () {
             beforeEach(function () {
-                spyOn(ctrl.cycle, 'update').and.callThrough();
                 spyOn($rootScope, '$emit');
 
                 ctrl.submit();
                 $rootScope.$digest();
+            });
+
+            it('marks the form as submitted', function () {
+                expect(ctrl.formSubmitted).toBe(true);
             });
 
             it('formats the datepicker dates', function () {
@@ -90,11 +149,62 @@ define([
         /**
          * Creates fake functions to inject in the controller
          */
-        function createSpies() {
+        function initSpies(_$filter_) {
             $modalInstance = jasmine.createSpyObj('modalInstance', ['close']);
-            $filter = jasmine.createSpy('filter').and.callFake(function () {
-                return _.noop;
+            $filter = jasmine.createSpy('filter').and.callFake(function (filter) {
+                return _$filter_.apply(null, arguments);
             });
+        }
+
+        /**
+         * Initializes the controller with fake dependencies
+         */
+        function initController() {
+            ctrl = $controller('EditDatesModalCtrl', {
+                $filter: $filter,
+                $modalInstance: $modalInstance,
+                $scope: (function (scope) {
+                    scope.cycle = cycle;
+                    return scope;
+                })($rootScope.$new())
+            });
+        }
+
+        /**
+         * Initializes the form the modal controller is tied to.
+         *
+         * It is necessary to remove the reference to the datepicker directive
+         * otherwise it will interfere with the direct insertions of values
+         * in the fields the directive it is applied to
+         *
+         * It compiles it against a scope and then assigns it to the
+         * internal `form` property (because of the "controller as" syntax)
+         */
+        function initForm() {
+            var template = $templateCache.get(CRM.vars.appraisals.baseURL + '/views/modals/edit-dates.html');
+            var $scope = $rootScope.$new();
+
+            template = template.replace(/datepicker-popup=(.+) ?/g, '');
+            $compile(angular.element(template))($scope);
+
+            ctrl.form = $scope.modal.form;
+        }
+
+        /**
+         * Prepares the form with the given values and then runs the digest
+         * cycles
+         *
+         * @param {object} formValues
+         */
+        function submitFormWith(formValues) {
+            _.forEach(formValues, function (value, field) {
+                ctrl.form[field].$setViewValue(value);
+            }) && $rootScope.$digest();
+
+            ctrl.cycle = _.assign(ctrl.cycle, formValues);
+
+            ctrl.submit();
+            $rootScope.$digest();
         }
     });
 });
