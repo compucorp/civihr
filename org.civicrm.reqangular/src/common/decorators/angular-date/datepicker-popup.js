dcecf69d93075e8a2ffc076b945db456a3c508e8
Krzysztof Kałamarski <kkalamarski@gmail.com>
2016-02-26 12:24:44 +0200

    PCHR-478: Adjust date picker popup position, so it won't be overflow the window

--- org.civicrm.reqangular/src/common/decorators/angular-date/datepicker-popup.js
+++ org.civicrm.reqangular/src/common/decorators/angular-date/datepicker-popup.js
@@ -1,5 +1,5 @@
 define([], function () {
-    return ['$delegate', 'HR_settings', function ($delegate, HR_settings) {
+    return ['$delegate', 'HR_settings', '$position', '$window', function ($delegate, HR_settings, $position, $window) {
         var original_link = $delegate[0].link;
 
         $delegate[0].compile = function () {
@@ -15,6 +15,23 @@ define([], function () {
                  * @type {string}
                  */
                 attrs.datepickerPopup = HR_settings.DATE_FORMAT;
+
+                scope.$watch('isOpen', function (value) {
+                    if (value) {
+                        scope.popupPosition = $position.position(element);
+                        scope.popupPosition.top += scope.popupPosition.height;
+
+                        var popupWidth = 316;
+
+                        var popupPosition = element.offset().left + element.width();
+                        popupPosition += popupWidth;
+                        
+                        if( popupPosition > $window.innerWidth ){
+                            var diff = popupPosition - $window.innerWidth;
+                            scope.popupPosition.left -= diff;
+                        }
+                    }
+                });
 
                 original_link.apply(this, arguments);
             };
