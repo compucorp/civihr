252f39e11b7b5876d7ce1de715fda51d3ffdd814
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-03-30 14:54:23 +0200

    PCHR-580: Fetch cycle appraisals on tab load

--- uk.co.compucorp.civicrm.appraisals/js/src/appraisals/controllers/appraisal-cycle-appraisals-ctrl.js
+++ uk.co.compucorp.civicrm.appraisals/js/src/appraisals/controllers/appraisal-cycle-appraisals-ctrl.js
@@ -6,18 +6,33 @@ define([
     'use strict';
 
     controllers.controller('AppraisalCycleAppraisalsCtrl', [
-        '$log', 'departments', 'levels', 'locations', 'regions',
-        function ($log, departments, levels, locations, regions) {
+        '$log', '$scope', 'departments', 'levels', 'locations', 'regions',
+        function ($log, $scope, departments, levels, locations, regions) {
             $log.debug('AppraisalCycleAppraisalsCtrl');
 
             var vm = {};
 
             vm.filtersCollapsed = true;
-
             vm.departments = departments;
             vm.levels = levels;
             vm.locations = locations;
             vm.regions = regions;
+            vm.loading = { appraisals: true };
+
+            $scope.$watch('cycle.loading.cycle', function (newValue) {
+                !newValue && init();
+            });
+
+            /**
+             * Initializing code
+             *
+             * Loads the cycle appraisals
+             */
+            function init() {
+                $scope.cycle.cycle.loadAppraisals().then(function () {
+                    vm.loading.appraisals = false;
+                });
+            }
 
             return vm;
         }
--- uk.co.compucorp.civicrm.appraisals/js/test/controllers/appraisal-cycle-appraisals-ctrl_test.js
+++ uk.co.compucorp.civicrm.appraisals/js/test/controllers/appraisal-cycle-appraisals-ctrl_test.js
@@ -1,25 +1,50 @@
 define([
     'common/angularMocks',
-    'appraisals/app'
+    'appraisals/app',
+    'common/mocks/services/hr-settings-mock',
+    'common/mocks/services/api/appraisal-mock',
+    'common/mocks/services/api/appraisal-cycle-mock',
 ], function () {
     'use strict';
 
     describe('AppraisalCycleAppraisalsCtrl', function () {
-        var $controller, $log, $modal, $provide, $scope, ctrl,
-            dialog;
+        var $controller, $log, $modal, $provide, $rootScope, $scope,
+            AppraisalCycle, appraisalAPI, appraisalCycleAPI, ctrl, dialog, cycle;
 
-        beforeEach(module('appraisals'));
-        beforeEach(inject(function (_$log_, _$modal_, _$controller_, _dialog_, $rootScope) {
-            ($modal = _$modal_) && spyOn($modal, 'open');
-            ($log = _$log_) && spyOn($log, 'debug');
+        beforeEach(function () {
+            module('appraisals', 'common.mocks', function (_$provide_) {
+                $provide = _$provide_;
+            });
 
-            $controller = _$controller_;
-            $scope = $rootScope.$new();
+            inject(['api.appraisal.mock', 'api.appraisal-cycle.mock', 'HR_settingsMock',
+                function (_appraisalAPIMock_, _appraisalCycleAPIMock_, HR_settingsMock) {
+                    appraisalAPI = _appraisalAPIMock_;
+                    appraisalCycleAPI = _appraisalCycleAPIMock_;
 
-            dialog = _dialog_;
+                    $provide.value('api.appraisal', appraisalAPI);
+                    $provide.value('api.appraisal-cycle', appraisalCycleAPI);
+                    $provide.value('HR_settings', HR_settingsMock);
+                }
+            ]);
+        });
+        beforeEach(inject([
+            '$log', '$modal', '$controller', '$rootScope', 'dialog', 'AppraisalCycle',
+            function (_$log_, _$modal_, _$controller_, _$rootScope_, _dialog_, _AppraisalCycle_) {
+                ($modal = _$modal_) && spyOn($modal, 'open');
+                ($log = _$log_) && spyOn($log, 'debug');
 
-            initController();
-        }));
+                $controller = _$controller_;
+                $rootScope = _$rootScope_;
+                $scope = $rootScope.$new();
+
+                AppraisalCycle = _AppraisalCycle_;
+                dialog = _dialog_;
+
+                cycle = appraisalCycleAPI.mockedCycles().list[0];
+
+                initController();
+            }
+        ]));
 
         describe('init', function () {
             it('is initialized', function () {
@@ -33,22 +58,66 @@ define([
                 expect(ctrl.regions).toBeDefined();
             });
 
+            it('is loading the cycle appraisals', function () {
+                expect(ctrl.loading.appraisals).toBe(true);
+            });
+
             it('has the filters form collapsed', function () {
                 expect(ctrl.filtersCollapsed).toBe(true);
+            });
+
+            describe('before the appraisals have been loaded', function () {
+                beforeEach(function () {
+                    $scope.$digest();
+                });
+
+                it('triggers the call to the method that loads them', function () {
+                    expect($scope.cycle.cycle.loadAppraisals).toHaveBeenCalled();
+                });
+            });
+
+            describe('when the appraisals have been loaded', function () {
+                beforeEach(function () {
+                    $scope.$digest();
+                });
+
+                it('marks the appraisals as loaded', function () {
+                    expect(ctrl.loading.appraisals).toBe(false);
+                });
             });
         });
 
         /**
-         * Initializes the controllers with its dependencies injected
+         * Initializes the controller with its dependencies injected
          */
         function initController() {
             ctrl = $controller('AppraisalCycleAppraisalsCtrl', {
-                $scope: $scope,
+                $scope: initParentController(),
                 departments: [],
                 levels: [],
                 locations: [],
                 regions: []
             });
+        }
+
+        /**
+         * Initializes the parent controller with its dependencies injected
+         *
+         * @return the $scope object with the parent controller property in it
+         */
+        function initParentController() {
+            $scope.cycle = $controller('AppraisalCycleCtrl', {
+                $scope: $rootScope.$new(),
+                $stateParams: { cycleId: cycle.id },
+                AppraisalCycle: AppraisalCycle,
+                statuses: [],
+                types: []
+            });
+
+            $scope.$digest();
+            spyOn($scope.cycle.cycle, 'loadAppraisals').and.callThrough();
+
+            return $scope;
         }
     });
 });
