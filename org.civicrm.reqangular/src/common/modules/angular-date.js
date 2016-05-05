e25844d0e523ba9e84dc208c362ead552ea2adb0
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-05-05 12:53:32 +0200

    PCHR-874: Customize datepicker templates

--- org.civicrm.reqangular/src/common/modules/angular-date.js
+++ org.civicrm.reqangular/src/common/modules/angular-date.js
@@ -8,11 +8,46 @@ define([
 ], function (angular, datepickerPopup, dateFilter) {
     'use strict';
 
+    var $templateCache;
+
+    /**
+     * Customizes the datepicker template with the given name
+     *
+     * Swaps the glyphicon chevron icons with the equivalent font-awesome ones
+     * Adds a horizontal line below the month name in the day template
+     *
+     * After the customization, it puts the html back in the template cache
+     *
+     * @param {string} name
+     */
+    function customizeDatepickerTpl(name) {
+        var tplPath = 'uib/template/datepicker/' + name + '.html';
+        var tpl = $templateCache.get(tplPath);
+
+        tpl = tpl.replace(/glyphicon glyphicon-chevron-(left|right)/gm, 'fa fa-chevron-$1');
+
+        if (name === 'day') {
+            tpl = (function ($tpl) {
+                jQuery($tpl).find('th[ng-if="showWeeks"]').parent().css({ 'border-top': '1px solid #DDD' });
+                return jQuery($tpl).wrap('<div/>').parent().html();
+            })(jQuery.parseHTML(tpl));
+        }
+
+        $templateCache.put(tplPath, tpl);
+    }
+
+    /**
+     * Provides decorator for the datepicker popup (sets default config) and date filter (adds suppported formats)
+     * It also customizes the datepicker templates
+     */
     return angular.module("common.angularDate", ['common.services', 'ui.bootstrap', 'common.templates']).config([
-        '$provide',
-        function ($provide){
+        '$provide', function ($provide) {
             $provide.decorator('uibDatepickerPopupDirective', datepickerPopup);
             $provide.decorator('dateFilter', dateFilter);
         }
-    ]);
+    ]).run(['$templateCache', function (_$templateCache_) {
+        $templateCache = _$templateCache_;
+
+        ['day', 'month', 'year'].forEach(customizeDatepickerTpl);
+    }]);
 });
