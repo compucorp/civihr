80f95d04906ae1eba2620df3a3eec95c85e00d04
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-05-05 11:08:36 +0200

    PCHR-874: Add dateFilter support for E date format

--- org.civicrm.reqangular/src/common/modules/angular-date.js
+++ org.civicrm.reqangular/src/common/modules/angular-date.js
@@ -1,16 +1,18 @@
 define([
     'common/angular',
     'common/decorators/angular-date/datepicker-popup',
+    'common/decorators/angular-date/date-filter',
     'common/modules/services',
     'common/angularBootstrap',
     'common/modules/templates'
-], function (angular, datepickerPopup) {
+], function (angular, datepickerPopup, dateFilter) {
     'use strict';
 
     return angular.module("common.angularDate", ['common.services', 'ui.bootstrap', 'common.templates']).config([
         '$provide',
         function ($provide){
             $provide.decorator('uibDatepickerPopupDirective', datepickerPopup);
+            $provide.decorator('dateFilter', dateFilter);
         }
     ]);
 });
