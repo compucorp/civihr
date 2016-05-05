8b5b270f0f7286acb5b11399273f5b06117d2937
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-05-05 11:10:43 +0200

    PCHR-874: Add default settings to uib-datepicker-popup

--- org.civicrm.reqangular/src/common/decorators/angular-date/datepicker-popup.js
+++ org.civicrm.reqangular/src/common/decorators/angular-date/datepicker-popup.js
@@ -1,5 +1,7 @@
-define([], function () {
-    return ['$delegate', 'HR_settings', '$uibPosition', '$window', function ($delegate, HR_settings, $position, $window) {
+define([
+    'common/lodash'
+], function (_) {
+    return ['$delegate', 'HR_settings', function ($delegate, HR_settings) {
         'use strict';
 
         var original_link = $delegate[0].link;
@@ -11,12 +13,14 @@ define([], function () {
              * @override
              */
             return function (scope, element, attrs, ngModel) {
-
-                /**
-                 * @override
-                 * @type {string}
-                 */
                 attrs.uibDatepickerPopup = attrs.uibDatepickerPopup || HR_settings.DATE_FORMAT || 'yyyy-MM-dd';
+                attrs.showButtonBar = attrs.showButtonBar || false;
+
+                scope.datepickerOptions = _.assign({
+                    formatDayHeader: 'E',
+                    showWeeks: false,
+                    startingDay: 1
+                }, scope.datepickerOptions);
 
                 original_link.apply(this, arguments);
             };
