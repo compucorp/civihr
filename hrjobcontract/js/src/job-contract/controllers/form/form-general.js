34f0cf45d90f982039882a27a183c4a4f80291ce
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-05-27 11:18:15 +0200

    PCHR-874: Use correct format for max and min date

--- hrjobcontract/js/src/job-contract/controllers/form/form-general.js
+++ hrjobcontract/js/src/job-contract/controllers/form/form-general.js
@@ -47,13 +47,13 @@ define([
 
             $scope.$watch('entity.details.period_start_date', function(){
                 moment(entityDetails.period_start_date);
-                $scope.dpDateEndMin = moment(entityDetails.period_start_date).add(1, 'day').format();
+                $scope.dpDateEndMin = moment(entityDetails.period_start_date).add(1, 'day').toDate();
                 $scope.duration = duration(entityDetails.period_start_date, entityDetails.period_end_date);
             });
 
             $scope.$watch('entity.details.period_end_date', function(){
                 if (entityDetails.period_end_date) {
-                    $scope.dpDateStartMax = moment(entityDetails.period_end_date).subtract(1, 'day').format();  
+                    $scope.dpDateStartMax = moment(entityDetails.period_end_date).subtract(1, 'day').toDate();  
                 } else {
                     $scope.dpDateStartMax = null;
                     entityDetails.end_reason = null;
