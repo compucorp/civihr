c531eb9af766603602aa1d440e76c182b5f686b5
Bruno Queiros <brunoofq@gmail.com>
2016-05-27 14:31:46 -0300

    PCHR-1034: Adjust HRJobRolesServiceFilters test code style

--- com.civicrm.hrjobroles/js/test/services/hr-job-roles-service-filter_test.js
+++ com.civicrm.hrjobroles/js/test/services/hr-job-roles-service-filter_test.js
@@ -8,19 +8,96 @@ define([
 
   describe('HRJobRolesServiceFilters', function () {
     var HRJobRolesServiceFilters;
+    var not_undefined_array;
+    var cost_centers;
+    var funders;
 
     beforeEach(module('hrjobroles'));
     beforeEach(inject(function (_HRJobRolesServiceFilters_) {
       HRJobRolesServiceFilters = _HRJobRolesServiceFilters_;
+
+      not_undefined_array = [1, 2, 'undefined', 'test', undefined];
+      cost_centers = [
+        {
+          amount:"0",
+          cost_centre_id:"879",
+          id:1,
+          percentage:"1",
+          type:"1",
+        },
+        {
+          amount:"0",
+          cost_centre_id:"890",
+          id:1,
+          percentage:"0",
+          type:"1",
+        },
+        {
+          amount:"2",
+          cost_centre_id:"",
+          id:1,
+          percentage:"0",
+          type:"0",
+        },
+        {
+          amount:"2",
+          cost_centre_id:"123",
+          id:1,
+          percentage:"0",
+          type:"0",
+        }
+      ];
+      funders = [
+        {
+          amount: "0",
+          funder_id: "",
+          id: 2,
+          percentage: "2",
+          type: "1"
+        },
+        {
+          amount: "0",
+          funder_id: {
+            id:"1",
+            sort_name:"Default Organization"
+          },
+          id: 1,
+          percentage: "1",
+          type: "1"
+        },
+        {
+          amount: "0",
+          funder_id: {
+            id:"1",
+            sort_name:"Default Organization"
+          },
+          id: 1,
+          percentage: "0",
+          type: "1"
+        },
+        {
+          amount: "1",
+          funder_id: {
+            id:"1",
+            sort_name:"Default Organization"
+          },
+          id: 1,
+          percentage: "0",
+          type: "0"
+        }
+      ];
     }));
 
     describe('isNotUndefined', function () {
-      it('should remove undefined values', function () {
-        var array = [1, 2, 'undefined', 'test', undefined];
-        var expectedArray = [1, 2, 'test'];
+      var expectedArray;
 
-        expect(HRJobRolesServiceFilters.isNotUndefined(array)).toEqual(expectedArray);
-        expect(HRJobRolesServiceFilters.isNotUndefined(array).length).toBe(3);
+      beforeEach(function () {
+        expectedArray = [1, 2, 'test'];
+      });
+
+      it('should remove undefined values', function () {
+        expect(HRJobRolesServiceFilters.isNotUndefined(not_undefined_array)).toEqual(expectedArray);
+        expect(HRJobRolesServiceFilters.isNotUndefined(not_undefined_array).length).toBe(3);
       });
 
       it('should return the passed value if isn\'t an array', function () {
@@ -32,9 +109,10 @@ define([
     });
 
     describe('issetFunder', function () {
-      it('should remove the entries which are without cost_centre_id', function () {
-        var cost_centers = angular.copy(Mock.roles_data[3]['cost_centers']);
-        var expectedArray = [
+      var expectedArray;
+
+      beforeEach(function () {
+        expectedArray = [
           {
             amount:"0",
             cost_centre_id:"879",
@@ -50,7 +128,9 @@ define([
             type:"0",
           }
         ];
+      });
 
+      it('should remove the entries which are without cost_centre_id', function () {
         expect(HRJobRolesServiceFilters.issetCostCentre(cost_centers)).toEqual(expectedArray);
         expect(HRJobRolesServiceFilters.issetCostCentre(cost_centers).length).toBe(2);
       });
@@ -64,9 +144,10 @@ define([
     });
 
     describe('issetFunder', function () {
-      it('should remove the entries which are without funder_id', function () {
-        var funders = angular.copy(Mock.roles_data[3]['funders']);
-        var expectedArray = [
+      var expectedArray;
+
+      beforeEach(function() {
+        expectedArray = [
           {
             amount: "0",
             funder_id: {
@@ -88,7 +169,9 @@ define([
             type: "0"
           }
         ];
+      });
 
+      it('should remove the entries which are without funder_id', function () {
         expect(HRJobRolesServiceFilters.issetFunder(funders)).toEqual(expectedArray);
         expect(HRJobRolesServiceFilters.issetFunder(funders).length).toBe(2);
       });
