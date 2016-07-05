48519cae057b5d9d31736f2ca6baaeaea39de8c2
Bruno Queiros <brunoofq@gmail.com>
2016-07-05 08:34:11 -0300

    PCHR-1223: Set the missing icons

--- hrui/js/contact.js
+++ hrui/js/contact.js
@@ -11,14 +11,14 @@
       'Change Log': 'fa fa-archive',
       'Groups': 'fa fa-group',
       'Cases': 'fa fa-bomb',
-      'Managers': 'fa fa-bomb',
-      'Absences': 'fa fa-bomb',
-      'Identification': 'fa fa-bomb',
-      'Immigration': 'fa fa-bomb',
-      'Bank Details': 'fa fa-bomb',
-      'Career History': 'fa fa-bomb',
-      'Medical & Disability': 'fa fa-bomb',
-      'Qualifications': 'fa fa-bomb',
+      'Managers': 'fa fa-sitemap',
+      'Absences': 'fa fa-calendar-times-o',
+      'Bank Details': 'fa fa-university',
+      'Identification': 'fa fa-credit-card',
+      'Immigration': 'fa fa-plane',
+      'Career History': 'fa fa-history',
+      'Medical & Disability': 'fa fa-wheelchair',
+      'Qualifications': 'fa fa-certificate',
     };
 
     for (var iconTabName in iconsMap) {
