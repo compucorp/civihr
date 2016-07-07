3242374c0951af52d34f54918e8e56ef436c345a
Bruno Queiros <brunoofq@gmail.com>
2016-07-07 08:44:49 -0300

    PCHR-1224: Styling input fields

--- hrui/js/contact.js
+++ hrui/js/contact.js
@@ -1,11 +1,10 @@
 (function ($, _) {
   $(document).on('crmLoad', function(e) {
-    $('.crm-address-block').one('DOMSubtreeModified', function () {
+    $('.crm-inline-edit').one('DOMSubtreeModified', function() {
       if ($(this).find('form').length === 1) {
-        $(this).find('form .form-layout-compressed label').each(function() {
+        $(this).find('form label').each(function() {
           var id = $(this).attr('for');
           $('#' + id).attr('placeholder', $(this).text());
-          $(this).remove();
         });
       }
     });
