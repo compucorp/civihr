b898246041bb5b23ba7838d066508aec8d3377f4
Danilo Cestari <danilo@compucorp.co.uk>
2016-08-11 11:51:56 +0200

    PCHR-1084: Style improvements

--- org.civicrm.bootstrapcivicrm/bootstrapcivicrm.php
+++ org.civicrm.bootstrapcivicrm/bootstrapcivicrm.php
@@ -129,4 +129,10 @@ function bootstrapcivicrm_civicrm_pageRun($page) {
   CRM_Core_Resources::singleton()->addStyleFile('org.civicrm.bootstrapcivicrm', 'css/bootstrap.css');
   CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.bootstrapcivicrm', 'js/radio-checkbox.js');
 }
-CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.bootstrapcivicrm', 'js/checkbox.js');
+
+/**
+ * Implementation of hook_civicrm_buildForm
+ */
+function bootstrapcivicrm_civicrm_buildForm($formName) {
+  CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.bootstrapcivicrm', 'js/radio-checkbox.js');
+}
