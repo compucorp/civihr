13ea06b49443c6b30168a0c9238cabee825e9195
coldrunKacper <kacper@coldrun.pl>
2016-09-14 10:57:40 +0200

    PCHR-1287 - PR changes (code formatting, renaming service method), including getLengthOfServiceYmd() BAO method into unit tests.

--- contactsummary/js/src/contact-summary/controllers/keyDetails.js
+++ contactsummary/js/src/contact-summary/controllers/keyDetails.js
@@ -1,51 +1,50 @@
 define([
-    'common/moment',
-    'contact-summary/modules/controllers',
-    'contact-summary/services/contactDetails',
-    'contact-summary/services/contract'
+  'common/moment',
+  'contact-summary/modules/controllers',
+  'contact-summary/services/contactDetails',
+  'contact-summary/services/contract'
 ], function (moment, controllers) {
-'use strict';
+  'use strict';
 
-    /**
-     * @ngdoc controller
-     * @name KeyDetailsCtrl
-     * @param $log
-     * @param {ContactDetailsService} ContactDetails
-     * @param {ContractService} Contract
-     * @constructor
-     */
-    function KeyDetailsCtrl($log, ContactDetails, Contract) {
-        $log.debug('Controller: KeyDetailsCtrl');
+  /**
+   * @ngdoc controller
+   * @name KeyDetailsCtrl
+   * @param $log
+   * @param {ContactDetailsService} ContactDetails
+   * @param {ContractService} Contract
+   * @constructor
+   */
+  function KeyDetailsCtrl($log, ContactDetails, Contract) {
+    $log.debug('Controller: KeyDetailsCtrl');
 
-        this.ready = false;
+    this.ready = false;
 
-        ContactDetails.get()
-            .then(function (response) {
-                this.contactDetails = response;
+    ContactDetails.get()
+      .then(function (response) {
+        this.contactDetails = response;
+        return Contract.getPrimary();
+      }.bind(this))
+      .then(function (response) {
+        if (_.isEmpty(response)) {
+          return;
+        }
+        this.primaryContract = response;
+      }.bind(this))
+      .then(function (response) {
+        return Contract.getLengthOfService();
+      })
+      .then(function (response) {
+        this.lengthOfService = response;
+      }
+      .bind(this))
+      .finally(function () {
+        this.ready = true;
+      }.bind(this));
+  }
 
-                return Contract.getPrimary();
-            }.bind(this))
-            .then(function (response) {
-                if (_.isEmpty(response)) {
-                    return;
-                }
+  /////////////////////
+  // Private Members //
+  /////////////////////
 
-                this.primaryContract = response;
-            }.bind(this)).then(function (response) {
-                return Contract.getLengthOfServiceYmd();
-            })
-            .then(function (response) {
-                this.lengthOfService = response;
-            }
-            .bind(this))
-            .finally(function () {
-                this.ready = true;
-            }.bind(this));
-    }
-
-    /////////////////////
-    // Private Members //
-    /////////////////////
-
-    controllers.controller('KeyDetailsCtrl', ['$log', 'ContactDetailsService', 'ContractService', KeyDetailsCtrl]);
+  controllers.controller('KeyDetailsCtrl', ['$log', 'ContactDetailsService', 'ContractService', KeyDetailsCtrl]);
 });
