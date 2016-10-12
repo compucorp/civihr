328a66dfce186b75bb1cec36bea9f234c7882590
coldrunKacper <kacper@coldrun.pl>
2016-10-12 12:39:02 +0200

    PCHR-1323 - Implementing caching for CRM_CiviHR::getValue() method.

--- uk.co.compucorp.civicrm.civihr/CRM/CiviHR.php
+++ uk.co.compucorp.civicrm.civihr/CRM/CiviHR.php
@@ -2,12 +2,20 @@
 
 class CRM_CiviHR {
   /**
-   * Get version value from info.xml file.
+   * Get version value from info.xml. This method uses caching so it reads
+   * xml file only once during CiviCRM session.
    *
    * @return string
    */
   public static function getVersion() {
-    $info = CRM_Extension_Info::loadFromFile(__DIR__ . '/../info.xml');
-    return $info->version;
+
+    $version = CRM_Core_BAO_Cache::getItem('CiviHR', 'version');
+    if (empty($version)) {
+      $info = CRM_Extension_Info::loadFromFile(__DIR__ . '/../info.xml');
+      $version = $info->version;
+      CRM_Core_BAO_Cache::setItem($version, 'CiviHR', 'version');
+    }
+
+    return $version;
   }
 }
