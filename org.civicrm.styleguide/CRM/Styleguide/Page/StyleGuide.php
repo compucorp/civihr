8f40fb85fbb61fee9a236ba257c72710501adaae
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-10-19 14:43:24 +0200

    PCHR-1353: First draft of monolinea styleguide implementation

--- org.civicrm.styleguide/CRM/Styleguide/Page/StyleGuide.php
+++ org.civicrm.styleguide/CRM/Styleguide/Page/StyleGuide.php
@@ -11,6 +11,11 @@ class CRM_Styleguide_Page_StyleGuide extends CRM_Core_Page {
   }
 
   private static function registerScripts() {
-    CRM_Core_Resources::singleton()->addStyleFile('org.civicrm.styleguide', 'css/styleguide.css');
+    CRM_Core_Resources::singleton()->addStyleFile('org.civicrm.styleguide', 'css/styleguide-default.css');
+
+    CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.styleguide', 'js/scrollspy.js', 1000);
+    CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.styleguide', 'js/dropdown.js', 1000);
+    CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.styleguide', 'js/sg-plugins.js', 1000);
+    CRM_Core_Resources::singleton()->addScriptFile('org.civicrm.styleguide', 'js/sg-scripts.js', 1000);
   }
 }
--- org.civicrm.styleguide/styleguide.php
+++ org.civicrm.styleguide/styleguide.php
@@ -123,3 +123,128 @@ _styleguide_civix_civicrm_angularModules($angularModules);
 function styleguide_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
   _styleguide_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
+
+function listElementsAsOptions ($type) {
+  $files = array();
+  $handle=opendir(CRM_Core_Resources::singleton()->getPath('org.civicrm.styleguide') . '/' . $type . '/');
+  var_dump($handle);
+  while (false !== ($file = readdir($handle))):
+      if(stristr($file,'.html')):
+          $files[] = $file;
+      endif;
+  endwhile;
+echo '<li role="presentation" class="divider"></li><li role="presentation" class="dropdown-header">'.$type.'</li>';
+  sort($files);
+  foreach ($files as $file):
+      $filename = preg_replace("/\.html$/i", "", $file);
+      $title = preg_replace("/\-/i", " ", $filename);
+      $title = ucwords($title);
+      echo '<li><a href="#sg-'.$filename.'">'.$title.'</a></li>';
+  endforeach;
+}
+
+// Display title of each markup samples as a select option
+function listMarkupAsOptions ($type) {
+  $files = array();
+  $handle=opendir(CRM_Core_Resources::singleton()->getPath('org.civicrm.styleguide') . '/markup/' . $type);
+  while (false !== ($file = readdir($handle))):
+      if(stristr($file,'.html')):
+          $files[] = $file;
+      endif;
+  endwhile;
+
+  sort($files);
+  foreach ($files as $file):
+      $filename = preg_replace("/\.html$/i", "", $file);
+      $title = preg_replace("/\-/i", " ", $filename);
+      $title = ucwords($title);
+      echo '<li><a href="#sg-'.$filename.'">'.$title.'</a></li>';
+  endforeach;
+}
+  // Display foundation elements
+  function showDocs() {
+    $files = array();
+    $handle=opendir(CRM_Core_Resources::singleton()->getPath('org.civicrm.styleguide') . '/docs/');
+    while (false !== ($file = readdir($handle))):
+        if(stristr($file,'.html')):
+            $files[] = $file;
+        endif;
+    endwhile;
+
+    sort($files);
+    foreach ($files as $file):
+        $filename = preg_replace("/\.html$/i", "", $file);
+        $title = preg_replace("/\-/i", " ", $filename);
+        echo '<div class="col-lg-12 sg-section" id="sg-'.$filename.'">';
+        echo '<div class="sg-display">';
+        echo '<h2 class="sg-h2">'.$title.'</h2>';
+        include('docs/'.$file);
+        echo '</div><!--/.sg-display-->';
+        echo '</div><!--/.sg-section-->';
+    endforeach;
+  }
+
+// Display foundation elements
+function showFoundation() {
+  $files = array();
+  $handle=opendir(CRM_Core_Resources::singleton()->getPath('org.civicrm.styleguide') . '/foundation/');
+  while (false !== ($file = readdir($handle))):
+      if(stristr($file,'.html')):
+          $files[] = $file;
+      endif;
+  endwhile;
+
+  sort($files);
+  foreach ($files as $file):
+      $filename = preg_replace("/\.html$/i", "", $file);
+      $title = preg_replace("/\-/i", " ", $filename);
+      echo '<div class="col-lg-12 sg-section" id="sg-'.$filename.'">';
+      echo '<div class="sg-display">';
+      echo '<h2 class="sg-h2">'.$title.'</h2>';
+      include('foundation/'.$file);
+      echo '</div><!--/.sg-display-->';
+      echo '</div><!--/.sg-section-->';
+  endforeach;
+}
+
+// Display markup view & source
+function showMarkup($type) {
+  $files = array();
+  $handle=opendir(CRM_Core_Resources::singleton()->getPath('org.civicrm.styleguide') . '/markup/'.$type);
+  while (false !== ($file = readdir($handle))):
+      if(stristr($file,'.html')):
+          $files[] = $file;
+      endif;
+  endwhile;
+
+  sort($files);
+  foreach ($files as $file):
+      $filename = preg_replace("/\.html$/i", "", $file);
+      $title = preg_replace("/\-/i", " ", $filename);
+      $documentation = CRM_Core_Resources::singleton()->getPath('org.civicrm.styleguide') . '/usage/'.$type.'/'.$file;
+      echo '<div class="sg-markup sg-section">';
+      echo '<div class="sg-display">';
+      echo '<h2 class="sg-h2"><a id="sg-'.$filename.'" class="sg-anchor">'.$title.'</a></h2>';
+      //echo '<div class="col-md-10 col-md-offset-1">';
+      echo '<div class="row"><div class="col-md-8">';
+      echo '<h3 class="sg-h3">Example</h3>';
+      include('markup/'.$type.'/'.$file);
+      echo '</div>';
+       if (file_exists($documentation)) {
+        echo '<div class="col-md-4"><div class="well sg-doc">';
+        echo '<h3 class="sg-h3">Usage Notes</h3>';
+        include($documentation);
+        echo '</div></div></div>';
+      }
+      echo '</div><!--/.sg-display-->';
+      echo '<div class="sg-markup-controls"><a class="btn btn-primary sg-btn sg-btn--source" href="#">View Source</a> <a class="sg-btn--top" href="#top">Back to Top</a> </div>';
+      echo '<div class="sg-source sg-animated">';
+      echo '<a class="btn btn-default sg-btn sg-btn--select" href="#">Copy Source</a>';
+      echo '<pre class="prettyprint linenums"><code>';
+      echo htmlspecialchars(file_get_contents(CRM_Core_Resources::singleton()->getPath('org.civicrm.styleguide') . '/markup/'.$type.'/'.$file));
+      echo '</code></pre>';
+      echo '</div><!--/.sg-source-->';
+      //echo '</div><!--/.colmd10-->';
+      echo '</div><!--/.sg-section-->';
+  endforeach;
+}
