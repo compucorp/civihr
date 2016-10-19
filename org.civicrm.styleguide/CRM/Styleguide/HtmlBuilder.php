3b245ae656955aa3e037a5df84031e6479b3958f
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-10-19 15:57:15 +0200

    PCHR-1353: Improve folder structure

--- org.civicrm.styleguide/CRM/Styleguide/HtmlBuilder.php
+++ org.civicrm.styleguide/CRM/Styleguide/HtmlBuilder.php
@@ -2,7 +2,7 @@
   class CRM_Styleguide_HtmlBuilder {
 
     public function __construct() {
-      $this->root = CRM_Core_Resources::singleton()->getPath('org.civicrm.styleguide') . '/';
+      $this->root = CRM_Core_Resources::singleton()->getPath('org.civicrm.styleguide') . '/partials/';
     }
 
     public function listElementsAsOptions ($type) {
@@ -69,7 +69,7 @@
         echo '<div class="col-lg-12 sg-section" id="sg-'.$filename.'">';
         echo '<div class="sg-display">';
         echo '<h2 class="sg-h2">'.$title.'</h2>';
-        include('docs/'.$file);
+        include($this->root . 'docs/' . $file);
         echo '</div><!--/.sg-display-->';
         echo '</div><!--/.sg-section-->';
       }
@@ -95,7 +95,7 @@
         echo '<div class="col-lg-12 sg-section" id="sg-'.$filename.'">';
         echo '<div class="sg-display">';
         echo '<h2 class="sg-h2">'.$title.'</h2>';
-        include('foundation/'.$file);
+        include($this->root . 'foundation/' . $file);
         echo '</div><!--/.sg-display-->';
         echo '</div><!--/.sg-section-->';
       }
@@ -117,15 +117,14 @@
       foreach ($files as $file) {
         $filename = preg_replace("/\.html$/i", "", $file);
         $title = preg_replace("/\-/i", " ", $filename);
-        $documentation = $this->root . 'usage/'.$type.'/'.$file;
+        $documentation = $this->root . 'usage/' . $type . '/' . $file;
 
         echo '<div class="sg-markup sg-section">';
         echo '<div class="sg-display">';
         echo '<h2 class="sg-h2"><a id="sg-'.$filename.'" class="sg-anchor">'.$title.'</a></h2>';
-        //echo '<div class="col-md-10 col-md-offset-1">';
         echo '<div class="row"><div class="col-md-8">';
         echo '<h3 class="sg-h3">Example</h3>';
-        include('markup/'.$type.'/'.$file);
+        include($this->root . 'markup/'.$type.'/'.$file);
         echo '</div>';
 
         if (file_exists($documentation)) {
@@ -140,10 +139,9 @@
         echo '<div class="sg-source sg-animated">';
         echo '<a class="btn btn-default sg-btn sg-btn--select" href="#">Copy Source</a>';
         echo '<pre class="prettyprint linenums"><code>';
-        echo htmlspecialchars(file_get_contents($this->root . 'markup/'.$type.'/'.$file));
+        echo htmlspecialchars(file_get_contents($this->root . 'markup/' . $type . '/' . $file));
         echo '</code></pre>';
         echo '</div><!--/.sg-source-->';
-        //echo '</div><!--/.colmd10-->';
         echo '</div><!--/.sg-section-->';
       }
     }
