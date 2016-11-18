d5bcc74f2a632b9370118a07d51dfc9abda25aa4
Davi Alexandre <davi@davialexandre.com.br>
2016-11-18 11:49:22 -0200

    PCHR-1197: Add a method to create public holiday leave requests for one contract
    Leave Request will be created for the public holidays whic are both in the future
    (i.e. date >= today) and are between the contract start and end dates

--- uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/CRM/HRLeaveAndAbsences/Service/PublicHolidayLeaveRequestCreationTest.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/CRM/HRLeaveAndAbsences/Service/PublicHolidayLeaveRequestCreationTest.php
@@ -130,4 +130,107 @@ class CRM_HRLeaveAndAbsences_Service_PublicHolidayLeaveRequestCreationTest exten
     // The Balance is still 0 because no Leave Request was created
     $this->assertEquals(0, LeaveBalanceChange::getLeaveRequestBalanceForEntitlement($periodEntitlement));
   }
+
+  public function testItCreatesLeaveRequestsForAllPublicHolidaysInTheFutureOverlappingTheContractDates() {
+    $contact = ContactFabricator::fabricate(['first_name' => 'Contact 1']);
+
+    $contract = HRJobContractFabricator::fabricate([
+      'contact_id' => $contact['id']
+    ], [
+      'period_start_date' =>  CRM_Utils_Date::processDate('yesterday'),
+      'period_end_date' =>   CRM_Utils_Date::processDate('+100 days'),
+    ]);
+
+    $periodEntitlement = $this->createLeavePeriodEntitlementMockForBalanceTests(
+      new DateTime('-10 days'),
+      new DateTime('+200 days')
+    );
+    $periodEntitlement->contact_id = $contact['id'];
+    $periodEntitlement->type_id = $this->absenceType->id;
+
+    $this->assertEquals(0, LeaveBalanceChange::getLeaveRequestBalanceForEntitlement($periodEntitlement));
+
+    PublicHolidayFabricator::fabricateWithoutValidation([
+      'date' =>  CRM_Utils_Date::processDate('yesterday')
+    ]);
+
+    PublicHolidayFabricator::fabricateWithoutValidation([
+      'date' =>  CRM_Utils_Date::processDate('+5 days')
+    ]);
+
+    PublicHolidayFabricator::fabricateWithoutValidation([
+      'date' =>  CRM_Utils_Date::processDate('+47 days')
+    ]);
+
+    PublicHolidayFabricator::fabricateWithoutValidation([
+      'date' =>  CRM_Utils_Date::processDate('+101 days')
+    ]);
+
+    $creationLogic = new PublicHolidayLeaveRequestCreation();
+    $creationLogic->createAllForContract($contract['id']);
+
+    // The balance should be -2 because only two leave requests were created:
+    // The one for +5 days and the other one for + 47 days.
+    // The holiday for "yesterday" overlaps the contract, but it is in the past,
+    // so nothing will be created. The holiday for "+101 days" is in the future,
+    // but it doesn't overlap the contract dates and no leave request will be
+    // created for it as well
+    $this->assertEquals(-2, LeaveBalanceChange::getLeaveRequestBalanceForEntitlement($periodEntitlement));
+  }
+
+  public function testItCreatesLeaveRequestsForAllPublicHolidaysInTheFutureOverlappingAContractWithNoEndDate() {
+    $contact = ContactFabricator::fabricate(['first_name' => 'Contact 1']);
+
+    $contract = HRJobContractFabricator::fabricate([
+      'contact_id' => $contact['id']
+    ], [
+      'period_start_date' =>  CRM_Utils_Date::processDate('yesterday'),
+    ]);
+
+    $periodEntitlement = $this->createLeavePeriodEntitlementMockForBalanceTests(
+      new DateTime('-10 days'),
+      new DateTime('+400 days')
+    );
+    $periodEntitlement->contact_id = $contact['id'];
+    $periodEntitlement->type_id = $this->absenceType->id;
+
+    $this->assertEquals(0, LeaveBalanceChange::getLeaveRequestBalanceForEntitlement($periodEntitlement));
+
+    PublicHolidayFabricator::fabricateWithoutValidation([
+      'date' =>  CRM_Utils_Date::processDate('+5 days')
+    ]);
+
+    PublicHolidayFabricator::fabricateWithoutValidation([
+      'date' =>  CRM_Utils_Date::processDate('+47 days')
+    ]);
+
+    PublicHolidayFabricator::fabricateWithoutValidation([
+      'date' =>  CRM_Utils_Date::processDate('+332 days')
+    ]);
+
+    $creationLogic = new PublicHolidayLeaveRequestCreation();
+    $creationLogic->createAllForContract($contract['id']);
+
+    // Since there's no end date for the contract,
+    // leave request will be created for all the public holidays in the
+    // future
+    $this->assertEquals(-3, LeaveBalanceChange::getLeaveRequestBalanceForEntitlement($periodEntitlement));
+  }
+
+  public function testItDoesntCreateAnythingIfTheContractIDDoesntExist() {
+    $creationLogic = new PublicHolidayLeaveRequestCreation();
+    $creationLogic->createAllForContract(9998398298);
+
+    $this->assertEquals(0, $this->countNumberOfPublicHolidayBalanceChanges());
+  }
+
+  private function countNumberOfPublicHolidayBalanceChanges() {
+    $balanceChangeTypes = array_flip(LeaveBalanceChange::buildOptions('type_id'));
+
+    $bao = new LeaveBalanceChange();
+    $bao->type_id = $balanceChangeTypes['Public Holiday'];
+    $bao->source_type = LeaveBalanceChange::SOURCE_LEAVE_REQUEST_DAY;
+
+    return $bao->count();
+  }
 }
