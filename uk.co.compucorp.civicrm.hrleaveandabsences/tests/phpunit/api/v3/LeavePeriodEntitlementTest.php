ade83f7254d2df8de0d986fc215802c686a9e76b
Tunbola Ogunwande <tunbolawande@yahoo.com>
2016-11-24 15:39:32 +0100

    PCHR-1132: changed to short array syntax

--- uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/api/v3/LeavePeriodEntitlementTest.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/api/v3/LeavePeriodEntitlementTest.php
@@ -10,23 +10,17 @@ class api_v3_LeavePeriodEntitlementTest extends BaseHeadlessTest {
   /**
    * @expectedException CiviCRM_API3_Exception
    * @expectedExceptionMessage You must include either the id of a specific entitlement, or both the contact and period id
-   *
    */
   public function testOnGetRemainderDoesNotAcceptContactIdWithoutPeriodId() {
-    civicrm_api3('LeavePeriodEntitlement', 'getremainder', array(
-      'contact_id' => 1
-    ));
+    civicrm_api3('LeavePeriodEntitlement', 'getremainder', ['contact_id' => 1]);
   }
 
   /**
    * @expectedException CiviCRM_API3_Exception
    * @expectedExceptionMessage You must include either the id of a specific entitlement, or both the contact and period id
-   *
    */
   public function testOnGetRemainderDoesNotAcceptPeriodIdWithoutContactId() {
-    civicrm_api3('LeavePeriodEntitlement', 'getremainder', array(
-      'period_id' => 1
-    ));
+    civicrm_api3('LeavePeriodEntitlement', 'getremainder', ['period_id' => 1]);
   }
 
 }
