029d2ad4968c7c075119e8e89c7bf7b9852162ae
Alessandro Verdura <alessandro@compucorp.co.uk>
2016-11-28 16:15:57 +0100

    PCHR-1283: Pass user CRM ID to <my-leave> component

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/my-leave/components/my-leave.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/my-leave/components/my-leave.js
@@ -2,11 +2,15 @@ define([
   'leave-absences/my-leave/modules/components'
 ], function (components) {
   components.component('myLeave', {
+    bindings: {
+      contactId: '<'
+    },
     templateUrl: ['settings', function (settings) {
       return settings.pathTpl + 'components/my-leave.html';
     }],
     controller: ['$log', function ($log) {
+      console.log('CRM id of the currently logged in user: ', this.contactId);
       $log.debug('Component: my-leave');
     }]
   });
-})
+});
