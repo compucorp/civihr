6d03b8f808d2206470aafde4798ef85555c8bc43
Tunbola Ogunwande <tunbolawande@yahoo.com>
2016-12-23 13:32:23 +0100

    PCHR-1832: Modify SicknessRequest Get API to return associated LeaveRequest Data. Add tests.

--- uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/api/v3/SicknessRequestTest.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/api/v3/SicknessRequestTest.php
@@ -71,4 +71,73 @@ class SicknessRequestTest extends BaseHeadlessTest {
     ];
     $this->assertArraySubset($expectedResult, $result);
   }
+
+  public function testSicknessRequestGetShouldReturnAssociatedLeaveRequestData() {
+    $fromDate = new DateTime("2016-11-14");
+    $toDate = new DateTime("2016-11-17");
+
+    $fromDate2 = new DateTime("2016-11-20");
+    $toDate2 = new DateTime("2016-11-30");
+
+    $fromType = $this->leaveRequestDayTypes['All Day']['id'];
+    $toType = $this->leaveRequestDayTypes['All Day']['id'];
+
+    $sicknessReasons = array_flip(SicknessRequest::buildOptions('reason'));
+
+    $sicknessRequest1 = SicknessRequest::create([
+      'type_id' => 1,
+      'contact_id' => 1,
+      'status_id' => 1,
+      'from_date' => $fromDate->format('YmdHis'),
+      'from_date_type' => $fromType,
+      'to_date' => $toDate->format('YmdHis'),
+      'to_date_type' => $toType,
+      'reason' => $sicknessReasons['Appointment'],
+      'required_documents' => $this->requiredDocumentOptions['Self certification form required']['value'],
+    ], false);
+
+    $sicknessRequest2 = SicknessRequest::create([
+      'type_id' => 1,
+      'contact_id' => 1,
+      'status_id' => 1,
+      'from_date' => $fromDate2->format('YmdHis'),
+      'from_date_type' => $fromType,
+      'to_date' => $toDate2->format('YmdHis'),
+      'to_date_type' => $toType,
+      'reason' => $sicknessReasons['Accident'],
+      'required_documents' => $this->requiredDocumentOptions['Self certification form required']['value']. ',' . $this->requiredDocumentOptions['Back to work interview required']['value'],
+    ], false);
+
+    $expectedResult = [
+      [
+        'id' => $sicknessRequest1->id,
+        'type_id' => 1,
+        'contact_id' => 1,
+        'status_id' => 1,
+        'from_date' => $fromDate->format('Y-m-d'),
+        'from_date_type' => $fromType,
+        'to_date' => $toDate->format('Y-m-d'),
+        'to_date_type' => $toType,
+        'leave_request_id' => $sicknessRequest1->leave_request_id,
+        'reason' => $sicknessRequest1->reason,
+        'required_documents' => $sicknessRequest1->required_documents
+      ],
+      [
+        'id' => $sicknessRequest2->id,
+        'type_id' => 1,
+        'contact_id' => 1,
+        'status_id' => 1,
+        'from_date' => $fromDate2->format('Y-m-d'),
+        'from_date_type' => $fromType,
+        'to_date' => $toDate2->format('Y-m-d'),
+        'to_date_type' => $toType,
+        'leave_request_id' => $sicknessRequest2->leave_request_id,
+        'reason' => $sicknessRequest2->reason,
+        'required_documents' => $sicknessRequest2->required_documents
+      ]
+    ];
+
+    $result = civicrm_api3('SicknessRequest', 'get', ['contact_id'=> 1, 'sequential' => 1]);
+    $this->assertEquals($expectedResult, $result['values']);
+  }
 }
