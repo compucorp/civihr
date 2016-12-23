8d6d50fd66e30c8d9cf39ae5732d277b310cb02a
Davi Alexandre <davi@davialexandre.com.br>
2016-12-23 17:27:32 -0200

    PCHR-1681: Include LeaveRequest fields on the spec to SicknessRequest.create
    This was done in order for the API properly handle the LeaveRequest params that
    are going to be passed to the SicknessRequest API

--- uk.co.compucorp.civicrm.hrleaveandabsences/api/v3/SicknessRequest.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/api/v3/SicknessRequest.php
@@ -9,7 +9,75 @@
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
  */
 function _civicrm_api3_sickness_request_create_spec(&$spec) {
-  // $spec['some_parameter']['api.required'] = 1;
+  $spec['type_id'] = [
+    'name'         => 'type_id',
+    'type'         => CRM_Utils_Type::T_INT,
+    'description'  => 'FK to AbsenceType',
+    'api.required' => TRUE,
+    'FKClassName'  => 'CRM_HRLeaveAndAbsences_DAO_AbsenceType',
+    'FKApiName'    => 'AbsenceType',
+  ];
+
+  $spec['contact_id'] = [
+    'name'         => 'contact_id',
+    'type'         => CRM_Utils_Type::T_INT,
+    'description'  => 'FK to Contact',
+    'api.required' => TRUE,
+    'FKClassName'  => 'CRM_HRLeaveAndAbsences_DAO_LeaveRequest',
+    'FKApiName'    => 'LeaveRequest',
+  ];
+
+  $spec['status_id'] = array(
+    'name'           => 'status_id',
+    'type'           => CRM_Utils_Type::T_INT,
+    'description'    => 'One of the values of the Leave Request Status option group',
+    'api.required'   => TRUE,
+    'pseudoconstant' => [
+      'optionGroupName' => 'hrleaveandabsences_leave_request_status',
+      'optionEditPath'  => 'civicrm/admin/options/hrleaveandabsences_leave_request_status'
+    ]
+  );
+  $spec['from_date'] = [
+    'name'         => 'from_date',
+    'type'         => CRM_Utils_Type::T_DATE,
+    'title'        => ts('From Date'),
+    'description'  => 'The date the leave request starts.',
+    'api.required' => TRUE,
+  ];
+
+  $spec['from_date_type'] = [
+    'name'           => 'from_date_type',
+    'type'           => CRM_Utils_Type::T_INT,
+    'title'          => ts('From Date Type'),
+    'description'    => 'One of the values of the Leave Request Day Type option group',
+    'api.required'   => TRUE,
+    'pseudoconstant' => [
+      'optionGroupName' => 'hrleaveandabsences_leave_request_day_type',
+      'optionEditPath'  => 'civicrm/admin/options/hrleaveandabsences_leave_request_day_type',
+    ]
+  ];
+
+  $spec['to_date'] = [
+    'name'        => 'to_date',
+    'type'        => CRM_Utils_Type::T_DATE,
+    'title'       => ts('To Date'),
+    'description' => 'The date the leave request ends. If null, it means is starts and ends at the same date',
+  ];
+
+  $spec['to_date_type'] = [
+    'name'           => 'to_date_type',
+    'type'           => CRM_Utils_Type::T_INT,
+    'title'          => ts('To Date Type'),
+    'description'    => 'One of the values of the Leave Request Day Type option group',
+    'pseudoconstant' => [
+      'optionGroupName' => 'hrleaveandabsences_leave_request_day_type',
+      'optionEditPath'  => 'civicrm/admin/options/hrleaveandabsences_leave_request_day_type',
+    ]
+  ];
+
+  $spec['leave_request_id'] = [
+    'api.required' => 0
+  ];
 }
 
 /**
