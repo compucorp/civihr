b8fd9f0f2d5ecae41968f82b851fbfb723aba6da
omar abu hussein <opetmar91@gmail.com>
2017-01-04 04:04:57 +0200

    PCHR-1687: Replace 'Website' with 'Social Account'
    Here we rename 'website_type' option value from 'Website Type' to 'Social Account Type'# modified: hrui/CRM/HRUI/Upgrader/Steps/4701.php and adding word replacment
    for ['Website', 'Website Type' and 'Add another website'] to be# ['Social Account', 'Social Account Type' and 'Add another social account'] respectively.

--- hrui/CRM/HRUI/Upgrader/Steps/4701.php
+++ hrui/CRM/HRUI/Upgrader/Steps/4701.php
@@ -6,12 +6,14 @@ trait CRM_HRUI_Upgrader_Steps_4701 {
    * Upgrader to :
    * 1) Set CiviHR theme by updating the custom CSS URL
    * 2) Sort Individual Prefixes alphabetically
+   * 3) Rename website to 'Social account'
    *
    * @return bool
    */
   public function upgrade_4701() {
     $this->up4701_setCustomCSSURL();
     $this->up4701_sortIndividualPrefixes();
+    $this->up4701_websiteToSocialAccountReplacement();
 
     return TRUE;
   }
@@ -48,6 +50,30 @@ trait CRM_HRUI_Upgrader_Steps_4701 {
           'weight' => $weight++
         ]);
       }
+    }
+  }
+
+  /**
+   * Replaces 'Website Type' option group label by 'Social Account Type'
+   * and adds word replacement for 'Website', 'Website Type' and 'Add another website'
+   */
+  private function up4701_websiteToSocialAccountReplacement() {
+    civicrm_api3('OptionGroup', 'get', [
+      'name' => "website_type",
+      'api.OptionGroup.create' => ['id' => '$value.id', 'title' => 'Social Account Type'],
+    ]);
+
+    $wordsToReplace = [
+      ['Website', 'Social Account'],
+      ['Website Type', 'Social Account Type'],
+      ['Add another website', 'Add another social account'],
+    ];
+
+    foreach ($wordsToReplace as $word) {
+      civicrm_api3('WordReplacement', 'create', [
+        'find_word' => $word[0],
+        'replace_word' => $word[1],
+      ]);
     }
   }
 
