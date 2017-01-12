93cc3edb7cf10f8554b12b4f8d651432243f6369
Tunbola Ogunwande <tunbolawande@yahoo.com>
2017-01-12 18:00:53 +0100

    PCHR-1866:  Create uk.co.compucorp.civicrm.hrcomments extension

---
+++ uk.co.compucorp.civicrm.hrcomments/README.md
@@ -0,0 +1,16 @@
+HR Comments extension
+======
+
+This extension provides Commenting functionality for CiviHR.
+Any CiviHR extension can use the comment functionality by invoking
+the API's provided by this extension.
+
+Installation
+------
+
+Go to 
+- Administer -> System Settings -> Manage Extensions
+
+and install CiviHR HR Comments (uk.co.compucorp.civicrm.hrcomments) extension.
+
+No additional steps are required.
---
+++ uk.co.compucorp.civicrm.hrcomments/hrcomments.php
@@ -0,0 +1,164 @@
+<?php
+
+require_once 'hrcomments.civix.php';
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function hrcomments_civicrm_config(&$config) {
+  _hrcomments_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @param array $files
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function hrcomments_civicrm_xmlMenu(&$files) {
+  _hrcomments_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function hrcomments_civicrm_install() {
+  _hrcomments_civix_civicrm_install();
+}
+
+/**
+* Implements hook_civicrm_postInstall().
+*
+* @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+*/
+function hrcomments_civicrm_postInstall() {
+  _hrcomments_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function hrcomments_civicrm_uninstall() {
+  _hrcomments_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function hrcomments_civicrm_enable() {
+  _hrcomments_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function hrcomments_civicrm_disable() {
+  _hrcomments_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed
+ *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function hrcomments_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _hrcomments_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function hrcomments_civicrm_managed(&$entities) {
+  _hrcomments_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * @param array $caseTypes
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function hrcomments_civicrm_caseTypes(&$caseTypes) {
+  _hrcomments_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function hrcomments_civicrm_angularModules(&$angularModules) {
+_hrcomments_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function hrcomments_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _hrcomments_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Functions below this ship commented out. Uncomment as required.
+ *
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ *
+function hrcomments_civicrm_preProcess($formName, &$form) {
+
+} // */
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
+ *
+function hrcomments_civicrm_navigationMenu(&$menu) {
+  _hrcomments_civix_insert_navigation_menu($menu, NULL, array(
+    'label' => ts('The Page', array('domain' => 'uk.co.compucorp.civicrm.hrcomments')),
+    'name' => 'the_page',
+    'url' => 'civicrm/the-page',
+    'permission' => 'access CiviReport,access CiviContribute',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+  _hrcomments_civix_navigationMenu($menu);
+} // */
