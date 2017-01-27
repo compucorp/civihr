e881e245711956032b5e50398bf9cebf1552d5e7
Tunbola Ogunwande <tunbolawande@yahoo.com>
2017-01-27 11:58:05 +0100

    PCHR-1895: Code Optimization

--- uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/api/v3/ApiPermissionTest.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/api/v3/ApiPermissionTest.php
@@ -17,8 +17,7 @@ class api_v3_ApiPermissionTest extends BaseHeadlessTest {
     $this->registerCurrentLoggedInContactInSession($contactID);
     CRM_Core_Config::singleton()->userPermissionClass->permissions = [];
 
-    $entityName = $this->formatSnakeToCamelCase($entity);
-    $this->setExpectedException('CiviCRM_API3_Exception', "API permission check failed for {$entityName}/{$action} call; insufficient permission: require access AJAX API");
+    $this->setExpectedException('CiviCRM_API3_Exception', "API permission check failed for {$entity}/{$action} call; insufficient permission: require access AJAX API");
 
     $payload = ['check_permissions' => true];
 
@@ -31,31 +30,21 @@ class api_v3_ApiPermissionTest extends BaseHeadlessTest {
 
   public function apiPermissionsDataProvider() {
     return [
-      ['leave_request', 'isvalid'],
-      ['leave_request', 'getfull'],
-      ['leave_request', 'ismanagedby'],
-      ['leave_request', 'update'],
-      ['leave_request', 'create'],
-      ['leave_request', 'calculatebalancechange'],
-      ['leave_request', 'getbalancechangebyabsencetype'],
-      ['work_pattern', 'getcalendar'],
-      ['absence_type', 'get'],
-      ['absence_period', 'get'],
-      ['option_group', 'get'],
-      ['option_value', 'get'],
-      ['leave_period_entitlement', 'get'],
-      ['public_holiday', 'get']
+      ['LeaveRequest', 'isvalid'],
+      ['LeaveRequest', 'getfull'],
+      ['LeaveRequest', 'ismanagedby'],
+      ['LeaveRequest', 'update'],
+      ['LeaveRequest', 'create'],
+      ['LeaveRequest', 'calculatebalancechange'],
+      ['LeaveRequest', 'getbalancechangebyabsencetype'],
+      ['WorkPattern', 'getcalendar'],
+      ['AbsenceType', 'get'],
+      ['AbsencePeriod', 'get'],
+      ['OptionGroup', 'get'],
+      ['OptionValue', 'get'],
+      ['LeavePeriodEntitlement', 'get'],
+      ['PublicHoliday', 'get']
     ];
-  }
-
-  private function formatSnakeToCamelCase($text) {
-    $text_array = explode('_', $text);
-    $camelCaseText = '';
-    foreach ($text_array as $value){
-      $camelCaseText .= ucfirst($value);
-    }
-
-    return $camelCaseText;
   }
 }
 
