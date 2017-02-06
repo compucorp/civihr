b8c144138dcc65f628c07898cde96ef79544465a
Tunbola Ogunwande <tunbolawande@yahoo.com>
2017-02-06 14:41:54 +0100

    PCHR-1836: Add more methods to LeaveRights Service. Add tests

--- uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Service/LeaveRequestRights.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Service/LeaveRequestRights.php
@@ -1,6 +1,7 @@
 <?php
 
 use CRM_HRLeaveAndAbsences_Service_LeaveManager as LeaveManagerService;
+use CRM_HRLeaveAndAbsences_BAO_LeaveRequest as LeaveRequest;
 
 class CRM_HRLeaveAndAbsences_Service_LeaveRequestRights {
 
@@ -8,6 +9,12 @@ class CRM_HRLeaveAndAbsences_Service_LeaveRequestRights {
    * @var \CRM_HRLeaveAndAbsences_Service_LeaveManager
    */
   private $leaveManagerService;
+
+  /**
+   * @var array|null
+   *   Stores the list of option values for the LeaveRequest status_id field.
+   */
+  private static $leaveStatuses;
 
   /**
    * CRM_HRLeaveAndAbsences_Service_LeaveRequestRights constructor.
@@ -28,7 +35,7 @@ class CRM_HRLeaveAndAbsences_Service_LeaveRequestRights {
    * @return bool
    */
   public function canCancelFor($contactID) {
-    return $this->currentUserIsManagerOrAdmin($contactID) || CRM_Core_Session::getLoggedInContactID() == $contactID;
+    return $this->currentUserIsManagerOrAdmin($contactID) || $this->currentUserIsLeaveContact($contactID);
   }
 
   /**
@@ -84,6 +91,50 @@ class CRM_HRLeaveAndAbsences_Service_LeaveRequestRights {
   }
 
   /**
+   * Checks whether the current user has permissions to create/update the leave request
+   *
+   * @param int $contactID
+   *   The contactID of the leave request
+   *
+   * @return bool
+   */
+  public function canCreateAndUpdateFor($contactID) {
+    return $this->currentUserIsLeaveContact($contactID) || $this->currentUserIsManagerOrAdmin($contactID);
+  }
+
+  /**
+   * Checks whether the current user can update the dates for the leave request or not.
+   *
+   * @param $contactID
+   *   The contactID of the leave request
+   * @param $statusID
+   *   The statusID of the leave request
+   *
+   * @return bool
+   */
+  public function canChangeDatesFor($contactID, $statusID) {
+    $leaveRequestStatuses = self::getLeaveRequestStatuses();
+    return $this->currentUserIsLeaveContact($contactID) &&
+           in_array($statusID, [$leaveRequestStatuses['waiting_approval'], $leaveRequestStatuses['more_information_requested']]);
+  }
+
+  /**
+   * Checks whether the current user can update the absence type for the leave request or not.
+   *
+   * @param int $contactID
+   *   The contactID of the leave request
+   * @param int $statusID
+   *   The statusID of the leave request
+   *
+   * @return bool
+   */
+  public function canChangeAbsenceTypeFor($contactID, $statusID) {
+    $leaveRequestStatuses = self::getLeaveRequestStatuses();
+    return $this->currentUserIsLeaveContact($contactID) &&
+           in_array($statusID, [$leaveRequestStatuses['waiting_approval'], $leaveRequestStatuses['more_information_requested']]);
+  }
+
+  /**
    * Checks if the current user is either a leave manager or an Admin
    *
    * @param int $contactID
@@ -94,5 +145,30 @@ class CRM_HRLeaveAndAbsences_Service_LeaveRequestRights {
   private function currentUserIsManagerOrAdmin($contactID) {
     return $this->leaveManagerService->currentUserIsLeaveManagerOf($contactID) ||
            $this->leaveManagerService->currentUserIsAdmin();
+  }
+
+  /**
+   * Checks whether the current user is the leave request contact or not.
+   *
+   * @param int $contactID
+   *   The contactID of the leave request
+   *
+   * @return bool
+   */
+  private function currentUserIsLeaveContact($contactID) {
+    return CRM_Core_Session::getLoggedInContactID() == $contactID;
+  }
+
+  /**
+   * Returns the array of the option values for the LeaveRequest status_id field.
+   *
+   * @return array
+   */
+  private static function getLeaveRequestStatuses() {
+    if (is_null(self::$leaveStatuses)) {
+      self::$leaveStatuses = array_flip(LeaveRequest::buildOptions('status_id', 'validate'));
+    }
+
+    return self::$leaveStatuses;
   }
 }
--- uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/CRM/HRLeaveAndAbsences/Service/LeaveRequestRightsTest.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/CRM/HRLeaveAndAbsences/Service/LeaveRequestRightsTest.php
@@ -11,6 +11,7 @@ class CRM_HRLeaveAndAbsences_Service_LeaveRequestRightsTest extends BaseHeadless
 
   use CRM_HRLeaveAndAbsences_SessionHelpersTrait;
   use CRM_HRLeaveAndAbsences_LeaveManagerHelpersTrait;
+  use CRM_HRLeaveAndAbsences_LeaveRequestHelpersTrait;
 
   /**
    * @var int
@@ -21,6 +22,8 @@ class CRM_HRLeaveAndAbsences_Service_LeaveRequestRightsTest extends BaseHeadless
     $this->leaveContact = 1;
     $this->registerCurrentLoggedInContactInSession($this->leaveContact);
     CRM_Core_Config::singleton()->userPermissionClass->permissions = [];
+
+    $this->leaveRequestStatuses = $this->getLeaveRequestStatuses();
   }
 
   public function testCanCancelForReturnsTrueWhenCurrentUserIsLeaveRequestContact() {
@@ -81,11 +84,150 @@ class CRM_HRLeaveAndAbsences_Service_LeaveRequestRightsTest extends BaseHeadless
   }
 
   public function testCanPutInWaitingForApprovalForReturnsTrueWhenCurrentUserIsAdmin() {
-    $this->assertTrue($this->getLeaveRequestRightsForAdminAsCurrentUser()->canPutInWaitingForApprovalFor($this->leaveContact['id']));
+    $this->assertTrue($this->getLeaveRequestRightsForAdminAsCurrentUser()->canPutInWaitingForApprovalFor($this->leaveContact));
   }
 
   public function testCanPutInWaitingForApprovalForReturnsFalseWhenCurrentUserIsLeaveContact() {
     $this->assertFalse($this->getLeaveRightsService()->canPutInWaitingForApprovalFor($this->leaveContact));
+  }
+
+  public function testCanCreateAndUpdateForReturnsTrueWhenCurrentUserIsLeaveRequestContact() {
+    $this->assertTrue($this->getLeaveRightsService()->canCreateAndUpdateFor($this->leaveContact));
+  }
+
+  public function testCanCreateAndUpdateForReturnsTrueWhenCurrentUserIsLeaveManager() {
+    $this->assertTrue($this->getLeaveRequestRightsForLeaveManagerAsCurrentUser()->canCreateAndUpdateFor($this->leaveContact));
+  }
+
+  public function testCanCreateAndUpdateForReturnsTrueWhenCurrentUserIsAdmin() {
+    $this->assertTrue($this->getLeaveRequestRightsForAdminAsCurrentUser()->canCreateAndUpdateFor($this->leaveContact));
+  }
+
+  public function testCanCreateAndUpdateForReturnsFalseWhenCurrentUserIsNotAnAdminOrLeaveManagerOrLeaveContact() {
+    $contactID = 3;
+    $this->assertFalse($this->getLeaveRightsService()->canCreateAndUpdateFor($contactID));
+  }
+
+  public function testCanChangeDatesForReturnsTrueWhenCurrentUserIsLeaveContactAndStatusPassedIsInAllowedStatuses() {
+    //When user is leave request contact and status is 'More information Requested'
+    $this->assertTrue(
+      $this->getLeaveRightsService()->canChangeDatesFor(
+        $this->leaveContact,
+        $this->leaveRequestStatuses['More Information Requested']['id']
+      )
+    );
+
+    //When user is leave request contact and status is 'Waiting Approval'
+    $this->assertTrue(
+      $this->getLeaveRightsService()->canChangeDatesFor(
+        $this->leaveContact,
+        $this->leaveRequestStatuses['Waiting Approval']['id']
+      )
+    );
+  }
+
+  public function testCanChangeDatesForReturnsFalseWhenCurrentUserIsLeaveContactAndStatusPassedIsNotInAllowedStatuses() {
+    //When user is leave request contact and status is 'Approved'
+    $this->assertFalse(
+      $this->getLeaveRightsService()->canChangeDatesFor(
+        $this->leaveContact,
+        $this->leaveRequestStatuses['Approved']['id']
+      )
+    );
+
+    //When user is leave request contact and status is 'Admin Approved'
+    $this->assertFalse(
+      $this->getLeaveRightsService()->canChangeDatesFor(
+        $this->leaveContact,
+        $this->leaveRequestStatuses['Admin Approved']['id']
+      )
+    );
+  }
+
+  public function testCanChangeDatesForReturnsFalseWhenCurrentUserNotLeaveContactIrrespectiveOfStatusPassed() {
+    $contactID = 2;
+
+    $this->assertFalse(
+      $this->getLeaveRequestRightsForLeaveManagerAsCurrentUser()->canChangeDatesFor(
+        $contactID,
+        $this->leaveRequestStatuses['More Information Requested']['id']
+      )
+    );
+
+    $this->assertFalse(
+      $this->getLeaveRequestRightsForAdminAsCurrentUser()->canChangeDatesFor(
+        $contactID,
+        $this->leaveRequestStatuses['Admin Approved']['id']
+      )
+    );
+
+    $this->assertFalse(
+      $this->getLeaveRightsService()->canChangeDatesFor(
+        $contactID,
+        $this->leaveRequestStatuses['Approved']['id']
+      )
+    );
+  }
+
+  public function testCanChangeAbsenceTypeForReturnsTrueWhenCurrentUserIsLeaveContactAndStatusPassedIsInAllowedStatuses() {
+    //When user is leave request contact and status is 'More information Requested'
+    $this->assertTrue(
+      $this->getLeaveRightsService()->canChangeAbsenceTypeFor(
+        $this->leaveContact,
+        $this->leaveRequestStatuses['More Information Requested']['id']
+      )
+    );
+
+    //When user is leave request contact and status is 'Waiting Approval'
+    $this->assertTrue(
+      $this->getLeaveRightsService()->canChangeAbsenceTypeFor(
+        $this->leaveContact,
+        $this->leaveRequestStatuses['Waiting Approval']['id']
+      )
+    );
+  }
+
+  public function testCanChangeAbsenceTypeForReturnsFalseWhenCurrentUserIsLeaveContactAndStatusPassedIsNotInAllowedStatuses() {
+    //When user is leave request contact and status is 'Approved'
+    $this->assertFalse(
+      $this->getLeaveRightsService()->canChangeAbsenceTypeFor(
+        $this->leaveContact,
+        $this->leaveRequestStatuses['Approved']['id']
+      )
+    );
+
+    //When user is leave request contact and status is 'Admin Approved'
+    $this->assertFalse(
+      $this->getLeaveRightsService()->canChangeAbsenceTypeFor(
+        $this->leaveContact,
+        $this->leaveRequestStatuses['Admin Approved']['id']
+      )
+    );
+  }
+
+  public function testCanChangeAbsenceTypeForReturnsFalseWhenCurrentUserNotLeaveContactIrrespectiveOfStatusPassed() {
+    $contactID = 2;
+
+    $this->assertFalse(
+      $this->getLeaveRequestRightsForLeaveManagerAsCurrentUser()->canChangeAbsenceTypeFor(
+        $contactID,
+        $this->leaveRequestStatuses['More Information Requested']['id']
+      )
+    );
+
+    $this->assertFalse(
+      $this->getLeaveRequestRightsForAdminAsCurrentUser()->canChangeAbsenceTypeFor(
+        $contactID,
+        $this->leaveRequestStatuses['Admin Approved']['id']
+      )
+    );
+
+    $this->assertFalse(
+      $this->getLeaveRightsService()->canChangeAbsenceTypeFor(
+        $contactID,
+        $this->leaveRequestStatuses['Approved']['id']
+      )
+    );
   }
 
   private function getLeaveRightsService($isAdmin = false, $isManager = false) {
