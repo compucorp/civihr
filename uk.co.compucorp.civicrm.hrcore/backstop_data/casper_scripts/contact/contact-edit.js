eacb4f1be0885f9fe3184e2bcdb0f84ba90189f9
Buddha Man Nepali <bmnepali980@gmail.com>
2017-03-06 18:45:34 +0545

    PCHR-1356: Implements PR review feedbacks

--- uk.co.compucorp.civicrm.hrcore/backstop_data/casper_scripts/contact/contact-edit.js
+++ uk.co.compucorp.civicrm.hrcore/backstop_data/casper_scripts/contact/contact-edit.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var page = require('../../page-objects/edit-form');
+var page = require('../../page-objects/contact-edit-form');
 
 module.exports = function (casper) {
   page.init(casper).editForm();
