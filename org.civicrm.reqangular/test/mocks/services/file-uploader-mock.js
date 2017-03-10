1a4aa7da84be67ec5b5b31ccef95872c7fd6ccfa
Dhaval Sharma <me@dhavalsharma.com>
2017-03-10 16:25:18 +0530

    PCHR-2053: PR Update 2

--- org.civicrm.reqangular/test/mocks/services/file-uploader-mock.js
+++ org.civicrm.reqangular/test/mocks/services/file-uploader-mock.js
@@ -1,51 +1,29 @@
 define([
-  'common/mocks/module'
-], function (mocks) {
+  'common/mocks/module',
+  'common/mocks/data/file-upload-data'
+], function (mocks, mockData) {
   'use strict';
 
   mocks.factory('FileUploaderMock', function () {
     var fileUploaderConstructor = function (settings) {
-      var response = {
-        "file": {
-          "is_error": 0,
-          "version": 3,
-          "count": 1,
-          "id": 1,
-          "values": {
-            "1": {
-              "id": "1",
-              "name": "PCHR_101089347_200217_0949.pdf",
-              "mime_type": "application/pdf",
-              "description": "",
-              "upload_date": "2017-02-21 14:29:58",
-              "entity_table": "civicrm_hrleaveandabsences_leave_request",
-              "entity_id": "1",
-              "url": "http://civicrm.host/index.php?q=civicrm/file&reset=1&id=1&eid=1"
-            },
-            "2": {
-              "id": "2",
-              "name": "SampleTextFile.txt",
-              "mime_type": "text/plain",
-              "description": "",
-              "upload_date": "2017-02-21 14:31:58",
-              "entity_table": "civicrm_hrleaveandabsences_leave_request",
-              "entity_id": "1",
-              "url": "http://civicrm.host/index.php?q=civicrm/file&reset=1&id=2&eid=1"
-            }
-          }
-        }
-      };
+
 
       function _onCompleteItem() {
         //call callbacks from callee
-        this.onCompleteItem({}, response);
+        this.onCompleteItem({}, mockData.getResponse());
         this.onCompleteAll();
       }
 
       return {
         uploadAll: function () {
+          this.onBeforeUploadItem({ formData: [] });
           _onCompleteItem.call(this);
-        }
+        },
+        //empty callbacks defintion requires for mocks to work
+        onCompleteItem: function () {},
+        onCompleteAll: function () {},
+        onErrorItem: function () {},
+        onBeforeUploadItem: function () {}
       };
     }
 
--- org.civicrm.reqangular/test/services/file-upload_test.js
+++ org.civicrm.reqangular/test/services/file-upload_test.js
@@ -7,7 +7,7 @@ define([
   'use strict';
 
   describe('FileUpload', function () {
-    var $provide, $rootScope, fileUpload, uploader, promise;
+    var $provide, $rootScope, $q, fileUpload, uploader, promise;
 
     beforeEach(module('common.services', 'common.mocks', function (_$provide_) {
       $provide = _$provide_;
@@ -23,12 +23,13 @@ define([
       }
     ]));
 
-    beforeEach(inject(function (_$rootScope_) {
+    beforeEach(inject(function (_$rootScope_, _$q_) {
       $rootScope = _$rootScope_;
+      $q = _$q_;
     }));
 
     it('has all endpoints', function () {
-      expect(Object.keys(fileUpload)).toContain('uploader', 'uploadAll');
+      expect(Object.keys(fileUpload)).toContain('uploader');
     });
 
     describe('uploader()', function () {
@@ -45,37 +46,35 @@ define([
 
       describe('missing params', function () {
         it('throws error if no param is passed', function () {
-          expect(fileUpload.uploader).toThrow('custom settings object need to be defined in parameter');
+          expect(fileUpload.uploader).toThrow('custom settings missing from parameter');
         });
 
         it('throws error if entityTable is not defined', function () {
           expect(function () { fileUpload.uploader({ crmAttachmentToken: '123abc' }) })
-            .toThrow('entityTable missing from custom settings');
+            .toThrow('entityTable missing from parameter');
         });
 
         it('throws error if crmAttachmentToken is not defined', function () {
           expect(function () { fileUpload.uploader({ entityTable: 'civicrm_hrleaveandabsences_leave_request' }) })
-            .toThrow('crmAttachmentToken missing from custom settings');
+            .toThrow('crmAttachmentToken missing from parameter');
         });
       });
     });
 
-    describe('uploadAll()', function () {
-      var param;
+    describe('uploading all files', function () {
+      var param = { entityID: '12' };
 
       beforeEach(function () {
-        spyOn(uploader, 'uploadAll').and.callThrough();
-        param = { entityID: '12' };
+        uploader = fileUpload.uploader({
+          entityTable: 'civicrm_hrleaveandabsences_leave_request',
+          crmAttachmentToken: '123abc'
+        });
 
-        promise = fileUpload.uploadAll(uploader, param);
+        promise = uploader.uploadAll(param);
       });
 
       afterEach(function () {
         $rootScope.$apply();
-      });
-
-      it('calls fileuploader endpoint to upload all', function () {
-        expect(uploader.uploadAll).toHaveBeenCalled();
       });
 
       it('returns array of success objects', function () {
@@ -88,6 +87,32 @@ define([
         promise.then(function (result) {
           var firstObject = result[0];
           expect(firstObject.file).toBeDefined();
+        });
+      });
+
+      describe('onBeforeUploadItem()', function () {
+        beforeEach(function () {
+          spyOn(uploader, 'onBeforeUploadItem').and.callThrough();
+          uploader.onBeforeUploadItem({ formData: [] });
+        });
+
+        it('gets called', function () {
+          //verify if method is getting called and hence a simple case
+          expect(uploader.onBeforeUploadItem).toHaveBeenCalled();
+        });
+      });
+
+      describe('onErrorItem()', function () {
+        var fileItem = { file: { name: 'filename' } };
+
+        beforeEach(function () {
+          spyOn(uploader, 'onErrorItem').and.callThrough();
+          promise = uploader.onErrorItem(fileItem);
+        });
+
+        it('gets called', function () {
+          //verify if method is getting called and hence a simple case
+          expect(uploader.onErrorItem).toHaveBeenCalled();
         });
       });
     });
