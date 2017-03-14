d2f17189c57a20a4ab578f9d70709e1f1e6a8f79
Tunbola Ogunwande <tunbolawande@yahoo.com>
2017-03-14 14:50:22 +0100

    PCHR-2026: Fix broken tests in LeaveRequestDateTest.

--- uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/CRM/HRLeaveAndAbsences/BAO/LeaveRequestDateTest.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/CRM/HRLeaveAndAbsences/BAO/LeaveRequestDateTest.php
@@ -41,48 +41,42 @@ class CRM_HRLeaveAndAbsences_BAO_LeaveRequestDateTest extends BaseHeadlessTest {
   }
 
   public function testGetDatesForLeaveRequestReturnsTheDatesForASpecificLeaveRequest() {
-    LeaveRequestDate::create([
-      'date' => date('YmdHis'),
-      'leave_request_id' => 1,
+    $leaveRequest1 = LeaveRequestFabricator::fabricateWithoutValidation([
+      'contact_id' => 1,
+      'type_id' => 1,
+      'from_date' => date('YmdHis'),
+      'to_date' => date('YmdHis', strtotime('+1 day')),
+      'status_id' => 1
     ]);
 
-    LeaveRequestDate::create([
-      'date' => date('YmdHis', strtotime('+1 day')),
-      'leave_request_id' => 1,
+    $leaveRequest2 = LeaveRequestFabricator::fabricateWithoutValidation([
+      'contact_id' => 1,
+      'type_id' => 1,
+      'from_date' => date('YmdHis', strtotime('+1 day')),
+      'to_date' => date('YmdHis', strtotime('+3 days')),
+      'status_id' => 1
     ]);
 
-    LeaveRequestDate::create([
-      'date' => date('YmdHis', strtotime('+1 day')),
-      'leave_request_id' => 2,
+    $leaveRequest3 = LeaveRequestFabricator::fabricateWithoutValidation([
+      'contact_id' => 1,
+      'type_id' => 1,
+      'from_date' => date('YmdHis'),
+      'to_date' => date('YmdHis'),
+      'status_id' => 1
     ]);
 
-    LeaveRequestDate::create([
-      'date' => date('YmdHis', strtotime('+2 days')),
-      'leave_request_id' => 2,
-    ]);
-
-    LeaveRequestDate::create([
-      'date' => date('YmdHis', strtotime('+3 days')),
-      'leave_request_id' => 2,
-    ]);
-
-    LeaveRequestDate::create([
-      'date' => date('YmdHis'),
-      'leave_request_id' => 3,
-    ]);
-
-    $datesLeaveRequest1 = LeaveRequestDate::getDatesForLeaveRequest(1);
+    $datesLeaveRequest1 = LeaveRequestDate::getDatesForLeaveRequest($leaveRequest1->id);
     $this->assertCount(2, $datesLeaveRequest1);
     $this->assertEquals(date('Y-m-d'), $datesLeaveRequest1[0]->date);
     $this->assertEquals(date('Y-m-d', strtotime('+1 day')), $datesLeaveRequest1[1]->date);
 
-    $datesLeaveRequest2 = LeaveRequestDate::getDatesForLeaveRequest(2);
+    $datesLeaveRequest2 = LeaveRequestDate::getDatesForLeaveRequest($leaveRequest2->id);
     $this->assertCount(3, $datesLeaveRequest2);
     $this->assertEquals(date('Y-m-d', strtotime('+1 day')), $datesLeaveRequest2[0]->date);
     $this->assertEquals(date('Y-m-d', strtotime('+2 days')), $datesLeaveRequest2[1]->date);
     $this->assertEquals(date('Y-m-d', strtotime('+3 days')), $datesLeaveRequest2[2]->date);
 
-    $datesLeaveRequest3 = LeaveRequestDate::getDatesForLeaveRequest(3);
+    $datesLeaveRequest3 = LeaveRequestDate::getDatesForLeaveRequest($leaveRequest3->id);
     $this->assertCount(1, $datesLeaveRequest3);
     $this->assertEquals(date('Y-m-d'), $datesLeaveRequest3[0]->date);
   }
@@ -92,31 +86,32 @@ class CRM_HRLeaveAndAbsences_BAO_LeaveRequestDateTest extends BaseHeadlessTest {
   }
 
   public function testDeleteDatesDeletesTheDatesForASpecificLeaveRequest() {
-    LeaveRequestDate::create([
-      'date' => date('YmdHis'),
-      'leave_request_id' => 1,
+    $leaveRequest1 = LeaveRequestFabricator::fabricateWithoutValidation([
+      'contact_id' => 1,
+      'type_id' => 1,
+      'from_date' => date('YmdHis'),
+      'to_date' => date('YmdHis'),
+      'status_id' => 1
     ]);
 
-    LeaveRequestDate::create([
-      'date' => date('YmdHis'),
-      'leave_request_id' => 2,
+    $leaveRequest2 = LeaveRequestFabricator::fabricateWithoutValidation([
+      'contact_id' => 1,
+      'type_id' => 1,
+      'from_date' => date('YmdHis'),
+      'to_date' => date('YmdHis', strtotime('+1 day')),
+      'status_id' => 1
     ]);
 
-    LeaveRequestDate::create([
-      'date' => date('YmdHis', strtotime('+1 day')),
-      'leave_request_id' => 2,
-    ]);
+    $this->assertCount(1, LeaveRequestDate::getDatesForLeaveRequest($leaveRequest1->id));
+    $this->assertCount(2, LeaveRequestDate::getDatesForLeaveRequest($leaveRequest2->id));
 
-    $this->assertCount(1, LeaveRequestDate::getDatesForLeaveRequest(1));
-    $this->assertCount(2, LeaveRequestDate::getDatesForLeaveRequest(2));
+    LeaveRequestDate::deleteDatesForLeaveRequest($leaveRequest1->id);
+    $this->assertCount(0, LeaveRequestDate::getDatesForLeaveRequest($leaveRequest1->id));
+    $this->assertCount(2, LeaveRequestDate::getDatesForLeaveRequest($leaveRequest2->id));
 
-    LeaveRequestDate::deleteDatesForLeaveRequest(1);
-    $this->assertCount(0, LeaveRequestDate::getDatesForLeaveRequest(1));
-    $this->assertCount(2, LeaveRequestDate::getDatesForLeaveRequest(2));
-
-    LeaveRequestDate::deleteDatesForLeaveRequest(2);
-    $this->assertCount(0, LeaveRequestDate::getDatesForLeaveRequest(1));
-    $this->assertCount(0, LeaveRequestDate::getDatesForLeaveRequest(2));
+    LeaveRequestDate::deleteDatesForLeaveRequest($leaveRequest2->id);
+    $this->assertCount(0, LeaveRequestDate::getDatesForLeaveRequest($leaveRequest1->id));
+    $this->assertCount(0, LeaveRequestDate::getDatesForLeaveRequest($leaveRequest2->id));
   }
 
   public function testGetDatesForLeaveRequestReturnsEmptyArrayForSoftDeletedLeaveRequest() {
