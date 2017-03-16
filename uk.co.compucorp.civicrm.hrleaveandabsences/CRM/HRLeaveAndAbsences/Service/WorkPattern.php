79ed1262ccca298b2d632734432d131ec02b4161
Tunbola Ogunwande <tunbolawande@yahoo.com>
2017-03-16 14:37:26 +0100

    PCHR-2046: Create WorkPattern Service, add workPatternHasEverBeenUsed and delete method. Add tests

---
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Service/WorkPattern.php
@@ -0,0 +1,77 @@
+<?php
+
+use CRM_HRLeaveAndAbsences_BAO_WorkPattern as WorkPattern;
+use CRM_HRLeaveAndAbsences_BAO_ContactWorkPattern as ContactWorkPattern;
+
+/**
+ * Class CRM_HRLeaveAndAbsences_Service_WorkPattern
+ */
+class CRM_HRLeaveAndAbsences_Service_WorkPattern {
+
+  /**
+   * Checks If a work pattern has ever been used by a contact
+   *
+   * @param int $workPatternID
+   *
+   * @return boolean
+   */
+  public function workPatternHasEverBeenUsed($workPatternID) {
+    if ($this->isDefaultWorkPattern($workPatternID) ||
+      $this->workPatternIsLinkedToAContactWorkPattern($workPatternID))
+    {
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * Deletes the WorkPattern with the given id.
+   * Checks first to see if the work pattern can be deleted or not.
+   *
+   * @param int $workPatternID
+   */
+  public function delete($workPatternID) {
+    if ($this->workPatternHasEverBeenUsed($workPatternID)) {
+      throw new UnexpectedValueException('Work pattern cannot be deleted because it is used by one or more contacts');
+    }
+
+    WorkPattern::del($workPatternID);
+  }
+
+  /**
+   * Checks if the work pattern is the default work pattern
+   *
+   * @param int $workPatternID
+   *
+   * @return boolean
+   */
+  private function isDefaultWorkPattern($workPatternID) {
+    $workPattern = WorkPattern::findById($workPatternID);
+
+    if ($workPattern->is_default) {
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * Checks if the work pattern is linked to at least one contact work pattern
+   *
+   * @param int $workPatternID
+   *
+   * @return boolean
+   */
+  private function workPatternIsLinkedToAContactWorkPattern($workPatternID) {
+    $contactWorkPattern = new ContactWorkPattern();
+    $contactWorkPattern->pattern_id = $workPatternID;
+    $contactWorkPattern->find();
+
+    if ($contactWorkPattern->N > 0) {
+      return true;
+    }
+
+    return false;
+  }
+}
