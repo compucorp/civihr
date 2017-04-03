bbf7e4c2b91c883bb6fa0fa37ffd23641110d05d
Bruno Queiros <brunoofq@gmail.com>
2017-04-03 18:58:11 -0300

    PCHR-1913: Decorator to fix focus

--- org.civicrm.reqangular/src/common/decorators/ui-select/ui-select.js
+++ org.civicrm.reqangular/src/common/decorators/ui-select/ui-select.js
@@ -1,9 +1,26 @@
 define([], function() {
   'use strict';
 
-  return ['$delegate', function($delegate) {
+  return ['$delegate', '$timeout', '$animate', function($delegate, $timeout, $animate) {
     var directive = $delegate[0];
     var origTemplateUrl = directive.templateUrl;
+    var directiveCompile = directive.compile;
+
+    // Fix the focus on first click
+    directive.compile = function (tElement, tAttrs) {
+      var link = directiveCompile.apply(this, arguments);
+
+      return function (scope, elem, attrs, uiSelect) {
+        link.apply(this, arguments);
+        scope.$watch('$select.open', function (val) {
+          $timeout(function () {
+            if (elem.hasClass('open')) {
+              elem.find('input').focus();
+            }
+          }, 100);
+        });
+      };
+    };
 
     // In order to add the "contact actions" the the dropdown, the template file
     // is changed depending on the presence of the "contacts" attribute
--- org.civicrm.reqangular/src/common/modules/directives.js
+++ org.civicrm.reqangular/src/common/modules/directives.js
@@ -1,17 +1,19 @@
 define([
   'common/angular',
   'common/decorators/angular-date/datepicker-addon',
+  'common/decorators/ui-select/ui-select',
   'common/ui-select',
   'common/modules/templates',
   'common/modules/controllers',
   'common/modules/services',
   'common/modules/apis'
-], function (angular, datepickerAddon) {
+], function (angular, datepickerAddon, uiSelect) {
   'use strict';
 
   return angular.module('common.directives', ['common.templates', 'common.controllers',
     'common.apis', 'ui.select', 'ngSanitize'])
     .config(['$provide', function ($provide) {
       $provide.decorator('uibDatepickerPopupDirective', datepickerAddon);
+      $provide.decorator('uiSelectDirective', uiSelect);
     }]);
 });
