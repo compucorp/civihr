da5c2402b2def8b53c4b1fb3596dd3184a3a2eac
Camilo Rodriguez <camilo@compucorp.co.uk>
2017-04-04 16:36:59 +0000

    PCHR-1820: Show Selected Pension Provider When Editing Contract
    Using "View Current Revision" or "Correct Error" buttons on a contract, and
    navigating to Pension Tab, always showed Pension Provider empty, even though a
    provider had been selected and saved, and currently shown on contract summary.
    
    The problem was due to the use of an uncapitalized 'pension_provider' key to
    reference the contact in the controller for the form. Fixed by changing
    'pension_provider' to 'Pension_Provider'.

--- hrjobcontract/js/src/job-contract/controllers/form/form-pension.js
+++ hrjobcontract/js/src/job-contract/controllers/form/form-pension.js
@@ -1,1 +1,34 @@
-define([    'job-contract/controllers/controllers',    'job-contract/services/contact'], function (controllers) {    'use strict';    controllers.controller('FormPensionCtrl',['$scope','settings','ContactService', '$log',        function ($scope, settings, ContactService, $log) {            $log.debug('Controller: FormPensionCtrl');            $scope.contacts = {                pension_provider: []            };            $scope.refreshContacts = function(input, contactSubType){                if (!input) {                    return                }                ContactService.search(input, {                    contact_type: 'Organization',                    contact_sub_type: contactSubType                }).then(function(results){                    $scope.contacts[contactSubType] = results;                });            };            if ($scope.entity.pension.pension_type) {                ContactService.getOne($scope.entity.pension.pension_type).then(function(result){                    $scope.contacts.pension_provider.push(result);                });            }        }]);});
+define([
+  'job-contract/controllers/controllers',
+  'job-contract/services/contact'
+], function (controllers) {
+  'use strict';
+
+  controllers.controller('FormPensionCtrl',['$scope','settings','ContactService', '$log',
+    function ($scope, settings, ContactService, $log) {
+      $log.debug('Controller: FormPensionCtrl');
+
+      $scope.contacts = {
+        Pension_Provider: []
+      };
+
+      $scope.refreshContacts = function(input, contactSubType){
+          if (!input) {
+            return;
+          }
+
+        ContactService.search(input, {
+          contact_type: 'Organization',
+          contact_sub_type: contactSubType
+        }).then(function(results){
+          $scope.contacts[contactSubType] = results;
+        });
+      };
+
+      if ($scope.entity.pension.pension_type) {
+        ContactService.getOne($scope.entity.pension.pension_type).then(function(result){
+          $scope.contacts.Pension_Provider.push(result);
+        });
+      }
+    }]);
+});
