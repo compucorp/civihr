75836d82deb1e0fa73ca354fad58df682bcb13f9
mickadoo <michaeldevery@gmail.com>
2017-04-06 10:59:12 +0100

    PCHR-2089: Add mapping for hours type to make it more readable

--- hrjobcontract/js/src/job-contract/controllers/form/form-hour.js
+++ hrjobcontract/js/src/job-contract/controllers/form/form-hour.js
@@ -10,7 +10,12 @@ define([
 
       var entityHour = $scope.entity.hour,
         utilsHoursLocation = $scope.utils.hoursLocation,
-        locStandHrs = {};
+        locStandHrs = {},
+        hourTypeMapping = {
+          0: 'CASUAL',
+          4: 'PART_TIME',
+          8: 'FULL_TIME'
+        };
 
       $scope.hrsTypeDefined = false;
       $scope.hrsAmountDefined = false;
@@ -23,15 +28,15 @@ define([
         entityHour.hours_unit = locStandHrs.periodicity;
 
         // reset if hours are not defined or if new choice is "full time"
-        if ($scope.hrsTypeDefined && (!$scope.hrsAmountDefined || +hrsTypeId === 8)) {
-          switch(+hrsTypeId) {
-            case 8:
+        if ($scope.hrsTypeDefined && (!$scope.hrsAmountDefined || hourTypeMapping[+hrsTypeId] === 'FULL_TIME')) {
+          switch(hourTypeMapping[+hrsTypeId]) {
+            case 'FULL_TIME':
               entityHour.hours_amount = locStandHrs.standard_hours;
               break;
-            case 4:
+            case 'PART_TIME':
               entityHour.hours_amount = Math.round(locStandHrs.standard_hours / 2);
               break;
-            case 0:
+            case 'CASUAL':
               entityHour.hours_amount = 0;
               break;
             default:
