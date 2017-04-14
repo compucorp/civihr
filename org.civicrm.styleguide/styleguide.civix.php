cda7628952ed277989287d376dd112ee892da2f8
Maged Adel <maged@compucorp.co.uk>
2017-04-14 16:13:21 +0200

    AI-109: Use empty() to fix undefined index issue

--- org.civicrm.styleguide/styleguide.civix.php
+++ org.civicrm.styleguide/styleguide.civix.php
@@ -278,7 +278,7 @@ function _styleguide_civix_insert_navigation_menu(&$menu, $path, $item) {
     $first = array_shift($path);
     foreach ($menu as $key => &$entry) {
       if ($entry['attributes']['name'] == $first) {
-        if (!$entry['child']) $entry['child'] = array();
+        if (empty($entry['child'])) $entry['child'] = array();
         $found = _styleguide_civix_insert_navigation_menu($entry['child'], implode('/', $path), $item, $key);
       }
     }
