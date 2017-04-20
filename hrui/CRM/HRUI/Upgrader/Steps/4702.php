66cd21bb0bd2743f10da34a57fa0c15ca5461429
Camilo Rodriguez <camilo@compucorp.co.uk>
2017-04-20 16:25:25 +0000

    PCHR-1687: Fix Broken Styles on Upgrade
    Upgrade was breaking styles for the site because it was using a full physical
    path for the customCSSURL setting.  Changed the path for the URL of the
    custom-civicrm.css file of shoreditch.

--- hrui/CRM/HRUI/Upgrader/Steps/4702.php
+++ hrui/CRM/HRUI/Upgrader/Steps/4702.php
@@ -20,7 +20,7 @@ trait CRM_HRUI_Upgrader_Steps_4702 {
   }
 
   private function up4702_setCustomCSSURL() {
-    $customCSSPath = CRM_Core_Resources::singleton()->getPath('org.civicrm.shoreditch', 'css/custom-civicrm.css');
+    $customCSSPath = CRM_Core_Resources::singleton()->getUrl('org.civicrm.shoreditch', 'css/custom-civicrm.css');
 
     if (!empty($customCSSPath)) {
       civicrm_api3('Setting', 'create', [
@@ -78,13 +78,13 @@ trait CRM_HRUI_Upgrader_Steps_4702 {
     $wordsToReplace = [
       ['Website', 'Social Account'],
       ['Website Type', 'Social Account Type'],
-      ['Add another website', 'Add another social account'],
+      ['Add another website', 'Add another social account']
     ];
 
     foreach ($wordsToReplace as $word) {
       civicrm_api3('WordReplacement', 'create', [
         'find_word' => $word[0],
-        'replace_word' => $word[1],
+        'replace_word' => $word[1]
       ]);
     }
   }
