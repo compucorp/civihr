190fee41d0f0ba0ef36b61f7b88e0925da588994
Debarshi Bhaumik <deb1990@gmail.com>
2017-04-24 16:24:17 +0530

    PCHR-2003: Remove uib-tabset module

--- org.civicrm.reqangular/src/common/modules/directives.js
+++ org.civicrm.reqangular/src/common/modules/directives.js
@@ -7,15 +7,24 @@ define([
   'common/ui-select',
   'common/modules/templates',
   'common/modules/controllers',
-  'common/modules/uib-tabset',
 ], function (angular, uiSelectChoicesCustomScrollBars, datepickerAddon, uibTabset) {
   'use strict';
 
-  return angular.module('common.directives', ['common.templates', 'common.controllers', 'common.uibTabset',
+  return angular.module('common.directives', ['common.templates', 'common.controllers',
     'ui.select', 'ui.bootstrap'])
     .config(['$provide', function ($provide) {
       $provide.decorator('uiSelectChoicesDirective', uiSelectChoicesCustomScrollBars);
       $provide.decorator('uibDatepickerPopupDirective', datepickerAddon);
       $provide.decorator('uibTabsetDirective', uibTabset);
+    }])
+    .run(['$templateCache', function ($templateCache) {
+      // Update uib-tabset HTML with header class
+      var tplPath = 'uib/template/tabs/tabset.html';
+      var tpl = jQuery($templateCache.get(tplPath));
+
+      tpl.find('ul').addClass('{{tabset.customHeaderClass}}');
+
+      $templateCache.put(tplPath, tpl.wrap('<div/>').parent().html());
+      //end of uib-tabset override
     }]);
 });
