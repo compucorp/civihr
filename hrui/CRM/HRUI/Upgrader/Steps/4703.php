c5920bbb09c88bce75e62ffdd5ed445db3e4f7ae
Camilo Rodriguez <camilo@compucorp.co.uk>
2017-05-08 12:03:55 +0000

    PCHR-1687: Do not Remove 'Case Coordinator' Relationship
    'Case Coordinator' relationship is being used in Tasks & Assignments extension
    to create assignments, so it shouldn't be deleted.

--- hrui/CRM/HRUI/Upgrader/Steps/4703.php
+++ hrui/CRM/HRUI/Upgrader/Steps/4703.php
@@ -5,35 +5,34 @@
  * 'CiviReport' and 'CiviCase' extensions.
  */
 trait CRM_HRUI_Upgrader_Steps_4703 {
-	
-	/**
-	 * Deletes relationship types that do not make sense for a CiviHR installation.
-	 *
-	 * @return bool
-	 *   True on success, false otherwise
-	 */
-	public function upgrade_4703() {
-		$relationshipsToDelete = [
-			'Benefits Specialist',
-			'Case Coordinator',
-			'Parent of',
-			'Health Services Coordinator',
-			'Homeless Services Coordinator',
-			'Senior Services Coordinator',
-			'Sibling of',
-			'Supervisor',
-			'Volunteer is',
-			'Spouse of',
-			'Partner of'
-		];
-		
-		$params = [
-			'name_b_a' => ['IN' => $relationshipsToDelete],
-			'api.RelationshipType.delete' => ['id' => '$value.id']
-		];
-		
-		civicrm_api3('RelationshipType', 'get', $params);
-		
-		return TRUE;
-	}
+
+  /**
+   * Deletes relationship types that do not make sense for a CiviHR installation.
+   *
+   * @return bool
+   *   True on success, false otherwise
+   */
+  public function upgrade_4703() {
+    $relationshipsToDelete = [
+      'Benefits Specialist',
+      'Parent of',
+      'Health Services Coordinator',
+      'Homeless Services Coordinator',
+      'Senior Services Coordinator',
+      'Sibling of',
+      'Supervisor',
+      'Volunteer is',
+      'Spouse of',
+      'Partner of'
+    ];
+
+    $params = [
+      'name_b_a' => ['IN' => $relationshipsToDelete],
+      'api.RelationshipType.delete' => ['id' => '$value.id']
+    ];
+
+    civicrm_api3('RelationshipType', 'get', $params);
+
+    return TRUE;
+  }
 }
