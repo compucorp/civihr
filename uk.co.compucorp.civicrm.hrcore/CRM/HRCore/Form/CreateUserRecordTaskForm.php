51fbe60496e82a0c9537b67dfe3966c40ba2976b
mickadoo <michaeldevery@gmail.com>
2017-05-09 18:44:49 +0100

    PCHR-2149: Move some of the methods to separate services

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Form/CreateUserRecordTaskForm.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Form/CreateUserRecordTaskForm.php
@@ -1,40 +1,152 @@
 <?php
 
+use CRM_Utils_Array as ArrayHelper;
+
 class CRM_HRCore_Form_CreateUserRecordTaskForm extends CRM_Contact_Form_Task {
+
   /**
    * @var array
    */
-  protected $contactMapping = [];
+  protected $contactDetails = [];
 
   /**
-   * @var string
+   * @var DrupalUserService
    */
-  private $checkboxName = 'send_email';
+  protected $drupalUserService;
+
+  /**
+   * @param null $state
+   * @param mixed $action
+   * @param string $method
+   * @param null $name
+   */
+  public function __construct(
+    $state = NULL,
+    $action = CRM_Core_Action::NONE,
+    $method = 'post',
+    $name = NULL
+  ) {
+    // todo change to container get()
+    $this->drupalUserService = new DrupalUserService();
+    parent::__construct($state, $action, $method, $name);
+  }
 
   /**
    * Build the form object.
    */
   public function buildQuickForm() {
     CRM_Utils_System::setTitle(ts('Create User Records'));
-    foreach ($this->_contactIds as $key => $contactId) {
-      $this->contactMapping[$key] = $contactId;
-      $this->add('advcheckbox', sprintf('%s[%d]', $this->checkboxName, $key));
-    }
     $this->addDefaultButtons(ts('Create Records'));
+  }
+
+  /**
+   * Fetch contact details and set some template variables
+   */
+  public function preProcess() {
+    parent::preProcess();
+    $this->initContactDetails();
+    // todo assign variables for form, num missing email etc..
   }
 
   /**
    * Process the form after the input has been submitted and validated.
    */
   public function postProcess() {
-    $values = $this->controller->exportValues($this->_name);
-    $sendEmailValues = CRM_Utils_Array::value($this->checkboxName, $values, []);
-    $sendEmailValues = array_map('boolval', $sendEmailValues);
-
-    foreach ($sendEmailValues as $contactKey => $shouldSendEmail) {
-      $contactID = CRM_Utils_Array::value($contactKey, $this->contactMapping);
-      // todo send the emails blah blah
+    foreach ($this->getValidContactForCreation() as $contact) {
+      $this->createAccount($contact['work_email']);
     }
+  }
+
+  /**
+   * Returns contacts that have a work email and no drupal account
+   *
+   * @return array
+   */
+  private function getValidContactForCreation() {
+    return array_diff(
+      $this->contactDetails,
+      $this->getContactsWithout('work_email'),
+      $this->getContactsWithout('uf_id')
+    );
+  }
+
+  /**
+   * Create a Drupal account for a contact
+   *
+   * @param $email
+   *
+   * @return object
+   */
+  private function createAccount($email) {
+    $user = $this->drupalUserService->createNew($email);
+    $user->status = 1; // unblock user (will send invitation mail)
+    $this->drupalUserService->addRoles($user, ['civihr_staff']);
+
+    return $user;
+  }
+
+  /**
+   * Creates an array to store contact details
+   */
+  private function initContactDetails() {
+    $emailParams = [
+      'contact_id' => '$value.id',
+      'return' => ['email'],
+      'location_type_id' => 'work'
+    ];
+    $ufMatchParams = [
+      'contact_id' => '$value.id',
+      'return' => ['uf_id']
+    ];
+    $params = [
+      'return' => ['display_name'],
+      'id' => ['IN' => $this->_contactIds],
+      'options' => ['limit' => 0],
+      'api.Email.getsingle' => $emailParams,
+      'api.UFMatch.getsingle' => $ufMatchParams,
+    ];
+    $contactDetails = civicrm_api3('Contact', 'get', $params);
+    $contactDetails = ArrayHelper::value('values', $contactDetails);
+
+    foreach ($contactDetails as $detail) {
+      $contactID = (int) ArrayHelper::value('contact_id', $detail);
+      $displayName = ArrayHelper::value('display_name', $detail);
+      $ufMatch = ArrayHelper::value('api.UFMatch.getsingle', $detail, []);
+      $ufId = (int) ArrayHelper::value('uf_id', $ufMatch);
+      $emailDetails = ArrayHelper::value('api.Email.getsingle', $detail, []);
+      $email = ArrayHelper::value('email', $emailDetails);
+
+      $this->setContactDetail($contactID, 'display_name', $displayName);
+      $this->setContactDetail($contactID, 'uf_id', $ufId);
+      $this->setContactDetail($contactID, 'work_email', $email);
+    }
+  }
+
+  /**
+   * Helper method to prevent overwriting of contact details
+   *
+   * @param int $contactId
+   * @param string $type
+   * @param mixed $value
+   */
+  private function setContactDetail($contactId, $type, $value) {
+    if (!isset($this->contactDetails[$contactId][$type])) {
+      $this->contactDetails[$contactId][$type] = $value;
+    }
+  }
+
+  /**
+   * @param $property
+   *  The property to check if empty
+   *
+   * @return array
+   */
+  protected function getContactsWithout($property) {
+    $checker = function ($contactDetail) use ($property) {
+      return empty($contactDetail[$property]);
+    };
+
+    return array_filter($this->contactDetails, $checker);
   }
 
 }
--- uk.co.compucorp.civicrm.hrcore/templates/CRM/HRCore/Form/CreateUserRecordTaskForm.tpl
+++ uk.co.compucorp.civicrm.hrcore/templates/CRM/HRCore/Form/CreateUserRecordTaskForm.tpl
@@ -1,21 +1,8 @@
-<h3>{ts 1=$totalSelectedContacts}Number of selected contacts: %1{/ts}</h3>
-
-<table id="create-user-record-task-table" class="display">
-  <thead>
-  <tr class="columnheader">
-    <th class="contact_details">{ts}Name{/ts}</th>
-    <th>{ts}Send Email{/ts}</th>
-  </tr>
-  </thead>
-
-  <tbody>
-  {foreach from=$value item=contactEmail key=contactID}
-    <tr class="{cycle values="odd-row,even-row"}">
-      <td class="name">{$contactEmail}</td>
-      <td>{$form.send_email.$contactID.html}</td>
-    </tr>
-  {/foreach}
-  </tbody>
-</table>
+<h3>{ts 1=$totalSelectedContacts}%1 new user accounts will be created.{/ts}</h3>
+<p class="warning">
+  {ts}A work e-mail is required to create the account.{/ts}
+  {ts 1=$numWithoutEmail}%1 contact(s) do not have a work email set.{/ts}
+  {* todo show list of contacts missing work email *}
+</p>
 
 <div class="crm-submit-buttons">{$form.buttons.html}</div>
