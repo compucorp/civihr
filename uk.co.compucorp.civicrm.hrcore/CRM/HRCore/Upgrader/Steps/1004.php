4eb5cbb29f745f98bb409df34cb9c5df90f987c9
mickadoo <michaeldevery@gmail.com>
2017-05-11 12:11:51 +0100

    PCHR-2149: Check if extension is enabled before creating CiviTask

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1004.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1004.php
@@ -1,10 +1,19 @@
 <?php
 
 trait CRM_HRCore_Upgrader_Steps_1004 {
+
+  /**
+   * @var bool
+   */
+  protected $tasksEnabled = FALSE;
+
   /**
    * Add new activity types
    */
   public function upgrade_1004() {
+    $key = 'uk.co.compucorp.civicrm.tasksassignments';
+    $this->tasksEnabled = $this->isExtensionEnabled($key);
+
     $newActivityTypes = [
       'Send Onboarding Email',
       'Create User Account'
@@ -29,7 +38,6 @@ trait CRM_HRCore_Upgrader_Steps_1004 {
     $existing = civicrm_api3('OptionValue', 'get', [
       'option_group_id' => 'activity_type',
       'name' => ['IN' => $newActivityTypes],
-      'component_id' => 'CiviTask',
     ]);
     $existing = CRM_Utils_Array::value('values', $existing, []);
 
@@ -40,11 +48,34 @@ trait CRM_HRCore_Upgrader_Steps_1004 {
    * @param $activityType
    */
   protected function createActivityType($activityType) {
-    civicrm_api3('OptionValue', 'create', [
+    $params = [
       'option_group_id' => 'activity_type',
       'name' => $activityType,
-      'component_id' => 'CiviTask',
-    ]);
+    ];
+
+    if ($this->tasksEnabled) {
+      $params['component_id'] = 'CiviTask';
+    }
+
+    civicrm_api3('OptionValue', 'create', $params);
+  }
+
+  /**
+   * Checks if tasks and assignments extension is installed or enabled
+   *
+   * @param string $key
+   *   Extension unique key
+   *
+   * @return bool
+   */
+  private function isExtensionEnabled($key) {
+    $isEnabled = CRM_Core_DAO::getFieldValue(
+      'CRM_Core_DAO_Extension',
+      $key,
+      'is_active',
+      'full_name'
+    );
+    return !empty($isEnabled) ? TRUE : FALSE;
   }
 
 }
