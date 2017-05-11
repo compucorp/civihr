9546a269f326e5ad2abf03d9191e4e2c5540be9f
mickadoo <michaeldevery@gmail.com>
2017-05-11 12:13:53 +0100

    PCHR-2149: Pass contact ID to drupal service creation methods

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Service/DrupalUserService.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Service/DrupalUserService.php
@@ -22,13 +22,14 @@ class CRM_HRCore_Service_DrupalUserService {
   }
 
   /**
+   * @param $id
    * @param $email
    * @param bool $active
    * @param array $roles
    *
    * @return object
    */
-  public function createNew($email, $active = FALSE, $roles = []) {
+  public function createNew($id, $email, $active = FALSE, $roles = []) {
     $params = [
       'name' => $email,
       'pass' => user_password(), // random password
@@ -41,45 +42,35 @@ class CRM_HRCore_Service_DrupalUserService {
       $params['roles'] = $this->roleService->getRoleIds($roles);
     }
 
+    /**
+     * UFMatch is created by CiviCRM function civicrm_user_insert listening
+     * for user_module_invoke('insert') from Drupal
+     */
     $user = user_save(drupal_anonymous_user(), $params);
-    $this->createActivity($user, 'Create User Account');
+    $this->createActivity($id, 'Create User Account');
 
     return $user;
   }
 
   /**
+   * @param int $contactID
    * @param object $user
    */
-  public function sendActivationMail($user) {
+  public function sendActivationMail($contactID, $user) {
     _user_mail_notify('status_activated', $user);
-    $this->createActivity($user, 'Send Onboarding Email');
+    $this->createActivity($contactID, 'Send Onboarding Email');
   }
 
   /**
-   * @param object $user
+   * @param int $contactID
    * @param string $type
    */
-  private function createActivity($user, $type) {
+  private function createActivity($contactID, $type) {
     civicrm_api3('Activity', 'create', [
       'activity_type_id' => $type,
       'source_contact_id' => $this->loggedInUserID, // who did it
-      'target_id' => $this->getContactId($user), // who is it for
+      'target_id' => $contactID, // who is it for
     ]);
-  }
-
-  /**
-   * Gets the contact for a certain user
-   *
-   * @param $user
-   *
-   * @return int
-   */
-  private function getContactId($user) {
-    $result = civicrm_api3('UFMatch', 'getsingle', [
-      'return' => ["contact_id"],
-      'uf_id' => $user->uid,
-    ]);
-    return (int) CRM_Utils_Array::value('contact_id', $result);
   }
 
 }
