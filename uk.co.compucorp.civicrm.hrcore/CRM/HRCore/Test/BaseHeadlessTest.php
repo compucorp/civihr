df3f287ccd11194caa79ebdeebcb1c3ed3aee458
mickadoo <michaeldevery@gmail.com>
2017-05-15 18:39:19 +0100

    PCHR-2149: Move trait to specific test class

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Test/BaseHeadlessTest.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Test/BaseHeadlessTest.php
@@ -2,12 +2,9 @@
 
 use Civi\Test\HeadlessInterface;
 use Civi\Test\TransactionalInterface;
-use CRM_HRCore_Test_Helpers_SessionHelpersTrait as SessionHelpersTrait;
 
 abstract class CRM_HRCore_Test_BaseHeadlessTest extends PHPUnit_Framework_TestCase
   implements HeadlessInterface, TransactionalInterface {
-
-  use SessionHelpersTrait;
 
   public function setUpHeadless() {
     return \Civi\Test::headless()
