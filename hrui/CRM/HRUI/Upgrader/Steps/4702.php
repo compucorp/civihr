c4146ed8f0c4868db39ced674afac6339bdbca8a
Camilo Rodriguez <camilo@compucorp.co.uk>
2017-05-25 20:26:23 +0000

    PCHR-1687: Remove Duplicate 'Miss' Prefix and Reorder Alphabetically
    Included functionality in upgrader for HRCore to remove duplicates for 'Miss',
    in case it finds them. Also, moved reordering of prefixes to hrcore extension
    to make sure they are run in the proper order.

--- hrui/CRM/HRUI/Upgrader/Steps/4702.php
+++ hrui/CRM/HRUI/Upgrader/Steps/4702.php
@@ -5,52 +5,30 @@ trait CRM_HRUI_Upgrader_Steps_4702 {
   /**
    * Upgrader to :
    * 1) Set CiviHR theme by updating the custom CSS URL
-   * 2) Sort Individual Prefixes alphabetically
-   * 3) Rename website to 'Social account'
+   * 2) Move Skype option to top of messenger services option group
+   * 3) Rename 'website' to 'Social account'
    *
    * @return bool
    */
   public function upgrade_4702() {
     $this->up4702_setCustomCSSURL();
-    $this->up4702_sortIndividualPrefixes();
     $this->up4702_moveSkypeTop();
     $this->up4702_websiteToSocialAccountReplacement();
 
     return TRUE;
   }
 
+  /**
+   * Sets CiviHR theme by updating the custom CSS URL
+   */
   private function up4702_setCustomCSSURL() {
-    $customCSSPath = CRM_Core_Resources::singleton()->getUrl('org.civicrm.shoreditch', 'css/custom-civicrm.css');
+    $customCSSPath = CRM_Core_Resources::singleton()
+      ->getUrl('org.civicrm.shoreditch', 'css/custom-civicrm.css');
 
     if (!empty($customCSSPath)) {
       civicrm_api3('Setting', 'create', [
         'customCSSURL' => $customCSSPath,
       ]);
-    }
-  }
-
-  /**
-   * Sorts Individual Prefixes alphabetically
-   */
-  private function up4702_sortIndividualPrefixes() {
-    // fetch all prefixes sorted alphabetically ( by their labels )
-    // hence ('sort' => 'label asc').
-    $prefixes = civicrm_api3('OptionValue', 'get', [
-      'sequential' => 1,
-      'return' => ['id'],
-      'option_group_id' => 'individual_prefix',
-      'options' => ['limit' => 0, 'sort' => 'label asc']
-    ]);
-
-    // update prefix weights
-    $weight = 1;
-    if (!empty($prefixes['values'])) {
-      foreach($prefixes['values'] as $prefix) {
-        civicrm_api3('OptionValue', 'create', [
-          'id' => $prefix['id'],
-          'weight' => $weight++
-        ]);
-      }
     }
   }
 
@@ -72,7 +50,10 @@ trait CRM_HRUI_Upgrader_Steps_4702 {
   private function up4702_websiteToSocialAccountReplacement() {
     civicrm_api3('OptionGroup', 'get', [
       'name' => 'website_type',
-      'api.OptionGroup.create' => ['id' => '$value.id', 'title' => 'Social Account Type'],
+      'api.OptionGroup.create' => [
+        'id' => '$value.id',
+        'title' => 'Social Account Type'
+      ],
     ]);
 
     $wordsToReplace = [
--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1002.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1002.php
@@ -8,21 +8,64 @@ trait CRM_HRCore_Upgrader_Steps_1002 {
    * @return bool
    */
   public function upgrade_1002() {
+
+    $this->up1002_createMissPrefix();
+    $this->up1002_sortIndividualPrefixes();
+
+    return TRUE;
+  }
+
+  /**
+   * Creates Miss Prefix if it doesn't exist.  If it finds duplicates, it
+   * deletes them.
+   */
+  private function up1002_createMissPrefix() {
     $missPrefix = civicrm_api3('OptionValue', 'get', [
+      'sequential' => 1,
       'option_group_id' => 'individual_prefix',
       'name' => 'Miss',
-      'options' => ['limit' => 1],
+      'options' => ['limit' => 0],
     ]);
 
-    if (empty($missPrefix['id'])) {
+    if ($missPrefix['count'] == 0) {
       civicrm_api3('OptionValue', 'create', [
         'option_group_id' => 'individual_prefix',
         'name' => 'Miss',
         'label' => 'Miss',
       ]);
     }
-
-    return TRUE;
+    elseif ($missPrefix['count'] > 1) {
+      // Skip value at position 0, delete the rest.
+      for ($i = 1; $i < $missPrefix['count']; $i++) {
+        civicrm_api3('OptionValue', 'delete', [
+          'id' => $missPrefix['values'][$i]['id']
+        ]);
+      }
+    }
   }
 
+  /**
+   * Sorts Individual Prefixes alphabetically
+   */
+  private function up1002_sortIndividualPrefixes() {
+    // fetch all prefixes sorted alphabetically ( by their labels )
+    // hence ('sort' => 'label asc').
+    $prefixes = civicrm_api3('OptionValue', 'get', [
+      'sequential' => 1,
+      'return' => ['id'],
+      'option_group_id' => 'individual_prefix',
+      'options' => ['limit' => 0, 'sort' => 'label asc']
+    ]);
+
+    // update prefix weights
+    $weight = 1;
+    if (!empty($prefixes['values'])) {
+      foreach ($prefixes['values'] as $prefix) {
+        civicrm_api3('OptionValue', 'create', [
+          'id' => $prefix['id'],
+          'weight' => $weight++
+        ]);
+      }
+    }
+  }
 }
