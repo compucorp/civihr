7b54ddfb64acb45b18ec57a9073c96a8f5737b02
Camilo Rodriguez <camilo@compucorp.co.uk>
2017-05-25 23:15:45 +0000

    PCHR-1687: Fix HREmergency Upgrader
    Upgrader 1000 of hremergency extension ran without errors, but didn't do what
    it was supposed to.  It was trying to add relationship values to an unexisting
    option group, calculated form the custom field column name.
    
    Removed logic to obtain option group name and replaced for real value being
    used.  Also added check to verify values being added don't already exist.

--- org.civicrm.hremergency/CRM/Hremergency/Upgrader/Steps/1000.php
+++ org.civicrm.hremergency/CRM/Hremergency/Upgrader/Steps/1000.php
@@ -12,25 +12,16 @@ trait CRM_Hremergency_Upgrader_Steps_1000 {
     // relationship with employee options to create
     $relationships = ['Friend', 'Spouse', 'Partner'];
 
-    // fetch 'Relationship with employee' option group name
-    $groupName= civicrm_api3('CustomField', 'get', [
-      'sequential' => 1,
-      'return' => ['column_name'],
-      'name' => 'Relationship_with_Employee',
-      'options' => ['limit' => 1]
-    ]);
-
     // create required options
-    if (!empty($groupName['values']['column_name'])) {
-      $groupName = $groupName['values']['column_name'];
-
-      foreach($relationships as $relationship) {
+    foreach($relationships as $relationship) {
+      if (!$this->up1000_valueExists($relationship, 'relationship_with_employee_20150304120408')) {
         civicrm_api3('OptionValue', 'create', [
           'sequential' => 1,
-          'option_group_id' => $groupName,
+          'option_group_id' => 'relationship_with_employee_20150304120408',
           'name' => $relationship,
           'label' => $relationship,
-          'is_active' => 1,
+          'value' => strtolower($relationship),
+          'is_active' => 1
         ]);
       }
     }
@@ -38,4 +29,25 @@ trait CRM_Hremergency_Upgrader_Steps_1000 {
     return TRUE;
   }
 
+  private function up1000_valueExists($value, $groupName) {
+    $result = civicrm_api3('OptionValue', 'get', [
+      'sequential' => 1,
+      'option_group_id' => $groupName,
+      'name' => $value,
+      'label' => $value,
+      'value' => $value,
+      'options' => [
+        'limit' => 0,
+        'or' => [
+          ['label', 'name', 'value']
+        ]
+      ]
+    ]);
+
+    if ($result['count'] > 0) {
+      return TRUE;
+    }
+
+    return FALSE;
+  }
 }
