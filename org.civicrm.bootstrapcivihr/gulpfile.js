75defb641c936c2376c75f1cadcfdd6396051a64
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-05-26 09:00:30 +0200

    PCHR-1439: Tweak bootstrapcivihr gulpfile

--- org.civicrm.bootstrapcivihr/gulpfile.js
+++ org.civicrm.bootstrapcivihr/gulpfile.js
@@ -3,16 +3,14 @@ var bulk = require('gulp-sass-bulk-import');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var postcssPrefix = require('postcss-prefix-selector');
-var postcssDiscardDuplicates = require('postcss-discard-duplicates');
 var stripCssComments = require('gulp-strip-css-comments');
-var transformSelectors = require("gulp-transform-selectors");
+var transformSelectors = require('gulp-transform-selectors');
 var civicrmScssRoot = require('civicrm-scssroot')();
 
 var bootstrapNamespace = '#bootstrap-theme';
 
-gulp.task('sass', function () {
-  civicrmScssRoot.updateSync();
-  gulp.src('scss/*.scss')
+gulp.task('sass', ['sass:sync'], function () {
+  return gulp.src('scss/*.scss')
     .pipe(bulk())
     .pipe(sass({
       outputStyle: 'compressed',
@@ -28,11 +26,15 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('css/'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('scss/**/*.scss', ['sass']);
+gulp.task('sass:sync', function () {
+  civicrmScssRoot.updateSync();
 });
 
-gulp.task('default', ['sass', 'watch']);;
+gulp.task('watch', function () {
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
 
 /**
  * Apply the namespace on html and body elements
@@ -40,13 +42,13 @@ gulp.task('default', ['sass', 'watch']);;
  * @param  {string} selector the current selector to be transformed
  * @return string
  */
-function namespaceRootElements(selector) {
+function namespaceRootElements (selector) {
   var regex = /^(body|html)/;
 
   if (regex.test(selector)) {
     selector = selector.replace(regex, function (match) {
       return match + bootstrapNamespace;
-    }) + ",\n" + selector.replace(regex, bootstrapNamespace);
+    }) + ',\n' + selector.replace(regex, bootstrapNamespace);
   }
 
   return selector;
