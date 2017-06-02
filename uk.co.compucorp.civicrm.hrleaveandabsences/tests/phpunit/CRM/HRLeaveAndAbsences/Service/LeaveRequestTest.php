5366b71ba3e62bb58f1d39f62201cf9a6bcb46d4
Tunbola Ogunwande <tunbolawande@yahoo.com>
2017-06-02 18:02:58 +0100

    PCHR-2167: Remove unneeded tests. Add test that shows that a leave contact cannot update the dates of a sickness request in closed status

--- uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/CRM/HRLeaveAndAbsences/Service/LeaveRequestTest.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/CRM/HRLeaveAndAbsences/Service/LeaveRequestTest.php
@@ -246,9 +246,8 @@ class CRM_HRLeaveAndAbsences_Service_LeaveRequestTest extends BaseHeadlessTest {
    * @expectedException RuntimeException
    * @expectedExceptionMessage You are not allowed to change the request dates
    */
-  public function testCreateThrowsAnExceptionWhenLeaveManagerUpdatesDatesForAClosedSicknessRequest($status) {
+  public function testCreateThrowsAnExceptionWhenLeaveContactUpdatesDatesForAClosedSicknessRequest($status) {
     $params = $this->getDefaultParams([
-      'contact_id' => 5,
       'status_id' => $status,
       'request_type' => LeaveRequest::REQUEST_TYPE_SICKNESS
     ]);
@@ -259,29 +258,7 @@ class CRM_HRLeaveAndAbsences_Service_LeaveRequestTest extends BaseHeadlessTest {
     $params['to_date'] = $toDate->modify('+10 days')->format('YmdHis');
     $params['id'] = $leaveRequest->id;
 
-    $this->getLeaveRequestServiceWhenCurrentUserIsLeaveManager()->create($params, false);
-  }
-
-  /**
-   * @dataProvider closedLeaveRequestStatusesDataProvider
-   *
-   * @expectedException RuntimeException
-   * @expectedExceptionMessage You are not allowed to change the request dates
-   */
-  public function testCreateThrowsAnExceptionWhenAdminUpdatesDatesForAClosedSicknessRequest($status) {
-    $params = $this->getDefaultParams([
-      'contact_id' => 5,
-      'status_id' => $status,
-      'request_type' => LeaveRequest::REQUEST_TYPE_SICKNESS
-    ]);
-
-    $leaveRequest = LeaveRequestFabricator::fabricateWithoutValidation($params);
-
-    $toDate = new DateTime($params['to_date']);
-    $params['to_date'] = $toDate->modify('+10 days')->format('YmdHis');
-    $params['id'] = $leaveRequest->id;
-
-    $this->getLeaveRequestServiceWhenCurrentUserIsAdmin()->create($params, false);
+    $this->getLeaveRequestService()->create($params, false);
   }
 
   /**
