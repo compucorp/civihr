586457c40e2a00c4c0cbda04dcbd618530b20fe6
Davi Alexandre <davi@davialexandre.com.br>
2017-06-02 15:01:17 -0300

    PCHR-2240: Change WorkPattern.getCalendar to accepts multiple Contact ID's at once

--- uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/api/v3/WorkPatternTest.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/tests/phpunit/api/v3/WorkPatternTest.php
@@ -47,6 +47,19 @@ class api_v3_WorkPatternTest extends BaseHeadlessTest {
     civicrm_api3('WorkPattern', 'getCalendar', ['contact_id' => 1, 'period_id' => 99989389121]);
   }
 
+  /**
+   * @expectedException CiviCRM_API3_Exception
+   * @expectedExceptionMessage The contact_id parameter only supports the IN operator
+   *
+   * @dataProvider invalidGetCalendarContactIDOperators
+   */
+  public function testGetCalendarContactIDOnlyAllowTheINOperator($operator) {
+    civicrm_api3('WorkPattern', 'getCalendar', [
+      'contact_id' => [$operator => [1]],
+      'period_id' => 1
+    ]);
+  }
+
   public function testGetCalendarShouldReturnEmptyIfTheGivenContactDoesntExists() {
     $absencePeriod = AbsencePeriodFabricator::fabricate([
       'start_date' => CRM_Utils_Date::processDate('2015-01-05'),
@@ -54,14 +67,15 @@ class api_v3_WorkPatternTest extends BaseHeadlessTest {
     ]);
 
     $result = civicrm_api3('WorkPattern', 'getCalendar', ['contact_id' => 321, 'period_id' => $absencePeriod->id]);
-    $this->assertEmpty($result['values']);
+    $this->assertEmpty($result['values'][0]['calendar']);
   }
 
   /**
    * Just an small test to make sure it can call the service and return the dates
    */
-  public function testGetCalendarUsesTheWorkPatternCalendarService() {
-    $contact = ContactFabricator::fabricate();
+  public function testGetCalendarCanReturnCalendarsForMultipleContacts() {
+    $contact1 = ContactFabricator::fabricate();
+    $contact2 = ContactFabricator::fabricate();
 
     // 15 days absence period
     $absencePeriod = AbsencePeriodFabricator::fabricate([
@@ -70,44 +84,74 @@ class api_v3_WorkPatternTest extends BaseHeadlessTest {
     ]);
 
     WorkPatternFabricator::fabricateWithA40HourWorkWeek(['is_default' => true]);
-    $multipleWeekWorkPattern = WorkPatternFabricator::fabricateWithTwoWeeksAnd31AndHalfHours();
 
-    // contract covers the whole period
+    // contracts covering the whole period
     HRJobContractFabricator::fabricate(
-      ['contact_id' => $contact['id']],
+      ['contact_id' => $contact1['id']],
       [
         'period_start_date' => '2015-01-05',
         'period_end_date' => '2015-01-19'
       ]
     );
 
-    // work pattern is effective only after the contract end
-    ContactWorkPatternFabricator::fabricate([
-      'contact_id' => $contact['id'],
-      'pattern_id' => $multipleWeekWorkPattern->id,
-      'effective_date' => CRM_Utils_Date::processDate('2015-02-05'),
-    ]);
+    HRJobContractFabricator::fabricate(
+      ['contact_id' => $contact2['id']],
+      [
+        'period_start_date' => '2015-01-05',
+        'period_end_date' => '2015-01-19'
+      ]
+    );
 
     $workDayTypes = $this->getWorkDayTypeOptionsArray();
 
-    $calendarDates = civicrm_api3('WorkPattern', 'getCalendar', [
+    $calendars = civicrm_api3('WorkPattern', 'getCalendar', [
       'period_id' => $absencePeriod->id,
-      'contact_id' => $contact['id']
+      'contact_id' => ['IN' => [$contact1['id'], $contact2['id']]]
     ])['values'];
+
+    $this->assertCount(2, $calendars);
+
+    // Since there's not guarantee about the order on which the calendars will
+    // be returned, we get all the contacts ID's from the result, put them in an
+    // array and then check if this array only contains the IDs we're expecting
+    $returnedContactsIDs = array_column($calendars, 'contact_id');
+    $this->assertContains($contact1['id'], $returnedContactsIDs);
+    $this->assertContains($contact2['id'], $returnedContactsIDs);
 
     // Asserting ALL the dates would make the test too big, so we assert the
     // total number of dates, the first and last ones and a few dates in the
     // middle, assuming all the other will be correct.
-    $this->assertCount(15, $calendarDates);
-    $this->assertEquals('2015-01-05', $calendarDates[0]['date']);
-    $this->assertEquals($workDayTypes['working_day'], $calendarDates[0]['type']);
-    $this->assertEquals('2015-01-08', $calendarDates[3]['date']);
-    $this->assertEquals($workDayTypes['working_day'], $calendarDates[3]['type']);
-    $this->assertEquals('2015-01-11', $calendarDates[6]['date']);
-    $this->assertEquals($workDayTypes['weekend'], $calendarDates[6]['type']);
-    $this->assertEquals('2015-01-16', $calendarDates[11]['date']);
-    $this->assertEquals($workDayTypes['working_day'], $calendarDates[11]['type']);
-    $this->assertEquals('2015-01-19', $calendarDates[14]['date']);
-    $this->assertEquals($workDayTypes['working_day'], $calendarDates[14]['type']);
+    // Since both contacts have the same work pattern, both calendars should be equal
+    foreach($calendars as $calendar) {
+      $this->assertEquals('2015-01-05', $calendar['calendar'][0]['date']);
+      $this->assertEquals($workDayTypes['working_day'], $calendar['calendar'][0]['type']);
+      $this->assertEquals('2015-01-08', $calendar['calendar'][3]['date']);
+      $this->assertEquals($workDayTypes['working_day'], $calendar['calendar'][3]['type']);
+      $this->assertEquals('2015-01-11', $calendar['calendar'][6]['date']);
+      $this->assertEquals($workDayTypes['weekend'], $calendar['calendar'][6]['type']);
+      $this->assertEquals('2015-01-16', $calendar['calendar'][11]['date']);
+      $this->assertEquals($workDayTypes['working_day'], $calendar['calendar'][11]['type']);
+      $this->assertEquals('2015-01-19', $calendar['calendar'][14]['date']);
+      $this->assertEquals($workDayTypes['working_day'], $calendar['calendar'][14]['type']);
+    }
+
+  }
+
+  public function invalidGetCalendarContactIDOperators() {
+    return [
+      ['>'],
+      ['>='],
+      ['<='],
+      ['<'],
+      ['<>'],
+      ['!='],
+      ['BETWEEN'],
+      ['NOT BETWEEN'],
+      ['LIKE'],
+      ['NOT LIKE'],
+      ['NOT IN'],
+      ['IS NULL'],
+      ['IS NOT NULL'],
+    ];
   }
 }
