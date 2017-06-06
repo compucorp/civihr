6756a467b3aacde058b1e4dbf888997f6d9d5331
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-06-06 21:29:18 +0200

    PCHR-2240: Update calendar mocked data

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/mocks/data/work-pattern-data.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/mocks/data/work-pattern-data.js
@@ -1,33 +1,69 @@
+/* eslint-env amd */
+
 define(function () {
   var mockData = {
     daysData: {
       'is_error': 0,
       'version': 3,
-      'count': 3,
+      'count': 2,
       'values': [
         {
-          'date': '2016-01-05',
-          'type': {
-            'value': 2,
-            'name': 'working_day',
-            'label': 'Working day'
-          }
+          'contact_id': 204,
+          'calendar': [
+            {
+              'date': '2016-01-05',
+              'type': {
+                'value': 2,
+                'name': 'working_day',
+                'label': 'Working day'
+              }
+            },
+            {
+              'date': '2016-02-02',
+              'type': {
+                'value': 1,
+                'name': 'non_working_day',
+                'label': 'Non-working day'
+              }
+            },
+            {
+              'date': '2016-03-03',
+              'type': {
+                'value': 3,
+                'name': 'weekend',
+                'label': 'Weekend'
+              }
+            }
+          ]
         },
         {
-          'date': '2016-02-02',
-          'type': {
-            'value': 1,
-            'name': 'non_working_day',
-            'label': 'Non-working day'
-          }
-        },
-        {
-          'date': '2016-03-03',
-          'type': {
-            'value': 3,
-            'name': 'weekend',
-            'label': 'Weekend'
-          }
+          'contact_id': 205,
+          'calendar': [
+            {
+              'date': '2016-01-05',
+              'type': {
+                'value': 3,
+                'name': 'weekend',
+                'label': 'Weekend'
+              }
+            },
+            {
+              'date': '2016-02-02',
+              'type': {
+                'value': 2,
+                'name': 'working_day',
+                'label': 'Working day'
+              }
+            },
+            {
+              'date': '2016-03-03',
+              'type': {
+                'value': 1,
+                'name': 'non_working_day',
+                'label': 'Non-working day'
+              }
+            }
+          ]
         }
       ]
     }
@@ -37,5 +73,5 @@ define(function () {
     daysData: function () {
       return mockData.daysData;
     }
-  }
+  };
 });
