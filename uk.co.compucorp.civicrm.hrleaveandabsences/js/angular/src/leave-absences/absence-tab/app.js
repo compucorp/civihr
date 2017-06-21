fd8a47458a07bc95e7d81607ef1d3639aab39c0a
Igor Pavlov <pavlov.org@gmail.com>
2017-06-21 16:45:11 +0100

    PCHR-2008: Add annual entitlements component

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/absence-tab/app.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/absence-tab/app.js
@@ -12,11 +12,13 @@ define([
   'leave-absences/absence-tab/components/absence-tab-entitlements',
   'leave-absences/absence-tab/components/absence-tab-work-patterns',
   'leave-absences/absence-tab/components/contract-entitlements',
+  'leave-absences/absence-tab/components/annual-entitlements',
   'leave-absences/shared/components/staff-leave-calendar',
   'leave-absences/shared/directives/leave-request-popup',
   'leave-absences/shared/models/calendar-model',
   'leave-absences/shared/models/leave-request-model',
   'leave-absences/shared/models/absence-type-model',
+  'leave-absences/shared/models/entitlement-model',
   'leave-absences/shared/modules/shared-settings'
 ], function (angular) {
   angular.module('absence-tab', [
@@ -25,6 +27,7 @@ define([
     'textAngular',
     'common.angularDate',
     'common.directives',
+    'common.mocks',
     'absence-tab.config',
     'absence-tab.components',
     'leave-absences.components',
