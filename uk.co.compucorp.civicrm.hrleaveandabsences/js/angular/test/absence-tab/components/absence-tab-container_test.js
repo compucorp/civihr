f9e9549bf09df5e7e53945a414bc13e95a1fd73c
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-06-29 18:36:37 +0200

    PCHR-2004: Fix absence-tab-container test suite

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/absence-tab/components/absence-tab-container_test.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/absence-tab/components/absence-tab-container_test.js
@@ -9,18 +9,15 @@
     'use strict';
 
     describe('absenceTabContainer', function () {
-      var $compile, $httpBackend, $log, $rootScope, component;
+      var $compile, $log, $rootScope, $templateCache, settings, component;
 
       beforeEach(module('leave-absences.templates', 'absence-tab'));
-      beforeEach(inject(function (_$compile_, _$httpBackend_, _$log_, _$rootScope_) {
+      beforeEach(inject(function (_$compile_, _$log_, _$rootScope_, _$templateCache_, _settings_) {
         $compile = _$compile_;
-        $httpBackend = _$httpBackend_;
         $log = _$log_;
         $rootScope = _$rootScope_;
-
-        // Catch any request created by the component belonging to
-        // first tab enabled by default
-        $httpBackend.whenGET(/./).respond(true);
+        $templateCache = _$templateCache_;
+        settings = _settings_;
 
         spyOn($log, 'debug');
         compileComponent();
@@ -33,11 +30,30 @@
       function compileComponent () {
         var $scope = $rootScope.$new();
 
+        disableUibTab();
+
         component = angular.element('<absence-tab-container></absence-tab-container>');
         $compile(component)($scope);
         $scope.$digest();
 
         component.controller('absenceTabContainer');
+      }
+
+      /**
+       * Disables the uib-tab directive, by simply renaming the tag
+       *
+       * The directive automatically compiled and ran the component in the
+       * tab enabled by default, causing a whole lot of errors in this set
+       *
+       * This is a brute-force approach to solve the issues and let the test
+       * focus only on the component rather than the content of the tabs
+       *
+       */
+      function disableUibTab () {
+        var tplPath = settings.pathTpl + 'components/absence-tab-container.html';
+        var tpl = $templateCache.get(tplPath);
+
+        $templateCache.put(tplPath, tpl.replace(/uib-tab/g, 'disabled-uib-tab'));
       }
     });
   });
