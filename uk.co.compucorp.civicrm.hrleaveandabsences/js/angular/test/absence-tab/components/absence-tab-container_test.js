2cf821ccd7cb68de585b3ba4eab15ac6959df681
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-07-03 10:03:28 +0200

    PCHR-2004: Remove unnecessary markup edit in absenceTabContainer test

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/absence-tab/components/absence-tab-container_test.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/absence-tab/components/absence-tab-container_test.js
@@ -9,15 +9,13 @@
     'use strict';
 
     describe('absenceTabContainer', function () {
-      var $componentController, $log, $rootScope, $templateCache, settings;
+      var $componentController, $log, $rootScope;
 
-      beforeEach(module('leave-absences.templates', 'absence-tab'));
-      beforeEach(inject(function (_$componentController_, _$log_, _$rootScope_, _$templateCache_, _settings_) {
+      beforeEach(module('absence-tab'));
+      beforeEach(inject(function (_$componentController_, _$log_, _$rootScope_) {
         $componentController = _$componentController_;
         $log = _$log_;
         $rootScope = _$rootScope_;
-        $templateCache = _$templateCache_;
-        settings = _settings_;
 
         spyOn($log, 'debug');
         compileComponent();
@@ -28,27 +26,8 @@
       });
 
       function compileComponent () {
-        disableUibTab();
-
         $componentController('absenceTabContainer');
         $rootScope.$digest();
-      }
-
-      /**
-       * Disables the uib-tab directive, by simply renaming the tag
-       *
-       * The directive automatically compiled and ran the component in the
-       * tab enabled by default, causing a whole lot of errors in this set
-       *
-       * This is a brute-force approach to solve the issues and let the test
-       * focus only on the component rather than the content of the tabs
-       *
-       */
-      function disableUibTab () {
-        var tplPath = settings.pathTpl + 'components/absence-tab-container.html';
-        var tpl = $templateCache.get(tplPath);
-
-        $templateCache.put(tplPath, tpl.replace(/uib-tab/g, 'disabled-uib-tab'));
       }
     });
   });
