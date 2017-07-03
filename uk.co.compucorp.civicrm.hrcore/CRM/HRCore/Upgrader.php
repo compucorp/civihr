fdeeb27e31a077f5d0c6e0fa5d5d8725de040f4c
Camilo Rodriguez <camilo@compucorp.co.uk>
2017-07-03 15:00:37 +0000

    PCHR-2340: Set 'Home' as Default Location and Primary Address
    Added upgrader step 1004 on hrcore extensions, which does the following:
    - Sets 'Home' location as default location and marks it as reserved.
    - Sets all 'Home' addresses as primary for contacts who have them.

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader.php
@@ -9,6 +9,7 @@ class CRM_HRCore_Upgrader extends CRM_HRCore_Upgrader_Base {
   use CRM_HRCore_Upgrader_Steps_1001;
   use CRM_HRCore_Upgrader_Steps_1002;
   use CRM_HRCore_Upgrader_Steps_1003;
+  use CRM_HRCore_Upgrader_Steps_1004;
 
   /**
    * @var array
---
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1004.php
@@ -0,0 +1,56 @@
+<?php
+
+trait CRM_HRCore_Upgrader_Steps_1004 {
+
+  /**
+   * Upgrader to set 'Home' as default location and set all 'Home' addresses as
+   * primary.
+   *
+   * @return bool
+   */
+  public function upgrade_1004() {
+    $this->up1004_setHomeLocationAsDefault();
+    $this->up1004_setAllHomeAddressesAsPrimary();
+
+    return true;
+  }
+
+  private function up1004_setHomeLocationAsDefault() {
+    civicrm_api3('LocationType', 'get', [
+      'sequential' => 1,
+      'name' => 'Home',
+      'api.LocationType.create' => [
+        'id' => '$value.id',
+        'is_default' => 1,
+        'is_reserved' => 1,
+      ],
+    ]);
+  }
+
+  private function up1004_setAllHomeAddressesAsPrimary() {
+    $homeLocation = civicrm_api3('LocationType', 'getsingle', [
+      'name' => "Home",
+    ]);
+
+    $contactsWithNonPrimaryHomeAddress = civicrm_api3('Address', 'get', [
+      'sequential' => 1,
+      'return' => array('contact_id'),
+      'location_type_id' => $homeLocation['id'],
+      'is_primary' => 0,
+    ]);
+
+    $contactIDs = [];
+    foreach ($contactsWithNonPrimaryHomeAddress['values'] as $currentContact) {
+      $contactIDs[] = $currentContact['contact_id'];
+    }
+
+    if (!empty($contactIDs)) {
+      $query = "
+        UPDATE civicrm_address
+        SET is_primary = CASE WHEN location_type_id = {$homeLocation['id']} THEN 1 ELSE 0 END
+        WHERE contact_id IN (" . implode(',', $contactIDs) . ")
+      ";
+      CRM_Core_DAO::executeQuery($query);
+    }
+  }
+}
