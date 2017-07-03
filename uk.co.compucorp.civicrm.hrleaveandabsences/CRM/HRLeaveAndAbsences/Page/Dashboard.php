360885fecaad9218e626f233859b7ab13d2c7f77
Igor Pavlov <pavlov.org@gmail.com>
2017-07-03 14:48:36 +0100

    PCHR-2031: PR Feedback

--- uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Page/Dashboard.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Page/Dashboard.php
@@ -11,16 +11,11 @@ class CRM_HRLeaveAndAbsences_Page_Dashboard extends CRM_Core_Page {
       ->addStyleFile('uk.co.compucorp.civicrm.hrleaveandabsences', 'css/leaveandabsence.css')
       ->addScriptFile('uk.co.compucorp.civicrm.hrleaveandabsences',
         CRM_Core_Config::singleton()->debug ? 'js/angular/src/admin-dashboard.js' : 'js/angular/dist/admin-dashboard.min.js', 1010)
-      ->addSettingsFactory(function () {
-        return array(
-          'vars' => array(
-            'leaveAndAbsences' => array(
-              'baseURL' => CRM_Core_Resources::singleton()->getUrl('uk.co.compucorp.civicrm.hrleaveandabsences'),
-              'attachmentToken' => CRM_Core_Page_AJAX_Attachment::createToken()
-            )
-          )
-        );
-      });
+      ->addVars('leaveAndAbsences', [
+        'baseURL' => CRM_Core_Resources::singleton()->getUrl('uk.co.compucorp.civicrm.hrleaveandabsences'),
+        'loggedInUserId' => CRM_Core_Session::getLoggedInContactID(),
+        'attachmentToken' => CRM_Core_Page_AJAX_Attachment::createToken()
+      ]);
 
     parent::run();
   }
--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/dashboard/components/admin-dashboard-container.spec.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/dashboard/components/admin-dashboard-container.spec.js
@@ -7,12 +7,12 @@ define([
   'use strict';
 
   describe('adminDashboardContainer', function () {
-    var $compile, $log, $rootScope, component, controller;
+    var $componentController, $log, $rootScope;
 
-    beforeEach(module('leave-absences.templates', 'admin-dashboard'));
+    beforeEach(module('admin-dashboard'));
 
-    beforeEach(inject(function (_$compile_, _$log_, _$rootScope_) {
-      $compile = _$compile_;
+    beforeEach(inject(function (_$componentController_, _$log_, _$rootScope_) {
+      $componentController = _$componentController_;
       $log = _$log_;
       $rootScope = _$rootScope_;
 
@@ -21,22 +21,13 @@ define([
       compileComponent();
     }));
 
-    it('has a controller', function () {
-      expect(controller).toBeDefined();
-    });
-
     it('is initialized', function () {
       expect($log.debug).toHaveBeenCalled();
     });
 
     function compileComponent () {
-      var $scope = $rootScope.$new();
-
-      component = angular.element('<admin-dashboard-container></admin-dashboard-container>');
-      $compile(component)($scope);
-      $scope.$digest();
-
-      controller = component.controller('adminDashboardContainer');
+      $componentController('adminDashboardContainer');
+      $rootScope.$digest();
     }
   });
 });
