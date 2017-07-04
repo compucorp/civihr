575db0788df7f88e630e26ccfd3afbcf3f8a1e16
Camilo Rodriguez <camilo@compucorp.co.uk>
2017-07-04 11:45:00 +0000

    PCHR-2340: Implement Dispatcher for Pre Hook on HRCore Extension
    Followed provided guidelines to implement civicrm_pre hook using dispatchers,
    by creating the CRM_HRCore_Hooks_Pre_PrimaryAddressSetter class which will
    handle the case of creating/editing an address, to make sure 'Home' location
    types are always set as primary.

--- uk.co.compucorp.civicrm.hrcore/hrcore.php
+++ uk.co.compucorp.civicrm.hrcore/hrcore.php
@@ -139,18 +139,20 @@ function hrcore_civicrm_apiWrappers(&$wrappers, $apiRequest) {
  * Implementation of hook_civicrm_pre hook.
  *
  * @param string $op
+ *   Operation being done
  * @param string $objectName
+ *   Name of the object on which the operation is being done
  * @param int $objectId
+ *   ID of the record the object instantiates
  * @param array $params
+ *   Parameter array being used to call the operation
  */
 function hrcore_civicrm_pre($op, $objectName, $objectId, &$params) {
-  if ($objectName === 'Address' && ($op === 'edit' || $op === 'create')) {
-    $homeLocation = civicrm_api3('LocationType', 'getsingle', [
-      'name' => 'Home',
-    ]);
+  $listeners = [
+    new CRM_HRCore_Hook_Pre_PrimaryAddressSetter(),
+  ];
 
-    if ($params['location_type_id'] == $homeLocation['id']) {
-      $params['is_primary'] = 1;
-    }
+  foreach ($listeners as $currentListener) {
+    $currentListener->handle($op, $objectName, $objectId, $params);
   }
 }
