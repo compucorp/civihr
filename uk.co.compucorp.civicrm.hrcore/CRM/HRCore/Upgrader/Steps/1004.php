81aabb7d66780823ad08050bfb84ed440fb31071
Camilo Rodriguez <camilo@compucorp.co.uk>
2017-07-04 13:51:38 +0000

    PCHR-2340: Use API to Set is_primary Flag on 'Home' Addresses

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1004.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1004.php
@@ -28,29 +28,11 @@ trait CRM_HRCore_Upgrader_Steps_1004 {
   }
 
   private function up1004_setAllHomeAddressesAsPrimary() {
-    $homeLocation = civicrm_api3('LocationType', 'getsingle', [
-      'name' => 'Home',
-    ]);
-
-    $contactsWithNonPrimaryHomeAddress = civicrm_api3('Address', 'get', [
+    civicrm_api3('Address', 'get', [
       'sequential' => 1,
-      'return' => ['contact_id'],
-      'location_type_id' => $homeLocation['id'],
+      'location_type_id' => 'Home',
       'is_primary' => 0,
+      'api.Address.create' => ['id' => '$value.id', 'is_primary' => 1],
     ]);
-
-    $contactIDs = [];
-    foreach ($contactsWithNonPrimaryHomeAddress['values'] as $currentContact) {
-      $contactIDs[] = $currentContact['contact_id'];
-    }
-
-    if (!empty($contactIDs)) {
-      $query = "
-        UPDATE civicrm_address
-        SET is_primary = CASE WHEN location_type_id = {$homeLocation['id']} THEN 1 ELSE 0 END
-        WHERE contact_id IN (" . implode(',', $contactIDs) . ")
-      ";
-      CRM_Core_DAO::executeQuery($query);
-    }
   }
 }
