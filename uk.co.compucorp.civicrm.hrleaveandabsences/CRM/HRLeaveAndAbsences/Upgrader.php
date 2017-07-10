bc0137a4056b8cb069636b38e99f06954a7e4b0c
Tunbola Ogunwande <tunbolawande@yahoo.com>
2017-07-10 14:14:46 +0100

    PCHR-2364: Execute upgraders during extension install.

--- uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader.php
@@ -20,6 +20,7 @@ class CRM_HRLeaveAndAbsences_Upgrader extends CRM_HRLeaveAndAbsences_Upgrader_Ba
    */
   public function install() {
     $this->processXMLInstallationFiles();
+    $this->runAllUpgraders();
   }
 
   /**
@@ -33,6 +34,19 @@ class CRM_HRLeaveAndAbsences_Upgrader extends CRM_HRLeaveAndAbsences_Upgrader_Ba
         foreach ($files as $file) {
           $this->executeCustomDataFileByAbsPath($file);
         }
+      }
+    }
+  }
+
+  /**
+   * Runs all the upgrader methods when installing the extension
+   */
+  private function runAllUpgraders() {
+    $revisions = $this->getRevisions();
+    foreach ($revisions as $revision) {
+      $methodName = 'upgrade_' . $revision;
+      if (is_callable([$this, $methodName])) {
+        $this->{$methodName}();
       }
     }
   }
