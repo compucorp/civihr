e590ceb1f2eb3a08fd5657a2531c5207e33a3bdb
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-07-15 15:02:53 +0200

    PCHR-2240: Remove duplicated tests in manager-leave-calendar test suite

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/shared/components/manager-leave-calendar.spec.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/shared/components/manager-leave-calendar.spec.js
@@ -23,9 +23,7 @@
 
     describe('managerLeaveCalendar', function () {
       var $componentController, $q, $log, $rootScope, controller, $provide,
-        OptionGroup, OptionGroupAPIMock, ContactAPIMock, AbsencePeriod,
-        Contact, LeaveRequest, WorkPatternAPI;
-      var isAdmin = false; // change this value to true before testing for Admin role
+        OptionGroup, OptionGroupAPIMock, ContactAPIMock, AbsencePeriod, Contact;
 
       beforeEach(module('leave-absences.templates', 'leave-absences.mocks', 'manager-leave', function (_$provide_) {
         $provide = _$provide_;
@@ -37,7 +35,7 @@
         $provide.value('LeaveRequestAPI', LeaveRequestAPIMock);
         $provide.value('PublicHolidayAPI', PublicHolidayAPIMock);
         $provide.value('WorkPatternAPI', WorkPatternAPIMock);
-        $provide.value('checkPermissions', function () { return $q.resolve(isAdmin); });
+        $provide.value('checkPermissions', function () { return $q.resolve(false); });
       }));
 
       beforeEach(inject(['api.contact.mock', function (_ContactAPIMock_) {
@@ -47,17 +45,15 @@
       beforeEach(inject(function (
         _$componentController_, _$q_, _$log_, _$rootScope_,
         _OptionGroup_, _OptionGroupAPIMock_, _AbsencePeriod_, _Contact_,
-        _LeaveRequest_, _WorkPatternAPI_, ContactInstance) {
+        ContactInstance) {
         $componentController = _$componentController_;
         $q = _$q_;
         $log = _$log_;
         $rootScope = _$rootScope_;
         AbsencePeriod = _AbsencePeriod_;
         Contact = _Contact_;
-        LeaveRequest = _LeaveRequest_;
         OptionGroup = _OptionGroup_;
         OptionGroupAPIMock = _OptionGroupAPIMock_;
-        WorkPatternAPI = _WorkPatternAPI_;
 
         spyOn($log, 'debug');
         spyOn(Contact, 'all').and.returnValue($q.resolve(ContactAPIMock.mockedContacts()));
@@ -74,32 +70,6 @@
           return $q.resolve(data);
         });
 
-        /**
-         * This is unfortunately a hack to make sure that
-         * all mocked contacts have a mocked calendar
-         *
-         * It takes the mocked WorkPattern.getcalendar response and expands it
-         * by making copies of the first mocked calendar and assigning it to
-         * each mocked contacts.
-         *
-         * The ideal solution would be to decouple the mocked data from the
-         * `api.contact.mock` (common/mocks/services/api/contact-mock) and put it
-         * in a separate file, so that mocks/data/work-pattern-data can inject it
-         * and dynamically generate the data by looping through all mocked contacts
-         */
-        spyOn(WorkPatternAPI, 'getCalendar').and.callFake(function () {
-          var mockedGetCalendarResponse = _.clone(workPatternMocked.getCalendar);
-          var singleMockedCalendarTemplate = mockedGetCalendarResponse.values[0];
-
-          return $q.resolve(_.assign(mockedGetCalendarResponse, {
-            values: ContactAPIMock.mockedContacts().list.map(function (contact) {
-              return _.assign(_.clone(singleMockedCalendarTemplate), {
-                contact_id: contact.id
-              });
-            })
-          }));
-        });
-
         compileComponent();
       }));
 
@@ -107,23 +77,7 @@
         expect($log.debug).toHaveBeenCalled();
       });
 
-      it('has the legend expanded', function () {
-        expect(controller.legendCollapsed).toBe(false);
-      });
-
       describe('on init', function () {
-        it('loader is hidden', function () {
-          expect(controller.loading.page).toBe(false);
-        });
-
-        it('absence periods have loaded', function () {
-          expect(controller.absencePeriods.length).not.toBe(0);
-        });
-
-        it('absence types have loaded', function () {
-          expect(controller.absenceTypes.length).not.toBe(0);
-        });
-
         it('regions have loaded', function () {
           expect(controller.regions).toEqual(optionGroupMock.getCollection('hrjc_region'));
         });
@@ -154,60 +108,6 @@
           _.each(controller.managedContacts, function (contact) {
             expect(Object.keys(contact.calendarData[0]).length).not.toBe(0);
           });
-        });
-      });
-
-      describe('isPublicHoliday', function () {
-        var date;
-
-        beforeEach(function () {
-          date = publicHolidayData.all().values[0].date;
-        });
-
-        it('checks whether date is a public holiday', function () {
-          expect(controller.isPublicHoliday(date)).toBe(true);
-        });
-      });
-
-      describe('period label', function () {
-        var label, period;
-
-        describe('when the period is current', function () {
-          beforeEach(function () {
-            period = _(controller.absencePeriods).find(function (period) {
-              return period.current;
-            });
-            label = controller.labelPeriod(period);
-          });
-
-          it('adds Current Period to the label', function () {
-            expect(label).toBe('Current Period (' + period.title + ')');
-          });
-        });
-
-        describe('when the period is not current', function () {
-          beforeEach(function () {
-            period = _(controller.absencePeriods).filter(function (period) {
-              return !period.current;
-            }).sample();
-            label = controller.labelPeriod(period);
-          });
-
-          it('returns the title as it is', function () {
-            expect(label).toBe(period.title);
-          });
-        });
-      });
-
-      describe('getDayName', function () {
-        var anySunday;
-
-        beforeEach(function () {
-          anySunday = '2017/02/05';
-        });
-
-        it('returns day name of a date', function () {
-          expect(controller.getDayName(anySunday)).toBe('Sun');
         });
       });
 
@@ -247,195 +147,9 @@
         });
       });
 
-      describe('refresh', function () {
-        it('page loading is true initially', function () {
-          controller.refresh();
-          expect(controller.loading.calendar).toBe(true);
-        });
-
-        describe('after data load is complete', function () {
-          beforeEach(function () {
-            controller.refresh();
-            $rootScope.$digest();
-          });
-
-          it('page loading is false', function () {
-            expect(controller.loading.calendar).toBe(false);
-          });
-        });
-
-        describe('isWeekend', function () {
-          beforeEach(function () {
-            controller.refresh();
-            $rootScope.$digest();
-          });
-
-          it('is set', function () {
-            _.each(controller.managedContacts, function (contact) {
-              var date = getDateFromCalendar(contact, 'weekend');
-              expect(date.UI.isWeekend).toBe(true);
-            });
-          });
-        });
-
-        describe('isNonWorkingDay', function () {
-          beforeEach(function () {
-            controller.refresh();
-            $rootScope.$digest();
-          });
-
-          it('is set', function () {
-            _.each(controller.managedContacts, function (contact) {
-              var date = getDateFromCalendar(contact, 'non_working_day');
-              expect(date.UI.isNonWorkingDay).toBe(true);
-            });
-          });
-        });
-
-        describe('isPublicHoliday', function () {
-          var dateObj;
-
-          beforeEach(function () {
-            spyOn(controller, 'isPublicHoliday').and.returnValue(true);
-            controller.refresh();
-            $rootScope.$digest();
-          });
-
-          it('is set', function () {
-            _.each(controller.managedContacts, function (contact) {
-              // any date
-              dateObj = contact.calendarData[0].data[0];
-              expect(dateObj.UI.isPublicHoliday).toBe(true);
-            });
-          });
-        });
-
-        describe('refresh', function () {
-          var dateObj,
-            workPattern,
-            leaveRequest;
-
-          beforeEach(function () {
-            workPattern = workPatternMocked.getCalendar;
-            leaveRequest = _.clone(leaveRequestData.singleDataSuccess().values[0]);
-            workPattern.values[0].calendar[0].date = leaveRequest.from_date;
-          });
-
-          describe('when leave request is not approved', function () {
-            beforeEach(function () {
-              var status = optionGroupMock.specificObject(
-                'hrleaveandabsences_leave_request_status', 'name', 'awaiting_approval');
-
-              leaveRequest.contact_id = '203';
-              leaveRequest.status_id = status.value;
-              leaveRequest.balance_change = -1;
-              commonSetup();
-            });
-
-            it('isRequested flag is true', function () {
-              _.each(controller.managedContacts, function (contact) {
-                dateObj = getDate(contact, leaveRequest.from_date);
-                expect(dateObj.UI.isRequested).toBe(true);
-              });
-            });
-
-            it('styles are fetched', function () {
-              var color = _.find(controller.absenceTypes, function (absenceType) {
-                return absenceType.id === leaveRequest.type_id;
-              }).color;
-
-              _.each(controller.managedContacts, function (contact) {
-                dateObj = getDate(contact, leaveRequest.from_date);
-                expect(dateObj.UI.styles).toEqual({
-                  backgroundColor: color,
-                  borderColor: color
-                });
-              });
-            });
-          });
-
-          describe('when leave request is for half day am', function () {
-            beforeEach(function () {
-              var halfDayAMValue = optionGroupMock.specificObject(
-                'hrleaveandabsences_leave_request_day_type', 'name', 'half_day_am').value;
-
-              leaveRequest.from_date_type = halfDayAMValue;
-              commonSetup();
-            });
-
-            it('AM flag is set', function () {
-              expect(dateObj.UI.isAM).toBe(true);
-            });
-          });
-
-          describe('when leave request is for half day pm', function () {
-            beforeEach(function () {
-              var halfDayPMValue = optionGroupMock.specificObject(
-                'hrleaveandabsences_leave_request_day_type', 'name', 'half_day_pm').value;
-
-              leaveRequest.from_date_type = halfDayPMValue;
-              commonSetup();
-            });
-
-            it('PM flag is set', function () {
-              expect(dateObj.UI.isPM).toBe(true);
-            });
-          });
-
-          describe('when balance change is positive', function () {
-            beforeEach(function () {
-              leaveRequest.balance_change = 2;
-              commonSetup();
-            });
-
-            it('AccruedTOIL flag is set', function () {
-              expect(dateObj.UI.isAccruedTOIL).toBe(true);
-            });
-          });
-
-          function commonSetup () {
-            spyOn(LeaveRequest, 'all').and.callFake(function () {
-              return $q.resolve({
-                list: [leaveRequest]
-              });
-            });
-
-            controller.refresh();
-            $rootScope.$digest();
-          }
-        });
-      });
-
       function compileComponent () {
         controller = $componentController('managerLeaveCalendar', null, { contactId: CRM.vars.leaveAndAbsences.contactId });
         $rootScope.$digest();
-      }
-
-      function getDate (contact, dateStr) {
-        var date;
-
-        _.each(contact.calendarData, function (month) {
-          _.each(month.data, function (dateObj) {
-            if (dateObj.date === dateStr) {
-              date = dateObj;
-            }
-          });
-        });
-
-        return date;
-      }
-
-      function getDateFromCalendar (contact, dayType) {
-        var date;
-        _.each(contact.calendarData, function (month) {
-          _.each(month.data, function (dateObj) {
-            if (dateObj.date === helper.getDate(dayType).date) {
-              date = dateObj;
-            }
-          });
-        });
-
-        return date;
       }
     });
   });
