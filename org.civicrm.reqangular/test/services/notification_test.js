4ca923fe691bac82eb17ae5590f7dcfe2cdb1064
Buddha Man Nepali <bmnepali980@gmail.com>
2017-07-20 13:57:24 +0545

    PCHR-2309: Implements PR review feedbacks

--- org.civicrm.reqangular/test/services/notification_test.js
+++ org.civicrm.reqangular/test/services/notification_test.js
@@ -25,29 +25,41 @@
       }));
 
       describe('alert()', function () {
-        it('calls CRM.alert to display alert notification', function () {
+        beforeEach(function () {
           notification.alert(title, body, options);
+        });
+
+        it('calls CRM.alert to display alert notification', function () {
           expect(CRM.alert).toHaveBeenCalledWith(body, title, 'alert', options);
         });
       });
 
       describe('success()', function () {
-        it('calls CRM.alert to display success notification', function () {
+        beforeEach(function () {
           notification.success(title, body, options);
+        });
+
+        it('calls CRM.alert to display success notification', function () {
           expect(CRM.alert).toHaveBeenCalledWith(body, title, 'success', options);
         });
       });
 
       describe('info()', function () {
-        it('calls CRM.alert to display info notification', function () {
+        beforeEach(function () {
           notification.info(title, body, options);
+        });
+
+        it('calls CRM.alert to display info notification', function () {
           expect(CRM.alert).toHaveBeenCalledWith(body, title, 'info', options);
         });
       });
 
       describe('error()', function () {
-        it('calls CRM.alert to display error notification', function () {
+        beforeEach(function () {
           notification.error(title, body, options);
+        });
+
+        it('calls CRM.alert to display error notification', function () {
           expect(CRM.alert).toHaveBeenCalledWith(body, title, 'error', options);
         });
       });
