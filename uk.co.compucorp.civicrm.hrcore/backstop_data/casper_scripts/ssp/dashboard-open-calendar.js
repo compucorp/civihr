f6dc31c0bd17ce4305eba8ab7a278f915a9cc916
Debarshi Bhaumik <deb1990@gmail.com>
2017-07-20 21:14:05 +0530

    PCHR-2417: Remove backstop tests not required for 1.7

--- uk.co.compucorp.civicrm.hrcore/backstop_data/casper_scripts/ssp/dashboard-open-calendar.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var page = require('../../page-objects/ssp/dashboard');
-
-module.exports = function (casper) {
-  page.init(casper).showModal('[href="/absence_calendar/nojs/show"]', '.view-calendar-absence-list');
-};
