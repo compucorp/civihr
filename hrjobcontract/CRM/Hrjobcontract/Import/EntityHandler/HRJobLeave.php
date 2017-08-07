2b83bc4eef152d0ead0c17a4bdba349c3af8ad11
Tunbola Ogunwande <tunbolawande@yahoo.com>
2017-08-07 21:01:35 +0100

    PCHR-2482: Refactor jobLeave handler as it was causing tests to break.

--- hrjobcontract/CRM/Hrjobcontract/Import/EntityHandler/HRJobLeave.php
+++ hrjobcontract/CRM/Hrjobcontract/Import/EntityHandler/HRJobLeave.php
@@ -9,22 +9,9 @@ class CRM_Hrjobcontract_Import_EntityHandler_HRJobLeave extends CRM_Hrjobcontrac
   }
 
   public function handle(array $params, CRM_Hrjobcontract_DAO_HRJobContractRevision $contractRevision, array &$previousRevision) {
-    $importExportUtility = ImportExportUtility::singleton();
-    $leaveTypes = $importExportUtility->getLeaveTypes();
-    $leaveData = [];
-
-    foreach($leaveTypes as $leaveType) {
-      $key = filter_var($leaveType['title'], FILTER_SANITIZE_STRING);
-      if(!empty($params[$key])){
-        $leaveData[] = [
-          'leave_type' => $leaveType['id'],
-          'leave_amount' => "{$params[$leaveType['title']]}",
-          'add_public_holidays' => $leaveType['add_public_holiday_to_entitlement'],
-          "jobcontract_revision_id" => $contractRevision->id,
-          "jobcontract_id" => $contractRevision->jobcontract_id,
-        ];
-      }
-    }
+    $leaveData = $this->prepareLeaveData(
+      $params, $contractRevision->jobcontract_id, $contractRevision->id
+    );
 
     return civicrm_api3('HRJobLeave', 'replace', [
       'sequential' => 1,
@@ -33,4 +20,35 @@ class CRM_Hrjobcontract_Import_EntityHandler_HRJobLeave extends CRM_Hrjobcontrac
       'jobcontract_revision_id' => $contractRevision->id,
     ])['values'][0];
   }
+
+  /**
+   * Prepares Job Leave entity data to a valid API format.
+   *
+   * @param array $params
+   * @param int $contractID
+   * @param int $revisionID
+   *
+   * @return array
+   */
+  private function prepareLeaveData($params, $contractID, $revisionID) {
+    $leaveRows = [];
+    $importExportUtility = ImportExportUtility::singleton();
+    $leaveTypes = $importExportUtility->getLeaveTypes();
+
+    foreach ($leaveTypes as $leaveType) {
+      $key = filter_var($leaveType['title'], FILTER_SANITIZE_STRING);
+      $leaveAmount = !empty($params[$key]) ? $params[$key] : 0;
+      $leaveRows[] = [
+        'leave_type' => $leaveType['id'],
+        'leave_amount' => $leaveAmount,
+        'add_public_holidays' => $leaveType['add_public_holiday_to_entitlement'],
+        'jobcontract_revision_id' => $revisionID,
+        'jobcontract_id' => $contractID,
+      ];
+
+      return $leaveRows;
+    }
+  }
 }
+
+
