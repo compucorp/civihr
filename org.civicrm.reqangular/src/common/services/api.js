5b3f4601c11524052f78b553f963b1a0e256ab44
Igor Pavlov <pavlov.org@gmail.com>
2017-08-22 15:37:12 +0100

    PCHR-2527: Allow custom options to be passed to API

--- org.civicrm.reqangular/src/common/services/api.js
+++ org.civicrm.reqangular/src/common/services/api.js
@@ -78,7 +78,7 @@ define([
         return $q.all([
           (function () {
             var params = _.assign({}, filters, (additionalParams || {}), {
-              options: { sort: sort || 'id DESC' }
+              options: _.assign({}, filters.options, { sort: sort || 'id DESC' })
             });
 
             if (pagination) {
