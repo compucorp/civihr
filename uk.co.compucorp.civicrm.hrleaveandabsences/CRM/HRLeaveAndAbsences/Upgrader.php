eee9c831c95cfa04fe2eec5c0321345424e5c1be
Davi Alexandre <davi@davialexandre.com.br>
2017-09-01 16:58:36 -0300

    PCHR-2542: Add the type field to the Leave Request Date table

--- uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader.php
@@ -9,6 +9,7 @@ class CRM_HRLeaveAndAbsences_Upgrader extends CRM_HRLeaveAndAbsences_Upgrader_Ba
   use CRM_HRLeaveAndAbsences_Upgrader_Step_1001;
   use CRM_HRLeaveAndAbsences_Upgrader_Step_1002;
   use CRM_HRLeaveAndAbsences_Upgrader_Step_1003;
+  use CRM_HRLeaveAndAbsences_Upgrader_Step_1004;
 
   /**
    * A list of directories to be scanned for XML installation files
