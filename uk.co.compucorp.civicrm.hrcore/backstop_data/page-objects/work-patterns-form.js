5868707467ad056a5b689e64cc275e12847b4e6b
René Olivo <minet.ws@gmail.com>
2017-09-04 18:26:26 -0400

    PCHR-2545: Migrate work patterns to Bootstrap

---
+++ uk.co.compucorp.civicrm.hrcore/backstop_data/page-objects/work-patterns-form.js
@@ -0,0 +1,25 @@
+var page = require('./page');
+
+module.exports = (function () {
+  return page.extend({
+    /**
+     * Displays the work pattern calendar form.
+     */
+    showCalendarForm: function () {
+      var casper = this.casper;
+
+      casper.then(function () {
+        casper.click('a[href="#work-pattern-calendar"]');
+      });
+
+      return this;
+    },
+
+    /**
+     * Waits until the work pattern form is visible.
+     */
+    waitForReady: function () {
+      this.waitUntilVisible('.work-pattern-form');
+    }
+  });
+})();
