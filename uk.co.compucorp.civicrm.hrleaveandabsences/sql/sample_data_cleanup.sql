bbd7899f1ba79200a7b6f6303c6202084417f074
Davi Alexandre <davi@davialexandre.com.br>
2017-09-18 13:57:48 -0300

    PCHR-2608: Make the sample data more dynamic
    The IDs of the civihr_staff and civihr_manager contacts can vary
    depending on a number of factors:
    
    - Installing the CiviCRM sample on the system
    - Having or not the civihr_local_admin user
    - Any other factor that might end adding contacts to the db before we
    create the civihr_staff and civihr_manager users
    
    For this reason, their IDs cannot be hardcoded the way they were before.
    
    This commit also includes a small optimization on the cleanup script
    regarding contracts. This is now done with a single query and we only
    delete the contracts related to civihr_staff.

--- uk.co.compucorp.civicrm.hrleaveandabsences/sql/sample_data_cleanup.sql
+++ uk.co.compucorp.civicrm.hrleaveandabsences/sql/sample_data_cleanup.sql
@@ -1,12 +1,48 @@
 SET foreign_key_checks = 0;
-TRUNCATE civicrm_hrjobcontract_revision;
-TRUNCATE civicrm_hrjobcontract_details;
-TRUNCATE civicrm_hrjobcontract_health;
-TRUNCATE civicrm_hrjobcontract_hour;
-TRUNCATE civicrm_hrjobcontract_leave;
-TRUNCATE civicrm_hrjobcontract_pay;
-TRUNCATE civicrm_hrjobcontract_pension;
-TRUNCATE civicrm_hrjobcontract_role;
-TRUNCATE civicrm_hrjobcontract;
-DELETE FROM `civicrm_relationship` WHERE contact_id_a = 202 AND contact_id_b = 203 and civicrm_relationship.relationship_type_id = (SELECT rt.id FROM `civicrm_relationship_type` rt WHERE rt.name_a_b = 'has Leave Approved By');
+SELECT @staffID:=id FROM civicrm_contact WHERE display_name = 'civihr_staff@compucorp.co.uk';
+SELECT @managerID:=id FROM civicrm_contact WHERE display_name = 'civihr_manager@compucorp.co.uk';
+
+DELETE details,
+  job_leave,
+  health,
+  hour,
+  pay,
+  pension,
+  role,
+  revision,
+  contract
+FROM civicrm_hrjobcontract contract
+  LEFT JOIN civicrm_hrjobcontract_revision revision
+    ON contract.id = revision.jobcontract_id
+  LEFT JOIN civicrm_hrjobcontract_details details
+    ON details.jobcontract_revision_id = revision.id
+  LEFT JOIN civicrm_hrjobcontract_leave job_leave
+    ON job_leave.jobcontract_revision_id = revision.id
+  LEFT JOIN civicrm_hrjobcontract_health health
+    ON health.jobcontract_revision_id = revision.id
+  LEFT JOIN civicrm_hrjobcontract_hour hour
+    ON hour.jobcontract_revision_id = revision.id
+  LEFT JOIN civicrm_hrjobcontract_pay pay
+    ON pay.jobcontract_revision_id = revision.id
+  LEFT JOIN civicrm_hrjobcontract_pension pension
+    ON pension.jobcontract_revision_id = revision.id
+  LEFT JOIN civicrm_hrjobcontract_role role
+    ON role.jobcontract_revision_id = revision.id
+WHERE contract.contact_id = @staffID;
+
+TRUNCATE civicrm_hrleaveandabsences_absence_period;
+TRUNCATE civicrm_hrleaveandabsences_public_holiday;
+TRUNCATE civicrm_hrleaveandabsences_leave_period_entitlement;
+TRUNCATE civicrm_hrleaveandabsences_leave_request;
+TRUNCATE civicrm_hrleaveandabsences_leave_request_date;
+TRUNCATE civicrm_hrleaveandabsences_leave_balance_change;
+
+DELETE FROM civicrm_relationship
+  WHERE contact_id_a = @staffID AND
+        contact_id_b = @managerID AND
+        relationship_type_id = (SELECt id FROM civicrm_relationship_type WHERE name_a_b = 'has Leave Approved by');
+
+SET @staffID = NULL;
+SET @managerID = NULL;
+
 SET foreign_key_checks = 1;
