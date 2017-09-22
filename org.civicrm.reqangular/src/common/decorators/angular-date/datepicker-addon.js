b88cde5ed3f71d882650b7b7b800539b70ec0524
Debarshi Bhaumik <deb1990@gmail.com>
2017-09-22 13:46:06 +0530

    PCHR-2593: Datepicker Component

--- org.civicrm.reqangular/src/common/decorators/angular-date/datepicker-addon.js
+++ org.civicrm.reqangular/src/common/decorators/angular-date/datepicker-addon.js
@@ -1,35 +1,72 @@
-define([], function () {
+define([
+  'common/moment'
+], function (moment) {
   'use strict';
 
   /**
    * Decorates the uib-datepicker-popup directive, making its sibling '.input-group-addon'
    * trigger the datepicker
    */
-  return ['$delegate', function ($delegate) {
+  return ['$delegate', '$compile', function ($delegate, $compile) {
     var directive = $delegate[0];
+    var isMobile = document.getElementsByTagName('body')[0].classList.contains('mobile');
     var origCompile = directive.compile;
+    var calendarIcon = '<span ' +
+      'class="input-group-addon pointer">' +
+      '<i class="fa fa-calendar"></i>' +
+      '</span>';
 
-    directive.compile = function compile() {
-      var link = origCompile.apply(this, arguments);
+    if (isMobile) {
+      directive.require = 'ngModel';
+      directive.compile = function compile() {
+        return function (scope, element, attrs, ngModel) {
+          element.parent('.input-group').css('display', 'block');
+          element.after(calendarIcon);
+          element.prop('type', 'date');
+          attrs.ngReadonly = false;
 
-      return function (scope, element) {
-        link.apply(this, arguments);
-
-        function onClick() {
-          element.trigger('click');
-        }
-
-        var inputGroupAddon = element.siblings('.input-group-addon');
-
-        if (inputGroupAddon.length) {
-          inputGroupAddon.on('click', onClick);
-
-          scope.$on('$destroy', function() {
-            inputGroupAddon.off('click', onClick);
+          ngModel.$formatters.push(function (viewValue) {
+            return convertToHTML5DatepickerFormat(viewValue);
           });
-        }
+
+          scope.$watch('datepickerOptions.minDate', function (value) {
+            attrs.$set('min', convertToHTML5DatepickerFormat(value));
+          });
+          scope.$watch('datepickerOptions.maxDate', function (value) {
+            attrs.$set('max', convertToHTML5DatepickerFormat(value));
+          });
+        };
       };
-    };
+    } else {
+      directive.compile = function compile() {
+        var link = origCompile.apply(this, arguments);
+
+        return function (scope, element, attrs) {
+          element.after(calendarIcon);
+
+          link.apply(this, arguments);
+
+          function onClick() {
+            scope.isOpen = true;
+            scope.$apply();
+          }
+
+          var inputGroupAddon = element.siblings('.input-group-addon');
+
+          if (inputGroupAddon.length) {
+            inputGroupAddon.on('click', onClick);
+
+            scope.$on('$destroy', function() {
+              inputGroupAddon.off('click', onClick);
+            });
+          }
+        };
+      };
+    }
+
+    function convertToHTML5DatepickerFormat(date) {
+      return date ? moment(date).format('Y-MM-DD') : '';
+    }
 
     return $delegate;
   }];
