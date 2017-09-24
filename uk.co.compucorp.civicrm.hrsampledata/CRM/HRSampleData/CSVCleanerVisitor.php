de46ab6d402632d04570e94ee13db1d0ef4982a5
Omar abu hussein <opetmar91@gmail.com>
2017-09-24 21:43:29 +0300

    PCHR-2134: Adding the needed data inside the extension instead of relying on default civihr data

--- uk.co.compucorp.civicrm.hrsampledata/CRM/HRSampleData/CSVCleanerVisitor.php
+++ uk.co.compucorp.civicrm.hrsampledata/CRM/HRSampleData/CSVCleanerVisitor.php
@@ -6,16 +6,18 @@ abstract class CRM_HRSampleData_CSVCleanerVisitor extends CSVProcessingVisitor {
 
   /**
    * Deletes entity record from the database based on
-   * search criteria
+   * search criteria if the record delete_on_uninstall field
+   * is set.
    *
    * @param string $entity
    * @param array $searchParams
    */
   protected function deleteRecord($entity, $searchParams) {
+    if (empty($searchParams['delete_on_uninstall'])) {
+      return;
+    }
     $searchParams['options'] = ['limit' => 0];
-
     $entityResult = $this->callAPI($entity, 'get', $searchParams);
-
     if (!empty($entityResult['id'])) {
       $entityID = $entityResult['id'];
       $this->callAPI($entity, 'delete', ['id' => $entityID]);
