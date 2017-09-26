9ea079b95de1b29e2511f4932b6365a54b8a096e
René Olivo <minet.ws@gmail.com>
2017-09-26 14:56:04 -0400

    PCHR-2545: Fix dropdown buttons issue

--- hrui/js/src/civihr-popup/civihr-popup.js
+++ hrui/js/src/civihr-popup/civihr-popup.js
@@ -4,14 +4,61 @@ Prevent Popups to overflow tables.
 (function ($) {
   'use strict';
 
-  // .btn-slide is a continer of popup and a button triggering it
-  $('.btn-slide').attrchange(function (e) {
-    var $body = $('body');
+  $('body').on('click', '.btn-slide', function (e) {
     var $button = $(this);
-    var $closestEntity, $popup, buttonOffset;
+    var $body = $('body');
+    var $popup, buttonOffset, $closestEntity;
 
-    // check if popup is open
-    if ($button.hasClass('btn-slide-active')) {
+    e.preventDefault();
+
+    (function init () {
+      openPopupPanel();
+      listenToMouseOutEvent();
+    })();
+
+    /**
+     * Closes the popup panel and removes the .civihr-popup-open class from the
+     * body.
+     */
+    function closePopupPanel () {
+      $body
+        .children('.civihr-popup')
+        .appendTo($button)
+        .removeClass('civihr-popup');
+
+      $body.removeClass('civihr-popup-open');
+    }
+
+    /**
+     * Listens to mouse events happening outside the of the panel in order to
+     * close it. The way to detected mouse out is by listening to the
+     * .btn-slide-active class change, which is added and removed by CiviCRM.
+     * the data *attrchange-is-on* is set to true in order to avoid adding
+     * multiple listeners to the same element.
+     */
+    function listenToMouseOutEvent () {
+      // AttrChange event is already listened, skip:
+      if ($button.data('attrchange-is-on')) {
+        return;
+      }
+
+      $button.data('attrchange-is-on', true);
+
+      $button.attrchange(function () {
+        // Button is already open, skip:
+        if ($button.hasClass('btn-slide-active')) {
+          return;
+        }
+
+        closePopupPanel();
+      });
+    }
+
+    /**
+     * Opens the pop panel and adds the .civihr-popup-open class to the
+     * body.
+     */
+    function openPopupPanel () {
       $popup = $button.children('ul.panel');
       $closestEntity = $button.closest('.crm-entity')[0];
 
@@ -25,17 +72,10 @@ Prevent Popups to overflow tables.
       buttonOffset = $button.offset();
 
       $popup
-        .css('top', parseInt(buttonOffset.top, 10) + $button.outerHeight())
-        .css('left', parseInt(buttonOffset.left, 10) - ($popup.width() - $button.outerWidth()));
+        .css('top', +buttonOffset.top + $button.outerHeight())
+        .css('left', +buttonOffset.left - $popup.width() - $button.outerWidth());
 
       $body.addClass('civihr-popup-open');
-    } else {
-      $body
-        .children('.civihr-popup')
-        .appendTo($button)
-        .removeClass('civihr-popup');
-
-      $body.removeClass('civihr-popup-open');
     }
   });
 })(CRM.$);
