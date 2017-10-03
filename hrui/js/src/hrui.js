f76653579e495350efe5cfabad62bbcf9235858a
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-10-03 22:53:28 +0200

    PCHR-2613: Move function to bottom of the file

--- hrui/js/src/hrui.js
+++ hrui/js/src/hrui.js
@@ -2,111 +2,6 @@
 
 // Copyright CiviCRM LLC 2013. See http://civicrm.org/licensing
 (function ($, _) {
-  /**
-   * Adds the Government ID field on the Personal Details page and on the Edit
-   * Contact form.
-   */
-  function addGovernmentIdField () {
-    // Updates Personal Details Page
-    if (CRM.cid && CRM.hideGId) {
-      var inlineDataBlock = $('.Inline_Custom_Data');
-
-      inlineDataBlock.appendTo($('#contactinfo-block').parent('div').parent('div'));
-      inlineDataBlock.removeClass('crm-collapsible');
-      inlineDataBlock.removeClass('collapsed');
-      inlineDataBlock.addClass('crm-summary-block');
-
-      $('.Inline_Custom_Data div.collapsible-title').css({display: 'none'});
-      $('.Inline_Custom_Data div.crm-summary-block').css({display: 'block'});
-    }
-
-    // Updates Edit Contact Form
-    if ($('#customFields').length < 1) {
-      $('#Inline_Custom_Data label').each(function () {
-        $('#nick_name').parent().after('<td id="customFields"></td>');
-        var nodeID = $(this).attr('for');
-        var customField = $('#' + nodeID).detach();
-        $('#customFields').append($(this));
-        $('#customFields').append(customField);
-      });
-
-      $('#Inline_Custom_Data').remove();
-    }
-  }
-
-  /**
-   * Misc changes to the page (hiding elements, inserting new ones, etc)
-   */
-  function miscPageChanges (target) {
-    // Hide current employer and job title
-    // Contact summary screen:
-    $('div.crm-contact-current_employer, div.crm-contact-job_title', '.crm-summary-contactinfo-block').parent('div.crm-summary-row').hide();
-    // Inline edit form
-    $('form#ContactInfo input#employer_id, form#ContactInfo input#job_title', target).closest('div.crm-summary-row').hide();
-    // Contact edit screen
-    $('input#employer_id, input#job_title', 'form#Contact').parent('td').hide();
-
-    /* Changes on Add Individual pages and Personal details tab for HR-358 */
-    // Move Job summary to top
-    $('.HRJobContract_Summary', target).insertBefore($('.crm-summary-contactinfo-block'));
-    // changes of email block, remove bulkmail and onhold
-    $('div.email-signature, td#Email-Bulkmail-html', 'form#Contact').hide();
-    $('#Email-Primary', 'form#Contact').prev('td').prev('td').hide();
-    $('td#Email-Bulkmail-html, #Email-Primary', 'form#Contact').prev('td').hide();
-
-    // shift demographic above extended demographic
-    $('.crm-demographics-accordion', 'form#Contact').insertAfter($('.crm-contactDetails-accordion'));
-
-    if ($('tr#Phone_Block_2', 'form#Contact').length < 1) {
-      $('#addPhone').click();
-    }
-  }
-
-  /**
-   * Update label 'for' attr to works with the datepicker
-   *
-   * @param  {jQuery object} $line [datepicker's line parent]
-   */
-  function linkLabelToDatepickerInput ($line) {
-    $line.find('label').attr('for', $line.find('.crm-form-date').attr('id'));
-  }
-
-  /**
-   * Add an event listener on input[type="file"]
-   * @param {jQuery Object} selector [selector from input file]
-   */
-  function addUploadFileListener (selector) {
-    if ($(selector).length === 1) {
-      $(selector).on('change', insertFile);
-    }
-  }
-
-  /**
-   * Insert a DOM node after input[type="file"]
-   * with the filename
-   */
-  function insertFile () {
-    var fileName = $(this)[0].files[0];
-
-    $('#js-uploaded-file').remove();
-    if (fileName !== undefined) {
-      $(this).after('<span id="js-uploaded-file" class="uploaded-file">' + fileName.name + ' <span class="uploaded-file-icon-trash"><i class="fa fa-trash-o"></i> Remove</span>');
-
-      $('.uploaded-file-icon-trash').on('click', removeFile);
-    }
-  }
-
-  /**
-   * Remove the #js-uploaded-file DIV and
-   * clean input[type="file"] value
-   */
-  function removeFile () {
-    var $input = $('#js-uploaded-file').parent().find('input[type="file"]');
-
-    $('#js-uploaded-file').remove();
-    $input.val('');
-  }
-
   $('.CRM_HRRecruitment_Form_Application').addClass('crm-form-block');
   $('.CRM_HRRecruitment_Form_Application .crm-profile-name-application_profile').addClass('form-layout-compressed');
 
@@ -193,5 +88,110 @@
         }
       }
     );
+  }
+
+  /**
+   * Adds the Government ID field on the Personal Details page and on the Edit
+   * Contact form.
+   */
+  function addGovernmentIdField () {
+    // Updates Personal Details Page
+    if (CRM.cid && CRM.hideGId) {
+      var inlineDataBlock = $('.Inline_Custom_Data');
+
+      inlineDataBlock.appendTo($('#contactinfo-block').parent('div').parent('div'));
+      inlineDataBlock.removeClass('crm-collapsible');
+      inlineDataBlock.removeClass('collapsed');
+      inlineDataBlock.addClass('crm-summary-block');
+
+      $('.Inline_Custom_Data div.collapsible-title').css({display: 'none'});
+      $('.Inline_Custom_Data div.crm-summary-block').css({display: 'block'});
+    }
+
+    // Updates Edit Contact Form
+    if ($('#customFields').length < 1) {
+      $('#Inline_Custom_Data label').each(function () {
+        $('#nick_name').parent().after('<td id="customFields"></td>');
+        var nodeID = $(this).attr('for');
+        var customField = $('#' + nodeID).detach();
+        $('#customFields').append($(this));
+        $('#customFields').append(customField);
+      });
+
+      $('#Inline_Custom_Data').remove();
+    }
+  }
+
+  /**
+   * Add an event listener on input[type="file"]
+   * @param {jQuery Object} selector [selector from input file]
+   */
+  function addUploadFileListener (selector) {
+    if ($(selector).length === 1) {
+      $(selector).on('change', insertFile);
+    }
+  }
+
+  /**
+   * Insert a DOM node after input[type="file"]
+   * with the filename
+   */
+  function insertFile () {
+    var fileName = $(this)[0].files[0];
+
+    $('#js-uploaded-file').remove();
+    if (fileName !== undefined) {
+      $(this).after('<span id="js-uploaded-file" class="uploaded-file">' + fileName.name + ' <span class="uploaded-file-icon-trash"><i class="fa fa-trash-o"></i> Remove</span>');
+
+      $('.uploaded-file-icon-trash').on('click', removeFile);
+    }
+  }
+
+  /**
+   * Update label 'for' attr to works with the datepicker
+   *
+   * @param  {jQuery object} $line [datepicker's line parent]
+   */
+  function linkLabelToDatepickerInput ($line) {
+    $line.find('label').attr('for', $line.find('.crm-form-date').attr('id'));
+  }
+
+  /**
+   * Misc changes to the page (hiding elements, inserting new ones, etc)
+   */
+  function miscPageChanges (target) {
+    // Hide current employer and job title
+    // Contact summary screen:
+    $('div.crm-contact-current_employer, div.crm-contact-job_title', '.crm-summary-contactinfo-block').parent('div.crm-summary-row').hide();
+    // Inline edit form
+    $('form#ContactInfo input#employer_id, form#ContactInfo input#job_title', target).closest('div.crm-summary-row').hide();
+    // Contact edit screen
+    $('input#employer_id, input#job_title', 'form#Contact').parent('td').hide();
+
+    /* Changes on Add Individual pages and Personal details tab for HR-358 */
+    // Move Job summary to top
+    $('.HRJobContract_Summary', target).insertBefore($('.crm-summary-contactinfo-block'));
+    // changes of email block, remove bulkmail and onhold
+    $('div.email-signature, td#Email-Bulkmail-html', 'form#Contact').hide();
+    $('#Email-Primary', 'form#Contact').prev('td').prev('td').hide();
+    $('td#Email-Bulkmail-html, #Email-Primary', 'form#Contact').prev('td').hide();
+
+    // shift demographic above extended demographic
+    $('.crm-demographics-accordion', 'form#Contact').insertAfter($('.crm-contactDetails-accordion'));
+
+    if ($('tr#Phone_Block_2', 'form#Contact').length < 1) {
+      $('#addPhone').click();
+    }
+  }
+
+  /**
+   * Remove the #js-uploaded-file DIV and
+   * clean input[type="file"] value
+   */
+  function removeFile () {
+    var $input = $('#js-uploaded-file').parent().find('input[type="file"]');
+
+    $('#js-uploaded-file').remove();
+    $input.val('');
   }
 }(CRM.$, CRM._));
