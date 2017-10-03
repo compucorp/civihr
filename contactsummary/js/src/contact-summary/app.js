55a919b435d2449088e1c0388591ec3bc408fd82
René Olivo <minet.ws@gmail.com>
2017-10-03 12:58:48 -0400

    PCHR-2683: Fix lint issues

--- contactsummary/js/src/contact-summary/app.js
+++ contactsummary/js/src/contact-summary/app.js
@@ -1,50 +1,51 @@
+/* eslint-env amd */
+
 define([
-    'common/angular',
-    'contact-summary/modules/filters',
-    'contact-summary/modules/services',
-    'contact-summary/modules/settings',
-    'contact-summary/controllers/contactSummary',
-    'contact-summary/controllers/keyDates',
-    'contact-summary/controllers/keyDetails',
-    'contact-summary/directives/donutChart'
+  'common/angular',
+  'contact-summary/modules/filters',
+  'contact-summary/modules/services',
+  'contact-summary/modules/settings',
+  'contact-summary/controllers/contactSummary',
+  'contact-summary/controllers/keyDates',
+  'contact-summary/controllers/keyDetails',
+  'contact-summary/directives/donutChart'
 ], function (angular) {
-    var app = angular.module('contactsummary', [
-        'ngRoute',
-        'ngResource',
-        'ui.bootstrap',
-        'common.services',
-        'contactsummary.controllers',
-        'contactsummary.directives',
-        'contactsummary.filters',
-        'contactsummary.services',
-        'contactsummary.settings'
-    ]);
+  var app = angular.module('contactsummary', [
+    'ngRoute',
+    'ngResource',
+    'ui.bootstrap',
+    'common.services',
+    'contactsummary.controllers',
+    'contactsummary.directives',
+    'contactsummary.filters',
+    'contactsummary.services',
+    'contactsummary.settings'
+  ]);
 
-    app.config(['settings', '$routeProvider', '$resourceProvider', '$httpProvider', '$logProvider',
-        function (settings, $routeProvider, $resourceProvider, $httpProvider, $logProvider) {
-            $logProvider.debugEnabled(settings.debug);
+  app.config(['settings', '$routeProvider', '$resourceProvider', '$httpProvider', '$logProvider',
+    function (settings, $routeProvider, $resourceProvider, $httpProvider, $logProvider) {
+      $logProvider.debugEnabled(settings.debug);
 
-            $routeProvider.
-                when('/', {
-                    controller: 'ContactSummaryCtrl',
-                    controllerAs: 'ContactSummaryCtrl',
-                    templateUrl: settings.pathBaseUrl + settings.pathTpl + 'mainTemplate.html',
-                    resolve: {}
-                }
-            ).otherwise({redirectTo: '/'});
-
-            $resourceProvider.defaults.stripTrailingSlashes = false;
-
-            $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+      $routeProvider
+        .when('/', {
+          controller: 'ContactSummaryCtrl',
+          controllerAs: 'ContactSummaryCtrl',
+          templateUrl: settings.pathBaseUrl + settings.pathTpl + 'mainTemplate.html',
+          resolve: {}
         }
-    ]);
+      ).otherwise({redirectTo: '/'});
 
-    app.run(['settings', '$rootScope', '$q', '$log',
-        function (settings, $rootScope, $q, $log) {
-            $log.debug('app.run');
+      $resourceProvider.defaults.stripTrailingSlashes = false;
+      $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    }
+  ]);
 
-            $rootScope.pathTpl = settings.pathTpl;
-            $rootScope.prefix = settings.classNamePrefix;
-        }
-    ]);
+  app.run(['settings', '$rootScope', '$q', '$log',
+    function (settings, $rootScope, $q, $log) {
+      $log.debug('app.run');
+
+      $rootScope.pathTpl = settings.pathTpl;
+      $rootScope.prefix = settings.classNamePrefix;
+    }
+  ]);
 });
