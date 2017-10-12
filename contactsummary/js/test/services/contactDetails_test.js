a4a2ecb91653399392d1f5b658122535e1a17e5e
René Olivo <minet.ws@gmail.com>
2017-10-12 09:04:38 -0400

    PCHR-2719: Fix lint issues in contact summary services tests

--- contactsummary/js/test/services/contactDetails_test.js
+++ contactsummary/js/test/services/contactDetails_test.js
@@ -1,69 +1,72 @@
+/* eslint-env amd, jasmine */
+
 define([
-    'common/moment',
-    'common/angularMocks',
-    'contact-summary/app',
-    'contact-summary/services/contactDetails',
-    'mocks/constants',
-    'mocks/services'
-], function (moment) {
-    'use strict';
+  'common/angular',
+  'common/moment',
+  'common/angularMocks',
+  'contact-summary/app',
+  'contact-summary/services/contactDetails',
+  'mocks/constants',
+  'mocks/services'
+], function (angular, moment) {
+  'use strict';
 
-    describe('ContactDetailsService', function () {
-        var ContactDetailsService,
-            ApiServiceMock, ModelServiceMock, settingsMock = {},
-            rootScope;
+  describe('ContactDetailsService', function () {
+    var ContactDetailsService, ApiServiceMock, ModelServiceMock, rootScope;
+    var settingsMock = {};
 
-        beforeEach(module('contactsummary', 'contactsummary.mocks'));
+    beforeEach(module('contactsummary', 'contactsummary.mocks'));
 
-        beforeEach(module(function ($provide) {
-            $provide.factory('ApiService', function () {
-                return ApiServiceMock;
-            });
+    beforeEach(module(function ($provide) {
+      $provide.factory('ApiService', function () {
+        return ApiServiceMock;
+      });
 
-            $provide.factory('ModelService', function () {
-                return ModelServiceMock;
-            });
+      $provide.factory('ModelService', function () {
+        return ModelServiceMock;
+      });
 
-            $provide.constant('settings', settingsMock);
-        }));
+      $provide.constant('settings', settingsMock);
+    }));
 
-        beforeEach(inject(function ($injector) {
-            ApiServiceMock = $injector.get('ApiServiceMock');
-            ModelServiceMock = $injector.get('ModelServiceMock');
-            rootScope = $injector.get('$rootScope');
+    beforeEach(inject(function ($injector) {
+      ApiServiceMock = $injector.get('ApiServiceMock');
+      ModelServiceMock = $injector.get('ModelServiceMock');
+      rootScope = $injector.get('$rootScope');
 
-            // We're extending because a reference to the original object was passed above, during bootstrap phase.
-            angular.extend(settingsMock, $injector.get('settingsMock'));
-        }));
+      // We're extending because a reference to the original object was passed above, during bootstrap phase.
+      angular.extend(settingsMock, $injector.get('settingsMock'));
+    }));
 
-        beforeEach(inject(function (_ContactDetailsService_) {
-            ContactDetailsService = _ContactDetailsService_;
-        }));
+    beforeEach(inject(function (_ContactDetailsService_) {
+      ContactDetailsService = _ContactDetailsService_;
+    }));
 
-        describe('get', function () {
-            var details,
-                expectedDateOfBirth = '1970/01/01',
-                expectedAge = moment().diff(moment(expectedDateOfBirth, 'YYYY-MM-DD'), 'years'),
-                expectedResponse = {values: [{birth_date: expectedDateOfBirth}]}, expectedContactId = 123;
+    describe('get', function () {
+      var details;
+      var expectedDateOfBirth = '1970/01/01';
+      var expectedAge = moment().diff(moment(expectedDateOfBirth, 'YYYY-MM-DD'), 'years');
+      var expectedResponse = { values: [{ birth_date: expectedDateOfBirth }] };
+      var expectedContactId = 123;
 
-            beforeEach(function () {
-                ApiServiceMock.respondGet('Contact', expectedResponse);
-                settingsMock.contactId = expectedContactId;
+      beforeEach(function () {
+        ApiServiceMock.respondGet('Contact', expectedResponse);
+        settingsMock.contactId = expectedContactId;
 
-                ContactDetailsService.get().then(function (response) {
-                    details = response;
-                });
-
-                rootScope.$digest();
-
-                ApiServiceMock.flush();
-            });
-
-            it('should return contact details', function () {
-                expect(details.id).toEqual(expectedContactId);
-                expect(details.dateOfBirth).toEqual(expectedResponse.values[0].birth_date);
-                expect(details.age).toEqual(expectedAge);
-            });
+        ContactDetailsService.get().then(function (response) {
+          details = response;
         });
+
+        rootScope.$digest();
+
+        ApiServiceMock.flush();
+      });
+
+      it('should return contact details', function () {
+        expect(details.id).toEqual(expectedContactId);
+        expect(details.dateOfBirth).toEqual(expectedResponse.values[0].birth_date);
+        expect(details.age).toEqual(expectedAge);
+      });
     });
+  });
 });
--- contactsummary/js/test/services/contract_test.js
+++ contactsummary/js/test/services/contract_test.js
@@ -1,97 +1,94 @@
+/* eslint-env amd, jasmine */
+
 define([
-    'common/lodash',
-    'common/angularMocks',
-    'contact-summary/app',
-    'contact-summary/services/contract',
-    'mocks/services'
-], function () {
-    'use strict';
+  'common/angular',
+  'common/lodash',
+  'common/angularMocks',
+  'contact-summary/app',
+  'contact-summary/services/contract',
+  'mocks/services'
+], function (angular, _) {
+  'use strict';
 
-    describe('ContractService', function () {
-        var ContractService,
-            ApiServiceMock, ContactDetailsServiceMock, ModelServiceMock,
-            rootScope;
+  describe('ContractService', function () {
+    var ContractService,
+      ApiServiceMock, ContactDetailsServiceMock, ModelServiceMock,
+      rootScope;
 
-        beforeEach(module('contactsummary', 'contactsummary.mocks'));
+    beforeEach(module('contactsummary', 'contactsummary.mocks'));
 
-        beforeEach(module(function ($provide) {
-            $provide.factory('ApiService', function () {
-                return ApiServiceMock;
-            });
+    beforeEach(module(function ($provide) {
+      $provide.factory('ApiService', function () {
+        return ApiServiceMock;
+      });
 
-            $provide.factory('ModelService', function () {
-                return ModelServiceMock;
-            });
+      $provide.factory('ModelService', function () {
+        return ModelServiceMock;
+      });
 
-            $provide.factory('ContactDetailsService', function () {
-                return ContactDetailsServiceMock;
-            });
-        }));
+      $provide.factory('ContactDetailsService', function () {
+        return ContactDetailsServiceMock;
+      });
+    }));
 
-        beforeEach(inject(function ($injector) {
-            ApiServiceMock = $injector.get('ApiServiceMock');
-            ContactDetailsServiceMock = $injector.get('ContactDetailsServiceMock');
-            ModelServiceMock = $injector.get('ModelServiceMock');
-            rootScope = $injector.get('$rootScope');
-        }));
+    beforeEach(inject(function ($injector) {
+      ApiServiceMock = $injector.get('ApiServiceMock');
+      ContactDetailsServiceMock = $injector.get('ContactDetailsServiceMock');
+      ModelServiceMock = $injector.get('ModelServiceMock');
+      rootScope = $injector.get('$rootScope');
+    }));
 
-        beforeEach(inject(function (_ContractService_) {
-            ContractService = _ContractService_;
-        }));
+    beforeEach(inject(function (_ContractService_) {
+      ContractService = _ContractService_;
+    }));
 
-        describe('get()', function () {
-            var contracts,
-                expectedContracts = {
-                    values: [
-                        {api_HRJobContractRevision_getcurrentrevision: {values: {id: '4'}}, deleted: '0', id: '1', is_current: '1'}
-                    ]
-                },
-                expectedContractDetails = {
-                    values: [
-                      {
-                        api_HRJobHour_get: {
-                          values: []
-                        },
-                        api_HRJobPay_get: {
-                          values: []
-                        },
-                        title: 'Project Manager',
-                        period_start_date: '2015-01-31',
-                        period_end_date: '2015-11-01'
-                      }
-                    ]
-                };
+    describe('get()', function () {
+      var contracts;
+      var expectedContracts = {
+        values: [
+          {api_HRJobContractRevision_getcurrentrevision: {values: {id: '4'}}, deleted: '0', id: '1', is_current: '1'}
+        ]
+      };
+      var expectedContractDetails = {
+        values: [{
+          api_HRJobHour_get: { values: [] },
+          api_HRJobPay_get: { values: [] },
+          title: 'Project Manager',
+          period_start_date: '2015-01-31',
+          period_end_date: '2015-11-01'
+        }]
+      };
 
-            beforeEach(function () {
-                ApiServiceMock.respondGet('HRJobContract', expectedContracts);
-                ApiServiceMock.respondGet('HRJobDetails', expectedContractDetails);
-                ContactDetailsServiceMock.respond('get', {id: 123});
+      beforeEach(function () {
+        ApiServiceMock.respondGet('HRJobContract', expectedContracts);
+        ApiServiceMock.respondGet('HRJobDetails', expectedContractDetails);
+        ContactDetailsServiceMock.respond('get', {id: 123});
 
-                ContractService.get().then(function (response) {
-                    contracts = response;
-                });
-
-                rootScope.$digest();
-
-                ApiServiceMock.flush();
-                ContactDetailsServiceMock.flush();
-            });
-
-            it('should return contracts', function () {
-                expect(angular.isObject(contracts)).toBe(true);
-            });
-
-            describe('a contract', function () {
-                it('should have the required fields', function () {
-                    expect(_.size(contracts)).toBeGreaterThan(0);
-
-                    angular.forEach(contracts, function (contract) {
-                        expect(contract.start_date).toBeDefined();
-                        expect(contract.end_date).toBeDefined();
-                        expect(contract.title).toBeDefined();
-                    });
-                });
-            });
+        ContractService.get().then(function (response) {
+          contracts = response;
         });
+
+        rootScope.$digest();
+
+        ApiServiceMock.flush();
+        ContactDetailsServiceMock.flush();
+      });
+
+      it('should return contracts', function () {
+        expect(angular.isObject(contracts)).toBe(true);
+      });
+
+      describe('a contract', function () {
+        it('should have the required fields', function () {
+          expect(_.size(contracts)).toBeGreaterThan(0);
+
+          angular.forEach(contracts, function (contract) {
+            expect(contract.start_date).toBeDefined();
+            expect(contract.end_date).toBeDefined();
+            expect(contract.title).toBeDefined();
+          });
+        });
+      });
     });
+  });
 });
