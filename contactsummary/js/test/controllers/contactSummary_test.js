c739eecc2350cfabbe6e6b6e2b5f51c8386fadfb
René Olivo <minet.ws@gmail.com>
2017-10-16 12:33:45 -0400

    PCHR-2719: Fix Contact Summary lint issues

--- contactsummary/js/test/controllers/contactSummary_test.js
+++ contactsummary/js/test/controllers/contactSummary_test.js
@@ -1,35 +1,35 @@
+/* eslint-env amd, jasmine */
+
 define([
-    'common/angularMocks',
-    'contact-summary/app',
-    'contact-summary/controllers/contactSummary',
-    'mocks/constants',
-    'mocks/services'
+  'common/angularMocks',
+  'contact-summary/app',
+  'contact-summary/controllers/contactSummary',
+  'mocks/constants',
+  'mocks/services'
 ], function () {
-    'use strict';
+  'use strict';
 
-    xdescribe('ContactSummaryCtrl', function () {
-        var ctrlConstructor;
-        var settingsMock;
-        var rootScope;
+  xdescribe('ContactSummaryCtrl', function () {
+    // var ctrlConstructor;
+    var settingsMock;
 
-        beforeEach(module('contactsummary', 'contactsummary.mocks'));
+    beforeEach(module('contactsummary', 'contactsummary.mocks'));
 
-        beforeEach(module(function ($provide) {
-            $provide.value('settings', function () {
-                return settingsMock;
-            });
-        }));
+    beforeEach(module(function ($provide) {
+      $provide.value('settings', function () {
+        return settingsMock;
+      });
+    }));
 
-        beforeEach(inject(function ($injector) {
-            // Instantiating injector isn't allowed before calls to 'module()'. Due to this limitation, we're returning
-            // references to mock variables above, and injecting the actual mocks into them here, since they would be
-            // lazily loaded anyway.
-            settingsMock = $injector.get('settingsMock');
-            rootScope = $injector.get('$rootScope');
-        }));
+    beforeEach(inject(function ($injector) {
+      // Instantiating injector isn't allowed before calls to 'module()'. Due to this limitation, we're returning
+      // references to mock variables above, and injecting the actual mocks into them here, since they would be
+      // lazily loaded anyway.
+      settingsMock = $injector.get('settingsMock');
+    }));
 
-        beforeEach(inject(function (_$controller_) {
-            ctrlConstructor = _$controller_;
-        }));
-    });
+    beforeEach(inject(function (_$controller_) {
+      // ctrlConstructor = _$controller_;
+    }));
+  });
 });
