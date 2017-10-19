10ad42c1ca1359ae5b1f66718858d9c3774d7443
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-10-19 16:18:48 +0200

    PCHR-2619: Add empty Paths classes for Joomla and WordPress

---
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/CMSData/PathsJoomla.php
@@ -0,0 +1,12 @@
+<?php
+  class CRM_HRCore_CMSData_PathsJoomla implements CRM_HRCore_CMSData_PathsInterface {
+    private $contactData;
+
+    public function __construct($contactData) {
+      $this->contactData = $contactData;
+    }
+
+    public function getDefaultImagePath() {}
+    public function getEditAccountPath() {}
+    public function getLogoutPath() {}
+  }
