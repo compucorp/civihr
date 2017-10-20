938809bcbc9c0d1f349135f81fb2458d94d1dd72
Igor Pavlov <pavlov.org@gmail.com>
2017-10-20 10:19:06 +0100

    PCHR-2771: Fix indentation in gulpfile

--- hrjobcontract/gulpfile.js
+++ hrjobcontract/gulpfile.js
@@ -10,8 +10,8 @@ var path = require('path');
 var fs = require('fs');
 
 gulp.task('sass', function (done) {
-    // The app style relies on compass's gems, so we need to rely on it
-    // for the time being
+  // The app style relies on compass's gems, so we need to rely on it
+  // for the time being
   exec('compass compile', function (_, stdout, stderr) {
     console.log(stdout);
     done();
@@ -42,12 +42,12 @@ gulp.task('test', function (done) {
 gulp.task('default', ['requirejs-bundle', 'sass', 'test', 'watch']);
 
 var test = (function () {
-    /**
-     * Runs the karma server which does a single run of the test/s
-     *
-     * @param {string} configFile - The full path to the karma config file
-     * @param {Function} cb - The callback to call when the server closes
-     */
+  /**
+   * Runs the karma server which does a single run of the test/s
+   *
+   * @param {string} configFile - The full path to the karma config file
+   * @param {Function} cb - The callback to call when the server closes
+   */
   function runServer (configFile, cb) {
     new karma.Server({
       configFile: __dirname + '/js/' + configFile,
@@ -59,51 +59,51 @@ var test = (function () {
 
   return {
 
-        /**
-         * Runs all the tests
-         */
+    /**
+     * Runs all the tests
+     */
     all: function () {
       runServer('karma.conf.js');
     },
 
-        /**
-         * Runs the tests for a specific source file
-         *
-         * Looks for a test file (*_test.js) in `test/`, using the same path
-         * of the source file in `src/job-contract/`
-         *   i.e. src/job-contract/models/model.js -> test/models/model_test.js
-         *
-         * @param {string} srcFile
-         */
+    /**
+     * Runs the tests for a specific source file
+     *
+     * Looks for a test file (*_test.js) in `test/`, using the same path
+     * of the source file in `src/job-contract/`
+     *   i.e. src/job-contract/models/model.js -> test/models/model_test.js
+     *
+     * @param {string} srcFile
+     */
     for: function (srcFile) {
       var srcFileNoExt = path.basename(srcFile, path.extname(srcFile));
       var testFile = srcFile
-                .replace('src/job-contract/', 'test/')
-                .replace(srcFileNoExt + '.js', srcFileNoExt + '_test.js');
+        .replace('src/job-contract/', 'test/')
+        .replace(srcFileNoExt + '.js', srcFileNoExt + '_test.js');
 
       fs.statSync(testFile).isFile() && this.single(testFile);
     },
 
-        /**
-         * Runs a single test file
-         *
-         * It passes to the karma server a temporary config file
-         * which is deleted once the test has been run
-         *
-         * @param {string} testFile - The full path of a test file
-         */
+    /**
+     * Runs a single test file
+     *
+     * It passes to the karma server a temporary config file
+     * which is deleted once the test has been run
+     *
+     * @param {string} testFile - The full path of a test file
+     */
     single: function (testFile) {
       var configFile = 'karma.' + path.basename(testFile, path.extname(testFile)) + '.conf.temp.js';
 
       gulp.src(__dirname + '/js/karma.conf.js')
-                .pipe(replace('*_test.js', path.basename(testFile)))
-                .pipe(rename(configFile))
-                .pipe(gulp.dest(__dirname + '/js'))
-                .on('end', function () {
-                  runServer(configFile, function () {
-                    gulp.src(__dirname + '/js/' + configFile, { read: false }).pipe(clean());
-                  });
-                });
+        .pipe(replace('*_test.js', path.basename(testFile)))
+        .pipe(rename(configFile))
+        .pipe(gulp.dest(__dirname + '/js'))
+        .on('end', function () {
+          runServer(configFile, function () {
+            gulp.src(__dirname + '/js/' + configFile, { read: false }).pipe(clean());
+          });
+        });
     }
   };
 })();
