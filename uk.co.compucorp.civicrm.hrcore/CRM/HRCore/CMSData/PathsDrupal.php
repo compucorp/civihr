00f37401ee6a408423b4692cde54eb410138fe45
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-10-20 10:48:15 +0200

    PCHR-2619: Use constants for CMS paths

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/CMSData/PathsDrupal.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/CMSData/PathsDrupal.php
@@ -3,22 +3,26 @@
 class CRM_HRCore_CMSData_PathsDrupal implements CRM_HRCore_CMSData_PathsInterface {
 
   /**
+   * @const string
+   */
+  const DEFAULT_IMAGE_PATH = '/%{base}/images/profile-default.png';
+
+  /**
+   * @const string
+   */
+  const EDIT_PATH = '/user/%{userId}/edit';
+
+  /**
+   * @const string
+   */
+  const LOGOUT_PATH = '/user/logout';
+
+  /**
    * The contact data used to build the paths
    *
    * @var array
    */
   private $contactData;
-
-  /**
-   * The Drupal paths
-   *
-   * @var array
-   */
-  private $paths = [
-    'defaultImage' =>  '/%{base}/images/profile-default.png',
-    'edit' => '/user/%{userId}/edit',
-    'logout' => '/user/logout'
-  ];
 
   /**
    * @param array $contactData
@@ -33,20 +37,20 @@ class CRM_HRCore_CMSData_PathsDrupal implements CRM_HRCore_CMSData_PathsInterfac
   public function getDefaultImagePath() {
     $modulePath = drupal_get_path('module', 'civihr_employee_portal');
 
-    return str_replace('%{base}', $modulePath, $this->paths['defaultImage']);
+    return str_replace('%{base}', $modulePath, self::DEFAULT_IMAGE_PATH);
   }
 
   /**
    * {@inheritdoc}
    */
   public function getEditAccountPath() {
-    return str_replace('%{userId}', $this->contactData['cmsId'], $this->paths['edit']);
+    return str_replace('%{userId}', $this->contactData['cmsId'], self::EDIT_PATH);
   }
 
   /**
    * {@inheritdoc}
    */
   public function getLogoutPath() {
-    return $this->paths['logout'];
+    return self::LOGOUT_PATH;
   }
 }
