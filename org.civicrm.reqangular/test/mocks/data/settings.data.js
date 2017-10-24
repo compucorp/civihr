90b2be2911ccbd9bb0a801c7a8f3fc487fc1a601
Buddha Man Nepali <bmnepali980@gmail.com>
2017-10-24 18:22:38 +0545

    PCHR-1849: Add mock data for settings

---
+++ org.civicrm.reqangular/test/mocks/data/settings.data.js
@@ -0,0 +1,18 @@
+/* eslint-env amd */
+
+define(function () {
+  return {
+    settings: {
+      'is_error': 0,
+      'version': 3,
+      'count': 1,
+      'id': 47,
+      'values': [
+        {
+          'monetaryThousandSeparator': "'",
+          'monetaryDecimalPoint': ','
+        }
+      ]
+    }
+  };
+});
