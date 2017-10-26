84ec3dc05d8a74593b9b796e97337b2ce243768f
Davi Alexandre <davi@davialexandre.com.br>
2017-10-26 15:17:25 -0200

    PCHR-2813: Make HRCore the owner of the default CiviHR relationship types
    Now that it's the owner, I also created a service to get the list of line
    managers, a feature that was done by a helper class in HRUi. The helpers
    and its related test were delete from hrui

---
+++ uk.co.compucorp.civicrm.hrcore/tests/phpunit/CRM/HRCore/Service/ManagerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use CRM_HRCore_Test_Fabricator_Contact as ContactFabricator;
+use CRM_HRCore_Test_Fabricator_Relationship as RelationshipFabricator;
+use CRM_HRCore_Service_Manager as ManagerService;
+
+/**
+ * @group headless
+ */
+class CRM_HRCore_Service_ManagerTest extends CRM_HRCore_Test_BaseHeadlessTest {
+
+  public function testGetLineManagersList() {
+    $contactA = ContactFabricator::fabricate([
+      'first_name' => 'chrollo',
+      'last_name' => 'lucilfer'
+    ]);
+
+    $contactB = ContactFabricator::fabricate([
+      'first_name' => 'hisoka',
+      'last_name' => 'morou'
+    ]);
+
+    $contactC = ContactFabricator::fabricate([
+      'first_name' => 'illumi',
+      'last_name' => 'zoldyck'
+    ]);
+
+    $managerService = new ManagerService();
+
+    $this->setContactAsLineManagerOf($contactB, $contactA);
+    $managers = $managerService->getLineManagersFor($contactA['id']);
+
+    $this->assertContains('hisoka morou', $managers);
+    $this->assertCount(1, $managers);
+
+    $this->setContactAsLineManagerOf($contactC, $contactA);
+    $managers = $managerService->getLineManagersFor($contactA['id']);
+
+    $this->assertContains('illumi zoldyck', $managers);
+    $this->assertContains('hisoka morou', $managers);
+    $this->assertCount(2, $managers);
+  }
+
+  private function setContactAsLineManagerOf($manager, $contact) {
+    $relationshipType = civicrm_api3('RelationshipType', 'getsingle', [
+      'return' => ['id'],
+      'name_a_b' => 'Line Manager is',
+    ]);
+
+    RelationshipFabricator::fabricate([
+      'contact_id_a' => $contact['id'],
+      'contact_id_b' => $manager['id'],
+      'relationship_type_id' => $relationshipType['id'],
+    ]);
+  }
+
+
+}
