0a5f241a7f5e07fb5b5738653300025b342c6a9c
Davi Alexandre <davi@davialexandre.com.br>
2017-10-25 22:30:42 -0200

    PCHR-2813: Mock Drupal functions to avoid having to bootstrap the CMS during the tests

--- uk.co.compucorp.civicrm.hrcore/tests/phpunit/CRM/HRCore/Service/DrupalUserServiceTest.php
+++ uk.co.compucorp.civicrm.hrcore/tests/phpunit/CRM/HRCore/Service/DrupalUserServiceTest.php
@@ -1,5 +1,7 @@
 <?php
 
+define('REQUEST_TIME', time());
+
 use CRM_HRCore_Service_DrupalUserService as DrupalUserService;
 use CRM_HRCore_Service_DrupalRoleService as DrupalRoleService;
 use CRM_HRCore_Test_Fabricator_Contact as ContactFabricator;
@@ -61,3 +63,18 @@ class DrupalUserServiceTest extends CRM_HRCore_Test_BaseHeadlessTest {
   }
 
 }
+
+function user_password() {}
+
+function user_load_by_mail($email) {}
+
+function user_save($user, $params) {
+  $user = new stdClass();
+  foreach($params as $key => $value) {
+    $user->$key = $value;
+  }
+
+  return $user;
+}
+
+function drupal_anonymous_user() {}
