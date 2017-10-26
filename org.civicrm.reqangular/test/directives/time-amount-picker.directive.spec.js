5776baa868407f3c0cd4ecbc47ca6210807e782d
Igor Pavlov <pavlov.org@gmail.com>
2017-10-26 15:56:20 +0100

    PCHR-2716: Create time amount picker directive

--- org.civicrm.reqangular/test/directives/time-amount-picker.directive.spec.js
+++ org.civicrm.reqangular/test/directives/time-amount-picker.directive.spec.js
@@ -1,58 +1,156 @@
-/* eslint-env amd */
+/* eslint-env amd, jasmine */
 
 define([
-  'common/moment',
-  'common/modules/directives'
-], function (moment, directives) {
+  'common/angular',
+  'common/lodash',
+  'common/angularMocks',
+  'common/directives/time-amount-picker.directive'
+], function (angular, _) {
   'use strict';
 
-  directives.directive('timepickerSelect', ['$templateCache', function ($templateCache) {
-    return {
-      scope: {
+  describe('timeAmountPicker directive', function () {
+    var vm, $compile, $scope, $rootScope;
 
-        timeAmountPickerValue: '=',
-        timeAmountPickerTimeTo: '<',
-        timeAmountPickerInterval: '<'
-      },
-      restrict: 'A',
-      controllerAs: 'selector',
-      controller: ['$scope', timepickerSelectController],
-      template: $templateCache.get('timepicker-select.html')
-    };
-  }]);
+    beforeEach(module('common.directives'));
+    beforeEach(inject(function (_$rootScope_, _$compile_) {
+      $rootScope = _$rootScope_;
+      $compile = _$compile_;
+    }));
 
-  timeAmountPickerSelectController.$inject = ['$scope', 'notificationService'];
+    describe('when "time-amount-picker" attribute is specified', function () {
+      describe('when no additional attributes are passed', function () {
+        beforeEach(function () {
+          buildDirective();
+        });
 
-  function timepickerSelectController ($scope) {
-    var vm = this;
+        it('builds correct amount of hours options', function () {
+          expect(vm.hoursOptions.length).toBe(25);
+        });
 
-    vm.placeholder = $scope.timepickerSelectPlaceholder;
-    vm.options = [];
+        it('builds correct amount of minutes options', function () {
+          expect(vm.minutesOptions.length).toBe(60);
+        });
+      });
+
+      describe('when "time-amount-picker-min-amount" attribute is passed', function () {
+        beforeEach(function () {
+          buildDirective([
+            { key: 'time-amount-picker-min-amount', value: '2.5', bind: '>' }
+          ]);
+        });
+
+        it('builds correct amount of hours options', function () {
+          expect(vm.hoursOptions.length).toBe(23);
+        });
+
+        it('has expected hours options', function () {
+          expect(_.includes(vm.hoursOptions, 3)).toBeTruthy();
+          expect(_.includes(vm.hoursOptions, 5)).toBeTruthy();
+          expect(_.includes(vm.hoursOptions, 24)).toBeTruthy();
+        });
+
+        it('builds correct amount of minutes options', function () {
+          expect(vm.minutesOptions.length).toBe(60);
+        });
+
+        describe('when hours value is changed and it is less than the minimum specified', function () {
+          beforeEach(function () {
+            vm.selectedHours = 2;
+            vm.buildMinutesOptions();
+          });
+
+          it('builds correct amount of minutes options', function () {
+            expect(vm.minutesOptions.length).toBe(30);
+          });
+        });
+      });
+
+      describe('when "time-amount-picker-max-amount" attribute is passed', function () {
+        beforeEach(function () {
+          buildDirective([
+            { key: 'time-amount-picker-max-amount', value: '7.5', bind: '>' }
+          ]);
+        });
+
+        it('builds correct amount of hours options', function () {
+          expect(vm.hoursOptions.length).toBe(8);
+        });
+
+        it('has expected hours options', function () {
+          expect(_.includes(vm.hoursOptions, 0)).toBeTruthy();
+          expect(_.includes(vm.hoursOptions, 3)).toBeTruthy();
+          expect(_.includes(vm.hoursOptions, 7)).toBeTruthy();
+        });
+
+        it('builds correct amount of minutes options', function () {
+          expect(vm.minutesOptions.length).toBe(60);
+        });
+
+        describe('when hours value is changed and it is same as the maximum specified', function () {
+          beforeEach(function () {
+            vm.selectedHours = 7;
+            vm.buildMinutesOptions();
+          });
+
+          it('builds correct amount of minutes options', function () {
+            expect(vm.minutesOptions.length).toBe(31);
+          });
+        });
+      });
+
+      describe('when "time-amount-picker-value" attribute is passed', function () {
+        beforeEach(function () {
+          buildDirective([
+            { key: 'time-amount-picker-value', value: '6.5', bind: '=' }
+          ]);
+        });
+
+        it('sets selected hours', function () {
+          expect(vm.selectedHours).toBe('6');
+        });
+
+        it('sets selected minutes', function () {
+          expect(vm.selectedMinutes).toBe('30');
+        });
+
+        describe('when selected values are changed', function () {
+          beforeEach(function () {
+            vm.selectedHours = 8;
+            vm.selectedMinutes = 45;
+
+            vm.calculateSelectedValue();
+          });
+
+          it('sets the result value back', function () {
+            expect($scope.$$childTail.timeAmountPickerValue).toBe(8.75);
+          });
+        });
+      });
+    });
 
     /**
-     * Builds options for the selector
+     * Builds a directive
+     *
+     * @param {Array} options objects of attributes keys, values and binding types
+     *   [{ key: 'key', value: 'value', bind: '<|@' }, {...}, ...]
      */
-    function buildOptions () {
-      var interval = +$scope.timepickerSelectInterval || 1;
-      var timeFrom = moment.duration($scope.timepickerSelectTimeFrom || '00:00');
-      var timeTo = moment.duration($scope.timepickerSelectTimeTo || '23:59');
+    function buildDirective (options) {
+      var attrWrapper = '';
+      var element = angular.element('<time-amount-picker></time-amount-picker>');
 
-      vm.options = [];
+      $scope = $rootScope.$new();
+      options = options || [];
 
-      while (timeFrom.asMinutes() <= timeTo.asMinutes()) {
-        var time = moment.utc(timeFrom.asMilliseconds());
+      _.each(options, function (option) {
+        // Depending on a binding type a wrapper might be needed
+        attrWrapper = option.bind === '<' ? '"' : '';
 
-        vm.options.push(time.format('HH:mm'));
-        timeFrom.add(interval, 'minutes');
-      }
+        element.attr(option.key, attrWrapper + option.value + attrWrapper);
+      });
+
+      vm = $compile(element)($scope).controller('timeAmountPicker');
+
+      $scope.$digest();
     }
-
-    $scope.$watchGroup([
-      'timepickerSelectTimeFrom',
-      'timepickerSelectTimeTo',
-      'timepickerSelectInterval'
-    ], function () {
-      buildOptions();
-    });
-  }
+  });
 });
