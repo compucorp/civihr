0eca6e6571d6a0846af0fc990278e68f0e040d9c
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-10-30 16:34:57 +0100

    PCHR-2578: Reindent file

--- hrcase/CRM/HRCase/HRCase.mgd.php
+++ hrcase/CRM/HRCase/HRCase.mgd.php
@@ -3,60 +3,60 @@
 // The record will be automatically inserted, updated, or deleted from the
 // database as appropriate. For more details, see "hook_civicrm_managed" at:
 // http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
- return array (
-          array (
-            'name' => 'WordReplacement People Management',
-            'entity' => 'WordReplacement',
-            'params' =>
-              array (
-                'version' => 3,
-                'find_word' => 'CiviCase',
-                'replace_word'=>'People Management',
-                'is_active' => 1,
-              ),
-          ),
-          array (
-            'name' => 'WordReplacement Assignment',
-            'entity' => 'WordReplacement',
-            'params' =>
-              array (
-                'version' => 3,
-                'find_word' => 'Case',
-                'replace_word'=>'Assignment',
-                'is_active' => 1,
-              ),
-          ),
-          array (
-            'name' => 'WordReplacement Assignments',
-            'entity' => 'WordReplacement',
-            'params' =>
-              array (
-                'version' => 3,
-                'find_word' => 'Cases',
-                'replace_word'=>'Assignments',
-                'is_active' => 1,
-              ),
-          ),
-          array (
-            'name' => 'WordReplacement Client',
-            'entity' => 'WordReplacement',
-            'params' =>
-            array (
-              'version' => 3,
-              'find_word' => 'Client',
-              'replace_word'=>'Contact',
-              'is_active' => 1,
-            ),
-          ),
-          array (
-            'name' => 'WordReplacement case',
-            'entity' => 'WordReplacement',
-            'params' =>
-            array (
-              'version' => 3,
-              'find_word' => 'case',
-              'replace_word'=>'assignment',
-              'is_active' => 1,
-            ),
-          )
-        );
+return array (
+  array (
+    'name' => 'WordReplacement People Management',
+    'entity' => 'WordReplacement',
+    'params' =>
+    array (
+      'version' => 3,
+      'find_word' => 'CiviCase',
+      'replace_word'=>'People Management',
+      'is_active' => 1,
+    ),
+  ),
+  array (
+    'name' => 'WordReplacement Assignment',
+    'entity' => 'WordReplacement',
+    'params' =>
+    array (
+      'version' => 3,
+      'find_word' => 'Case',
+      'replace_word'=>'Assignment',
+      'is_active' => 1,
+    ),
+  ),
+  array (
+    'name' => 'WordReplacement Assignments',
+    'entity' => 'WordReplacement',
+    'params' =>
+    array (
+      'version' => 3,
+      'find_word' => 'Cases',
+      'replace_word'=>'Assignments',
+      'is_active' => 1,
+    ),
+  ),
+  array (
+    'name' => 'WordReplacement Client',
+    'entity' => 'WordReplacement',
+    'params' =>
+    array (
+      'version' => 3,
+      'find_word' => 'Client',
+      'replace_word'=>'Contact',
+      'is_active' => 1,
+    ),
+  ),
+  array (
+    'name' => 'WordReplacement case',
+    'entity' => 'WordReplacement',
+    'params' =>
+    array (
+      'version' => 3,
+      'find_word' => 'case',
+      'replace_word'=>'assignment',
+      'is_active' => 1,
+    ),
+  )
+);
