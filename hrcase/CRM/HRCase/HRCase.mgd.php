9bc9a118aa679aec03a96198ccf1101740cf9ecc
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-10-30 16:43:06 +0100

    PCHR-2638: Use shorter array notation

--- hrcase/CRM/HRCase/HRCase.mgd.php
+++ hrcase/CRM/HRCase/HRCase.mgd.php
@@ -3,71 +3,71 @@
 // The record will be automatically inserted, updated, or deleted from the
 // database as appropriate. For more details, see "hook_civicrm_managed" at:
 // http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
-return array (
-  array (
+return [
+  [
     'name' => 'WordReplacement CiviCase -> Workflow',
     'entity' => 'WordReplacement',
     'params' =>
-    array (
+    [
       'version' => 3,
       'find_word' => 'CiviCase',
       'replace_word'=>'Workflow',
       'is_active' => 1,
-    ),
-  ),
-  array (
+    ],
+  ],
+  [
     'name' => 'WordReplacement Case -> Workflow',
     'entity' => 'WordReplacement',
     'params' =>
-    array (
+    [
       'version' => 3,
       'find_word' => 'Case',
       'replace_word'=>'Workflow',
       'is_active' => 1,
-    ),
-  ),
-  array (
+    ],
+  ],
+  [
     'name' => 'WordReplacement Cases -> Workflows',
     'entity' => 'WordReplacement',
     'params' =>
-    array (
+    [
       'version' => 3,
       'find_word' => 'Cases',
       'replace_word'=>'Workflows',
       'is_active' => 1,
-    ),
-  ),
-  array (
+    ],
+  ],
+  [
     'name' => 'WordReplacement Client -> Contact',
     'entity' => 'WordReplacement',
     'params' =>
-    array (
+    [
       'version' => 3,
       'find_word' => 'Client',
       'replace_word'=>'Contact',
       'is_active' => 1,
-    ),
-  ),
-  array (
+    ],
+  ],
+  [
     'name' => 'WordReplacement case -> workflow',
     'entity' => 'WordReplacement',
     'params' =>
-    array (
+    [
       'version' => 3,
       'find_word' => 'case',
       'replace_word'=>'workflow',
       'is_active' => 1,
-    ),
-  ),
-  array (
+    ],
+  ],
+  [
     'name' => 'WordReplacement cases -> workflows',
     'entity' => 'WordReplacement',
     'params' =>
-    array (
+    [
       'version' => 3,
       'find_word' => 'cases',
       'replace_word'=>'workflows',
       'is_active' => 1,
-    ),
-  )
-);
+    ],
+  ]
+];
