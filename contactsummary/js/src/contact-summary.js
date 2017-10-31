6090f3db477acecaa6bb3afc06c3f4ce4a1346b0
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-10-31 23:37:36 +0100

    PCHR-2861: Apply semistandard to misc files

--- contactsummary/js/src/contact-summary.js
+++ contactsummary/js/src/contact-summary.js
@@ -1,3 +1,5 @@
+/* eslint-env amd */
+
 (function (CRM, require) {
   require.config({
     urlArgs: 'bust=' + (new Date()).getTime(),
@@ -7,7 +9,9 @@
   });
 
   require(['contact-summary/modules/contact-summary.module'], function () {
-    document.dispatchEvent(typeof window.CustomEvent == "function" ? new CustomEvent('contactsummaryReady') : (function () {
+    'use strict';
+
+    document.dispatchEvent(typeof window.CustomEvent === 'function' ? new window.CustomEvent('contactsummaryReady') : (function () {
       var e = document.createEvent('Event');
       e.initEvent('contactsummaryReady', true, true);
       return e;
