d3e0da4ec99f32920d333084fe138d53043a5931
Buddha Man Nepali <bmnepali980@gmail.com>
2017-10-31 19:25:18 +0545

    PCHR-1849: Update currency format service + tests

--- org.civicrm.reqangular/src/common/services/format-currency.service.js
+++ org.civicrm.reqangular/src/common/services/format-currency.service.js
@@ -2,66 +2,34 @@
 
 define([
   'common/modules/services',
-  'common/services/api'
+  'common/services/api',
+  'common/models/settings.model'
 ], function (services) {
   'use strict';
 
   services.factory('FormatCurrencyService', FormatCurrencyService);
 
-  FormatCurrencyService.$inject = ['api', '$q', '$log'];
+  FormatCurrencyService.$inject = ['$log', '$q', 'Settings'];
 
-  function FormatCurrencyService (api, $q, $log) {
+  function FormatCurrencyService ($log, $q, Settings) {
     $log.debug('Service: FormatCurrencyService');
 
-    var promise;
-
     return {
-      addSeperators: addSeperators,
-      removeCharacters: removeCharacters
+      format: format
     };
 
     /**
-     * Adds defined thousand and decimal separators in the given amount
-     *
-     * @param {string} amount
-     * @return {string}
-     */
-    function addSeperators (amount) {
-      return fetchSeparators().then(function (separators) {
-        // Add thousand seperator to amount using regex expression
-        var withThousandSeparator = amount.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1' + separators.thousand);
-        var formattedCurrency = withThousandSeparator.replace('.', separators.decimal);
-
-        return (+amount) ? formattedCurrency : ('0' + separators.decimal + '00');
-      });
-    }
-
-    /**
-     * Fetched the thousand and decimal seperators form backend
-     *
-     * @return {promise}
-     */
-    function fetchSeparators () {
-      promise = promise || getSettings();
-
-      return promise.then(function (result) {
+    * Adds defined thousand and decimal separators in the given amount
+    *
+    * @param {string} amount
+    * @return {promise}
+    */
+    function format (amount) {
+      return Settings.fetchSeparators().then(function (separators) {
         return {
-          decimal: result.monetaryDecimalPoint,
-          thousand: result.monetaryThousandSeparator
+          formatted: addSeparators(amount, separators),
+          unformatted: removeSeparators(amount, separators)
         };
-      });
-    }
-
-    /**
-     * Gets defined settings data form backend
-     *
-     * @param  {Object} params
-     *
-     * @return {Promise}
-     */
-    function getSettings () {
-      return api.sendGET('Setting', 'get').then(function (data) {
-        return data.values[0];
       });
     }
 
@@ -71,16 +39,86 @@ define([
      * removing the thousand separators and replacing the decimal separator by " ."
      *
      * @param  {string|Integer} amount
+     * @param  {object} separators
      * @return {integer}
      */
-    function removeCharacters (amount) {
-      return fetchSeparators().then(function (separators) {
-        var expression = '[^0-9' + separators.decimal + ']';
-        var strippedAmount = amount.toString().replace(new RegExp(expression, 'g'), '');
-        var amountWithDecimal = strippedAmount.replace(separators.decimal, '.');
+    function removeSeparators (amount, separators) {
+      var expression = configureExpression(separators.decimal);
+      var strippedAmount = amount.toString().replace(new RegExp(expression, 'g'), '');
+      var amountWithDecimal = strippedAmount.replace(separators.decimal, '.');
 
-        return amountWithDecimal;
-      });
+      return amountWithDecimal;
+    }
+
+    /**
+     * Configure expression for special characters like \ and ]
+     *
+     * @param  {string} decimalSeparator
+     * @return {string}
+     */
+    function configureExpression (decimalSeparator) {
+      var expression;
+
+      // Handle reserved regex characters that might be used as separators
+      switch (decimalSeparator) {
+        case '\\':
+          expression = '[^0-9\\\\]';
+          break;
+        case ']':
+          expression = '[^0-9\\]]';
+          break;
+        default:
+          expression = '[^0-9' + decimalSeparator + ']';
+      }
+
+      return expression;
+    }
+
+    /**
+     * Formats the given amount with thousand separators
+     *
+     * @param  {string|integer} amount
+     * @param  {object} separators
+     * @return {string}
+     */
+    function addSeparators (amount, separators) {
+      var amountAfterDecimal; // Keep 2 decimal values after decimal
+      var amountBeforeDecimal;
+      var decimalIndex;
+      var expression;
+
+      // Handling special case:
+      // If we are given to format the amount with decimal as "." ,
+      // we need to change "." into defined decimal separator for proper formatting
+      if (+amount && (amount.toString()).indexOf('.') !== -1) {
+        amount = (amount.toString()).replace('.', separators.decimal);
+      }
+
+      // Regex expression to remove all non numeric values
+      // except defined decimal separator in the string
+      expression = configureExpression(separators.decimal);
+
+      // Remove all characters except decimal characters
+      amount = amount.toString().replace(new RegExp(expression, 'g'), '');
+
+      // Find the index of first ocuring decimal separator
+      decimalIndex = amount.indexOf(separators.decimal);
+
+      if (decimalIndex !== -1) {
+        // Hold the whole number before decimal separator
+        amountBeforeDecimal = +(amount.substring(0, decimalIndex));
+        // Remove all non numeric characters form values after decimal separator
+        amountAfterDecimal = amount.substring(decimalIndex + 1).replace(/[^0-9]/g, '');
+      } else {
+        amountBeforeDecimal = +(amount.replace(/[^0-9]/g, ''));
+      }
+
+      // Add thousand separators to the value before decimal separator
+      // converting it to string
+      amountBeforeDecimal = amountBeforeDecimal.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1' + separators.thousand);
+
+      // if amountAfterDecimal exists, concat amountBeforeDecimal and amountAfterDecimal by separator
+      return (decimalIndex !== -1) ? (amountBeforeDecimal.toString() + separators.decimal + amountAfterDecimal) : amountBeforeDecimal;
     }
   }
 });
--- org.civicrm.reqangular/test/services/format-currency.spec.js
+++ org.civicrm.reqangular/test/services/format-currency.spec.js
@@ -1,113 +1,81 @@
 /* eslint-env amd, jasmine */
 
 define([
-  'common/mocks/data/settings.data',
   'common/angular',
+  'common/lodash',
   'common/angularMocks',
-  'common/services/api',
   'common/services/format-currency.service'
-], function (setingsMock) {
+], function (angular, _) {
   'use strict';
 
   describe('FormatCurrencyService', function () {
-    var $httpBackend, $rootScope, FormatCurrencyService;
+    var $rootScope, FormatCurrencyService, Settings, deffered, promise;
 
-    beforeEach(module('common.apis', 'common.services'));
-    beforeEach(inject(function (_FormatCurrencyService_, _$httpBackend_, _$rootScope_) {
-      FormatCurrencyService = _FormatCurrencyService_;
-      $httpBackend = _$httpBackend_;
-      $rootScope = _$rootScope_;
+    beforeEach(module('common.services'));
+    beforeEach(module(function ($provide) {
+      $provide.factory('Settings', function () {
+        return {
+          fetchSeparators: jasmine.any(Function)
+        };
+      });
     }));
 
-    afterEach(function () {
-      $httpBackend.flush();
-      $rootScope.$apply();
-    });
+    beforeEach(inject(function (_$q_, _$rootScope_, _Settings_, _FormatCurrencyService_) {
+      $rootScope = _$rootScope_;
+      Settings = _Settings_;
+      FormatCurrencyService = _FormatCurrencyService_;
+      deffered = _$q_.defer();
+    }));
 
-    describe("when ' and , are thousand and decimal separators respectively", function () {
-      beforeEach(function () {
-        $httpBackend.whenGET(/entity=Setting/).respond(setingsMock.settings);
-      });
-      describe('addSeperators()', function () {
-        describe('when the amount is without decimal', function () {
-          it('formats the given amount using defined decimal and thousands separator', function () {
-            FormatCurrencyService.addSeperators(6000000000).then(function (result) {
-              expect(result).toEqual("6'000'000'000");
+    var testMap = [
+      // [thousandSeparator, decimalSeparator, inputAmount, outputAmount]
+      // Common cases
+      [',', '*', 12345675, '12,345,675'],
+      [',', '*', 123456.75, '123,456*75'],
+      [',', '*', '123456.7.5', '12,345,675'],
+      [',', '*', '12345675', '12,345,675'],
+      [',', '*', '12,345,675', '12,345,675'],
+      [',', '*', '00,456,75*76', '45,675*76'],
+      [',', '*', '12345675*76', '12,345,675*76'],
+      [',', '*', '12,34,56*75*76', '123,456*7576'],
+      [',', '*', '123 456 75*76', '12,345,675*76'],
+
+      // Edge cases
+      ['?', '^', 'abcd!89', '89'],
+      ['?', '!', '00000!89', '0!89'],
+      ["'", ',', '1234567,76', "1'234'567,76"],
+      ['*', '^', '1234567^76', '1*234*567^76'],
+      ['$', '%', '12#345&67^76', '123$456$776'],
+      [' ', ',', '1234567,76', '1 234 567,76'],
+      ['^', ']', '12,34,567]76', '1^234^567]76'],
+      ['#', ')', '1#234#567)76', '1#234#567)76'],
+      ['!', '@', '   78  9098 @65', '789!098@65'],
+      ['^', '\\', '1,234,567\\76', '1^234^567\\76']
+    ];
+
+    _.forEach(testMap, function (test) {
+      describe('format()', function () {
+        beforeEach(function () {
+          spyOn(Settings, 'fetchSeparators').and.callFake(function () {
+            deffered.resolve({
+              decimal: test[1],
+              thousand: test[0]
             });
+
+            return deffered.promise;
           });
+
+          promise = FormatCurrencyService.format(test[2]);
         });
 
-        describe('when the amount is with decimal', function () {
-          it('formats the given amount using defined decimal and thousands separator', function () {
-            FormatCurrencyService.addSeperators(60000000.67).then(function (result) {
-              expect(result).toEqual("60'000'000,67");
-            });
-          });
-        });
-      });
-
-      describe('removeCharacters()', function () {
-        describe('when the amount is without decimal', function () {
-          it('removes the non numerical characters except decimal character "." making it calulable', function () {
-            FormatCurrencyService.removeCharacters("799'600'000'00").then(function (result) {
-              expect(result).toEqual('79960000000');
-              expect(+result).toEqual(jasmine.any(Number));
-            });
-          });
+        afterEach(function () {
+          $rootScope.$apply();
         });
 
-        describe('when the amount is with decimal', function () {
-          it('removes the non numerical characters except decimal character "." making it calulable', function () {
-            FormatCurrencyService.removeCharacters("799'600'000,00").then(function (result) {
-              expect(result).toEqual('799600000.00');
-              expect(+result).toEqual(jasmine.any(Number));
-            });
-          });
-        });
-      });
-    });
-
-    describe('when # and @ are thousand and decimal separators respectively', function () {
-      beforeEach(function () {
-        setingsMock.settings.values[0].monetaryThousandSeparator = '#';
-        setingsMock.settings.values[0].monetaryDecimalPoint = '@';
-        $httpBackend.whenGET(/entity=Setting/).respond(setingsMock.settings);
-      });
-
-      describe('addSeperators()', function () {
-        describe('when the amount is without decimal', function () {
-          it('formats the given amount using defined decimal and thousands separator', function () {
-            FormatCurrencyService.addSeperators(6000000000).then(function (result) {
-              expect(result).toEqual('6#000#000#000');
-            });
-          });
-        });
-
-        describe('when the amount is with decimal', function () {
-          it('formats the given amount using defined decimal and thousands separator', function () {
-            FormatCurrencyService.addSeperators(60000000.67).then(function (result) {
-              expect(result).toEqual('60#000#000@67');
-            });
-          });
-        });
-      });
-
-      describe('removeCharacters()', function () {
-        describe('when the amount is without decimal', function () {
-          it('removes the non numerical characters except decimal character "." making it calulable', function () {
-            FormatCurrencyService.removeCharacters('799#600#000$00').then(function (result) {
-              expect(result).toEqual('79960000000');
-              expect(+result).toEqual(jasmine.any(Number));
-            });
-          });
-        });
-
-        describe('when the amount is with decimal', function () {
-          it('removes the non numerical characters except decimal character "." making it calulable', function () {
-            FormatCurrencyService.removeCharacters('799#600#000@00').then(function (result) {
-              expect(result).toEqual('799600000.00');
-              expect(+result).toEqual(jasmine.any(Number));
-            });
+        it('formats' + test[2] + ' to ' + test[3] + ' by ' + test[1] + ' and ' + test[0] + ' as decimal and thousand separators', function () {
+          promise.then(function (amount) {
+            expect(amount.formatted).toBe(test[3]);
+            expect(+amount.unformatted).toEqual(jasmine.any(Number));
           });
         });
       });
