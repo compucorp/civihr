c65a5320211372795055bb95cd4fd26b666aaadb
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-11-01 01:06:09 +0100

    PCHR-2861: Create core module

--- contactsummary/js/src/contact-summary/modules/contact-summary.module.js
+++ contactsummary/js/src/contact-summary/modules/contact-summary.module.js
@@ -2,19 +2,16 @@
 
 define([
   'common/angular',
-  'common/services/pub-sub',
   'contact-summary/modules/contact-summary.config',
   'contact-summary/modules/contact-summary.constants',
   'contact-summary/modules/contact-summary.controllers',
+  'contact-summary/modules/contact-summary.core',
   'contact-summary/modules/contact-summary.directives',
   'contact-summary/modules/contact-summary.run',
   'contact-summary/modules/contact-summary.services'
 ], function (angular) {
   angular.module('contactsummary', [
-    'ngRoute',
-    'ngResource',
-    'ui.bootstrap',
-    'common.services',
+    'contactsummary.core',
     'contactsummary.config',
     'contactsummary.run',
     'contactsummary.constants',
