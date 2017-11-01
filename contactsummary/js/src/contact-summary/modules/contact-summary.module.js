bafa57ce9e7e21ef972aaf5cdc52f6d17decb966
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-11-01 01:02:22 +0100

    PCHR-2861: Remove unused filters module

--- contactsummary/js/src/contact-summary/modules/contact-summary.module.js
+++ contactsummary/js/src/contact-summary/modules/contact-summary.module.js
@@ -7,7 +7,6 @@ define([
   'contact-summary/modules/contact-summary.constants',
   'contact-summary/modules/contact-summary.controllers',
   'contact-summary/modules/contact-summary.directives',
-  'contact-summary/modules/contact-summary.filters',
   'contact-summary/modules/contact-summary.run',
   'contact-summary/modules/contact-summary.services'
 ], function (angular) {
@@ -21,7 +20,6 @@ define([
     'contactsummary.constants',
     'contactsummary.controllers',
     'contactsummary.directives',
-    'contactsummary.filters',
     'contactsummary.services'
   ]);
 });
