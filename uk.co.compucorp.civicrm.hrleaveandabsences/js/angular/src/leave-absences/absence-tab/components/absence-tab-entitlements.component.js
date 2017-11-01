0a76376bcc2430948f942da97205abe5f2d9083b
René Olivo <minet.ws@gmail.com>
2017-11-01 10:17:58 -0400

    PCHR-2713: Load absence types and their calculation units

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/absence-tab/components/absence-tab-entitlements.component.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/absence-tab/components/absence-tab-entitlements.component.js
@@ -1,8 +1,9 @@
 /* eslint-env amd */
 
 define([
+  'common/lodash',
   'leave-absences/absence-tab/modules/components'
-], function (components) {
+], function (_, components) {
   components.component('absenceTabEntitlements', {
     bindings: {
       contactId: '<'
@@ -11,16 +12,53 @@ define([
       return settings.pathTpl + 'components/absence-tab-entitlements.html';
     }],
     controllerAs: 'entitlements',
-    controller: ['$log', controller]
+    controller: AbsenceTabEntitlementsController
   });
 
-  function controller ($log) {
+  AbsenceTabEntitlementsController.$inject = ['$q', '$log', 'AbsenceType',
+    'OptionGroup'];
+
+  function AbsenceTabEntitlementsController ($q, $log, AbsenceType, OptionGroup) {
     $log.debug('Component: absence-tab-entitlements');
 
-    var vm = {};
+    var calculationUnits = [];
+    var vm = this;
 
-    vm.contactId = this.contactId;
+    vm.absenceTypes = [];
+    vm.loading = { component: true };
 
-    return vm;
+    (function init () {
+      $q.all([
+        loadAbsenceTypes(),
+        loadCalculationUnits()
+      ])
+      .then(mapAbsenceTypeUnits)
+      .finally(function () {
+        vm.loading.component = false;
+      });
+    })();
+
+    function loadAbsenceTypes () {
+      return AbsenceType.all().then(function (data) {
+        vm.absenceTypes = data;
+      });
+    }
+
+    function loadCalculationUnits () {
+      return OptionGroup
+        .valuesOf('hrleaveandabsences_absence_type_calculation_unit')
+        .then(function (_calculationUnits_) {
+          calculationUnits = _.indexBy(_calculationUnits_, 'value');
+        });
+    }
+
+    function mapAbsenceTypeUnits () {
+      vm.absenceTypes.forEach(function (absenceType) {
+        var unit = calculationUnits[absenceType.calculation_unit];
+
+        absenceType['calculation_unit.name'] = unit.name;
+        absenceType['calculation_unit.label'] = unit.label;
+      });
+    }
   }
 });
--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/absence-tab/components/absence-tab-entitlements.component.spec.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/absence-tab/components/absence-tab-entitlements.component.spec.js
@@ -1,6 +1,7 @@
 /* eslint-env amd, jasmine */
 
 define([
+  'common/lodash',
   'common/moment',
   'common/angular',
   'common/angularMocks',
@@ -12,11 +13,12 @@ define([
   'mocks/apis/absence-period-api-mock',
   'mocks/apis/entitlement-api-mock',
   'leave-absences/shared/modules/shared-settings'
-], function (moment, angular) {
+], function (_, moment, angular) {
   'use strict';
 
   describe('absenceTabEntitlements', function () {
-    var $componentController, $log, $rootScope, controller, $provide;
+    var $componentController, $log, $provide, $rootScope, AbsenceType,
+      controller, OptionGroup;
 
     beforeEach(module('leave-absences.templates', 'absence-tab', 'common.mocks', 'leave-absences.mocks', function (_$provide_) {
       $provide = _$provide_;
@@ -29,15 +31,20 @@ define([
     }));
 
     beforeEach(inject(['api.contract.mock', 'api.contact.mock',
-      function (_ContractAPIMock_, _ContactAPIMock_) {
+      'api.optionGroup.mock', function (_ContractAPIMock_, _ContactAPIMock_,
+      _OptionGroupAPIMock_) {
         $provide.value('api.contract', _ContractAPIMock_);
         $provide.value('api.contact', _ContactAPIMock_);
+        $provide.value('api.optionGroup', _OptionGroupAPIMock_);
       }]));
 
-    beforeEach(inject(function (_$componentController_, _$log_, _$rootScope_) {
+    beforeEach(inject(function (_$componentController_, _$log_, _$rootScope_,
+    _AbsenceType_, _OptionGroup_) {
       $componentController = _$componentController_;
       $log = _$log_;
       $rootScope = _$rootScope_;
+      AbsenceType = _AbsenceType_;
+      OptionGroup = _OptionGroup_;
 
       spyOn($log, 'debug');
 
@@ -52,9 +59,68 @@ define([
       expect(controller.contactId).toBeDefined();
     });
 
+    it('sets loading component to true', function () {
+      expect(controller.loading.component).toBe(true);
+    });
+
+    describe('After Loading absence types', function () {
+      var expectedAbsenceTypes;
+
+      beforeEach(function () {
+        getIndexedCalculationUnits()
+        .then(getExpectedAbsenceTypes)
+        .then(function (absenceTypes) {
+          expectedAbsenceTypes = absenceTypes;
+        });
+        $rootScope.$digest();
+      });
+
+      it('stores the absence types and their calculation units', function () {
+        expect(controller.absenceTypes).toEqual(expectedAbsenceTypes);
+      });
+
+      it('sets loading component to false', function () {
+        expect(controller.loading.component).toBe(false);
+      });
+    });
+
+    /**
+     * Compiles and store the absence tab entitlement controller.
+     */
     function compileComponent () {
       controller = $componentController('absenceTabEntitlements', null, { contactId: '202' });
-      $rootScope.$digest();
+    }
+
+    /**
+     * Returns a calculation units map indexed by value.
+     *
+     * @return {Promise} - resolves to the calculation units map.
+     */
+    function getIndexedCalculationUnits () {
+      return OptionGroup
+      .valuesOf('hrleaveandabsences_absence_type_calculation_unit')
+      .then(function (calculationUnits) {
+        return _.indexBy(calculationUnits, 'value');
+      });
+    }
+
+    /**
+     * Returns a list of absence types with their calculation units names and
+     * labels.
+     *
+     * @return {Promise} - resolves to a list of absence types.
+     */
+    function getExpectedAbsenceTypes (calculationUnits) {
+      return AbsenceType.all().then(function (absenceTypes) {
+        return absenceTypes.map(function (absenceType) {
+          var unit = calculationUnits[absenceType.calculation_unit];
+
+          return _.extend({
+            'calculation_unit.name': unit.name,
+            'calculation_unit.label': unit.label
+          }, absenceType);
+        });
+      });
     }
   });
 });
