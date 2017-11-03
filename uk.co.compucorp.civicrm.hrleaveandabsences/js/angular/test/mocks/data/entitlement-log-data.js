214a5a74dc66d2a5e3fe1c623cc5659322d903ac
René Olivo <minet.ws@gmail.com>
2017-11-03 00:09:49 -0400

    PCHR-2713: Create EntitlementLog API

---
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/mocks/data/entitlement-log-data.js
@@ -0,0 +1,84 @@
+/* eslint-env amd */
+
+define(function () {
+  var allData = {
+    'is_error': 0,
+    'version': 3,
+    'count': 9,
+    'values': [
+      {
+        'id': '1',
+        'entitlement_id': '1',
+        'editor_id': '207',
+        'entitlement_amount': '33.00',
+        'created_date': '2017-11-02 14:36:38'
+      },
+      {
+        'id': '2',
+        'entitlement_id': '2',
+        'editor_id': '207',
+        'entitlement_amount': '0.00',
+        'created_date': '2017-11-02 14:36:38'
+      },
+      {
+        'id': '3',
+        'entitlement_id': '3',
+        'editor_id': '207',
+        'entitlement_amount': '5.00',
+        'created_date': '2017-11-02 14:36:38'
+      },
+      {
+        'id': '4',
+        'entitlement_id': '1',
+        'editor_id': '207',
+        'entitlement_amount': '33.00',
+        'created_date': '2017-11-02 14:57:27'
+      },
+      {
+        'id': '5',
+        'entitlement_id': '2',
+        'editor_id': '207',
+        'entitlement_amount': '10.00',
+        'comment': 'Increased TOIL entitlement',
+        'created_date': '2017-11-02 14:57:27'
+      },
+      {
+        'id': '6',
+        'entitlement_id': '3',
+        'editor_id': '207',
+        'entitlement_amount': '15.00',
+        'comment': 'Fixed wrong sick leaves entitlement amount',
+        'created_date': '2017-11-02 14:57:27'
+      },
+      {
+        'id': '7',
+        'entitlement_id': '1',
+        'editor_id': '207',
+        'entitlement_amount': '33.00',
+        'created_date': '2017-11-02 15:25:49'
+      },
+      {
+        'id': '8',
+        'entitlement_id': '2',
+        'editor_id': '207',
+        'entitlement_amount': '20.00',
+        'comment': 'Added missing TOIL entitlements.',
+        'created_date': '2017-11-02 15:25:49'
+      },
+      {
+        'id': '9',
+        'entitlement_id': '3',
+        'editor_id': '207',
+        'entitlement_amount': '15.00',
+        'comment': 'Fixed wrong sick leaves entitlement amount',
+        'created_date': '2017-11-02 15:25:49'
+      }
+    ]
+  };
+
+  return {
+    all: function () {
+      return allData;
+    }
+  };
+});
