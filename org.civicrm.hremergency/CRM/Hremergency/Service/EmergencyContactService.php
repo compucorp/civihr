ddf4d1dcffc3baf46024af0054c38bb06828d38c
Michael Devery <michael@compucorp.co.uk>
2017-11-09 14:10:26 +0000

    PCHR-2884: Change service to return emergency contact with field names as array keys instead of column names

--- org.civicrm.hremergency/CRM/Hremergency/Service/EmergencyContactService.php
+++ org.civicrm.hremergency/CRM/Hremergency/Service/EmergencyContactService.php
@@ -8,6 +8,13 @@ class CRM_Hremergency_Service_EmergencyContactService {
   private $tableName = 'civicrm_value_emergency_contacts_21';
 
   /**
+   * @var array
+   */
+  private $fieldMapping = [];
+
+  /**
+   * Finds an emergency contact record by ID
+   *
    * @param int $id
    *
    * @return array|NULL
@@ -22,16 +29,46 @@ class CRM_Hremergency_Service_EmergencyContactService {
       return NULL;
     }
 
-    return current($emergencyContacts);
+    $emergencyContact = current($emergencyContacts);
+
+    return $this->replaceColumnsWithName($emergencyContact);
   }
 
   /**
+   * Deletes an emergency contact by ID
+   *
    * @param int $id
    */
   public function delete($id) {
     $dao = CRM_Core_BAO_CustomGroup::class;
     $groupID = CRM_Core_DAO::getFieldValue($dao, 'Emergency_Contacts', 'id', 'name');
     CRM_Core_BAO_CustomValue::deleteCustomValue($id, $groupID);
+  }
+
+  /**
+   * Replaces database column keys in the contact data with their field name.
+   *
+   * @param array $emergencyContact
+   *
+   * @return array
+   */
+  private function replaceColumnsWithName($emergencyContact) {
+    if (empty($this->fieldMapping)) {
+      $params = ['custom_group_id' => 'Emergency_Contacts'];
+      $fields = civicrm_api3('CustomField', 'get', $params);
+      $this->fieldMapping = $fields['values'];
+    }
+
+    foreach ($this->fieldMapping as $fieldInfo) {
+      $column = $fieldInfo['column_name'];
+      $name = $fieldInfo['name'];
+      if (array_key_exists($column, $emergencyContact)) {
+        $emergencyContact[$name] = $emergencyContact[$column];
+        unset($emergencyContact[$column]);
+      }
+    }
+
+    return $emergencyContact;
   }
 
 }
