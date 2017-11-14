bd1b759e03f4e93719a7e4a503dfd7c97b7d6ade
Igor Pavlov <pavlov.org@gmail.com>
2017-11-14 16:53:38 +0000

    PCHR-2923: Wrap tests in Contract Entitlements

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/absence-tab/components/contract-entitlements.component.spec.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/test/absence-tab/components/contract-entitlements.component.spec.js
@@ -17,7 +17,7 @@ define([
   describe('contactEntitlements', function () {
     var contactId = 202;
     var $componentController, $log, $rootScope, controller, $provide,
-      absenceTypes, ContractAPIMock, HRSettingsMock;
+      ContractAPIMock, HRSettingsMock;
 
     beforeEach(module('leave-absences.templates', 'absence-tab', 'common.mocks', 'leave-absences.mocks', function (_$provide_) {
       $provide = _$provide_;
@@ -36,69 +36,76 @@ define([
       $rootScope = _$rootScope_;
 
       spyOn($log, 'debug');
-
-      compileComponent();
     }));
 
-    it('is initialized', function () {
-      expect($log.debug).toHaveBeenCalled();
-    });
-
-    it('has a contact to load for', function () {
-      expect(controller.contactId).toEqual(contactId);
-    });
-
-    it('has loaded contracts', function () {
-      expect(controller.contracts).toEqual(jasmine.any(Array));
-    });
-
-    describe('contract', function () {
-      var contract, mockedContract;
-
+    describe('common tests', function () {
       beforeEach(function () {
-        contract = controller.contracts[0];
-        mockedContract = ContractAPIMock.mockedContracts()[0].info.details;
+        compileComponent();
       });
 
-      it('has position', function () {
-        expect(contract.position).toEqual(mockedContract.position);
+      it('is initialized', function () {
+        expect($log.debug).toHaveBeenCalled();
       });
 
-      it('has start date', function () {
-        expect(contract.start_date).toEqual(formatDate(mockedContract.period_start_date));
+      it('has a contact to load for', function () {
+        expect(controller.contactId).toEqual(contactId);
       });
 
-      it('has end date', function () {
-        expect(contract.end_date).toEqual(formatDate(mockedContract.period_end_date));
+      it('has loaded contracts', function () {
+        expect(controller.contracts).toEqual(jasmine.any(Array));
       });
 
-      it('has absences', function () {
-        expect(contract.absences).toEqual(jasmine.any(Array));
-      });
-
-      describe('absence', function () {
-        var absence, mockedAbsence;
+      describe('contract', function () {
+        var contract, mockedContract;
 
         beforeEach(function () {
-          absence = contract.absences[0];
-          mockedAbsence = ContractAPIMock.mockedContracts()[0].info.leave[0];
+          contract = controller.contracts[0];
+          mockedContract = ContractAPIMock.mockedContracts()[0].info.details;
         });
 
-        it('has amount', function () {
-          expect(absence.amount).toEqual(mockedAbsence.leave_amount);
+        it('has position', function () {
+          expect(contract.position).toEqual(mockedContract.position);
         });
 
-        it('has the calculation unit name', function () {
-          expect(absence.calculation_unit).toMatch(/days|hours/);
+        it('has start date', function () {
+          expect(contract.start_date).toEqual(formatDate(mockedContract.period_start_date));
+        });
+
+        it('has end date', function () {
+          expect(contract.end_date).toEqual(formatDate(mockedContract.period_end_date));
+        });
+
+        it('has absences', function () {
+          expect(contract.absences).toEqual(jasmine.any(Array));
+        });
+
+        describe('absence', function () {
+          var absence, mockedAbsence;
+
+          beforeEach(function () {
+            absence = contract.absences[0];
+            mockedAbsence = ContractAPIMock.mockedContracts()[0].info.leave[0];
+          });
+
+          it('has amount', function () {
+            expect(absence.amount).toEqual(mockedAbsence.leave_amount);
+          });
+
+          it('has the calculation unit name', function () {
+            expect(absence.calculation_unit).toMatch(/days|hours/);
+          });
         });
       });
     });
 
     /**
      * Compiles the controller
+     *
+     * @param {Array} absenceTypes
      */
-    function compileComponent () {
-      absenceTypes = absenceTypeMocked.getAllAndTheirCalculationUnits();
+    function compileComponent (absenceTypes) {
+      absenceTypes = absenceTypes ||
+        absenceTypeMocked.getAllAndTheirCalculationUnits();
       controller = $componentController('contractEntitlements', null, {
         absenceTypes: absenceTypes,
         contactId: contactId
