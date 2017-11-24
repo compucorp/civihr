2938b80c44fb5f3ca1744318810b09b689c5e0bc
Igor Pavlov <pavlov.org@gmail.com>
2017-11-24 16:17:58 +0000

    PCHR-2963: Apply Styleguide to Absence Tab Container

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/absence-tab/components/absence-tab-container.component.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/absence-tab/components/absence-tab-container.component.js
@@ -9,15 +9,19 @@
         return settings.pathTpl + 'components/absence-tab-container.html';
       }],
       controllerAs: 'absence',
-      controller: ['$log', '$rootScope', function ($log, $rootScope) {
-        $log.debug('Component: absence-tab-container');
-
-        $rootScope.section = 'absence-tab';
-
-        var vm = this;
-
-        vm.contactId = CRM.adminId;
-      }]
+      controller: AbsenceTabContainerController
     });
+
+    AbsenceTabContainerController.$inject = ['$log', '$rootScope'];
+
+    function AbsenceTabContainerController ($log, $rootScope) {
+      $log.debug('Component: absence-tab-container');
+
+      $rootScope.section = 'absence-tab';
+
+      var vm = this;
+
+      vm.contactId = CRM.adminId;
+    }
   });
 })(CRM);
