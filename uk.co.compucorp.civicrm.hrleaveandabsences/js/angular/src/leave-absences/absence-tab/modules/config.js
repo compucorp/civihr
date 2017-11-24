c3c477c3c5ac3eab4707384e046d01dd258e8a32
Igor Pavlov <pavlov.org@gmail.com>
2017-11-24 15:56:23 +0000

    PCHR-2963: Unwrap Absence Tab config from CRM

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/absence-tab/modules/config.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/absence-tab/modules/config.js
@@ -1,17 +1,15 @@
-(function (CRM) {
-  define([
-    'common/angular',
-    'leave-absences/absence-tab/modules/settings',
-  ], function (angular) {
-    return angular.module('absence-tab.config', ['absence-tab.settings'])
-      .config([
-        '$resourceProvider', '$httpProvider', '$logProvider', 'settings',
-        function ($resourceProvider, $httpProvider, $logProvider, settings) {
-          $logProvider.debugEnabled(settings.debug);
+define([
+  'common/angular',
+  'leave-absences/absence-tab/modules/settings',
+], function (angular) {
+  return angular.module('absence-tab.config', ['absence-tab.settings'])
+    .config([
+      '$resourceProvider', '$httpProvider', '$logProvider', 'settings',
+      function ($resourceProvider, $httpProvider, $logProvider, settings) {
+        $logProvider.debugEnabled(settings.debug);
 
-          $resourceProvider.defaults.stripTrailingSlashes = false;
-          $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-        }
-      ]);
-  });
-})(CRM);
+        $resourceProvider.defaults.stripTrailingSlashes = false;
+        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+      }
+    ]);
+});
