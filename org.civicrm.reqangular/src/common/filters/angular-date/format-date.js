d94c833c6c521700cb1bf3404dad1102a283582f
Igor Pavlov <pavlov.org@gmail.com>
2017-11-24 15:43:44 +0000

    PCHR-2963: Run semistandard on the date filter

--- org.civicrm.reqangular/src/common/filters/angular-date/format-date.js
+++ org.civicrm.reqangular/src/common/filters/angular-date/format-date.js
@@ -1,31 +1,30 @@
 define([
-    'common/moment',
-    'common/modules/angular-date',
-    'common/services/hr-settings'
+  'common/moment',
+  'common/modules/angular-date',
+  'common/services/hr-settings'
 ], function (moment, module) {
-    'use strict';
+  'use strict';
 
-    module.filter('formatDate', ['HR_settings', function (HR_settings) {
+  module.filter('formatDate', ['HR_settings', function (HR_settings) {
+    var validFormats = ['DD-MM-YYYY', 'DD-MM-YYYY HH:mm:ss', 'YYYY-MM-DD',
+      'YYYY-MM-DD HH:mm:ss', 'DD/MM/YYYY', 'x'];
 
-        var validFormats = ['DD-MM-YYYY', 'DD-MM-YYYY HH:mm:ss', 'YYYY-MM-DD',
-            'YYYY-MM-DD HH:mm:ss', 'DD/MM/YYYY', 'x'];
+    return function (datetime, format) {
+      var date;
+      var dateFormat = format || HR_settings.DATE_FORMAT || 'YYYY-MM-DD';
+      var beginningOfEra = moment(0);
 
-        return function (datetime, format) {
-            var date;
-            var dateFormat = format || HR_settings.DATE_FORMAT || 'YYYY-MM-DD';
-            var beginningOfEra = moment(0);
+      if (datetime instanceof Date) {
+        datetime = moment(datetime).format('YYYY-MM-DD HH:mm:ss');
+      }
 
-            if (datetime instanceof Date) {
-                datetime = moment(datetime).format('YYYY-MM-DD HH:mm:ss');
-            }
+      date = moment(datetime, validFormats, true);
 
-            date = moment(datetime, validFormats, true);
+      if (date.isValid() && !date.isSame(beginningOfEra)) {
+        return format === Date ? date.toDate() : date.format(dateFormat.toUpperCase());
+      }
 
-            if (date.isValid() && !date.isSame(beginningOfEra)) {
-                return format === Date ? date.toDate() : date.format(dateFormat.toUpperCase());
-            }
-
-            return 'Unspecified';
-        };
-    }]);
+      return 'Unspecified';
+    };
+  }]);
 });
