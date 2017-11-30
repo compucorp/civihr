d6837a50972b2b74dd02692224820d10f9c973be
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-11-30 16:35:52 +0100

    PCHR-3008: Apply semistandard to the directives

--- hrjobcontract/js/src/job-contract/directives/loader.directive.js
+++ hrjobcontract/js/src/job-contract/directives/loader.directive.js
@@ -1,56 +1,57 @@
+/* eslint-env amd */
+
 define([
-    'job-contract/modules/job-contract.directives'
+  'job-contract/modules/job-contract.directives'
 ], function (directives) {
-    'use strict';
+  'use strict';
 
-    directives.directive('hrjcLoader',['$rootScope','$log', function ($rootScope, $log) {
-        $log.debug('Directive: hrjcLoader');
+  directives.directive('hrjcLoader', ['$rootScope', '$log', function ($rootScope, $log) {
+    $log.debug('Directive: hrjcLoader');
 
-        return {
-            link: function ($scope, el, attrs) {
-                var loader = document.createElement('div'),
-                    loaderSet = false,
-                    positionSet = false;
+    return {
+      link: function ($scope, el, attrs) {
+        var loader = document.createElement('div');
+        var loaderSet = false;
+        var positionSet = false;
 
-                loader.className = 'hrjc-loader';
+        loader.className = 'hrjc-loader';
 
-                function isPositioned(){
-                    var elPosition = window.getComputedStyle(el[0]).position;
-                    return elPosition == 'relative' || elPosition == 'absolute' || elPosition == 'fixed'
-                }
-
-                function appendLoader() {
-                    if (!isPositioned()) {
-                        el.css('position','relative');
-                        positionSet = true;
-                    }
-
-                    el.append(loader);
-                    loaderSet = true;
-                }
-
-                function removeLoader(){
-                    loaderSet && loader.parentNode.removeChild(loader);
-                    loaderSet = false;
-
-                    if (positionSet) {
-                        el.css('position','');
-                    }
-                }
-
-                if (attrs.hrjcLoaderShow) {
-                    appendLoader();
-                }
-
-                $scope.$on('hrjc-loader-show',function(){
-                    appendLoader();
-                });
-
-                $scope.$on('hrjc-loader-hide',function(){
-                    removeLoader();
-                });
-
-            }
+        function isPositioned () {
+          var elPosition = window.getComputedStyle(el[0]).position;
+          return elPosition === 'relative' || elPosition === 'absolute' || elPosition === 'fixed';
         }
-    }]);
+
+        function appendLoader () {
+          if (!isPositioned()) {
+            el.css('position', 'relative');
+            positionSet = true;
+          }
+
+          el.append(loader);
+          loaderSet = true;
+        }
+
+        function removeLoader () {
+          loaderSet && loader.parentNode.removeChild(loader);
+          loaderSet = false;
+
+          if (positionSet) {
+            el.css('position', '');
+          }
+        }
+
+        if (attrs.hrjcLoaderShow) {
+          appendLoader();
+        }
+
+        $scope.$on('hrjc-loader-show', function () {
+          appendLoader();
+        });
+
+        $scope.$on('hrjc-loader-hide', function () {
+          removeLoader();
+        });
+      }
+    };
+  }]);
 });
--- hrjobcontract/js/src/job-contract/directives/validate.directive.js
+++ hrjobcontract/js/src/job-contract/directives/validate.directive.js
@@ -1,70 +1,73 @@
+/* eslint-env amd */
+
 define([
-    'job-contract/modules/job-contract.directives'
-], function (directives) {
-    'use strict';
+  'common/angular',
+  'job-contract/modules/job-contract.directives'
+], function (angular, directives) {
+  'use strict';
 
-    directives.directive('hrjcValidate',['$log', function ($log) {
-        $log.debug('Directive: hrjcValidate');
+  directives.directive('hrjcValidate', ['$log', function ($log) {
+    $log.debug('Directive: hrjcValidate');
 
-        return {
-            restrict: 'A',
-            require:  '^form',
-            scope: {
-                isWarning: '=?hrjcValidateWarning'
-            },
-            link: function ($scope, el, attrs, formCtrl) {
-                var inputEl   = el[0].querySelector("[name]"),
-                    inputNgEl = angular.element(inputEl),
-                    inputName = inputNgEl.attr('name'),
-                    iconEl = document.createElement('span'),
-                    iconNgEl = angular.element(iconEl);
+    return {
+      restrict: 'A',
+      require: '^form',
+      scope: {
+        isWarning: '=?hrjcValidateWarning'
+      },
+      link: function ($scope, el, attrs, formCtrl) {
+        var inputEl = el[0].querySelector('[name]');
+        var inputNgEl = angular.element(inputEl);
+        var inputName = inputNgEl.attr('name');
+        var iconEl = document.createElement('span');
+        var iconNgEl = angular.element(iconEl);
 
-                if (!inputName) {
-                    return;
-                }
-
-                el.addClass('has-feedback');
-                iconNgEl.addClass('glyphicon form-control-feedback');
-                inputNgEl.after(iconNgEl);
-
-                function toggleSuccess(invalid, isWarning) {
-                    el.toggleClass('has-success', !invalid && !isWarning);
-                    iconNgEl.toggleClass('glyphicon-ok', !invalid && !isWarning);
-                }
-
-                function toggleWarning(invalid, isWarning) {
-                    el.toggleClass('has-warning', !invalid && isWarning);
-                    iconNgEl.toggleClass('glyphicon-warning-sign', !invalid && isWarning);
-                }
-
-                function toggleError(invalid) {
-                    el.toggleClass('has-error', invalid);
-                    iconNgEl.toggleClass('glyphicon-remove', invalid);
-                }
-
-                $scope.$watch(function() {
-                    return formCtrl[inputName] && formCtrl[inputName].$invalid;
-                }, function(invalid) {
-                    if (formCtrl[inputName].$dirty) {
-                        toggleSuccess(invalid, $scope.isWarning);
-                        toggleError(invalid);
-                    }
-                });
-
-                if (typeof $scope.isWarning !== 'undefined') {
-                    $scope.$watch('isWarning', function(isWarning) {
-                        var invalid = formCtrl[inputName].$invalid;
-                        if (formCtrl[inputName].$dirty) {
-                            toggleSuccess(invalid, isWarning);
-                            toggleWarning(invalid, isWarning);
-                        }
-                    });
-                }
-
-                inputNgEl.bind('blur', function() {
-                    toggleError(formCtrl[inputName].$invalid)
-                });
-            }
+        if (!inputName) {
+          return;
         }
-    }]);
+
+        el.addClass('has-feedback');
+        iconNgEl.addClass('glyphicon form-control-feedback');
+        inputNgEl.after(iconNgEl);
+
+        function toggleSuccess (invalid, isWarning) {
+          el.toggleClass('has-success', !invalid && !isWarning);
+          iconNgEl.toggleClass('glyphicon-ok', !invalid && !isWarning);
+        }
+
+        function toggleWarning (invalid, isWarning) {
+          el.toggleClass('has-warning', !invalid && isWarning);
+          iconNgEl.toggleClass('glyphicon-warning-sign', !invalid && isWarning);
+        }
+
+        function toggleError (invalid) {
+          el.toggleClass('has-error', invalid);
+          iconNgEl.toggleClass('glyphicon-remove', invalid);
+        }
+
+        $scope.$watch(function () {
+          return formCtrl[inputName] && formCtrl[inputName].$invalid;
+        }, function (invalid) {
+          if (formCtrl[inputName].$dirty) {
+            toggleSuccess(invalid, $scope.isWarning);
+            toggleError(invalid);
+          }
+        });
+
+        if (typeof $scope.isWarning !== 'undefined') {
+          $scope.$watch('isWarning', function (isWarning) {
+            var invalid = formCtrl[inputName].$invalid;
+            if (formCtrl[inputName].$dirty) {
+              toggleSuccess(invalid, isWarning);
+              toggleWarning(invalid, isWarning);
+            }
+          });
+        }
+
+        inputNgEl.bind('blur', function () {
+          toggleError(formCtrl[inputName].$invalid);
+        });
+      }
+    };
+  }]);
 });
