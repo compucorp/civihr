5a3263c915cc415bdb2097189d16093964dd01fd
Michael Devery <michael@compucorp.co.uk>
2017-12-06 15:06:09 +0000

    PCHR-3030: Add upgrader to create scheduled job

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader.php
@@ -11,6 +11,7 @@ class CRM_HRCore_Upgrader extends CRM_HRCore_Upgrader_Base {
   use CRM_HRCore_Upgrader_Steps_1003;
   use CRM_HRCore_Upgrader_Steps_1004;
   use CRM_HRCore_Upgrader_Steps_1005;
+  use CRM_HRCore_Upgrader_Steps_1006;
 
   /**
    * @var array
