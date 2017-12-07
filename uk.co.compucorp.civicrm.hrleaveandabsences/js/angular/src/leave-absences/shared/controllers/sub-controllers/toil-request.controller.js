202ad2ce51813be552ff3d5daf3d3005f6513efa
Igor Pavlov <pavlov.org@gmail.com>
2017-12-07 11:01:42 +0000

    PCHR-2814: Rename variable in TOIL controller

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/shared/controllers/sub-controllers/toil-request.controller.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/shared/controllers/sub-controllers/toil-request.controller.js
@@ -11,19 +11,17 @@ define([
   function ToilRequestCtrl ($log, $q, $rootScope, OptionGroup, AbsenceType, parentCtrl) {
     $log.debug('ToilRequestCtrl');
 
-    var vm = parentCtrl;
+    parentCtrl.requestCanExpire = true;
 
-    vm.requestCanExpire = true;
-
-    vm.calculateBalanceChange = calculateBalanceChange;
-    vm.calculateToilExpiryDate = calculateToilExpiryDate;
-    vm.canCalculateChange = canCalculateChange;
-    vm.changeInNoOfDaysExtended = changeInNoOfDaysExtended;
-    vm.checkSubmitConditions = checkSubmitConditions;
-    vm.clearExpiryDate = clearExpiryDate;
-    vm.initChildController = initChildController;
-    vm.setDatesFromUIExtended = setDatesFromUIExtended;
-    vm.updateExpiryDate = updateExpiryDate;
+    parentCtrl.calculateBalanceChange = calculateBalanceChange;
+    parentCtrl.calculateToilExpiryDate = calculateToilExpiryDate;
+    parentCtrl.canCalculateChange = canCalculateChange;
+    parentCtrl.changeInNoOfDaysExtended = changeInNoOfDaysExtended;
+    parentCtrl.checkSubmitConditions = checkSubmitConditions;
+    parentCtrl.clearExpiryDate = clearExpiryDate;
+    parentCtrl.initChildController = initChildController;
+    parentCtrl.setDatesFromUIExtended = setDatesFromUIExtended;
+    parentCtrl.updateExpiryDate = updateExpiryDate;
 
     (function init () {
       initAccrueValueWatcher();
@@ -36,9 +34,9 @@ define([
      * @return {Promise} resolves with the balance change
      */
     function calculateBalanceChange () {
-      vm.balance.change.amount = +vm.request.toil_to_accrue;
+      parentCtrl.balance.change.amount = +parentCtrl.request.toil_to_accrue;
 
-      return $q.resolve(vm.balance.change);
+      return $q.resolve(parentCtrl.balance.change);
     }
 
     /**
@@ -49,29 +47,29 @@ define([
     function calculateToilExpiryDate () {
       // blocks the expiry date from updating if this is an existing request
       // and user is not a manager or admin
-      if (!vm.canManage && vm.request.id) {
-        return $q.resolve(vm.request.toil_expiry_date);
+      if (!parentCtrl.canManage && parentCtrl.request.id) {
+        return $q.resolve(parentCtrl.request.toil_expiry_date);
       }
 
       // skips calculation of expiration date if request never expires
       // according to admin setting
-      if (!vm.requestCanExpire) {
-        vm.request.toil_expiry_date = false;
+      if (!parentCtrl.requestCanExpire) {
+        parentCtrl.request.toil_expiry_date = false;
         return $q.resolve(false);
       }
 
       return getReferenceDate().catch(function (errors) {
-        if (errors.length) vm.errors = errors;
+        if (errors.length) parentCtrl.errors = errors;
         return $q.reject(errors);
       }).then(function (referenceDate) {
         return AbsenceType.calculateToilExpiryDate(
-          vm.request.type_id,
+          parentCtrl.request.type_id,
           referenceDate
         );
       })
       .then(function (expiryDate) {
-        vm.request.toil_expiry_date = expiryDate;
-        vm.uiOptions.expiryDate = new Date(expiryDate);
+        parentCtrl.request.toil_expiry_date = expiryDate;
+        parentCtrl.uiOptions.expiryDate = new Date(expiryDate);
 
         return expiryDate;
       });
@@ -83,7 +81,7 @@ define([
      * @return {Boolean}
      */
     function canCalculateChange () {
-      return !!vm.request.toil_to_accrue;
+      return !!parentCtrl.request.toil_to_accrue;
     }
 
     /**
@@ -93,8 +91,8 @@ define([
      * @return {Boolean}
      */
     function canCalculateExpiryDate () {
-      return (vm.uiOptions.multipleDays && vm.request.to_date) ||
-        (!vm.uiOptions.multipleDays && vm.request.from_date);
+      return (parentCtrl.uiOptions.multipleDays && parentCtrl.request.to_date) ||
+        (!parentCtrl.uiOptions.multipleDays && parentCtrl.request.from_date);
     }
 
     /**
@@ -103,8 +101,8 @@ define([
      * @return {Boolean}
      */
     function checkSubmitConditions () {
-      return !!vm.request.from_date && !!vm.request.to_date &&
-        !!vm.request.toil_duration && !!vm.request.toil_to_accrue;
+      return !!parentCtrl.request.from_date && !!parentCtrl.request.to_date &&
+        !!parentCtrl.request.toil_duration && !!parentCtrl.request.toil_to_accrue;
     }
 
     /**
@@ -121,8 +119,8 @@ define([
      * Clears the request's expiry date and the UI expiry date picker.
      */
     function clearExpiryDate () {
-      vm.request.toil_expiry_date = false;
-      vm.uiOptions.expiryDate = null;
+      parentCtrl.request.toil_expiry_date = false;
+      parentCtrl.uiOptions.expiryDate = null;
     }
 
     /**
@@ -132,17 +130,17 @@ define([
      * @return {Promise}
      */
     function getReferenceDate () {
-      if (vm.uiOptions.multipleDays) {
+      if (parentCtrl.uiOptions.multipleDays) {
         return getReferenceDateForField({
-          hasErrors: !vm.request.to_date && !vm.request.from_date,
+          hasErrors: !parentCtrl.request.to_date && !parentCtrl.request.from_date,
           label: 'To Date',
-          value: vm.request.to_date
+          value: parentCtrl.request.to_date
         });
       } else {
         return getReferenceDateForField({
-          hasErrors: !vm.request.from_date,
+          hasErrors: !parentCtrl.request.from_date,
           label: 'From Date',
-          value: vm.request.from_date
+          value: parentCtrl.request.from_date
         });
       }
     }
@@ -175,7 +173,7 @@ define([
      * @return {Promise}
      */
     function initChildController () {
-      vm.request.to_date_type = vm.request.from_date_type = '1';
+      parentCtrl.request.to_date_type = parentCtrl.request.from_date_type = '1';
 
       return initRequestCanExpire()
         .then(function () {
@@ -189,8 +187,8 @@ define([
      * Initialize expiryDate on UI from server's toil_expiry_date
      */
     function initExpiryDate () {
-      if (vm.canManage) {
-        vm.uiOptions.expiryDate = vm.convertDateFormatFromServer(vm.request.toil_expiry_date);
+      if (parentCtrl.canManage) {
+        parentCtrl.uiOptions.expiryDate = parentCtrl.convertDateFormatFromServer(parentCtrl.request.toil_expiry_date);
       }
     }
 
@@ -198,13 +196,13 @@ define([
      * Initialises watcher for accrue value
      */
     function initAccrueValueWatcher () {
-      if (vm.isMode('view')) { return; }
+      if (parentCtrl.isMode('view')) { return; }
 
       $rootScope.$watch(
-        function () { return vm.request.toil_to_accrue; },
+        function () { return parentCtrl.request.toil_to_accrue; },
         function (oldValue, newValue) {
           if (+oldValue !== +newValue) {
-            vm.attemptCalculateBalanceChange();
+            parentCtrl.attemptCalculateBalanceChange();
           }
         });
     }
@@ -215,9 +213,9 @@ define([
      * @return {Promise}
      */
     function initRequestCanExpire () {
-      return AbsenceType.canExpire(vm.request.type_id)
+      return AbsenceType.canExpire(parentCtrl.request.type_id)
       .then(function (canExpire) {
-        vm.requestCanExpire = canExpire;
+        parentCtrl.requestCanExpire = canExpire;
       });
     }
 
@@ -229,7 +227,7 @@ define([
     function loadToilAmounts () {
       return OptionGroup.valuesOf('hrleaveandabsences_toil_amounts')
         .then(function (amounts) {
-          vm.toilAmounts = _.indexBy(amounts, 'value');
+          parentCtrl.toilAmounts = _.indexBy(amounts, 'value');
         });
     }
 
@@ -239,15 +237,15 @@ define([
      * @return {Promise}
      */
     function setDatesFromUIExtended () {
-      return vm.calculateToilExpiryDate().catch($q.resolve);
+      return parentCtrl.calculateToilExpiryDate().catch($q.resolve);
     }
 
     /**
      * Updates expiry date when user changes it on ui
      */
     function updateExpiryDate () {
-      if (vm.uiOptions.expiryDate) {
-        vm.request.toil_expiry_date = vm.convertDateToServerFormat(vm.uiOptions.expiryDate);
+      if (parentCtrl.uiOptions.expiryDate) {
+        parentCtrl.request.toil_expiry_date = parentCtrl.convertDateToServerFormat(parentCtrl.uiOptions.expiryDate);
       }
     }
   }
