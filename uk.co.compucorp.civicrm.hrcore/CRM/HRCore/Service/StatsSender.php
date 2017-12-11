d8de1b9499c6207c8ef8b57ae8cc10786493c375
Michael Devery <michael@compucorp.co.uk>
2017-12-11 18:11:09 +0000

    PCHR-3029: Add logging

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Service/StatsSender.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Service/StatsSender.php
@@ -3,6 +3,7 @@
 use CRM_HRCore_Service_CiviHRStatsGatherer as StatsGatherer;
 use CRM_HRCore_Model_CiviHRStatistics as CiviHRStatistics;
 use CRM_HRCore_Service_CiviHRStatisticsJSONConvertor as CiviHRStatisticsJSONConvertor;
+use Psr\Log\LoggerInterface;
 
 class CRM_HRCore_Service_StatsSender {
 
@@ -20,15 +21,23 @@ class CRM_HRCore_Service_StatsSender {
   protected $statsGatherer;
 
   /**
+   * @var LoggerInterface
+   */
+  protected $logger;
+
+  /**
    * @param CRM_Core_BAO_Cache $cache
    * @param StatsGatherer $statsGatherer
+   * @param LoggerInterface $logger
    */
   public function __construct(
     CRM_Core_BAO_Cache $cache,
-    StatsGatherer $statsGatherer
+    StatsGatherer $statsGatherer,
+    LoggerInterface $logger
   ) {
     $this->cache = $cache;
     $this->statsGatherer = $statsGatherer;
+    $this->logger = $logger;
   }
 
   /**
@@ -45,7 +54,21 @@ class CRM_HRCore_Service_StatsSender {
     }
 
     $json = CiviHRStatisticsJSONConvertor::toJson($cachedStats);
-    // todo send to server
+    $ch = $this->prepareRequest($json);
+    $response = curl_exec($ch);
+
+    // todo error checking on real response
+    if (!$response) {
+      $msg = sprintf('Failed sending CiviHR stats: %s', curl_error($ch));
+      $this->logger->error($msg);
+      throw new \Exception($msg);
+    }
+
+    // Log successful sending
+    $siteName = $cachedStats->getSiteName();
+    $nowFmt = (new \DateTime())->format(\DateTime::ISO8601);
+    $msg = sprintf('Successfully sent stats from %s at %s', $siteName, $nowFmt);
+    $this->logger->info($msg);
   }
 
   /**
@@ -59,6 +82,26 @@ class CRM_HRCore_Service_StatsSender {
     $oneWeekAgo = new \DateTime('now - 1 week 00:00:00');
 
     return $oneWeekAgo > $cachedStats->getGenerationDate();
+  }
+
+  /**
+   * @param $json
+   *
+   * @return resource
+   */
+  private function prepareRequest($json) {
+    $headers = [
+      'Content-Type: application/json',
+      'Content-Length: ' . strlen($json)
+    ];
+
+    $ch = curl_init('http://localhost:8000');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    return $ch;
   }
 
 }
