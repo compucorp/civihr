bf4ddf40dbe83c5e0c6385cf7a6dc0b52be49090
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-12-12 09:31:52 +0100

    PCHR-3008: Avoid conflicts between native and filter parseInt

--- hrjobcontract/js/src/job-contract/modules/job-contract.filters.js
+++ hrjobcontract/js/src/job-contract/modules/job-contract.filters.js
@@ -6,8 +6,8 @@ define([
   'job-contract/filters/format-amount.filter',
   'job-contract/filters/format-period.filter',
   'job-contract/filters/get-obj-by-id.filter',
-  'job-contract/filters/parse-int.filter'
-], function (angular, capitalize, formatAmount, formatPeriod, getObjById, parseInt) {
+  'job-contract/filters/parse-integer.filter'
+], function (angular, capitalize, formatAmount, formatPeriod, getObjById, parseInteger) {
   'use strict';
 
   return angular.module('job-contract.filters', [])
@@ -15,5 +15,5 @@ define([
     .filter(formatAmount.__name, formatAmount)
     .filter(formatPeriod.__name, formatPeriod)
     .filter(getObjById.__name, getObjById)
-    .filter(parseInt.__name, parseInt);
+    .filter(parseInteger.__name, parseInteger);
 });
