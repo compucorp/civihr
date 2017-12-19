d8bc29ba37f621ab4b81a769e58914bdc10496fb
Michael Devery <michael@compucorp.co.uk>
2017-12-19 10:53:31 +0000

    PCHR-3046: Add implementation for latestLoginByRole
    Also remove internal cache as it's not likely to improve performance
    much and could lead to problems if a role is changed during the request
    lifecycle

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/CMSData/Role/DrupalRoleService.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/CMSData/Role/DrupalRoleService.php
@@ -5,12 +5,6 @@ use CRM_HRCore_CMSData_Role_RoleServiceInterface as RoleServiceInterface;
 class CRM_HRCore_CMSData_Role_DrupalRoleService implements RoleServiceInterface{
 
   /**
-   * @var array
-   *   A cache of the Drupal roles
-   */
-  private $roleList = [];
-
-  /**
    * @inheritdoc
    */
   public function getRoleNames() {
@@ -21,19 +15,39 @@ class CRM_HRCore_CMSData_Role_DrupalRoleService implements RoleServiceInterface{
    * @inheritdoc
    */
   public function getLatestLoginByRole($roleName) {
+    $rids = $this->getRoleIds([$roleName]);
+    $rid = array_shift($rids);
 
+    if (empty($rid)) {
+      throw new \Exception(sprintf('Role "%s" not found', $roleName));
+    }
+
+    $query = db_select('users', 'u');
+    $query->addExpression('MAX(u.login)');
+    $query->leftJoin('users_roles', 'ur', 'ur.uid = u.uid');
+    $query->where('ur.rid = :rid', ['rid' => $rid]);
+
+    $result = $query->execute()->fetchCol();
+    $latestLoginTimestamp = array_shift($result);
+
+    if ($latestLoginTimestamp == 0) {
+      return NULL;
+    }
+
+    $latestLoginDate = new \DateTime();
+    $latestLoginDate->setTimestamp($latestLoginTimestamp);
+
+    return $latestLoginDate;
   }
 
   /**
    * @inheritdoc
    */
   public function getRoleIds($roles) {
-    if (empty($this->roleList)) {
-      $this->roleList = user_roles(TRUE);
-    }
+    $roleList = user_roles(TRUE);
 
     $roleIds = [];
-    foreach ($this->roleList as $rid => $role) {
+    foreach ($roleList as $rid => $role) {
       if (in_array($role, $roles)) {
         $roleIds[$rid] = $rid;
       }
