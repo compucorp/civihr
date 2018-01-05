b8a3efd936b03fa3a4d930079ba32ba1fd72accd
Davi Alexandre <davi@davialexandre.com.br>
2018-01-05 13:37:38 -0200

    PCHR-3211: Add the Reports page to CiviHR admin

---
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Page/Reports.php
@@ -0,0 +1,11 @@
+<?php
+
+class CRM_HRCore_Page_Reports extends CRM_Core_Page {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function run() {
+    return parent::run();
+  }
+}
---
+++ uk.co.compucorp.civicrm.hrcore/templates/CRM/HRCore/Page/Reports.tpl
@@ -0,0 +1,3 @@
+<iframe id="reportIframe" src="/reports" frameborder="0" scrolling="no" style="width: 100%"></iframe>
+<script type="text/javascript">
+</script>
