4ffd34985f521c3402150d151f1ca74dc5aa8fc6
Tunbola Ogunwande <tunbolawande@yahoo.com>
2018-01-12 18:55:05 +0100

    PCHR-3073: Use the UserNotifier container in the notifier page url callbacks.

--- uk.co.compucorp.civicrm.hrcontactactionsmenu/CRM/HRContactActionsMenu/Page/UserMailNotifier.php
+++ uk.co.compucorp.civicrm.hrcontactactionsmenu/CRM/HRContactActionsMenu/Page/UserMailNotifier.php
@@ -1,6 +1,5 @@
 <?php
 
-use CRM_HRCore_CMSData_UserMailNotifierFactory as CMSUserMailNotifierFactory;
 use CRM_HRContactActionsMenu_Helper_Contact as ContactHelper;
 
 /**
@@ -14,8 +13,10 @@ class CRM_HRContactActionsMenu_Page_UserMailNotifier {
    */
   public static function sendPasswordResetEmail() {
     $contactID = CRM_Utils_Array::value('cid', $_GET);
-    $cmsUserMailNotifier = self::getCMSUserMailNotifier($contactID);
-    $cmsUserMailNotifier->sendPasswordResetEmail();
+    $contactInfo = ContactHelper::getUserInformation($contactID);
+    $cmsUserMailNotifier = Civi::container()->get('hrcore.cms_notifier');
+    $user = $cmsUserMailNotifier->getUser($contactInfo);
+    $cmsUserMailNotifier->sendPasswordResetEmail($user);
 
     CRM_Core_Session::setStatus(ts('Password Reset Email sent'), 'Success', 'success');
 
@@ -29,29 +30,14 @@ class CRM_HRContactActionsMenu_Page_UserMailNotifier {
    */
   public static function sendWelcomeEmail() {
     $contactID = CRM_Utils_Array::value('cid', $_GET);
-    $cmsUserMailNotifier = self::getCMSUserMailNotifier($contactID);
-    $cmsUserMailNotifier->sendWelcomeEmail();
+    $contactInfo = ContactHelper::getUserInformation($contactID);
+    $cmsUserMailNotifier = Civi::container()->get('hrcore.cms_notifier');
+    $user = $cmsUserMailNotifier->getUser($contactInfo);
+    $cmsUserMailNotifier->sendWelcomeEmail($user);
 
     CRM_Core_Session::setStatus(ts('Welcome Email sent'), 'Success', 'success');
 
     $url = CRM_Utils_System::url('civicrm/contact/view', "reset=1&cid=$contactID");
     CRM_Utils_System::redirect($url);
-  }
-
-  /**
-   * Gets the CMSNotifier class depending on the CMS framework
-   *
-   * @param int $contactID
-   *
-   * @return \CRM_HRCore_CMSData_UserMailNotifierInterface
-   *
-   * @throws \Exception
-   */
-  private static function getCMSUserMailNotifier($contactID) {
-    $cmsFramework = CRM_Core_Config::singleton()->userFramework;
-    $contactUserInfo = ContactHelper::getUserInformation($contactID);
-    $contactUserInfo['cmsId'] = $contactUserInfo['id'];
-
-    return CMSUserMailNotifierFactory::create($cmsFramework, $contactUserInfo);
   }
 }
