a1a16f4cf438d6777f7bde881a3e4441a5f56fa4
Buddha Man Nepali <bmnepali980@gmail.com>
2018-01-12 17:13:25 +0545

    PCHR-3087: Move code from hrui to contact actionsmenu extension

--- hrui/js/src/contact.js
+++ hrui/js/src/contact.js
@@ -22,16 +22,6 @@
       if (typeof data.roles !== 'undefined')  {
         updateContactHeaderRolesDetails(data.roles);
       }
-    })
-
-    // Actions menu
-    .on('click', function(e) {
-      if ($(e.target).is('#crm-contact-actions-link, #crm-contact-actions-link *')) {
-        $('#crm-contact-actions-link').addClass('active');
-        
-        return false;
-      }
-      $('#crm-contact-actions-link').removeClass('active');
     });
 
   /**
