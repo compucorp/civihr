bea5329653bb60612bc726fe4d7cb20a42a952f5
Buddha Man Nepali <bmnepali980@gmail.com>
2018-01-12 19:27:29 +0545

    PCHR-3087: Add civicrm scssroot module

--- uk.co.compucorp.civicrm.hrcontactactionsmenu/gulpfile.js
+++ uk.co.compucorp.civicrm.hrcontactactionsmenu/gulpfile.js
@@ -1,14 +1,20 @@
+var civicrmScssRoot = require('civicrm-scssroot')();
 var gulp = require('gulp');
 var bulk = require('gulp-sass-bulk-import');
 var sass = require('gulp-sass');
 
-gulp.task('sass', [], function () {
+gulp.task('sass', ['sass:sync'], function () {
   return gulp.src('scss/*.scss')
     .pipe(bulk())
     .pipe(sass({
-      outputStyle: 'compressed'
+      outputStyle: 'compressed',
+      includePaths: civicrmScssRoot.getPath()
     }).on('error', sass.logError))
     .pipe(gulp.dest('css/'));
+});
+
+gulp.task('sass:sync', function () {
+  civicrmScssRoot.updateSync();
 });
 
 gulp.task('watch', function () {
