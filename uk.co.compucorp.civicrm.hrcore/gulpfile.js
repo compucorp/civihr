9bb9302c0120142d20115644c3aa7c0b81db1902
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-01-13 18:54:12 +0100

    PCHR-3084: Make test object accessible in whole gulpfile

--- uk.co.compucorp.civicrm.hrcore/gulpfile.js
+++ uk.co.compucorp.civicrm.hrcore/gulpfile.js
@@ -227,11 +227,6 @@ var xml = require("xml-parse");
 
 // Test
 (function () {
-  var find = require('find');
-  var karma = require('karma');
-  var replace = require('gulp-replace');
-  var rename = require('gulp-rename');
-
   gulp.task('test', function (cb) {
     var sequence = addExtensionCustomTasksToSequence(['test:main'], 'test');
 
@@ -253,62 +248,6 @@ var xml = require("xml-parse");
       test.single(file.path);
     });
   });
-
-  var test = (function () {
-    /**
-     * Runs the karma server which does a single run of the test/s
-     *
-     * @param {string} configFile - The full path to the karma config file
-     * @param {Function} cb - The callback to call when the server closes
-     */
-    function runServer (configFile, cb) {
-      var reporters = argv.reporters ? argv.reporters.split(',') : ['progress'];
-
-      new karma.Server({
-        configFile: configFile,
-        reporters: reporters,
-        singleRun: true
-      }, function () {
-        cb && cb();
-      }).start();
-    }
-
-    return {
-
-      all: function () {
-        var configFile = find.fileSync('karma.conf.js', getExtensionPath())[0];
-
-        runServer(configFile);
-      },
-      for: function (srcFile) {
-        var srcFileNoExt = path.basename(srcFile, path.extname(srcFile));
-        var testFile = srcFile
-          .replace('src/job-roles/', 'test/')
-          .replace(srcFileNoExt + '.js', srcFileNoExt + '.spec.js');
-
-        fs.statSync(testFile).isFile() && this.single(testFile);
-      },
-      single: function (testFile) {
-        var configFilePath = findUp.sync('karma.conf.js', { cwd: testFile });
-        var jsFolderPath = path.dirname(configFilePath);
-
-        var tempConfigFile = 'karma.' + path.basename(testFile, path.extname(testFile)) + '.conf.temp.js';
-
-        gulp
-          .src(configFilePath)
-          .pipe(replace('*.spec.js', path.basename(testFile)))
-          .pipe(rename(tempConfigFile))
-          .pipe(gulp.dest(jsFolderPath))
-          .on('end', function () {
-            runServer(path.join(jsFolderPath, tempConfigFile), function () {
-              gulp
-                .src(path.join(jsFolderPath, tempConfigFile), { read: false })
-                .pipe(clean({ force: true }));
-            });
-          });
-      }
-    };
-  })();
 }());
 
 // Watch
@@ -411,3 +350,65 @@ function throwError (plugin, msg) {
     message: gutil.colors.red(msg),
   });
 }
+
+var test = (function () {
+  var find = require('find');
+  var karma = require('karma');
+  var replace = require('gulp-replace');
+  var rename = require('gulp-rename');
+
+  /**
+   * Runs the karma server which does a single run of the test/s
+   *
+   * @param {string} configFile - The full path to the karma config file
+   * @param {Function} cb - The callback to call when the server closes
+   */
+  function runServer (configFile, cb) {
+    var reporters = argv.reporters ? argv.reporters.split(',') : ['progress'];
+
+    new karma.Server({
+      configFile: configFile,
+      reporters: reporters,
+      singleRun: true
+    }, function () {
+      cb && cb();
+    }).start();
+  }
+
+  return {
+
+    all: function () {
+      var configFile = find.fileSync('karma.conf.js', getExtensionPath())[0];
+
+      runServer(configFile);
+    },
+    for: function (srcFile) {
+      var srcFileNoExt = path.basename(srcFile, path.extname(srcFile));
+
+      var testFile = srcFile
+        .replace(/src\/[^\/]+\//, 'test/')
+        .replace(srcFileNoExt + '.js', srcFileNoExt + '.spec.js');
+
+      fs.statSync(testFile).isFile() && this.single(testFile);
+    },
+    single: function (testFile) {
+      var configFilePath = findUp.sync('karma.conf.js', { cwd: testFile });
+      var jsFolderPath = path.dirname(configFilePath);
+
+      var tempConfigFile = 'karma.' + path.basename(testFile, path.extname(testFile)) + '.conf.temp.js';
+
+      gulp
+        .src(configFilePath)
+        .pipe(replace('*.spec.js', path.basename(testFile)))
+        .pipe(rename(tempConfigFile))
+        .pipe(gulp.dest(jsFolderPath))
+        .on('end', function () {
+          runServer(path.join(jsFolderPath, tempConfigFile), function () {
+            gulp
+              .src(path.join(jsFolderPath, tempConfigFile), { read: false })
+              .pipe(clean({ force: true }));
+          });
+        });
+    }
+  };
+})();
