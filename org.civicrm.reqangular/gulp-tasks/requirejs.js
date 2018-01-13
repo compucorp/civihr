fc7b6e3ec1aee7bd4a74e9a83ec8c7db08d18a2f
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-01-13 16:36:43 +0100

    PCHR-3084: Add cache-templates task as pre-requirejs task for reqangular

---
+++ org.civicrm.reqangular/gulp-tasks/requirejs.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var path = require('path');
+var replace = require('gulp-replace');
+var templateCache = require('gulp-angular-templatecache');
+
+module.exports = function () {
+  var commonFolder = path.join(__dirname, '..', 'src/common');
+
+  return {
+    pre: [
+      {
+        name: 'cache-templates',
+        fn: function (cb) {
+          gulp.src(path.join(commonFolder, 'templates', '/**/*.html'))
+            .pipe(templateCache({
+              moduleSystem: 'RequireJS'
+            }))
+            .pipe(replace("['angular']", "['common/angular']"))
+            .pipe(replace('module("templates")', 'module("common.templates", [])'))
+            .pipe(gulp.dest(path.join(commonFolder, 'modules')));
+
+          cb();
+        }
+      }
+    ]
+  };
+};
