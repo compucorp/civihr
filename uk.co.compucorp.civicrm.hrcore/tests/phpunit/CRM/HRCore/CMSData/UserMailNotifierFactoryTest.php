5c6238a4373b3265a35a62301910312c044da7ca
Tunbola Ogunwande <tunbolawande@yahoo.com>
2018-01-15 11:01:10 +0100

    PCHR-3073: Get value for user framework and set it back after test execution.

--- uk.co.compucorp.civicrm.hrcore/tests/phpunit/CRM/HRCore/CMSData/UserMailNotifierFactoryTest.php
+++ uk.co.compucorp.civicrm.hrcore/tests/phpunit/CRM/HRCore/CMSData/UserMailNotifierFactoryTest.php
@@ -11,16 +11,18 @@ use CRM_HRCore_CMSData_UserMailNotifierInterface as UserMailNotifierInterface;
 class CRM_HRCore_CMSData_CMSUserMailNotifierFactoryTest extends CRM_HRCore_Test_BaseHeadlessTest {
 
   public function testItReturnsAnInstanceOfTheExpectedClassWhenCMSIsSupported() {
-    //We need to manually set this value because civicrm sets it to UnitTests
+    $previousUserFramework = CRM_Core_Config::singleton()->userFramework;
+    //We need to manually set this value because civicrm sets it to some other value
     //when running in testing environment.
     CRM_Core_Config::singleton()->userFramework = 'Drupal';
     $mailNotifier = UserMailNotifierFactory::create();
     $this->assertInstanceOf(UserMailNotifierInterface::class, $mailNotifier);
-    CRM_Core_Config::singleton()->userFramework = 'UnitTests';
+    CRM_Core_Config::singleton()->userFramework = $previousUserFramework;
   }
 
   public function testItThrowsAnExceptionWhenCMSIsNotSupported() {
-    //We need to manually set this value because civicrm sets it to UnitTests
+    $previousUserFramework = CRM_Core_Config::singleton()->userFramework;
+    //We need to manually set this value because civicrm sets it to some other value
     //when running in testing environment.
     $cmsFramework = 'UnrecognizedCMS';
     CRM_Core_Config::singleton()->userFramework = $cmsFramework;
@@ -28,7 +30,7 @@ class CRM_HRCore_CMSData_CMSUserMailNotifierFactoryTest extends CRM_HRCore_Test_
     $this->setExpectedException('Exception', $msg);
 
     UserMailNotifierFactory::create();
-    CRM_Core_Config::singleton()->userFramework = 'UnitTests';
+    CRM_Core_Config::singleton()->userFramework = $previousUserFramework;
   }
 }
 
