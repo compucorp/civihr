72114ed21a741236dc937c1b5090524257251027
Buddha Man Nepali <bmnepali980@gmail.com>
2018-01-16 18:06:34 +0545

    PCHR-3142: Add handler to display confirmation dialog on deleting user

--- uk.co.compucorp.civicrm.hrcontactactionsmenu/js/contactactions.js
+++ uk.co.compucorp.civicrm.hrcontactactionsmenu/js/contactactions.js
@@ -1,11 +1,24 @@
-(function ($) {
+(function ($, CRM) {
   $(document)
     // Actions menu
-    .on('click', function(e) {
+    .on('click', function (e) {
       if ($(e.target).is('#crm-contact-actions-link, #crm-contact-actions-link *')) {
         $('#crm-contact-actions-link').addClass('active');
       } else {
         $('#crm-contact-actions-link').removeClass('active');
       }
     });
-}(CRM.$));
+
+  // Handler for deleting user account
+  $('[data-delete-user-url]').on('click', function () {
+    var url = $(this).attr('data-delete-user-url');
+
+    CRM.confirm({
+      'title': 'Confirmation dialog',
+      'message': 'Are you sure you want to continue?'
+    })
+    .on('crmConfirm:yes', function () {
+      window.location = url;
+    });
+  });
+}(CRM.$, CRM));
