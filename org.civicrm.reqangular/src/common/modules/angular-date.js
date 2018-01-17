d475cae96d49b96f6b56642616b73abf078b8686
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-01-17 12:00:50 +0100

    PCHR-2604: Apply semistandard to angular-date module file

--- org.civicrm.reqangular/src/common/modules/angular-date.js
+++ org.civicrm.reqangular/src/common/modules/angular-date.js
@@ -1,11 +1,14 @@
-define([
+/* eslint-env amd */
+
+(function ($) {
+  define([
     'common/angular',
     'common/decorators/angular-date/datepicker-popup',
     'common/decorators/angular-date/date-filter',
     'common/modules/services',
     'common/angularBootstrap',
     'common/modules/templates'
-], function (angular, datepickerPopup, dateFilter) {
+  ], function (angular, datepickerPopup, dateFilter) {
     'use strict';
 
     var $templateCache;
@@ -20,35 +23,36 @@ define([
      *
      * @param {string} name
      */
-    function customizeDatepickerTpl(name) {
-        var tplPath = 'uib/template/datepicker/' + name + '.html';
-        var tpl = $templateCache.get(tplPath);
+    function customizeDatepickerTpl (name) {
+      var tplPath = 'uib/template/datepicker/' + name + '.html';
+      var tpl = $templateCache.get(tplPath);
 
-        tpl = tpl.replace(/glyphicon glyphicon-chevron-(left|right)/gm, 'fa fa-chevron-$1');
+      tpl = tpl.replace(/glyphicon glyphicon-chevron-(left|right)/gm, 'fa fa-chevron-$1');
 
-        if (name === 'day') {
-            tpl = (function ($tpl) {
-                jQuery($tpl).find('th[ng-if="showWeeks"]').parent().css({ 'border-top': '1px solid #DDD' });
+      if (name === 'day') {
+        tpl = (function ($tpl) {
+          $($tpl).find('th[ng-if="showWeeks"]').parent().css({ 'border-top': '1px solid #DDD' });
 
-                return jQuery($tpl).prop('outerHTML');
-            })(jQuery.parseHTML(tpl));
-        }
+          return $($tpl).prop('outerHTML');
+        })($.parseHTML(tpl));
+      }
 
-        $templateCache.put(tplPath, tpl);
+      $templateCache.put(tplPath, tpl);
     }
 
     /**
      * Provides decorator for the datepicker popup (sets default config) and date filter (adds suppported formats)
      * It also customizes the datepicker templates
      */
-    return angular.module("common.angularDate", ['common.services', 'ui.bootstrap', 'common.templates']).config([
-        '$provide', function ($provide) {
-            $provide.decorator('uibDatepickerPopupDirective', datepickerPopup);
-            $provide.decorator('dateFilter', dateFilter);
-        }
+    return angular.module('common.angularDate', ['common.services', 'ui.bootstrap', 'common.templates']).config([
+      '$provide', function ($provide) {
+        $provide.decorator('uibDatepickerPopupDirective', datepickerPopup);
+        $provide.decorator('dateFilter', dateFilter);
+      }
     ]).run(['$templateCache', function (_$templateCache_) {
-        $templateCache = _$templateCache_;
+      $templateCache = _$templateCache_;
 
-        ['day', 'month', 'year'].forEach(customizeDatepickerTpl);
+      ['day', 'month', 'year'].forEach(customizeDatepickerTpl);
     }]);
-});
+  });
+}(CRM.$));
