ed93a57383d735981b84d6a27d481cf94c217794
Buddha Man Nepali <bmnepali980@gmail.com>
2018-01-17 18:39:57 +0545

    PCHR-3142: PR review feedbacks

--- uk.co.compucorp.civicrm.hrcontactactionsmenu/js/contactactions.js
+++ uk.co.compucorp.civicrm.hrcontactactionsmenu/js/contactactions.js
@@ -6,26 +6,10 @@
   }());
 
   /**
-   * Contains event listeners
+   * Displays confirmation dialog before deleting user account
    */
-  function initListeners () {
-    // Toogle Actions Menu's 'active' class
-    $(document).on('click', function (e) {
-      toggleActionMenuButtonStatus(e);
-    });
-
-    // Handler for deleting user account
-    $('[data-delete-user-url]').on('click', function () {
-      confirmUserAccountDeletion(this);
-    });
-  }
-
-  /**
-   * Displays confirmation before deleting user account
-   * @param {Object} buttonInstance
-   */
-  function confirmUserAccountDeletion (buttonInstance) {
-    var url = $(buttonInstance).attr('data-delete-user-url');
+  function confirmUserAccountDeletion () {
+    var url = $('[data-delete-user-url]').attr('data-delete-user-url');
 
     CRM.confirm({
       'title': 'Confirmation dialog',
@@ -37,10 +21,26 @@
   }
 
   /**
+   * Initializes click listeners for
+   * - Actions Button
+   * - Delete User Account Button
+   */
+  function initListeners () {
+    $(document).on('click', function (e) {
+      toggleActionMenuButtonClass(e);
+    });
+
+    $('[data-delete-user-url]').on('click', function () {
+      confirmUserAccountDeletion();
+    });
+  }
+
+  /**
    * Toggles the "active" class in Actions Menu button
+   *
    * @param {Object} event
    */
-  function toggleActionMenuButtonStatus (event) {
+  function toggleActionMenuButtonClass (event) {
     var selector = $('#crm-contact-actions-link');
 
     if ($(event.target).is('#crm-contact-actions-link, #crm-contact-actions-link *')) {
--- uk.co.compucorp.civicrm.hrcore/backstop_data/page-objects/contact-actions-menu.js
+++ uk.co.compucorp.civicrm.hrcore/backstop_data/page-objects/contact-actions-menu.js
@@ -2,36 +2,33 @@ var Promise = require('es6-promise').Promise;
 var page = require('./page');
 
 module.exports = (function () {
-  var contactActionsMenuButton = '#crm-contact-actions-link';
-  var deleteUserAccountButton = '[data-delete-user-url]';
+  var contactActionsMenuButtonSelector = '#crm-contact-actions-link';
+  var deleteUserAccountButtonSelector = '[data-delete-user-url]';
 
   return page.extend({
 
     /**
-     * Clicks the contact action menu button
+     * Click the "Actions" button on contact page
      */
     openContactActionMenu: function () {
       var casper = this.casper;
 
       return new Promise(function (resolve) {
         casper.then(function () {
-          casper.click(contactActionsMenuButton);
-          casper.wait(5000);
+          casper.click(contactActionsMenuButtonSelector);
         });
       });
     },
 
     /**
-     * Clicks the contact action menu > delete user account button
+     * Click the "Delete User Account" button in the contact action menu
      */
     deleteUserAccount: function () {
       var casper = this.casper;
 
       casper.then(function () {
-        casper.click(contactActionsMenuButton);
-        casper.wait(5000);
-        casper.click(deleteUserAccountButton);
-        casper.wait(5000);
+        casper.click(contactActionsMenuButtonSelector);
+        casper.click(deleteUserAccountButtonSelector);
       });
 
       return this;
