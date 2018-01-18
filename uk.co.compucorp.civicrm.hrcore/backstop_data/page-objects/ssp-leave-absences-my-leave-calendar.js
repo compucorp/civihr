679c1e886bc754bd837de78279ea1663e8f605ed
René Olivo <minet.ws@gmail.com>
2018-01-18 16:18:08 -0400

    PCHR-2984: Add show all months method to ssp calendar page object

--- uk.co.compucorp.civicrm.hrcore/backstop_data/page-objects/ssp-leave-absences-my-leave-calendar.js
+++ uk.co.compucorp.civicrm.hrcore/backstop_data/page-objects/ssp-leave-absences-my-leave-calendar.js
@@ -1,9 +1,36 @@
+/* globals jQuery */
+
 var page = require('./page');
 
 module.exports = (function () {
   return page.extend({
     /**
-     * Wait for the page to be ready by looking at 
+     * Clears all selected months from the calendar "Selected Months" field.
+     */
+    clearAllSelectedMonths: function () {
+      this.casper.evaluate(function () {
+        jQuery('.chr_leave-calendar__day-selector .close.ui-select-match-close')
+          .each(function () {
+            jQuery(this).click();
+          });
+      });
+
+      return this;
+    },
+
+    /**
+     * Display all months for the leave calendar by clearing the
+     * "Selected Months" field.
+     */
+    showAllMonths: function () {
+      this.clearAllSelectedMonths();
+      this.waitUntilVisible('leave-calendar-month:nth-child(12) leave-calendar-day');
+
+      return this;
+    },
+
+    /**
+     * Wait for the page to be ready by looking at
      * the visibility of a leave calendar item element
      */
     waitForReady: function () {
