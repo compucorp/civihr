ab81edcbc0ea75c5b4f850e64690c78dc77050a7
Tunbola Ogunwande <tunbolawande@yahoo.com>
2018-01-18 17:27:24 +0100

    PCHR-3088: Add try catch block to the ContactHelper::getUserInformation function as calling the API throws exception when user has no CMS account.

--- uk.co.compucorp.civicrm.hrcontactactionsmenu/CRM/HRContactActionsMenu/Helper/Contact.php
+++ uk.co.compucorp.civicrm.hrcontactactionsmenu/CRM/HRContactActionsMenu/Helper/Contact.php
@@ -13,19 +13,20 @@ class CRM_HRContactActionsMenu_Helper_Contact {
    * @return array
    */
   public static function getUserInformation($contactID) {
-    $result = civicrm_api3('User', 'getsingle', [
-      'contact_id' => $contactID,
-    ]);
+    try {
+      $result = civicrm_api3('User', 'getsingle', [
+        'contact_id' => $contactID,
+      ]);
 
-    //When a user has no CMS account, the contact_id parameter is not present
-    //we need to add it here.
-    //Also when contact has CMS account, the cmsId parameter is needed for the
-    //HRCore CMSData classes
-    if(empty($result['id'])) {
-      $result['contact_id'] = $contactID;
-    }
-    else {
+      //When a user has no CMS account, the contact_id parameter is not present
+      //we need to add it here.
+      //Also when contact has CMS account, the cmsId parameter is needed for the
+      //HRCore CMSData classes
       $result['cmsId'] = $result['id'];
+    } catch(Exception $e) {
+      //When a user has no CMS account, the contact_id parameter is not present
+      //and an exception is thrown, we need to add it here.
+      $result['contact_id'] = $contactID;
     }
 
     return $result;
