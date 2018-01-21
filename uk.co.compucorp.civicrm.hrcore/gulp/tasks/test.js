375eb7fe3e10ea092b26a5a1de66fe65ee290997
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-01-21 19:47:17 +0100

    PCHR-3084: Run test task only if the ext has a karma.conf.js file

--- uk.co.compucorp.civicrm.hrcore/gulp/tasks/test.js
+++ uk.co.compucorp.civicrm.hrcore/gulp/tasks/test.js
@@ -1,3 +1,5 @@
+var colors = require('ansi-colors');
+var find = require('find');
 var gulp = require('gulp');
 var gulpSequence = require('gulp-sequence');
 var path = require('path');
@@ -9,30 +11,61 @@ module.exports = [
   {
     name: 'test',
     fn: function (cb) {
-      var sequence = utils.addExtensionCustomTasksToSequence([
-        utils.spawnTaskForExtension('test:main', mainTask)
-      ], 'test');
+      var sequence;
 
-      gulpSequence.apply(null, sequence)(cb);
+      if (hasCurrentExtensionKarmaFile()) {
+        sequence = utils.addExtensionCustomTasksToSequence([
+          utils.spawnTaskForExtension('test:main', mainTask)
+        ], 'test');
+
+        gulpSequence.apply(null, sequence)(cb);
+      } else {
+        console.log(colors.yellow('No karma.conf.js file found, skipping...'));
+        cb();
+      }
     }
   },
   {
     name: 'test:watch',
     fn: function (cb) {
-      var extPath = utils.getExtensionPath();
-      var watchPatterns = utils.addExtensionCustomWatchPatternsToDefaultList([
-        path.join(extPath, '**', 'test/**/*.spec.js'),
-        '!' + path.join(extPath, '**', 'test/mocks/**/*.js'),
-        '!' + path.join(extPath, '**', 'test/test-main.js')
-      ], 'test');
+      var extPath, watchPatterns;
 
-      gulp.watch(watchPatterns).on('change', function (file) {
-        test.single(file.path);
-      });
-      cb();
+      if (hasCurrentExtensionKarmaFile()) {
+        extPath = utils.getExtensionPath();
+        watchPatterns = utils.addExtensionCustomWatchPatternsToDefaultList([
+          path.join(extPath, '**', 'test/**/*.spec.js'),
+          '!' + path.join(extPath, '**', 'test/mocks/**/*.js'),
+          '!' + path.join(extPath, '**', 'test/test-main.js')
+        ], 'test');
+
+        gulp.watch(watchPatterns).on('change', function (file) {
+          test.single(file.path);
+        });
+        cb();
+      } else {
+        console.log(colors.yellow('No karma.conf.js file found, skipping...'));
+        cb();
+      }
     }
   }
 ];
+
+/**
+ * Check if the current extension has a karma.conf.js file
+ * Allows for the file to be either in the root, or in up to two levels of folders
+ *
+ * @return {Boolean}
+ */
+function hasCurrentExtensionKarmaFile () {
+  var extPath = utils.getExtensionPath();
+  var karmaConfRegExp = new RegExp(extPath + '(/[^/]+)?(/[^/]+)?/karma.conf.js');
+
+  return !!find.fileSync(karmaConfRegExp, extPath)
+    // files from the node_modules/ folder might get caught up in the query
+    .filter(function (filePath) {
+      return !(filePath.indexOf('node_modules') > -1);
+    })[0];
+}
 
 /**
  * Runs all the JS unit tests of the extension
