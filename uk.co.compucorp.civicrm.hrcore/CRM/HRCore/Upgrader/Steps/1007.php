8118ffd44b84cfffde2be6a23cacf33175f3ebc4
Tunbola Ogunwande <tunbolawande@yahoo.com>
2018-01-24 18:32:47 +0100

    PCHR-3083: Add Upgrader in HRCore extension to make the Line Manager/Line Manager is Relationship reserved.

---
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1007.php
@@ -0,0 +1,21 @@
+<?php
+
+trait CRM_HRCore_Upgrader_Steps_1007 {
+
+  /**
+   * Make the Line Manager/Line Manager is Relationship
+   * to be reserved.
+   *
+   * @return bool
+   */
+  public function upgrade_1007() {
+    civicrm_api3('RelationshipType', 'get', [
+      'sequential' => 1,
+      'name_a_b' => 'Line Manager is',
+      'name_b_a' => 'Line Manager',
+      'api.RelationshipType.create' => ['id' => '$value.id', 'is_reserved' => 1],
+    ]);
+
+    return TRUE;
+  }
+}
