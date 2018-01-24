188a0ac9ca871ae510f025b65eeb48696e20e4b3
Igor Pavlov <pavlov.org@gmail.com>
2018-01-24 10:25:49 +0000

    PCHR-3166: Order functions

--- uk.co.compucorp.civicrm.hrcore/backstop_data/page-objects/ssp-leave-absences-my-leave-report.js
+++ uk.co.compucorp.civicrm.hrcore/backstop_data/page-objects/ssp-leave-absences-my-leave-report.js
@@ -3,44 +3,6 @@ var page = require('./page');
 
 module.exports = (function () {
   return page.extend({
-    /**
-     * Wait for the page to be ready
-     * @return {Object} this object
-     */
-    waitForReady: function () {
-      this.waitUntilVisible('td[ng-click="report.toggleSection(\'pending\')"]');
-    },
-    /**
-     * Opens the given section of my report pageName
-     * @param {String} section
-     * @return {Object} this object
-     */
-    openSection: function (section) {
-      var casper = this.casper;
-
-      casper.then(function () {
-        casper.click('td[ng-click="report.toggleSection(\'' + section + '\')"]');
-        casper.waitWhileVisible('.spinner');
-      });
-
-      return this;
-    },
-    /**
-     * Opens the dropdown for staff actions like edit/respond, cancel.
-     * @param {Number} row number corresponding to leave request in the list
-     * @return {Object} this object
-     */
-    openActionsForRow: function (row) {
-      var casper = this.casper;
-
-      casper.then(function () {
-        casper.waitForSelector('tr:nth-child(1)  div[uib-dropdown] a:nth-child(1)', function () {
-          casper.click('div:nth-child(2) > div > table > tbody > tr:nth-child(' + (row || 1) + ')  div[uib-dropdown] a:nth-child(1)');
-        });
-      });
-
-      return this;
-    },
 
     /**
      * User clicks on the edit/respond action
@@ -60,6 +22,47 @@ module.exports = (function () {
           resolve(this.waitForModal('ssp-leave-request', '.chr_leave-request-modal__form'));
         }.bind(this));
       }.bind(this));
+    },
+
+    /**
+     * Opens the dropdown for staff actions like edit/respond, cancel.
+     * @param {Number} row number corresponding to leave request in the list
+     * @return {Object} this object
+     */
+    openActionsForRow: function (row) {
+      var casper = this.casper;
+
+      casper.then(function () {
+        casper.waitForSelector('tr:nth-child(1)  div[uib-dropdown] a:nth-child(1)', function () {
+          casper.click('div:nth-child(2) > div > table > tbody > tr:nth-child(' + (row || 1) + ')  div[uib-dropdown] a:nth-child(1)');
+        });
+      });
+
+      return this;
+    },
+
+    /**
+     * Opens the given section of my report pageName
+     * @param {String} section
+     * @return {Object} this object
+     */
+    openSection: function (section) {
+      var casper = this.casper;
+
+      casper.then(function () {
+        casper.click('td[ng-click="report.toggleSection(\'' + section + '\')"]');
+        casper.waitWhileVisible('.spinner');
+      });
+
+      return this;
+    },
+
+    /**
+     * Wait for the page to be ready
+     * @return {Object} this object
+     */
+    waitForReady: function () {
+      this.waitUntilVisible('td[ng-click="report.toggleSection(\'pending\')"]');
     }
   });
 })();
