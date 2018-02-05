ed0d2c5f7be4639f1c070264ccf7dea1d56a5bad
Debarshi Bhaumik <deb1990@gmail.com>
2018-02-05 17:25:26 +0530

    PCHR-3061: Fix Semistandard issues

--- uk.co.compucorp.civicrm.hrcore/backstop_data/casper_scripts/initialise.js
+++ uk.co.compucorp.civicrm.hrcore/backstop_data/casper_scripts/initialise.js
@@ -10,30 +10,30 @@ module.exports = function (casper, scenario) {
 
   if (scenario.performLogin) {
     casper
-    .then(function () {
-      if (scenario.performLogout) {
-        casper.echo('Current scenario has different login credentials from previous, logging out is necessary', 'INFO');
-        casper.echo('Logging Out', 'INFO');
+      .then(function () {
+        if (scenario.performLogout) {
+          casper.echo('Current scenario has different login credentials from previous, logging out is necessary', 'INFO');
+          casper.echo('Logging Out', 'INFO');
 
-        return phantom.clearCookies();
-      }
-    })
-    .then(function () {
-      casper.echo('Logging in with "' + (scenario.credential || 'admin') + '" credentials before starting ...', 'INFO');
-    })
-    .thenOpen(config.url + '/welcome-page', function () {
-      casper.then(function () {
-        casper.waitForSelector(loginFormSelector, function () {
-          casper.waitWhileSelector(loginFormSelector, function () {
-            casper.echo('Logged in', 'INFO');
+          return phantom.clearCookies();
+        }
+      })
+      .then(function () {
+        casper.echo('Logging in with "' + (scenario.credential || 'admin') + '" credentials before starting ...', 'INFO');
+      })
+      .thenOpen(config.url + '/welcome-page', function () {
+        casper.then(function () {
+          casper.waitForSelector(loginFormSelector, function () {
+            casper.waitWhileSelector(loginFormSelector, function () {
+              casper.echo('Logged in', 'INFO');
+            }, function () {
+              casper.echo('Login form visible and timeout reached!', 'RED_BAR');
+            }, 5000);
+            casper.fill(loginFormSelector, credentials, true);
           }, function () {
-            casper.echo('Login form visible and timeout reached!', 'RED_BAR');
-          }, 5000);
-          casper.fill(loginFormSelector, credentials, true);
-        }, function () {
-          casper.echo('Login form not found!', 'RED_BAR');
-        }, 8000);
+            casper.echo('Login form not found!', 'RED_BAR');
+          }, 8000);
+        });
       });
-    });
   }
 };
