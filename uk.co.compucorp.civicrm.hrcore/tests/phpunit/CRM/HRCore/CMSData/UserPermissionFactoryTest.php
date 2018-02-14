4cc8f8c1cd91a59e05614f7e12788476ee438932
Tunbola Ogunwande <tunbolawande@yahoo.com>
2018-02-14 12:43:12 +0100

    PCHR-3074: Use setup and teardown method to assign the userframework variable.

--- uk.co.compucorp.civicrm.hrcore/tests/phpunit/CRM/HRCore/CMSData/UserPermissionFactoryTest.php
+++ uk.co.compucorp.civicrm.hrcore/tests/phpunit/CRM/HRCore/CMSData/UserPermissionFactoryTest.php
@@ -10,18 +10,25 @@ use CRM_HRCore_CMSData_UserPermissionInterface as UserPermissionInterface;
  */
 class UserPermissionFactoryTest extends CRM_HRCore_Test_BaseHeadlessTest {
 
+  private $previousUserFramework;
+
+  public function setUp() {
+    $this->previousUserFramework = CRM_Core_Config::singleton()->userFramework;
+  }
+
+  public function tearDown() {
+    CRM_Core_Config::singleton()->userFramework = $this->previousUserFramework;
+  }
+
   public function testItReturnsAnInstanceOfTheExpectedClassWhenCMSIsSupported() {
-    $previousUserFramework = CRM_Core_Config::singleton()->userFramework;
     //We need to manually set this value because civicrm sets it to some other value
     //when running in testing environment.
     CRM_Core_Config::singleton()->userFramework = 'Drupal';
     $userPermission = UserPermissionFactory::create();
     $this->assertInstanceOf(UserPermissionInterface::class, $userPermission);
-    CRM_Core_Config::singleton()->userFramework = $previousUserFramework;
   }
 
   public function testItThrowsAnExceptionWhenCMSIsNotSupported() {
-    $previousUserFramework = CRM_Core_Config::singleton()->userFramework;
     //We need to manually set this value because civicrm sets it to some other value
     //when running in testing environment.
     $cmsFramework = 'UnrecognizedCMS';
@@ -30,6 +37,6 @@ class UserPermissionFactoryTest extends CRM_HRCore_Test_BaseHeadlessTest {
     $this->setExpectedException('Exception', $msg);
 
     UserPermissionFactory::create();
-    CRM_Core_Config::singleton()->userFramework = $previousUserFramework;
   }
+
 }
