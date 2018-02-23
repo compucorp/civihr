f4e78101a8265f72800fd4891a8957ddf7daf413
Tunbola Ogunwande <tunbolawande@yahoo.com>
2018-02-23 17:08:14 +0100

    PCHR-3346: Refresh extension list

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1010.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1010.php
@@ -7,12 +7,18 @@ trait CRM_HRCore_Upgrader_Steps_1010 {
   /**
    * Installs the Contact Actions Menu extension if not
    * installed already.
+   *
+   * @return boolean
    */
   public function upgrade_1010() {
     $key = 'uk.co.compucorp.civicrm.hrcontactactionsmenu';
     if (ExtensionHelper::isExtensionEnabled($key)) {
       return TRUE;
     }
+
+    //refresh the extension list so the extension can be
+    //available.
+    civicrm_api3('Extension', 'refresh');
 
     civicrm_api3('Extension', 'install', [
       'keys' => [$key]
