e0329936c932fb7dc97d8e459aa1a926cd4db166
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-03-13 14:11:53 +0100

    PCHR-3221: Add chromy scripts for work patterns

--- uk.co.compucorp.civicrm.hrcore/backstop_data/page-objects/work-patterns-form.js
+++ uk.co.compucorp.civicrm.hrcore/backstop_data/page-objects/work-patterns-form.js
@@ -8,11 +8,7 @@ module.exports = (function () {
      * @return The Page instance.
      */
     showCalendarForm: function () {
-      var casper = this.casper;
-
-      casper.then(function () {
-        casper.click('a[href="#work-pattern-calendar"]');
-      });
+      this.chromy.click('a[href="#work-pattern-calendar"]');
 
       return this;
     },
@@ -21,7 +17,7 @@ module.exports = (function () {
      * Waits until the work pattern form is visible.
      */
     waitForReady: function () {
-      this.waitUntilVisible('.work-pattern-form');
+      this.chromy.visible('.work-pattern-form');
     }
   });
 })();
