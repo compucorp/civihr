2b754e25927cd338dba6ae57648240cacfd1b855
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-03-16 11:52:26 +0100

    PCHR-3221: Show scenario name in the onBefore script

--- uk.co.compucorp.civicrm.hrcore/backstop_data/engine_scripts/chromy/onBefore.js
+++ uk.co.compucorp.civicrm.hrcore/backstop_data/engine_scripts/chromy/onBefore.js
@@ -1,4 +1,5 @@
 module.exports = function (chromy, scenario, vp) {
+  console.log('SCENARIO > ' + scenario.label);
   require('./loadCookies')(chromy, scenario);
 
   // IGNORE ANY CERT WARNINGS
--- uk.co.compucorp.civicrm.hrcore/backstop_data/engine_scripts/chromy/onReady.js
+++ uk.co.compucorp.civicrm.hrcore/backstop_data/engine_scripts/chromy/onReady.js
@@ -1,7 +1,6 @@
 var page = require('../../page-objects/contact-summary');
 
 module.exports = function (chromy, scenario, vp) {
-  console.log('SCENARIO > ' + scenario.label);
   require('./clickAndHoverHelper')(chromy, scenario);
 
   page.init(chromy);
