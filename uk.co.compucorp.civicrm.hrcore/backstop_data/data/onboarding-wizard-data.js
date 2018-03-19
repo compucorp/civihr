7fdd664de60ff4653b1df149685c1e772a4b0f06
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-03-19 16:29:58 +0100

    PCHR-3221: Remove left over files

--- uk.co.compucorp.civicrm.hrcore/backstop_data/data/onboarding-wizard-data.js
+++
@@ -1,9 +0,0 @@
-module.exports = (function () {
-  return {
-    dependents: {
-      '#edit-submitted-first-dependant-civicrm-1-contact-3-cg99999-custom-100000': 'Duke',
-      '#edit-submitted-first-dependant-civicrm-1-contact-3-cg99999-custom-100001': '1234',
-      '#edit-submitted-first-dependant-civicrm-1-contact-3-cg99999-custom-100010': 'sibling'
-    }
-  };
-})();
