fa292cab4e7c311f954cd7ff5b5824e7f980e118
Igor Pavlov <pavlov.org@gmail.com>
2018-03-27 10:07:11 +0100

    PCHR-3494: Allow not to cache group options

--- org.civicrm.reqangular/test/models/option-group.spec.js
+++ org.civicrm.reqangular/test/models/option-group.spec.js
@@ -14,7 +14,7 @@ define([
       module('common.models', 'common.mocks', function (_$provide_) {
         $provide = _$provide_;
       });
-            // Override api.optionGroup with the mocked version
+      // Override api.optionGroup with the mocked version
       inject(['api.optionGroup.mock', function (_optionGroupAPIMock_) {
         $provide.value('api.optionGroup', _optionGroupAPIMock_);
       }]);
@@ -35,12 +35,44 @@ define([
     });
 
     describe('valuesOf()', function () {
-      beforeEach(function () {
-        OptionGroup.valuesOf('hrjc_department');
+      var optionGroupName = 'hrjc_department';
+
+      describe('when just a single option group name is passed', function () {
+        beforeEach(function () {
+          OptionGroup.valuesOf(optionGroupName);
+        });
+
+        it('calls the correct API method', function () {
+          expect(optionGroupAPI.valuesOf.calls.mostRecent().args[0]).toBe(optionGroupName);
+        });
+
+        it('caches requests by default', function () {
+          expect(optionGroupAPI.valuesOf.calls.mostRecent().args[1]).toBe(undefined);
+        });
       });
 
-      it('calls the correct API method', function () {
-        expect(optionGroupAPI.valuesOf).toHaveBeenCalled();
+      describe('when several option group names are passed', function () {
+        var multipleOptionGroupNames = [optionGroupName, 'some_other_option'];
+
+        beforeEach(function () {
+          OptionGroup.valuesOf(multipleOptionGroupNames);
+        });
+
+        it('passes the array of option group names to the API method', function () {
+          expect(optionGroupAPI.valuesOf.calls.mostRecent().args[0]).toBe(multipleOptionGroupNames);
+        });
+      });
+
+      describe('when caching is disabled', function () {
+        var cache = false;
+
+        beforeEach(function () {
+          OptionGroup.valuesOf(optionGroupName, cache);
+        });
+
+        it('does not cache requests', function () {
+          expect(optionGroupAPI.valuesOf.calls.mostRecent().args[1]).toBe(cache);
+        });
       });
     });
   });
