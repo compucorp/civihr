f2b8ad23e39ee624dcc5a85044435b8b28a291d8
Igor Pavlov <pavlov.org@gmail.com>
2018-03-27 10:06:00 +0100

    PCHR-3494: Create a CRM service

---
+++ org.civicrm.reqangular/test/services/crm-ang.service.spec.js
@@ -0,0 +1,43 @@
+/* eslint-env amd, jasmine */
+
+(function (CRM) {
+  define([
+    'common/angular',
+    'common/angularMocks',
+    'common/services/crm-ang.service'
+  ], function () {
+    'use strict';
+
+    describe('crmAng', function () {
+      var crmAngService;
+
+      beforeEach(module('common.services'));
+      beforeEach(inject(function (_crmAngService_) {
+        crmAngService = _crmAngService_;
+      }));
+
+      describe('loadForm()', function () {
+        var result;
+        var url = '/some-url';
+        var options = { optionKey: 'optionValue' };
+        var originalResult = 'jQuery object';
+
+        beforeEach(function () {
+          CRM.loadForm = function () {};
+
+          spyOn(CRM, 'loadForm').and.returnValue(originalResult);
+
+          result = crmAngService.loadForm(url, options);
+        });
+
+        it('calls CRM.loadForm with according parameters', function () {
+          expect(CRM.loadForm).toHaveBeenCalledWith(url, options);
+        });
+
+        it('returns the same as the original method', function () {
+          expect(result).toBe(originalResult);
+        });
+      });
+    });
+  });
+})(CRM);
