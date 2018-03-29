e7c57966eb8957a2eb409ce5fcd2893ace90c8f2
René Olivo <minet.ws@gmail.com>
2018-03-29 11:14:00 -0400

    PCHR-3411: PR Feedback

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1012.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1012.php
@@ -3,9 +3,24 @@
 trait CRM_HRCore_Upgrader_Steps_1012 {
 
   /**
-   * Removes relationship types that are not necessary for CiviHR.
+   * Disables relationship types that are not necessary for CiviHR and
+   * removes existing relationships belonging to these types.
    */
   public function upgrade_1012() {
+    $relationshipTypesToBeDisabled = $this->up1012_getRelationshipTypesToBeDisabled();
+
+    $this->up1012_disableRelationshipTypes($relationshipTypesToBeDisabled);
+    $this->up1012_removeRelationshipsForTypes($relationshipTypesToBeDisabled);
+
+    return TRUE;
+  }
+
+  /**
+   * Returns the Ids of the the relationship types to be disabled.
+   *
+   * @return Array[int]
+   */
+  private function up1012_getRelationshipTypesToBeDisabled() {
     $relationshipsToBeDeleted = [
       'Case Coordinator is',
       'Employee of',
@@ -17,12 +32,60 @@ trait CRM_HRCore_Upgrader_Steps_1012 {
       'name_a_b' => [ 'IN' => $relationshipsToBeDeleted ]
     ]);
 
-    foreach($result['values'] as $relationship) {
-      civicrm_api3('RelationshipType', 'delete', [
+    return array_column($result['values'], 'id');
+  }
+
+  /**
+   * Disables relationship types.
+   *
+   * @param Array[int] $relationshipTypeIds
+   */
+  private function up1012_disableRelationshipTypes($relationshipTypeIds) {
+    foreach ($relationshipTypeIds as $relationshipTypeId) {
+      civicrm_api3('RelationshipType', 'create', [
+        'id' => $relationshipTypeId,
+        'is_active' => 0
+      ]);
+    }
+  }
+
+  /**
+   * Removes relationships for the given relationship types.
+   *
+   * @param Array[int] $relationshipTypeIds
+   */
+  private function up1012_removeRelationshipsForTypes($relationshipTypeIds) {
+    foreach ($relationshipTypeIds as $relationshipTypeId) {
+      $relationships = $this->up1012_getRelationshipsForType($relationshipTypeId);
+      $this->up1012_removeRelationships($relationships);
+    }
+  }
+
+  /**
+   * Returns all the relationships associated with a given type.
+   *
+   * @param Array[int] $relationshipTypeIds
+   *
+   * @return Array
+   */
+  private function up1012_getRelationshipsForType($relationshipTypeId) {
+    $result = civicrm_api3('Relationship', 'get', [
+      'relationship_type_id' => $relationshipTypeId
+    ]);
+
+    return $result['values'];
+  }
+
+  /**
+   * Removes the relationships provided.
+   *
+   * @param Array
+   */
+  private function up1012_removeRelationships($relationships) {
+    foreach ($relationships as $relationship) {
+      civicrm_api3('Relationship', 'delete', [
         'id' => $relationship['id']
       ]);
     }
-
-    return TRUE;
   }
 }
