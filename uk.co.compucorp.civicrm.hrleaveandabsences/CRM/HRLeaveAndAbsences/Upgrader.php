a23425391159e99cc68d5b88512a2bc6ba00d6ea
Samson Alajede <ajesamson@yahoo.com>
2018-03-30 16:39:23 +0100

    PCHR-3506: Move leave request template to system workflow message

--- uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader.php
@@ -29,6 +29,7 @@ class CRM_HRLeaveAndAbsences_Upgrader extends CRM_HRLeaveAndAbsences_Upgrader_Ba
   use CRM_HRLeaveAndAbsences_Upgrader_Step_1021;
   use CRM_HRLeaveAndAbsences_Upgrader_Step_1022;
   use CRM_HRLeaveAndAbsences_Upgrader_Step_1023;
+  use CRM_HRLeaveAndAbsences_Upgrader_Step_1024;
 
   /**
    * A list of directories to be scanned for XML installation files
---
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader/Step/1024.php
@@ -0,0 +1,86 @@
+<?php
+
+trait CRM_HRLeaveAndAbsences_Upgrader_Step_1024 {
+  
+  /**
+   * Update leave request templates to system workflow message
+   *
+   * @return bool
+   */
+  public function upgrade_1024() {
+    $result = $this->up1024_createLeaveWorkflowOptionGroup();
+    
+    if ($result['count'] > 0) {
+      $this->up1024_createLeaveWorkflowOptionValue($result['id']);
+    }
+    
+    return TRUE;
+  }
+  
+  /**
+   * Create Leave Message Template Workflow Option Group.
+   *
+   * @return array
+   */
+  public function up1024_createLeaveWorkflowOptionGroup() {
+    $workflow = 'Message Template Workflow for Leave';
+    
+    return civicrm_api3('OptionGroup', 'create', [
+      'name' => 'msg_tpl_workflow_leave',
+      'title' => $workflow,
+      'description' => $workflow,
+    ]);
+  }
+  
+  /**
+   * Create Leave Message Template Workflow Option Values.
+   *
+   * @param $id
+   */
+  public function up1024_createLeaveWorkflowOptionValue($id) {
+    $optionValues = [
+      [
+        'option_group_id' => $id,
+        'label' => 'CiviHR Leave Request Notification',
+        'name' => 'civihr_leave_request',
+      ],
+      [
+        'option_group_id' => $id,
+        'label' => 'CiviHR TOIL Request Notification',
+        'name' => 'civihr_toil_request',
+      ],
+      [
+        'option_group_id' => $id,
+        'label' => 'CiviHR Sickness Record Notification',
+        'name' => 'civihr_sickness_record',
+      ]
+    ];
+  
+    foreach ($optionValues as $optionValue) {
+      $result = civicrm_api3('OptionValue', 'create', $optionValue);
+      if ($result['count'] > 0) {
+        $this->up1024_updateLeaveMsgTemplate(
+          $result['id'],
+          $optionValue['label']
+        );
+      }
+    }
+  }
+  
+  /**
+   * Update message template workflow id for option group msg_tpl_workflow_leave
+   *
+   * @param $workflowId
+   * @param $msgLabel
+   */
+  public function up1024_updateLeaveMsgTemplate($workflowId, $msgLabel) {
+    civicrm_api3('MessageTemplate', 'get', array(
+      'return' => ['id'],
+      'msg_title' => $msgLabel,
+      'api.MessageTemplate.create' => [
+        'workflow_id' => $workflowId,
+        'id' => "\$values['id']"
+      ],
+    ));
+  }
+}
