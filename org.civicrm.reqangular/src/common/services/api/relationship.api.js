2d4d8a3e4991d6284e479ee34cdd353bfce74e90
René Olivo <minet.ws@gmail.com>
2018-04-04 10:42:01 -0400

    PCHR-3469: Add relationship API

---
+++ org.civicrm.reqangular/src/common/services/api/relationship.api.js
@@ -0,0 +1,29 @@
+/* eslint-env amd */
+
+define([
+  'common/modules/apis',
+  'common/services/api'
+], function (apis) {
+  'use strict';
+
+  apis.factory('RelationshipAPI', ['$log', 'api', function ($log, api) {
+    $log.debug('RelationshipAPI');
+
+    return api.extend({
+
+      /**
+       * Returns the list of relationships
+       *
+       * @param {object} filters
+       * @param {object} pagination
+       * @param {string} sort
+       * @return {Promise}
+       */
+      all: function (filters, pagination, sort, additionalParams) {
+        $log.debug('Relationship.all()');
+
+        return this.getAll('Relationship', filters, pagination, sort, additionalParams);
+      }
+    });
+  }]);
+});
