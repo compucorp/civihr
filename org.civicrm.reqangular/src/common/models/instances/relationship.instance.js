bf8aa4ca9ed35e348b2cb3f22c45365da6f77e2a
René Olivo <minet.ws@gmail.com>
2018-04-04 19:20:25 -0400

    PCHR-3469: Add relationship validation method

--- org.civicrm.reqangular/src/common/models/instances/relationship.instance.js
+++ org.civicrm.reqangular/src/common/models/instances/relationship.instance.js
@@ -1,9 +1,10 @@
 /* eslint-env amd */
 
 define([
+  'common/moment',
   'common/modules/models-instances',
   'common/models/instances/instance'
-], function (instances) {
+], function (moment, instances) {
   'use strict';
 
   instances.factory('RelationshipInstance', RelationshipInstance);
@@ -11,6 +12,23 @@ define([
   RelationshipInstance.$inject = ['ModelInstance'];
 
   function RelationshipInstance (ModelInstance) {
-    return ModelInstance.extend({});
+    return ModelInstance.extend({
+      /**
+       * Determines if the relationship is valid by checking if it's active,
+       * the start date is in the past, and the end date is in the future.
+       *
+       * @return {Boolean}
+       */
+      isValid: function () {
+        var isActive = parseInt(this.is_active) === 1;
+        var startDateIsInThePast = moment().isSameOrAfter(this.start_date);
+        var startDateIsNotDefined = !this.start_date;
+        var endDateIsInTheFuture = moment().isSameOrBefore(this.end_date);
+        var endDateIsNotDefined = !this.end_date;
+
+        return isActive && (startDateIsNotDefined || startDateIsInThePast) &&
+          (endDateIsNotDefined || endDateIsInTheFuture);
+      }
+    });
   }
 });
--- org.civicrm.reqangular/test/models/instances/relationship.instance.spec.js
+++ org.civicrm.reqangular/test/models/instances/relationship.instance.spec.js
@@ -2,9 +2,10 @@
 
 define([
   'common/lodash',
+  'common/moment',
   'common/angularMocks',
   'common/models/instances/relationship.instance'
-], function (_) {
+], function (_, moment) {
   'use strict';
 
   describe('RelationshipInstance', function () {
@@ -19,6 +20,61 @@ define([
     it('inherits from ModelInstance', function () {
       expect(_.functions(RelationshipInstance))
         .toEqual(jasmine.arrayContaining(_.functions(ModelInstance)));
+    });
+
+    describe('isValid()', function () {
+      var nextWeek = moment().add(7, 'day').format('YYYY-MM-DD');
+      var previousWeek = moment().subtract(7, 'day').format('YYYY-MM-DD');
+      var relationships = {
+        isNotActive: { id: '1', is_active: '0' },
+        isInThePast: { id: '2', is_active: '1', end_date: previousWeek },
+        isInTheFuture: { id: '3', is_active: '1', start_date: nextWeek },
+        isActive: { id: '4', is_active: '1' },
+        hasStarted: { id: '5', is_active: '1', start_date: previousWeek },
+        hasNotFinished: { id: '6', is_active: '1', end_date: nextWeek }
+      };
+
+      beforeEach(function () {
+        _.forEach(relationships, function (relationship, index) {
+          relationships[index] = RelationshipInstance.init(relationship);
+        });
+      });
+
+      describe('when the relationship is active', function () {
+        it('returns true', function () {
+          expect(relationships.isActive.isValid()).toBe(true);
+        });
+      });
+
+      describe('when the relationship is not active', function () {
+        it('returns false', function () {
+          expect(relationships.isNotActive.isValid()).toBe(false);
+        });
+      });
+
+      describe('when the relationship has a start date in the past', function () {
+        it('returns true', function () {
+          expect(relationships.hasStarted.isValid()).toBe(true);
+        });
+      });
+
+      describe('when the relationship has a start date in the future', function () {
+        it('returns false', function () {
+          expect(relationships.isInTheFuture.isValid()).toBe(false);
+        });
+      });
+
+      describe('when the relationship end date is in the future', function () {
+        it('returns true', function () {
+          expect(relationships.hasNotFinished.isValid()).toBe(true);
+        });
+      });
+
+      describe('when the relationship end date is in the past', function () {
+        it('returns false', function () {
+          expect(relationships.isInThePast.isValid()).toBe(false);
+        });
+      });
     });
   });
 });
