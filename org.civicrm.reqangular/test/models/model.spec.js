78a1d5037c3e9e1fcbc1c43632f9bc0cccb4ea64
Igor Pavlov <pavlov.org@gmail.com>
2018-05-01 16:51:56 +0100

    PCHR-3642: Augment Model processor

--- org.civicrm.reqangular/test/models/model.spec.js
+++ org.civicrm.reqangular/test/models/model.spec.js
@@ -127,29 +127,69 @@ define([
         });
 
         describe('period-related filters', function () {
-          beforeEach(function () {
-            filters = Model.processFilters({
-              filter_1: { from: '20/01/2006', to: '16/02/2006' },
-              filter_2: { from: '31/12/2016' },
-              filter_3: { to: '02/07/2016' }
+          describe('when the dates are passed', function () {
+            beforeEach(function () {
+              filters = Model.processFilters({
+                filter_1: { from: jasmine.any(String), to: jasmine.any(String) },
+                filter_2: { from: jasmine.any(String) },
+                filter_3: { to: jasmine.any(String) }
+              });
+            });
+
+            it('build the correct filter structure', function () {
+              expect(filters).toEqual({
+                filter_1: { 'BETWEEN': [jasmine.any(String), jasmine.any(String)] },
+                filter_2: { '>=': jasmine.any(String) },
+                filter_3: { '<=': jasmine.any(String) }
+              });
             });
           });
 
-          it('uses the correct operator depending on the filter name', function () {
-            expect(filters).toEqual({
-              filter_1: {
-                'BETWEEN': [jasmine.any(String), jasmine.any(String)]
-              },
-              filter_2: { '>=': jasmine.any(String) },
-              filter_3: { '<=': jasmine.any(String) }
+          describe('when the dates are passed in DD/MM/YYYY format', function () {
+            beforeEach(function () {
+              filters = Model.processFilters({
+                filter_1: { from: '20/01/2006', to: '16/02/2006' },
+                filter_2: { from: '31/12/2016' },
+                filter_3: { to: '02/07/2016' }
+              });
+            });
+
+            it('converts the dates to the expected standard format', function () {
+              expect(filters).toEqual({
+                filter_1: { 'BETWEEN': ['2006-01-20', '2006-02-16'] },
+                filter_2: { '>=': '2016-12-31' },
+                filter_3: { '<=': '2016-07-02' }
+              });
             });
           });
 
-          it('converts the filter values to the expected date format', function () {
-            expect(filters).toEqual({
-              filter_1: { 'BETWEEN': ['2006-01-20', '2006-02-16'] },
-              filter_2: { '>=': '2016-12-31' },
-              filter_3: { '<=': '2016-07-02' }
+          describe('when the dates are passed in "YYYY-MM-DD[ HH:mm[:ss]]" formats', function () {
+            beforeEach(function () {
+              filters = Model.processFilters({
+                filter_1: { from: '2018-01-02', to: '2019-02-03' },
+                filter_2: { from: '2020-03-04' },
+                filter_3: { to: '2021-04-05' },
+                filter_4: { from: '2018-01-02 00:00', to: '2019-02-03 01:00' },
+                filter_5: { from: '2020-03-04 23:59' },
+                filter_6: { to: '2021-04-05 15:18' },
+                filter_7: { from: '2018-01-02 00:00:00', to: '2019-02-03 01:00:01' },
+                filter_8: { from: '2020-03-04 23:59:59' },
+                filter_9: { to: '2021-04-05 15:18:00' }
+              });
+            });
+
+            it('converts the dates to the expected standard format', function () {
+              expect(filters).toEqual({
+                filter_1: { 'BETWEEN': ['2018-01-02', '2019-02-03'] },
+                filter_2: { '>=': '2020-03-04' },
+                filter_3: { '<=': '2021-04-05' },
+                filter_4: { 'BETWEEN': ['2018-01-02 00:00', '2019-02-03 01:00'] },
+                filter_5: { '>=': '2020-03-04 23:59' },
+                filter_6: { '<=': '2021-04-05 15:18' },
+                filter_7: { 'BETWEEN': ['2018-01-02 00:00:00', '2019-02-03 01:00:01'] },
+                filter_8: { '>=': '2020-03-04 23:59:59' },
+                filter_9: { '<=': '2021-04-05 15:18:00' }
+              });
             });
           });
         });
