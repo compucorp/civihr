10c3154d3fd267d56f052c3aa6e86152fe8fcd03
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-05-04 10:42:39 +0200

    PCHR-3221: Fix readySelector for leave balance tab

--- uk.co.compucorp.civicrm.hrcore/backstop_data/page-objects/tabs/leave-balances.js
+++ uk.co.compucorp.civicrm.hrcore/backstop_data/page-objects/tabs/leave-balances.js
@@ -1,7 +1,7 @@
 const tab = require('./tab');
 
 module.exports = tab.extend({
-  readySelector: '.chr_leave-balance-tab',
+  readySelector: '.chr_leave-balance-tab__body .table-responsive',
   tabTitle: 'Leave Balance',
   tabUiSref: 'leave-balances'
 });
