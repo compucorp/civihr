5571688a0a704afb79ef9dfae565730c5bf67b86
Michael Devery <michael@compucorp.co.uk>
2018-05-15 15:08:17 +0100

    PCHR-3624: Move XML files to subdirectory

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader.php
@@ -56,14 +56,41 @@ class CRM_HRCore_Upgrader extends CRM_HRCore_Upgrader_Base {
   ];
 
   /**
+   * A list of directories to be scanned for XML installation files
+   *
+   * @var array
+   */
+  private $xmlDirectories = ['CustomGroups'];
+
+  /**
    * Callback called when the extension is installed
    */
   public function install() {
+    $this->processXMLInstallationFiles();
     $this->setScheduledJobsDefaultStatus();
     $this->deleteLocationTypes();
     $this->deleteUnneededCustomGroups();
     $this->createDefaultRelationshipTypes();
     $this->runAllUpgraders();
+  }
+
+  /**
+   * Scans all the directories in $xmlDirectories for installation files
+   * (xml files ending with _install.xml) and processes them.
+   */
+  private function processXMLInstallationFiles() {
+    foreach($this->xmlDirectories as $directory) {
+      $files = glob($this->extensionDir . "/xml/{$directory}/*_install.xml");
+      if (is_array($files)) {
+        foreach ($files as $file) {
+          $this->executeCustomDataFileByAbsPath($file);
+        }
+      }
+    }
+    // Flush the cache so that all pseudoconstants can be re-read from db
+    // This is to avoid issues when running upgraders during installation
+    // whereby some pseudoconstants were not available.
+    CRM_Core_PseudoConstant::flush();
   }
 
   /**
