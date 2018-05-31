97aff728cd373f5f03c8dff6de14c9534eddbb32
Michael Devery <michael@compucorp.co.uk>
2018-05-31 10:38:22 +0100

    PCHR-3611: Replace usages of "Home" location type with "Personal"

--- com.civicrm.hrjobroles/tests/phpunit/api/v3/ContactHRJobRoleTest.php
+++ com.civicrm.hrjobroles/tests/phpunit/api/v3/ContactHRJobRoleTest.php
@@ -39,7 +39,7 @@ class api_v3_ContactHRJobRoleTest extends CRM_Hrjobroles_Test_BaseHeadlessTest {
       'funder' => 'asddsa',
       'job_contract_id' => $contract['id'],
       'department' => $departments['IT'],
-      'location' => $locations['Home'],
+      'location' => $locations['Personal'],
       'level_type' => $levelTypes['Senior Manager']
     ]);
 
