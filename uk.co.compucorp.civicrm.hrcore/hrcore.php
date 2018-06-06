dac108ff37d2e5d3d523c88d8c7f98205ba6830b
Michael Devery <michael@compucorp.co.uk>
2018-06-06 11:30:29 +0100

    PCHR-3549: Remove primary address setter
    This was introduced just because in the SSP the address shown would
    always be primary. However, since the SSP now shows the first "Personal"
     address type we no longer need it to always set the primary address.

--- uk.co.compucorp.civicrm.hrcore/hrcore.php
+++ uk.co.compucorp.civicrm.hrcore/hrcore.php
@@ -276,28 +276,6 @@ function hrcore_civicrm_apiWrappers(&$wrappers, $apiRequest) {
 }
 
 /**
- * Implementation of hook_civicrm_pre hook.
- *
- * @param string $op
- *   Operation being done
- * @param string $objectName
- *   Name of the object on which the operation is being done
- * @param int $objectId
- *   ID of the record the object instantiates
- * @param array $params
- *   Parameter array being used to call the operation
- */
-function hrcore_civicrm_pre($op, $objectName, $objectId, &$params) {
-  $listeners = [
-    new CRM_HRCore_Hook_Pre_PrimaryAddressSetter(),
-  ];
-
-  foreach ($listeners as $currentListener) {
-    $currentListener->handle($op, $objectName, $objectId, $params);
-  }
-}
-
-/**
  * Implements hrcore_civicrm_pageRun.
  *
  * @link https://docs.civicrm.org/dev/en/master/hooks/hook_civicrm_pageRun/
