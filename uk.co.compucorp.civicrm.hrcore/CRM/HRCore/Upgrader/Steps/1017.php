ca78e1a97874ecc2c98a5f151e9279e611c6d71e
Michael Devery <michael@compucorp.co.uk>
2018-06-19 09:22:42 +0100

    PCHR-3537: Fix merge conflict in upgrader names
    Original upgrader creators were:
    
    1017: Michael Devery
    1018: Michael Devery
    1019: Elisio Leonardo

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1017.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1017.php
@@ -3,220 +3,24 @@
 trait CRM_HRCore_Upgrader_Steps_1017 {
 
   /**
-   * @var array
-   */
-  private $up1017_locationTypeIds = [];
-
-  /**
-   * Handles default location type settings and migrating existing data
+   * Updates some site configuration
    */
   public function upgrade_1017() {
-    $this->up1017_createPersonalLocationType();
-    $this->up1017_migrateLocationTypes();
-    $this->up1017_setDefaultLocationType('Work');
-    $this->up1017_reserveLocationTypes(['Work', 'Personal']);
-    $this->up1017_disableLocationType('Billing');
-    $this->up1017_deleteLocationTypes(['Home', 'Correspondence']);
+    $this->up1017_setSettingValue('logging', TRUE);
+    $this->up1017_setSettingValue('max_attachments', 10);
+    $this->up1017_setSettingValue('maxFileSize', 10);
 
     return TRUE;
   }
 
   /**
-   * The "Personal" location type should be created by the CiviHR installer
-   * script, but it could have been deleted so we need to ensure it exists
-   * before using it and making it reserved.
-   */
-  private function up1017_createPersonalLocationType() {
-    $params = [
-      'name' => 'Personal',
-      'description' => 'Place of Residence',
-      'display_name' => 'Personal',
-      'vcard_name' => 'PERSONAL',
-    ];
-
-    $existingId = $this->up1017_getLocationTypeID('Personal');
-    if ($existingId) {
-      $params['id'] = $existingId;
-    }
-
-    $result = civicrm_api3('LocationType', 'create', $params);
-
-    // update the cached IDs
-    $this->up1017_locationTypeIds['Personal'] = $result['id'];
-  }
-
-  /**
-   * Migrates location type data from one type to another
-   */
-  private function up1017_migrateLocationTypes() {
-    // All these entities have a reference to location_type_id
-    $entityTypesToMigrate = [
-      'Address',
-      'Email',
-      'IM',
-      'Mailing',
-      'OpenID',
-      'MappingField',
-      'Phone',
-      'UFField'
-    ];
-    // This is the map of old location type names to new ones
-    $locationTypeMap = [
-      'Billing' => 'Personal',
-      'Home' => 'Personal',
-      'Main' => 'Personal',
-      'Other' => 'Personal',
-    ];
-    $locationTypeMap = $this->up1017_convertLocationNamesToIds($locationTypeMap);
-
-    foreach ($locationTypeMap as $oldLocationId => $newLocationId) {
-      foreach ($entityTypesToMigrate as $entityName) {
-        $this->up1017_updateLocationTypeForEntities(
-          $entityName,
-          $oldLocationId,
-          $newLocationId
-        );
-      }
-    }
-  }
-
-  /**
-   * Sets the default location type
+   * Sets a CiviCRM setting value
    *
-   * @param string $locationType
+   * @param string $name
+   * @param mixed $value
    */
-  private function up1017_setDefaultLocationType($locationType) {
-    $locationTypeId = $this->up1017_getLocationTypeID($locationType);
-
-    civicrm_api3('LocationType', 'create', [
-      'id' => $locationTypeId,
-      'is_default' => TRUE
-    ]);
-  }
-
-  /**
-   * Sets all given location types as reserved
-   *
-   * @param array $locationTypes
-   */
-  private function up1017_reserveLocationTypes($locationTypes) {
-    foreach ($locationTypes as $locationType) {
-      $locationTypeId = $this->up1017_getLocationTypeID($locationType);
-      civicrm_api3('LocationType', 'create', [
-        'id' => $locationTypeId,
-        'is_reserved' => TRUE
-      ]);
-    }
-  }
-
-  /**
-   * Sets the given location type to be not active
-   *
-   * @param string $locationType
-   */
-  private function up1017_disableLocationType($locationType) {
-    $locationTypeId = $this->up1017_getLocationTypeID($locationType);
-    civicrm_api3('LocationType', 'create', [
-      'id' => $locationTypeId,
-      'is_active' => FALSE
-    ]);
-  }
-
-  /**
-   * Deletes all given location types if they exist
-   *
-   * @param array $locationTypes
-   */
-  private function up1017_deleteLocationTypes($locationTypes) {
-    foreach ($locationTypes as $locationType) {
-      $locationTypeId = $this->up1017_getLocationTypeID($locationType);
-      if ($locationTypeId) {
-        civicrm_api3('LocationType', 'delete', ['id' => $locationTypeId]);
-      }
-    }
-  }
-
-  /**
-   * Fetch all entities of a given type with a certain location type, then
-   * update them to have the new location type
-   *
-   * @param string $entityName
-   * @param int $oldLocationId
-   * @param int $newLocationId
-   */
-  private function up1017_updateLocationTypeForEntities(
-    $entityName,
-    $oldLocationId,
-    $newLocationId
-  ) {
-    $params = ['location_type_id' => $oldLocationId, 'return' => ['id']];
-
-    // UFField is wacky for updates. It complains about missing fields and
-    // does a dupe check that disregards whether you're trying to update, so
-    // we return all the existing params for use in the update
-    if ($entityName === 'UFField') {
-      unset($params['return']);
-    }
-
-    $entitiesToMigrate = civicrm_api3($entityName, 'get', $params);
-
-    foreach ($entitiesToMigrate['values'] as $entity) {
-      $entity['location_type_id'] = $newLocationId;
-      civicrm_api3($entityName, 'create', $entity);
-    }
-  }
-
-  /**
-   * Convert location type names to their IDs. This is required for now since
-   * not all API endpoints support sending a string location type
-   *
-   * @param array $locationTypeMap
-   *
-   * @return array
-   */
-  private function up1017_convertLocationNamesToIds($locationTypeMap) {
-    $locationTypeIds = $this->up1017_getLocationTypeIds();
-
-    foreach ($locationTypeMap as $oldLocationName => $newLocationName) {
-
-      unset($locationTypeMap[$oldLocationName]);
-      $oldLocationId = CRM_Utils_Array::value($oldLocationName, $locationTypeIds);
-      $newLocationId = CRM_Utils_Array::value($newLocationName, $locationTypeIds);
-
-      if ($oldLocationId && $newLocationId) {
-        $locationTypeMap[$oldLocationId] = $newLocationId;
-      }
-    }
-
-    return $locationTypeMap;
-  }
-
-  /**
-   * Looks up the location type ID for a given location type name
-   *
-   * @param string $locationTypeName
-   *
-   * @return int|null
-   */
-  private function up1017_getLocationTypeID($locationTypeName) {
-    $locationIds = $this->up1017_getLocationTypeIds();
-    $existing = CRM_Utils_Array::value($locationTypeName, $locationIds);
-
-    return $existing ? (int) $existing : NULL;
-  }
-
-  /**
-   * Gets a list of location type names mapped to their database ID
-   *
-   * @return array
-   */
-  private function up1017_getLocationTypeIds() {
-    if (empty($this->up1017_locationTypeIds)) {
-      $locationTypeIds = civicrm_api3('LocationType', 'get')['values'];
-      $this->up1017_locationTypeIds = array_column($locationTypeIds, 'id', 'name');
-    }
-
-    return $this->up1017_locationTypeIds;
+  private function up1017_setSettingValue($name, $value) {
+    civicrm_api3('Setting', 'create', [$name => $value]);
   }
 
 }
--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1018.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1018.php
@@ -3,119 +3,220 @@
 trait CRM_HRCore_Upgrader_Steps_1018 {
 
   /**
-   * Migrating Phone Types and Deleting unwanted Phone Types
+   * @var array
+   */
+  private $up1018_locationTypeIds = [];
+
+  /**
+   * Handles default location type settings and migrating existing data
    */
   public function upgrade_1018() {
-    $this->migratePhoneTypes();
-    $this->relabelPhoneType('Phone', 'Landline');
-    $this->reservePhoneTypes(['Phone', 'Mobile']);
-    $this->deletePhoneTypes(['Fax', 'Pager', 'Voicemail']);
+    $this->up1018_createPersonalLocationType();
+    $this->up1018_migrateLocationTypes();
+    $this->up1018_setDefaultLocationType('Work');
+    $this->up1018_reserveLocationTypes(['Work', 'Personal']);
+    $this->up1018_disableLocationType('Billing');
+    $this->up1018_deleteLocationTypes(['Home', 'Correspondence']);
 
     return TRUE;
   }
 
   /**
-   * Migrate the Phone Type Data as Follows:
-   * Fax -> Phone
-   * Pager -> Mobile
-   * Voicemail -> Mobile
+   * The "Personal" location type should be created by the CiviHR installer
+   * script, but it could have been deleted so we need to ensure it exists
+   * before using it and making it reserved.
    */
-  private function migratePhoneTypes() {
-    $phoneTypes = [
-      'Fax' => 'Phone',
-      'Pager' => 'Mobile',
-      'Voicemail' => 'Mobile',
+  private function up1018_createPersonalLocationType() {
+    $params = [
+      'name' => 'Personal',
+      'description' => 'Place of Residence',
+      'display_name' => 'Personal',
+      'vcard_name' => 'PERSONAL',
     ];
-    foreach ($phoneTypes as $originalType => $newType) {
-      $result = civicrm_api3('Phone', 'get', [
-        'sequential' => 1,
-        'return' => ['id', 'phone_type_id', 'is_primary', 'phone'],
-        'phone_type_id' => $originalType,
-      ]);
-      if ($result['count'] == 0) {
-        continue;
-      }
 
-      foreach ($result['values'] as $phone) {
-        $result = civicrm_api3('Phone', 'create', [
-          'id' => $phone['id'],
-          'phone_type_id' => $newType,
-        ]);
+    $existingId = $this->up1018_getLocationTypeID('Personal');
+    if ($existingId) {
+      $params['id'] = $existingId;
+    }
+
+    $result = civicrm_api3('LocationType', 'create', $params);
+
+    // update the cached IDs
+    $this->up1018_locationTypeIds['Personal'] = $result['id'];
+  }
+
+  /**
+   * Migrates location type data from one type to another
+   */
+  private function up1018_migrateLocationTypes() {
+    // All these entities have a reference to location_type_id
+    $entityTypesToMigrate = [
+      'Address',
+      'Email',
+      'IM',
+      'Mailing',
+      'OpenID',
+      'MappingField',
+      'Phone',
+      'UFField'
+    ];
+    // This is the map of old location type names to new ones
+    $locationTypeMap = [
+      'Billing' => 'Personal',
+      'Home' => 'Personal',
+      'Main' => 'Personal',
+      'Other' => 'Personal',
+    ];
+    $locationTypeMap = $this->up1018_convertLocationNamesToIds($locationTypeMap);
+
+    foreach ($locationTypeMap as $oldLocationId => $newLocationId) {
+      foreach ($entityTypesToMigrate as $entityName) {
+        $this->up1018_updateLocationTypeForEntities(
+          $entityName,
+          $oldLocationId,
+          $newLocationId
+        );
       }
     }
   }
 
   /**
-   * Changes the Label of Phone to Landline
+   * Sets the default location type
    *
-   * @param string $originalName the name of the phone_type to change
-   * @param string $newLabel the new Label to set to the phone_type
+   * @param string $locationType
    */
-  private function relabelPhoneType($originalName, $newLabel) {
-    $result = civicrm_api3('OptionValue', 'get', [
-      'return' => ['id', 'value', 'label'],
-      'option_group_id' => 'phone_type',
-      'name' => $originalName,
+  private function up1018_setDefaultLocationType($locationType) {
+    $locationTypeId = $this->up1018_getLocationTypeID($locationType);
+
+    civicrm_api3('LocationType', 'create', [
+      'id' => $locationTypeId,
+      'is_default' => TRUE
     ]);
-    if ($result['count'] != 1) {
-      return;
+  }
+
+  /**
+   * Sets all given location types as reserved
+   *
+   * @param array $locationTypes
+   */
+  private function up1018_reserveLocationTypes($locationTypes) {
+    foreach ($locationTypes as $locationType) {
+      $locationTypeId = $this->up1018_getLocationTypeID($locationType);
+      civicrm_api3('LocationType', 'create', [
+        'id' => $locationTypeId,
+        'is_reserved' => TRUE
+      ]);
     }
-    $optionValue = array_shift($result['values']);
-    $result = civicrm_api3('OptionValue', 'create', [
-      'id' => $optionValue['id'],
-      'label' => $newLabel,
+  }
+
+  /**
+   * Sets the given location type to be not active
+   *
+   * @param string $locationType
+   */
+  private function up1018_disableLocationType($locationType) {
+    $locationTypeId = $this->up1018_getLocationTypeID($locationType);
+    civicrm_api3('LocationType', 'create', [
+      'id' => $locationTypeId,
+      'is_active' => FALSE
     ]);
   }
 
   /**
-   * Marks the Phone Types as Reserved
+   * Deletes all given location types if they exist
    *
-   * @param array $phoneTypes
-   *
+   * @param array $locationTypes
    */
-  private function reservePhoneTypes($phoneTypes) {
-    foreach ($phoneTypes as $phoneType) {
-      $result = civicrm_api3('OptionValue', 'get', [
-        'return' => ['id', 'value', 'label'],
-        'option_group_id' => 'phone_type',
-        'name' => $phoneType,
-      ]);
-
-      if ($result['count'] != 1) {
-        return;
+  private function up1018_deleteLocationTypes($locationTypes) {
+    foreach ($locationTypes as $locationType) {
+      $locationTypeId = $this->up1018_getLocationTypeID($locationType);
+      if ($locationTypeId) {
+        civicrm_api3('LocationType', 'delete', ['id' => $locationTypeId]);
       }
-
-      $optionValue = array_shift($result['values']);
-      $result = civicrm_api3('OptionValue', 'create', [
-        'id' => $optionValue['id'],
-        'is_reserved' => 1,
-      ]);
     }
   }
 
   /**
-   * Deletes Phone Types Passed as arguments
+   * Fetch all entities of a given type with a certain location type, then
+   * update them to have the new location type
    *
-   * @param array $phoneTypes
-   *
+   * @param string $entityName
+   * @param int $oldLocationId
+   * @param int $newLocationId
    */
-  private function deletePhoneTypes($phoneTypes) {
-    foreach ($phoneTypes as $phoneType) {
-      $result = civicrm_api3('OptionValue', 'get', [
-        'return' => ['id', 'value', 'label'],
-        'option_group_id' => 'phone_type',
-        'name' => $phoneType,
-      ]);
+  private function up1018_updateLocationTypeForEntities(
+    $entityName,
+    $oldLocationId,
+    $newLocationId
+  ) {
+    $params = ['location_type_id' => $oldLocationId, 'return' => ['id']];
 
-      if ($result['count'] != 1) {
-        return;
-      }
-
-      $optionValue = array_shift($result['values']);
-      $result = civicrm_api3('OptionValue', 'delete', [
-        'id' => $optionValue['id'],
-      ]);
+    // UFField is wacky for updates. It complains about missing fields and
+    // does a dupe check that disregards whether you're trying to update, so
+    // we return all the existing params for use in the update
+    if ($entityName === 'UFField') {
+      unset($params['return']);
     }
+
+    $entitiesToMigrate = civicrm_api3($entityName, 'get', $params);
+
+    foreach ($entitiesToMigrate['values'] as $entity) {
+      $entity['location_type_id'] = $newLocationId;
+      civicrm_api3($entityName, 'create', $entity);
+    }
+  }
+
+  /**
+   * Convert location type names to their IDs. This is required for now since
+   * not all API endpoints support sending a string location type
+   *
+   * @param array $locationTypeMap
+   *
+   * @return array
+   */
+  private function up1018_convertLocationNamesToIds($locationTypeMap) {
+    $locationTypeIds = $this->up1018_getLocationTypeIds();
+
+    foreach ($locationTypeMap as $oldLocationName => $newLocationName) {
+
+      unset($locationTypeMap[$oldLocationName]);
+      $oldLocationId = CRM_Utils_Array::value($oldLocationName, $locationTypeIds);
+      $newLocationId = CRM_Utils_Array::value($newLocationName, $locationTypeIds);
+
+      if ($oldLocationId && $newLocationId) {
+        $locationTypeMap[$oldLocationId] = $newLocationId;
+      }
+    }
+
+    return $locationTypeMap;
+  }
+
+  /**
+   * Looks up the location type ID for a given location type name
+   *
+   * @param string $locationTypeName
+   *
+   * @return int|null
+   */
+  private function up1018_getLocationTypeID($locationTypeName) {
+    $locationIds = $this->up1018_getLocationTypeIds();
+    $existing = CRM_Utils_Array::value($locationTypeName, $locationIds);
+
+    return $existing ? (int) $existing : NULL;
+  }
+
+  /**
+   * Gets a list of location type names mapped to their database ID
+   *
+   * @return array
+   */
+  private function up1018_getLocationTypeIds() {
+    if (empty($this->up1018_locationTypeIds)) {
+      $locationTypeIds = civicrm_api3('LocationType', 'get')['values'];
+      $this->up1018_locationTypeIds = array_column($locationTypeIds, 'id', 'name');
+    }
+
+    return $this->up1018_locationTypeIds;
   }
 
 }
