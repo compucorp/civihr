2ba2277905d4a2fb26dc16ba740fee6ddfed846e
René Olivo <minet.ws@gmail.com>
2018-06-20 19:57:45 -0400

    PCHR-3910: Implement calendar feeds module on list module

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/calendar-feeds/list/list.module.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/calendar-feeds/list/list.module.js
@@ -2,24 +2,11 @@
 
 define([
   'common/angular',
-  '../calendar-feed-config.api',
-  '../calendar-feed-config.instance',
-  '../calendar-feed-config.model',
   './list.component',
-  'common/models/instances/instance',
-  'common/models/model',
-  'common/services/api',
-  'leave-absences/shared/modules/shared-settings'
-], function (angular, calendarFeedConfigAPI, calendarFeedConfigInstance, calendarFeedConfig,
-  listComponent) {
+  '../calendar-feeds.module'
+], function (angular, listComponent) {
   return angular.module('calendar-feeds.list', [
-    'common.apis',
-    'common.models.instances',
-    'common.models',
-    'leave-absences.settings'
+    'calendar-feeds'
   ])
-    .factory(calendarFeedConfigAPI.__name, calendarFeedConfigAPI)
-    .factory(calendarFeedConfigInstance.__name, calendarFeedConfigInstance)
-    .factory(calendarFeedConfig.__name, calendarFeedConfig)
     .component(listComponent.__name, listComponent);
 });
