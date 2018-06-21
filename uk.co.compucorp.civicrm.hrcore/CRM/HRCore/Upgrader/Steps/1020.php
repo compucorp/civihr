a2c1bccc362b91b59d5a6ac3606399b5e458e2ee
Elísio Leonardo <elisio.leonardo@gmail.com>
2018-06-21 09:43:45 +0200

    PCHR-3603: Changing Rename to Relabel

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1020.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1020.php
@@ -3,22 +3,22 @@
 trait CRM_HRCore_Upgrader_Steps_1020 {
 
   /**
-   * Rename Line Manager Relationship
+   * Relabel Line Manager Relationship
    */
   public function upgrade_1020() {
-    $this->up1020_renameLineManagerRelationship();
+    $this->up1020_reLabelLineManagerRelationship();
 
     return TRUE;
   }
 
   /**
-   * Renaming Line Manager Relationship
+   * ReLabeling Line Manager Relationship
    */
-  private function up1020_renameLineManagerRelationship() {
+  private function up1020_reLabelLineManagerRelationship() {
     $relationshipType = civicrm_api3('RelationshipType', 'get', [
       'name_b_a' => 'Line Manager',
     ]);
-    if (empty($relationshipType['values'])) {
+    if (empty($relationshipType['id'])) {
       return;
     }
     civicrm_api3('RelationshipType', 'create', [
