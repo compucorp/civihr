45176dd82be3b288b179f6f8e5baa2f6f275c6dc
Elísio Leonardo <elisio.leonardo@gmail.com>
2018-06-21 15:08:37 +0200

    PCHR-3536: Fixing Merge Conflicts

---
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1023.php
@@ -0,0 +1,52 @@
+<?php
+
+trait CRM_HRCore_Upgrader_Steps_1023 {
+
+  /**
+   * Changes the Order Of Individual Prefix
+   */
+  public function upgrade_1023() {
+    $this->up1023_changeOrderOfIndividualPrefix();
+
+    return TRUE;
+  }
+
+  /**
+   * Changes The Order of Individual Prefixes
+   */
+  private function up1023_changeOrderOfIndividualPrefix() {
+    $optionValues = civicrm_api3('OptionValue', 'get', [
+      'option_group_id' => 'individual_prefix',
+    ]);
+
+    $optionValues = $optionValues['values'];
+    foreach ($optionValues as $optionValueId => $optionValue) {
+      switch ($optionValue['name']) {
+        case 'Mr.':
+          $newWeight = 1;
+          break;
+
+        case 'Mrs.':
+          $newWeight = 2;
+          break;
+
+        case 'Ms.':
+          $newWeight = 3;
+          break;
+
+        case 'Miss':
+          $newWeight = 4;
+          break;
+
+        case 'Dr.':
+          $newWeight = 5;
+          break;
+      }
+      civicrm_api3('OptionValue', 'create', [
+        'id' => $optionValueId,
+        'weight' => $newWeight,
+      ]);
+    }
+  }
+
+}
