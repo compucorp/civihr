f2598929d9ff98098bfbb6059c9a5a5a4fe01448
Tunbola Ogunwande <tunbolawande@yahoo.com>
2018-06-22 15:47:00 +0100

    PCHR-3871: Make schema related changes.

--- uk.co.compucorp.civicrm.hrleaveandabsences/sql/auto_install.sql
+++ uk.co.compucorp.civicrm.hrleaveandabsences/sql/auto_install.sql
@@ -452,8 +452,10 @@ CREATE TABLE `civicrm_hrleaveandabsences_calendar_feed_config` (
      `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'Unique LeaveRequestCalendarFeedConfig ID',
      `title` varchar(127) NOT NULL   COMMENT 'The Calendar Feed Config Title. There cannot be more than one entity with the same title',
      `timezone` varchar(50) NOT NULL   COMMENT 'The Calendar Feed Config Timezone',
-     `hash` varchar(50) NOT NULL   COMMENT 'The Calendar Feed Config Hash. Should be unique per config',
-     `is_active` tinyint   DEFAULT 1 COMMENT 'Whether the feed is active or not',
-     `created_date` datetime    COMMENT 'The date and time this Calendar Feed Config was created',
-    PRIMARY KEY (`id`)
+     `hash` varchar(32) NOT NULL   COMMENT 'The Calendar Feed Config Hash. Should be unique per config',
+     `is_active` tinyint NOT NULL  DEFAULT 1 COMMENT 'Whether the feed is active or not',
+     `created_date` datetime NOT NULL   COMMENT 'The date and time this Calendar Feed Config was created',
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `unique_calendar_title`(title),
+    UNIQUE INDEX `unique_calendar_hash`(hash)
 )  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
