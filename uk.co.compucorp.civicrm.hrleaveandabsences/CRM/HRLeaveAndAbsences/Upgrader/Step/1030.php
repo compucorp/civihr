1ffd8183933d38512d09157c78990f1d51b3d17b
Tunbola Ogunwande <tunbolawande@yahoo.com>
2018-06-22 12:53:06 +0100

    PCHR-3871: Add the SQl for creating the calendar feed config table. Add Upgrader to create table for existing sites.

---
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader/Step/1030.php
@@ -0,0 +1,26 @@
+<?php
+
+trait CRM_HRLeaveAndAbsences_Upgrader_Step_1030 {
+
+  /**
+   * Creates the LeaveRequestCalendarFeedConfig table if
+   * it does not already exist.
+   *
+   * @return bool
+   */
+  public function upgrade_1030() {
+    CRM_Core_DAO::executeQuery("
+      CREATE TABLE IF NOT EXISTS `civicrm_hrleaveandabsences_calendar_feed_config` (
+      
+           `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'Unique LeaveRequestCalendarFeedConfig ID',
+           `title` varchar(127) NOT NULL   COMMENT 'The Calendar Feed Config Title. There cannot be more than one entity with the same title',
+           `timezone` varchar(50) NOT NULL   COMMENT 'The Calendar Feed Config Timezone',
+           `hash` varchar(50) NOT NULL   COMMENT 'The Calendar Feed Config Hash. Should be unique per config',
+           `is_active` tinyint   DEFAULT 1 COMMENT 'Whether the feed is active or not',
+           `created_date` datetime    COMMENT 'The date and time this Calendar Feed Config was created',
+          PRIMARY KEY (`id`)
+      )  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;");
+
+    return TRUE;
+  }
+}
--- uk.co.compucorp.civicrm.hrleaveandabsences/sql/auto_install.sql
+++ uk.co.compucorp.civicrm.hrleaveandabsences/sql/auto_install.sql
@@ -438,3 +438,22 @@ CREATE TABLE `civicrm_hrleaveandabsences_leave_balance_change_expiry_log` (
      `created_date` datetime    COMMENT 'The date and time this log was created' ,
     PRIMARY KEY (`id`)
 )  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
+
+-- /*******************************************************
+-- *
+-- * civicrm_hrleaveandabsences_calendar_feed_config
+-- *
+-- * Leave Request Calendar Feed Config
+-- *
+-- *******************************************************/
+CREATE TABLE `civicrm_hrleaveandabsences_calendar_feed_config` (
+
+
+     `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'Unique LeaveRequestCalendarFeedConfig ID',
+     `title` varchar(127) NOT NULL   COMMENT 'The Calendar Feed Config Title. There cannot be more than one entity with the same title',
+     `timezone` varchar(50) NOT NULL   COMMENT 'The Calendar Feed Config Timezone',
+     `hash` varchar(50) NOT NULL   COMMENT 'The Calendar Feed Config Hash. Should be unique per config',
+     `is_active` tinyint   DEFAULT 1 COMMENT 'Whether the feed is active or not',
+     `created_date` datetime    COMMENT 'The date and time this Calendar Feed Config was created',
+    PRIMARY KEY (`id`)
+)  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
