a8993c3e113f8cf59a92462c5a223a2ed9c5405a
Igor Pavlov <pavlov.org@gmail.com>
2018-06-29 17:30:10 +0100

    PCHR-3874: Add open feed link functionality

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/calendar-feeds/dropdown-button/calendar-feeds-dropdown-button.component.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/calendar-feeds/dropdown-button/calendar-feeds-dropdown-button.component.js
@@ -1,50 +1,62 @@
 /* eslint-env amd */
 
-define([
-  'common/lodash',
-  'leave-absences/calendar-feeds/calendar-feed/calendar-feeds.calendar-feed.module'
-], function (_) {
-  CalendarFeedsDropdownButtonController.$inject = ['CalendarFeed'];
+(function (CRM) {
+  define([
+    'common/lodash'
+  ], function (_) {
+    CalendarFeedsDropdownButtonController.$inject = ['CalendarFeed', 'CalendarFeedsLinkModal'];
 
-  return {
-    __name: 'calendarFeedsDropdownButton',
-    bindings: {
-      dropdownPosition: '@'
-    },
-    controller: CalendarFeedsDropdownButtonController,
-    controllerAs: 'dropdownButton',
-    templateUrl: ['shared-settings', function (sharedSettings) {
-      return sharedSettings.sourcePath + 'calendar-feeds/dropdown-button/calendar-feeds-dropdown-button.html';
-    }]
-  };
-
-  function CalendarFeedsDropdownButtonController (CalendarFeed) {
-    var vm = this;
-
-    vm.feeds = [];
-    vm.loading = {
-      feeds: false
+    return {
+      __name: 'calendarFeedsDropdownButton',
+      bindings: {
+        dropdownPosition: '@'
+      },
+      controller: CalendarFeedsDropdownButtonController,
+      controllerAs: 'dropdownButton',
+      templateUrl: ['shared-settings', function (sharedSettings) {
+        return sharedSettings.sourcePath + 'calendar-feeds/dropdown-button/calendar-feeds-dropdown-button.html';
+      }]
     };
 
-    (function init () {
-      loadFeeds();
-    }());
+    function CalendarFeedsDropdownButtonController (CalendarFeed, CalendarFeedsLinkModal) {
+      var vm = this;
 
-    /**
-     * Loads Calendar Feeds
-     *
-     * @return {Promise}
-     */
-    function loadFeeds () {
-      vm.loading.feeds = true;
+      vm.feeds = [];
+      vm.loading = {
+        feeds: false
+      };
 
-      return CalendarFeed.all()
-        .then(function (feeds) {
-          vm.feeds = feeds;
-        })
-        .finally(function () {
-          vm.loading.feeds = false;
-        });
+      vm.openLinkModal = openLinkModal;
+
+      (function init () {
+        loadFeeds();
+      }());
+
+      /**
+       * Loads Calendar Feeds
+       *
+       * @return {Promise}
+       */
+      function loadFeeds () {
+        vm.loading.feeds = true;
+
+        return CalendarFeed.all()
+          .then(function (feeds) {
+            vm.feeds = feeds;
+          })
+          .finally(function () {
+            vm.loading.feeds = false;
+          });
+      }
+
+      /**
+       * Opens Link Modal for a particular feed
+       *
+       * @param {String} feedHash - the hash for the feed to open
+       */
+      function openLinkModal (feedHash) {
+        CalendarFeedsLinkModal.open(feedHash);
+      }
     }
-  }
-});
+  });
+}(CRM));
--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/calendar-feeds/dropdown-button/calendar-feeds.dropdown-button.module.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/calendar-feeds/dropdown-button/calendar-feeds.dropdown-button.module.js
@@ -2,14 +2,16 @@
 
 define([
   'common/angular',
-  './calendar-feeds-dropdown-button.component',
-  '../calendar-feed/calendar-feeds.calendar-feed.module',
+  'leave-absences/calendar-feeds/dropdown-button/calendar-feeds-dropdown-button.component',
   'common/angularBootstrap',
-  '../calendar-feeds.core'
+  'leave-absences/calendar-feeds/calendar-feeds.core',
+  'leave-absences/calendar-feeds/calendar-feed/calendar-feeds.calendar-feed.module',
+  'leave-absences/calendar-feeds/link-modal/calendar-feeds.link-modal.module'
 ], function (angular, CalendarFeedsDropdownButtonComponent) {
   return angular.module('calendar-feeds.dropdown-button', [
     'ui.bootstrap',
     'calendar-feeds.core',
+    'calendar-feeds.link-modal',
     'calendar-feeds.models'
   ])
     .component(CalendarFeedsDropdownButtonComponent.__name, CalendarFeedsDropdownButtonComponent);
