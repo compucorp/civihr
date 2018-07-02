bd7fa9f0ee2366729e672479d9587f7844079347
Igor Pavlov <pavlov.org@gmail.com>
2018-07-02 10:09:55 +0100

    PCHR-3874: Remove unnecessary code

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/calendar-feeds/dropdown-button/calendar-feeds-dropdown-button.component.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/calendar-feeds/dropdown-button/calendar-feeds-dropdown-button.component.js
@@ -1,62 +1,58 @@
 /* eslint-env amd */
 
-(function (CRM) {
-  define([
-    'common/lodash'
-  ], function (_) {
-    CalendarFeedsDropdownButtonController.$inject = ['CalendarFeed', 'CalendarFeedsLinkModal'];
+define([], function () {
+  CalendarFeedsDropdownButtonController.$inject = ['CalendarFeed', 'CalendarFeedsLinkModal'];
 
-    return {
-      __name: 'calendarFeedsDropdownButton',
-      bindings: {
-        dropdownPosition: '@'
-      },
-      controller: CalendarFeedsDropdownButtonController,
-      controllerAs: 'dropdownButton',
-      templateUrl: ['shared-settings', function (sharedSettings) {
-        return sharedSettings.sourcePath + 'calendar-feeds/dropdown-button/calendar-feeds-dropdown-button.html';
-      }]
+  return {
+    __name: 'calendarFeedsDropdownButton',
+    bindings: {
+      dropdownPosition: '@'
+    },
+    controller: CalendarFeedsDropdownButtonController,
+    controllerAs: 'dropdownButton',
+    templateUrl: ['shared-settings', function (sharedSettings) {
+      return sharedSettings.sourcePath + 'calendar-feeds/dropdown-button/calendar-feeds-dropdown-button.html';
+    }]
+  };
+
+  function CalendarFeedsDropdownButtonController (CalendarFeed, CalendarFeedsLinkModal) {
+    var vm = this;
+
+    vm.feeds = [];
+    vm.loading = {
+      feeds: false
     };
 
-    function CalendarFeedsDropdownButtonController (CalendarFeed, CalendarFeedsLinkModal) {
-      var vm = this;
+    vm.openLinkModal = openLinkModal;
 
-      vm.feeds = [];
-      vm.loading = {
-        feeds: false
-      };
+    (function init () {
+      loadFeeds();
+    }());
 
-      vm.openLinkModal = openLinkModal;
+    /**
+     * Loads Calendar Feeds
+     *
+     * @return {Promise}
+     */
+    function loadFeeds () {
+      vm.loading.feeds = true;
 
-      (function init () {
-        loadFeeds();
-      }());
-
-      /**
-       * Loads Calendar Feeds
-       *
-       * @return {Promise}
-       */
-      function loadFeeds () {
-        vm.loading.feeds = true;
-
-        return CalendarFeed.all()
-          .then(function (feeds) {
-            vm.feeds = feeds;
-          })
-          .finally(function () {
-            vm.loading.feeds = false;
-          });
-      }
-
-      /**
-       * Opens Link Modal for a particular feed
-       *
-       * @param {String} feedHash - the hash for the feed to open
-       */
-      function openLinkModal (feedHash) {
-        CalendarFeedsLinkModal.open(feedHash);
-      }
+      return CalendarFeed.all()
+        .then(function (feeds) {
+          vm.feeds = feeds;
+        })
+        .finally(function () {
+          vm.loading.feeds = false;
+        });
     }
-  });
-}(CRM));
+
+    /**
+     * Opens Link Modal for a particular feed
+     *
+     * @param {String} feedHash - the hash for the feed to open
+     */
+    function openLinkModal (feedHash) {
+      CalendarFeedsLinkModal.open(feedHash);
+    }
+  }
+});
