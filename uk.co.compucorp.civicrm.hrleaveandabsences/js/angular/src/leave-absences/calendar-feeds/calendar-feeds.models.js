854b19524051565c16a00761e8ba5850f6c69892
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-07-03 08:57:01 +0200

    Add `calendar-feeds.apis` as a dependency to `calendar-feeds.models`
    Why
    1. As pointed out in earlier calls, we don't want the root modules to bother declaring explicitly the dependency to any `xyz.apis` modules, given that the api services are in the end an implementation details of Models (same goes for instances). So we declare `calendar-feeds.apis` as a dependency to `calendar-feeds.models`, and all a root module needs to do is to declare the dependency to `calendar-feeds.models`, and the apis will be included automatically
    2. Even if the root modules will only explicitly declare `xyz.models` as a dependency, keeping two separate modules for models and apis (`xyz.models` and `xyz.apis`) follows the modularity principle and allows the two modules to be single-purpose and to scale nicely

--- uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/calendar-feeds/calendar-feeds.models.js
+++ uk.co.compucorp.civicrm.hrleaveandabsences/js/angular/src/leave-absences/calendar-feeds/calendar-feeds.models.js
@@ -3,10 +3,12 @@
 define([
   'common/angular',
   'leave-absences/calendar-feeds/calendar-feed.model',
-  'common/models/model'
+  'common/models/model',
+  'leave-absences/calendar-feeds/calendar-feeds.apis'
 ], function (angular, CalendarFeed) {
   return angular.module('calendar-feeds.models', [
-    'common.models'
+    'common.models',
+    'calendar-feeds.apis'
   ])
     .factory(CalendarFeed.__name, CalendarFeed);
 });
