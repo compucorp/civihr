d0f60ece7cc15cb37341f6732b291052f221a0dc
Elísio Leonardo <elisio.leonardo@gmail.com>
2018-07-03 18:17:05 +0200

    PCHR-3605: Extracted some logic to their own method and renamed the main method

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1025.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1025.php
@@ -4,9 +4,11 @@ trait CRM_HRCore_Upgrader_Steps_1025 {
 
   /**
    * Removes Instant Messenger options
+   *
+   * @return bool
    */
   public function upgrade_1025() {
-    $this->up1025_removeInstantMessengerOptions([
+    $this->up1025_deleteOrDisableSocialAccounts([
       'Yahoo',
       'MSN',
       'AIM',
@@ -19,7 +21,7 @@ trait CRM_HRCore_Upgrader_Steps_1025 {
   /**
    * Removes some instant messenger options if not used, and disable it if used
    */
-  private function up1025_removeInstantMessengerOptions($accounts) {
+  private function up1025_deleteOrDisableSocialAccounts($accounts) {
     $socialAccounts = civicrm_api3('OptionValue', 'get', [
       'option_group_id' => 'instant_messenger_service',
       'name' => ['IN' => $accounts],
@@ -31,17 +33,31 @@ trait CRM_HRCore_Upgrader_Steps_1025 {
         'provider_id' => $optionValue['name'],
       ]);
       if ($socialAccountIsUsed['count'] == 0) {
-        civicrm_api3('OptionValue', 'delete', [
-          'id' => $optionValueId,
-        ]);
+        $this->up1025_deleteSocialAccount($optionValueId);
       }
       else {
-        civicrm_api3('OptionValue', 'create', [
-          'id' => $optionValueId,
-          'is_active' => 0,
-        ]);
+        $this->up1025_disableSocialAccount($optionValueId);
       }
     }
+  }
+
+  /**
+   * @param int $optionValueId
+   */
+  private function up1025_deleteSocialAccount($optionValueId) {
+    civicrm_api3('OptionValue', 'delete', [
+      'id' => $optionValueId,
+    ]);
+  }
+
+  /**
+   * @param int $optionValueId
+   */
+  private function up1025_disableSocialAccount($optionValueId) {
+    civicrm_api3('OptionValue', 'create', [
+      'id' => $optionValueId,
+      'is_active' => 0,
+    ]);
   }
 
 }
