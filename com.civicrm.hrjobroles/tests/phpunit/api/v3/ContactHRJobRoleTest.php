57fb6eec5d0d16d84642669c778619dd96742352
Tunbola Ogunwande <tunbolawande@yahoo.com>
2018-07-04 15:08:12 +0100

    PCHR-3903: Add support for contact_id filtering in the ContactHrJobRoles.get API.

--- com.civicrm.hrjobroles/tests/phpunit/api/v3/ContactHRJobRoleTest.php
+++ com.civicrm.hrjobroles/tests/phpunit/api/v3/ContactHRJobRoleTest.php
@@ -118,6 +118,64 @@ class api_v3_ContactHRJobRoleTest extends CRM_Hrjobroles_Test_BaseHeadlessTest {
     $this->assertEquals($expectedJobRole3, $contactJobRoles[$jobRole3['id']]);
   }
 
+  /**
+   * @expectedException CiviCRM_API3_Exception
+   * @expectedExceptionMessage The contact_id parameter only supports the IN operator
+   *
+   * @dataProvider invalidGetContactIdOperators
+   */
+  public function testGetThrowsExceptionForOperatorThatIsNotTheINOperatorForContactIDParameter($operator) {
+    civicrm_api3('ContactHrJobRoles', 'get', ['contact_id' => [$operator => [1]]]);
+  }
+
+  public function testGeDoesNotThrowExceptionWhenUsingTheINOperatorForTheContactIdParameter() {
+    $values = civicrm_api3('ContactHrJobRoles', 'get', ['contact_id' => ['IN' => [1]]]);
+
+    $this->assertEquals(0, $values['is_error']);
+  }
+
+  public function testGeDoesNotThrowExceptionWhenUsingTheEqualsToOperatorForTheContactIdParameter() {
+    $values = civicrm_api3('ContactHrJobRoles', 'get', ['contact_id' => 1]);
+
+    $this->assertEquals(0, $values['is_error']);
+  }
+
+  public function testGetReturnsJobRolesForPassedInContacts() {
+    $contact1 = ContactFabricator::fabricate();
+    $contact2 = ContactFabricator::fabricate();
+    $contact3 = ContactFabricator::fabricate();
+    $contract1 = HRJobContractFabricator::fabricate(['contact_id' => $contact1['id']]);
+    $contract2 = HRJobContractFabricator::fabricate(['contact_id' => $contact2['id']]);
+    $contract3 = HRJobContractFabricator::fabricate(['contact_id' => $contact3['id']]);
+    $jobRole1 = HRJobRolesFabricator::fabricate(['job_contract_id' => $contract1['id']]);
+    $jobRole2 = HRJobRolesFabricator::fabricate(['job_contract_id' => $contract2['id']]);
+    $jobRole3 = HRJobRolesFabricator::fabricate(['job_contract_id' => $contract3['id']]);
+
+    //Get Jobroles for contact1 and contact3
+    $contactJobRoles = civicrm_api3($this->entity, $this->action, [
+      'contact_id' => ['IN' => [$contact1['id'], $contact3['id']]]
+    ])['values'];
+
+    $this->assertCount(2, $contactJobRoles);
+
+    // The CiviCRM only returns non-empty fields. Since we only set the job
+    // contract id, only the ID and the contact_id fields will be returned
+    $expectedJobRole1 = ['id' => $jobRole1['id'], 'contact_id' => $contact1['id']];
+    $expectedJobRole3 = ['id' => $jobRole3['id'], 'contact_id' => $contact3['id']];
+
+    $this->assertEquals($expectedJobRole1, $contactJobRoles[$jobRole1['id']]);
+    $this->assertEquals($expectedJobRole3, $contactJobRoles[$jobRole3['id']]);
+
+    //Get Jobroles for contact2 only
+    $contactJobRoles = civicrm_api3($this->entity, $this->action, [
+      'contact_id' => $contact2['id']
+    ])['values'];
+
+    $this->assertCount(1, $contactJobRoles);
+    $expectedJobRole2 = ['id' => $jobRole2['id'], 'contact_id' => $contact2['id']];
+    $this->assertEquals($expectedJobRole2, $contactJobRoles[$jobRole2['id']]);
+  }
+
   private function setExpectedApiPermissionException($permission) {
     $message = "API permission check failed for {$this->entity}/{$this->action} call; insufficient permission: require {$permission}";
     $this->setExpectedException('CiviCRM_API3_Exception', $message);
@@ -126,5 +184,23 @@ class api_v3_ContactHRJobRoleTest extends CRM_Hrjobroles_Test_BaseHeadlessTest {
   private function registerCurrentLoggedInContactInSession($contactID) {
     $session = CRM_Core_Session::singleton();
     $session->set('userID', $contactID);
+  }
+
+  public function invalidGetContactIdOperators() {
+    return [
+      ['>'],
+      ['>='],
+      ['<='],
+      ['<'],
+      ['<>'],
+      ['!='],
+      ['BETWEEN'],
+      ['NOT BETWEEN'],
+      ['LIKE'],
+      ['NOT LIKE'],
+      ['NOT IN'],
+      ['IS NULL'],
+      ['IS NOT NULL'],
+    ];
   }
 }
