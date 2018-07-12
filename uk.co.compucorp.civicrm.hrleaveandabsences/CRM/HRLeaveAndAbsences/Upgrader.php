4c4273b605d849f4047bc6a3e519d959993f87a0
Tunbola Ogunwande <tunbolawande@yahoo.com>
2018-07-12 10:24:44 +0100

    Rename Upgrader to fix conflicts

--- uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader.php
@@ -35,8 +35,8 @@ class CRM_HRLeaveAndAbsences_Upgrader extends CRM_HRLeaveAndAbsences_Upgrader_Ba
   use CRM_HRLeaveAndAbsences_Upgrader_Step_1027;
   use CRM_HRLeaveAndAbsences_Upgrader_Step_1028;
   use CRM_HRLeaveAndAbsences_Upgrader_Step_1029;
-  use CRM_HRLeaveAndAbsences_Upgrader_Step_1030;
   use CRM_HRLeaveAndAbsences_Upgrader_Step_1031;
+  use CRM_HRLeaveAndAbsences_Upgrader_Step_1032;
 
   /**
    * A list of directories to be scanned for XML installation files
--- uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader/Step/1031.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader/Step/1031.php
@@ -3,34 +3,27 @@
 trait CRM_HRLeaveAndAbsences_Upgrader_Step_1031 {
 
   /**
-   * Adds the Calendar Feeds menu.
+   * Creates the LeaveRequestCalendarFeedConfig table if
+   * it does not already exist.
    *
    * @return bool
    */
   public function upgrade_1031() {
-    $result = civicrm_api3('Navigation', 'get', ['name' => 'leave_and_absence_calendar_feeds']);
-
-    if ($result['count'] > 0) {
-      return TRUE;
-    }
-
-    $leaveAndAbsenceNav = civicrm_api3('Navigation', 'get', ['name' => 'leave_and_absences']);
-
-    if (empty($leaveAndAbsenceNav['id'])) {
-      return TRUE;
-    }
-
-    $weight = CRM_Core_BAO_Navigation::calculateWeight($leaveAndAbsenceNav['id']);
-
-    civicrm_api3('Navigation', 'create', [
-      'label' => ts('Calendar Feeds'),
-      'name' => 'leave_and_absence_calendar_feeds',
-      'url' => 'civicrm/admin/leaveandabsences/calendar-feeds',
-      'permission' => 'can administer calendar feeds',
-      'parent_id' => $leaveAndAbsenceNav['id'],
-      'weight' => $weight,
-      'is_active' => 1
-    ]);
+    CRM_Core_DAO::executeQuery("
+      CREATE TABLE IF NOT EXISTS `civicrm_hrleaveandabsences_calendar_feed_config` (
+      
+           `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'Unique LeaveRequestCalendarFeedConfig ID',
+           `title` varchar(127) NOT NULL   COMMENT 'The Calendar Feed Config Title. There cannot be more than one entity with the same title',
+           `timezone` varchar(50) NOT NULL   COMMENT 'The Calendar Feed Config Timezone',
+           `hash` varchar(32) NOT NULL   COMMENT 'The Calendar Feed Config Hash. Should be unique per config',
+           `composed_of` text NOT NULL   COMMENT 'The components of the leave request feed, e.g leave types, departments',
+           `visible_to` text NOT NULL   COMMENT 'Who gets to view the feed link e.g staff in certain departments or location',
+           `is_active` tinyint NOT NULL  DEFAULT 1 COMMENT 'Whether the feed is active or not',
+           `created_date` datetime NOT NULL   COMMENT 'The date and time this Calendar Feed Config was created',
+          PRIMARY KEY (`id`),     
+          UNIQUE INDEX `unique_calendar_title`(title),
+          UNIQUE INDEX `unique_calendar_hash`(hash)
+      )  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;");
 
     return TRUE;
   }
