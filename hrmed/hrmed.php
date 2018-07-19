3053d24c77c7d6390d9024e84ee81a3d67543bc4
Debarshi Bhaumik <deb1990@gmail.com>
2018-07-19 18:59:59 +0530

    PCHR-3941: Use RequireJS in HR MED

--- hrmed/hrmed.php
+++ hrmed/hrmed.php
@@ -163,6 +163,6 @@ function hrmed_civicrm_pageRun($page) {
     CRM_Core_Resources::singleton()
       ->addScriptFile('civicrm', 'js/jquery/jquery.crmRevisionLink.js', CRM_Core_Resources::DEFAULT_WEIGHT, 'html-header');
     CRM_Core_Resources::singleton()
-      ->addScriptFile('org.civicrm.hrmed', 'js/hrmed.js');
+      ->addScriptFile('org.civicrm.hrmed', 'js/dist/hrmed.min.js', 1010);
   }
 }
