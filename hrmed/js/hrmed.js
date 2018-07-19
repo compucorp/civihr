dbd097c3ed837ff32682c0d78e5ac5023feeb419
Debarshi Bhaumik <deb1990@gmail.com>
2018-07-19 18:49:52 +0530

    PCHR-3941: Fix linting issues for HR MED

--- hrmed/js/hrmed.js
+++ hrmed/js/hrmed.js
@@ -1,12 +1,12 @@
 // Copyright CiviCRM LLC 2013. See http://civicrm.org/licensing
-(function($, _) {
+(function ($, _) {
   // add helpicon for conitions
-  $('body').on('crmFormLoad', function(event) {
-    if (event.profileName == 'hrmed_tab') {
+  $('body').on('crmFormLoad', function (event) {
+    if (event.profileName === 'hrmed_tab') {
       var accessName = $('[data-crm-custom="Medical_Disability:Condition"]').attr('name');
-      if($('div#editrow-' + accessName + ' a.helpicon').length == 0) {
-        var helpIcon = $( "<span class ='crm-container'><a class='helpicon' onclick='CRM.help(\"\", {\"id\":\"hrmed-med-condition\",\"file\":\"CRM\/HRMed\/Page\/helptext\"}); return false;' title='Condition Help'></a></span>" );
-        $('div#editrow-' + accessName +' div label').append(helpIcon);
+      if ($('div#editrow-' + accessName + ' a.helpicon').length === 0) {
+        var helpIcon = $("<span class ='crm-container'><a class='helpicon' onclick='CRM.help(\"\", {\"id\":\"hrmed-med-condition\",\"file\":\"CRM/HRMed/Page/helptext\"}); return false;' title='Condition Help'></a></span>");
+        $('div#editrow-' + accessName + ' div label').append(helpIcon);
       }
     }
   });
