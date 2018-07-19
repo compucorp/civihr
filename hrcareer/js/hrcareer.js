14d020362d5243e6cf81353d8a6c0dd9cf42e95e
Debarshi Bhaumik <deb1990@gmail.com>
2018-07-19 12:45:28 +0530

    PCHR-3941: Fix linting issues for HR Career

--- hrcareer/js/hrcareer.js
+++ hrcareer/js/hrcareer.js
@@ -1,11 +1,11 @@
 // http://civicrm.org/licensing
-CRM.$(function($) {
-  $('body').on('crmFormLoad', function(event) {
-    if (event.profileName == 'hrcareer_tab') {
+CRM.$(function ($) {
+  $('body').on('crmFormLoad', function (event) {
+    if (event.profileName === 'hrcareer_tab') {
       var accessName = $('[data-crm-custom="Career:End_Date"]').attr('name');
-      if($('div#editrow-' + accessName + ' a.helpicon').length == 0) {
-        var helpIcon = $( "<span class ='crm-container'><a class='helpicon' onclick='CRM.help(\"\", {\"id\":\"hrcareer-enddate\",\"file\":\"CRM\/HRCareer\/Page\/helptext\"}); return false;' title='End Date Help'></a></span>" );
-        $('div#editrow-' + accessName +' div label').append(helpIcon);
+      if ($('div#editrow-' + accessName + ' a.helpicon').length === 0) {
+        var helpIcon = $("<span class ='crm-container'><a class='helpicon' onclick='CRM.help(\"\", {\"id\":\"hrcareer-enddate\",\"file\":\"CRM/HRCareer/Page/helptext\"}); return false;' title='End Date Help'></a></span>");
+        $('div#editrow-' + accessName + ' div label').append(helpIcon);
       }
     }
   });
