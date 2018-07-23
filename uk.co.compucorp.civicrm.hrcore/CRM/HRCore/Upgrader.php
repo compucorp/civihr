cee46a489cc1717e94fffba6135209096dd27530
Elísio Leonardo <elisio.leonardo@gmail.com>
2018-07-23 10:20:45 +0200

    PCHR-4003: Hide Recruitment Menu on Admin Portal

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader.php
@@ -28,7 +28,6 @@ class CRM_HRCore_Upgrader extends CRM_HRCore_Upgrader_Base {
   use CRM_HRCore_Upgrader_Steps_1018;
   use CRM_HRCore_Upgrader_Steps_1019;
   use CRM_HRCore_Upgrader_Steps_1020;
-  use CRM_HRCore_Upgrader_Steps_1021;
 
   /**
    * @var array
