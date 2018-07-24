abc0b7b094de2177fceb4387e90cef076d93e5b6
René Olivo <minet.ws@gmail.com>
2018-07-24 12:40:10 +0000

    PCHR-3935: PR Feedback

--- uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1021.php
+++ uk.co.compucorp.civicrm.hrcore/CRM/HRCore/Upgrader/Steps/1021.php
@@ -10,28 +10,28 @@ trait CRM_HRCore_Upgrader_Steps_1021 {
    */
   public function upgrade_1021() {
     // Add option group for activity default assignees:
-    CRM_Core_BAO_OptionGroup::ensureOptionGroupExists(array(
+    CRM_Core_BAO_OptionGroup::ensureOptionGroupExists([
       'name' => 'activity_default_assignee',
       'title' => ts('Activity default assignee'),
       'is_reserved' => 1,
-    ));
+    ]);
 
     // Add option values for activity default assignees:
-    $options = array(
-      array('name' => 'NONE', 'label' => ts('None'), 'is_default' => 1),
-      array('name' => 'BY_RELATIONSHIP', 'label' => ts('By relationship to case client')),
-      array('name' => 'SPECIFIC_CONTACT', 'label' => ts('Specific contact')),
-      array('name' => 'USER_CREATING_THE_CASE', 'label' => ts('User creating the case')),
-    );
+    $options = [
+      ['name' => 'NONE', 'label' => ts('None'), 'is_default' => 1],
+      ['name' => 'BY_RELATIONSHIP', 'label' => ts('By relationship to case client')],
+      ['name' => 'SPECIFIC_CONTACT', 'label' => ts('Specific contact')],
+      ['name' => 'USER_CREATING_THE_CASE', 'label' => ts('User creating the case')],
+    ];
 
     foreach ($options as $option) {
-      CRM_Core_BAO_OptionValue::ensureOptionValueExists(array(
+      CRM_Core_BAO_OptionValue::ensureOptionValueExists([
         'option_group_id' => 'activity_default_assignee',
         'name' => $option['name'],
         'label' => $option['label'],
         'is_default' => CRM_Utils_Array::value('is_default', $option, 0),
         'is_active' => TRUE,
-      ));
+      ]);
     }
 
     return TRUE;
