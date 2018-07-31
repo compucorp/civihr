38f18086e9896dfad87432e1a9d70f405af6e84d
Tunbola Ogunwande <tunbolawande@yahoo.com>
2018-07-31 17:01:38 +0100

    PCHR-4051: Refactor method for clarity.

--- uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Service/LeaveRequestRights.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Service/LeaveRequestRights.php
@@ -232,9 +232,9 @@ class CRM_HRLeaveAndAbsences_Service_LeaveRequestRights {
 
     $today = new DateTime('today');
 
-    $absenceTypeRequestCancellationNotNo =
-      $absenceType->allow_request_cancelation != AbsenceType::REQUEST_CANCELATION_NO;
-    if ($absenceTypeRequestCancellationNotNo && $leaveFromDate > $today) {
+    $absenceTypeAllowsFutureCancellation =
+      $absenceType->allow_request_cancelation == AbsenceType::REQUEST_CANCELATION_IN_ADVANCE_OF_START_DATE;
+    if ($absenceTypeAllowsFutureCancellation && $leaveFromDate > $today) {
       return TRUE;
     }
 
