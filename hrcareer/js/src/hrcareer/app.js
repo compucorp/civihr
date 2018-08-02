6e89b0ec8a8bf3c68087eeb7b52b87347e6eb64d
Igor Pavlov <pavlov.org@gmail.com>
2018-08-02 14:46:25 +0100

    PCHR-4005: PR Feedback

--- hrcareer/js/src/hrcareer/app.js
+++ hrcareer/js/src/hrcareer/app.js
@@ -3,26 +3,53 @@
 define(function (CRM) {
   'use strict';
 
-  CRM.$('body').on('crmFormLoad', function () {
+  CRM.$('body').on('crmFormLoad', injectHelpIcons);
+
+  /**
+   * Injects a help icon to a form field
+   *
+   * @param {jQuery} $form
+   * @param {String} accessNameSelector - a selector to the field to populate to
+   * @param {String} title - the title of the help icon
+   * @param {String} helpId - the ID of the helper
+   * @param {String} helpFile - the path to the helper template
+   */
+  function injectHelpIcon ($form, accessNameSelector, title, helpId, helpFile) {
+    var $helpIcon;
+    var accessName = $form.find('[data-crm-custom="' + accessNameSelector + '"]').attr('name');
+    var labelSelector = 'label[for=' + accessName + ']';
+
+    if ($form.find(labelSelector + ' .helpicon').length) {
+      return;
+    }
+
+    $helpIcon = CRM.$('<a href class="helpicon" title="' + title + '"></a>');
+
+    // Populates a space before the icon if there are no other markers
+    if (!$form.find(labelSelector + ' .crm-marker').length) {
+      $form.find(labelSelector).append('&#160;');
+    }
+
+    $form.find(labelSelector).append($helpIcon);
+
+    $helpIcon.on('click', function () {
+      CRM.help('', { id: helpId, file: helpFile });
+
+      return false;
+    });
+  }
+
+  /**
+   * Injects help icons into the Career CiviCRM form
+   */
+  function injectHelpIcons () {
     var form = CRM.$('.custom-group-Career');
 
-    if (form.length) {
-      var accessName = form.find('[data-crm-custom="Career:End_Date"]').attr('name');
-      var labelSelector = 'label[for=' + accessName + ']';
-
-      if (!form.find(labelSelector + ' .helpicon').length) {
-        var helpIcon = CRM.$('<a href class="helpicon" title="End Date Help"></a>');
-
-        form.find(labelSelector).append('&#160;').append(helpIcon);
-        helpIcon.on('click', function () {
-          CRM.help('', {
-            id: 'hrcareer-enddate',
-            file: 'CRM/HRCareer/Page/helptext'
-          });
-
-          return false;
-        });
-      }
+    if (!form.length) {
+      return;
     }
-  });
+
+    injectHelpIcon(form, 'Career:End_Date', 'End Date Help',
+      'hrcareer-enddate', 'CRM/HRCareer/Page/helptext');
+  }
 }(CRM));
--- hrmed/js/src/hrmed/app.js
+++ hrmed/js/src/hrmed/app.js
@@ -3,26 +3,53 @@
 define(function (CRM) {
   'use strict';
 
-  CRM.$('body').on('crmFormLoad', function () {
+  CRM.$('body').on('crmFormLoad', injectHelpIcons);
+
+  /**
+   * Injects a help icon to a form field
+   *
+   * @param {jQuery} $form
+   * @param {String} accessNameSelector - a selector to the field to populate to
+   * @param {String} title - the title of the help icon
+   * @param {String} helpId - the ID of the helper
+   * @param {String} helpFile - the path to the helper template
+   */
+  function injectHelpIcon ($form, accessNameSelector, title, helpId, helpFile) {
+    var $helpIcon;
+    var accessName = $form.find('[data-crm-custom="' + accessNameSelector + '"]').attr('name');
+    var labelSelector = 'label[for=' + accessName + ']';
+
+    if ($form.find(labelSelector + ' .helpicon').length) {
+      return;
+    }
+
+    $helpIcon = CRM.$('<a href class="helpicon" title="' + title + '"></a>');
+
+    // Populates a space before the icon if there are no other markers
+    if (!$form.find(labelSelector + ' .crm-marker').length) {
+      $form.find(labelSelector).append('&#160;');
+    }
+
+    $form.find(labelSelector).append($helpIcon);
+
+    $helpIcon.on('click', function () {
+      CRM.help('', { id: helpId, file: helpFile });
+
+      return false;
+    });
+  }
+
+  /**
+   * Injects help icons into the Career CiviCRM form
+   */
+  function injectHelpIcons () {
     var form = CRM.$('.custom-group-Medical_Disability');
 
-    if (form.length) {
-      var accessName = form.find('[data-crm-custom="Medical_Disability:Condition"]').attr('name');
-      var labelSelector = 'label[for=' + accessName + ']';
-
-      if (!form.find(labelSelector + ' .helpicon').length) {
-        var helpIcon = CRM.$('<a href class="helpicon" title="Condition Help"></a>');
-
-        form.find(labelSelector).append(helpIcon);
-        helpIcon.on('click', function () {
-          CRM.help('', {
-            id: 'hrmed-med-condition',
-            file: 'CRM/HRMed/Page/helptext'
-          });
-
-          return false;
-        });
-      }
+    if (!form.length) {
+      return;
     }
-  });
+
+    injectHelpIcon(form, 'Medical_Disability:Condition', 'Condition Help',
+      'hrmed-med-condition', 'CRM/HRMed/Page/helptext');
+  }
 }(CRM));
