15cae4fe18251890ac8cc23c61b77753f590de46
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-08-26 10:02:00 +0200

    PCHR-4144: Remove backstop_data folder

--- uk.co.compucorp.civicrm.hrcore/backstop_data/engine_scripts/onReady.js
+++
@@ -1,8 +0,0 @@
-const Page = require('../page-objects/page');
-
-module.exports = async (puppet, scenario, vp) => {
-  const page = new Page(puppet);
-
-  await require('./clickAndHoverHelper')(puppet, scenario);
-  await page.init();
-};
