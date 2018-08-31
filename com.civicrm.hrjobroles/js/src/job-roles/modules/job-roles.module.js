ffa69cd6329608463476d184ef8a0e7dc3a5b45c
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-08-31 14:50:17 +0200

    PCHR-3945: (Job Roles) Register components on main module

--- com.civicrm.hrjobroles/js/src/job-roles/modules/job-roles.module.js
+++ com.civicrm.hrjobroles/js/src/job-roles/modules/job-roles.module.js
@@ -2,23 +2,30 @@
 
 define([
   'common/angular',
+  'job-roles/controllers/job-roles.controller',
+  'job-roles/controllers/modal-dialog.controller',
+  'job-roles/filters/get-active-values.filter',
+  'job-roles/services/date-validation.service',
+  'job-roles/services/filters.service',
+  'job-roles/services/job-role.service',
   'job-roles/modules/job-roles.config',
   'job-roles/modules/job-roles.constants',
-  'job-roles/modules/job-roles.controllers',
   'job-roles/modules/job-roles.core',
-  'job-roles/modules/job-roles.filters',
-  'job-roles/modules/job-roles.run',
-  'job-roles/modules/job-roles.services'
-], function (angular) {
+  'job-roles/modules/job-roles.run'
+], function (angular, JobRolesController, ModalDialogController, getActiveValues,
+  dateValidation, filtersService, jobRoleService) {
   'use strict';
 
   angular.module('hrjobroles', [
     'hrjobroles.core',
     'hrjobroles.config',
     'hrjobroles.run',
-    'hrjobroles.constants',
-    'hrjobroles.controllers',
-    'hrjobroles.filters',
-    'hrjobroles.services'
-  ]);
+    'hrjobroles.constants'
+  ])
+    .controller(JobRolesController)
+    .controller(ModalDialogController)
+    .filter(getActiveValues)
+    .factory(dateValidation)
+    .factory(filtersService)
+    .factory(jobRoleService);
 });
