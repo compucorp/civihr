04405d62842a5d9435ecbdfec9706dab84dffda5
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-08-31 15:08:14 +0200

    PCHR-3945: (Access Rights) Remove access-rights.controllers

--- contactaccessrights/js/src/access-rights/modules/access-rights.module.js
+++ contactaccessrights/js/src/access-rights/modules/access-rights.module.js
@@ -2,21 +2,23 @@
 
 define([
   'common/angular',
+  'access-rights/controllers/access-rights.controller',
+  'access-rights/controllers/access-rights-modal.controller',
   'access-rights/modules/access-rights.config',
-  'access-rights/modules/access-rights.controllers',
   'access-rights/modules/access-rights.core',
   'access-rights/modules/access-rights.models',
   'access-rights/modules/access-rights.run'
-], function (angular) {
+], function (angular, AccessRightsController, AccessRightsModalController) {
   'use strict';
 
   angular.module('access-rights', [
     'access-rights.core',
     'access-rights.config',
     'access-rights.run',
-    'access-rights.controllers',
     'access-rights.models'
-  ]);
+  ])
+    .controller(AccessRightsController)
+    .controller(AccessRightsModalController);
 
   return angular;
 });
