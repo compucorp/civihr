969d560b794208e52dbfd3eb9aa303a8afc9557b
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-08-31 15:13:52 +0200

    PCHR-3945: (Access Rights) Move apis/, models/ into services/

--- contactaccessrights/js/src/access-rights/access-rights.models.js
+++ contactaccessrights/js/src/access-rights/access-rights.models.js
@@ -2,10 +2,10 @@
 
 define([
   'common/angular',
-  'access-rights/apis/rights.api',
-  'access-rights/models/location.model',
-  'access-rights/models/region.model',
-  'access-rights/models/right.model',
+  'access-rights/services/apis/rights.api',
+  'access-rights/services/models/location.model',
+  'access-rights/services/models/region.model',
+  'access-rights/services/models/right.model',
   'common/services/api',
   'common/models/model'
 ], function (angular, RightsAPI, Location, Region, Right) {
