d47171312bfbdc2596a6f8d2201539012def6efa
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-09-01 12:26:02 +0200

    PCHR-3945: (Contact Summary) Initialize settings.debug correctly

--- contactsummary/js/src/contact-summary/contact-summary.constants.js
+++ contactsummary/js/src/contact-summary/contact-summary.constants.js
@@ -9,7 +9,7 @@ define([
   angular.module('contactsummary.constants', []).constant('settings', {
     classNamePrefix: 'contactSummary-',
     contactId: decodeURIComponent((new RegExp('[?|&]cid=([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null,
-    debug: true,
+    debug: +CRM.debug,
     pathBaseUrl: CRM.vars.contactsummary.baseURL + '/',
     pathTpl: 'views/',
     CRM: {
