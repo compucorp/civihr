76433af744bb9c2458120160b1326e5cb05e759b
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-09-01 12:20:41 +0200

    PCHR-3945: (Contact Summary) Remove unused settings

--- contactsummary/js/src/contact-summary/contact-summary.constants.js
+++ contactsummary/js/src/contact-summary/contact-summary.constants.js
@@ -10,8 +10,6 @@ define([
     classNamePrefix: 'contactSummary-',
     contactId: decodeURIComponent((new RegExp('[?|&]cid=([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null,
     debug: true,
-    pathApp: '',
-    pathRest: CRM.url('civicrm/ajax/rest'),
     pathBaseUrl: CRM.vars.contactsummary.baseURL + '/',
     pathTpl: 'views/',
     CRM: {
