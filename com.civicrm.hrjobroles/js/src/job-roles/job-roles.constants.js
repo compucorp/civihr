31249cbe2002e840b77ef5b2e43c07af0f2dcf62
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-09-04 13:39:45 +0200

    PCHR-3945: Wrap .constants modules in IIFE

--- com.civicrm.hrjobroles/js/src/job-roles/job-roles.constants.js
+++ com.civicrm.hrjobroles/js/src/job-roles/job-roles.constants.js
@@ -1,16 +1,18 @@
 /* eslint-env amd */
 /* globals location */
 
-define([
-  'common/angular'
-], function (angular) {
-  'use strict';
+(function (CRM) {
+  define([
+    'common/angular'
+  ], function (angular) {
+    'use strict';
 
-  angular.module('hrjobroles.constants', [])
-    .constant('settings', {
-      classNamePrefix: 'hrjobroles-',
-      contactId: decodeURIComponent((new RegExp('[?|&]cid=([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null,
-      debug: +CRM.debug,
-      baseUrl: CRM.vars.hrjobroles.baseURL + '/js/src/job-roles/'
-    });
-});
+    angular.module('hrjobroles.constants', [])
+      .constant('settings', {
+        classNamePrefix: 'hrjobroles-',
+        contactId: decodeURIComponent((new RegExp('[?|&]cid=([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null,
+        debug: +CRM.debug,
+        baseUrl: CRM.vars.hrjobroles.baseURL + '/js/src/job-roles/'
+      });
+  });
+}(CRM));
--- contactaccessrights/js/src/access-rights/access-rights.constants.js
+++ contactaccessrights/js/src/access-rights/access-rights.constants.js
@@ -1,13 +1,15 @@
 /* eslint-env amd */
 
-define([
-  'common/angular'
-], function (angular) {
-  'use strict';
+(function (CRM) {
+  define([
+    'common/angular'
+  ], function (angular) {
+    'use strict';
 
-  angular.module('access-rights.constants', [])
-    .constant('settings', {
-      debug: +CRM.debug,
-      baseUrl: CRM.vars.contactAccessRights.baseURL + '/js/src/access-rights/'
-    });
-});
+    angular.module('access-rights.constants', [])
+      .constant('settings', {
+        debug: +CRM.debug,
+        baseUrl: CRM.vars.contactAccessRights.baseURL + '/js/src/access-rights/'
+      });
+  });
+}(CRM));
