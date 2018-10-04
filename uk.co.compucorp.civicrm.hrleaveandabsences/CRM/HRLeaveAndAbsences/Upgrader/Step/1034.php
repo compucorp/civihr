60b8392b55ff782b48abb2ce755e8e1978163a62
Samson Alajede <ajesamson@yahoo.com>
2018-10-04 10:26:44 +0100

    PCHR-4213: PR Feedback - Code refactoring

--- uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader/Step/1034.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader/Step/1034.php
@@ -36,25 +36,12 @@ trait CRM_HRLeaveAndAbsences_Upgrader_Step_1034 {
    * Ensures absence type category option group and its values exist, creating one if not
    */
   private function up1034_createCategoryOptionValues() {
-    CRM_Core_BAO_OptionGroup::ensureOptionGroupExists([
+    $result = civicrm_api3('OptionGroup', 'getcount', [
       'name' => 'hrleaveandabsences_absence_type_category',
-      'title' => ts('Category'),
-      'is_reserved' => 1,
     ]);
-
-    $options = [
-      ['name' => 'leave', 'label' => ts('Leave')],
-      ['name' => 'sickness', 'label' => ts('Sickness')],
-      ['name' => 'toil', 'label' => ts('TOIL')],
-      ['name' => 'custom', 'label' => ts('Custom')],
-    ];
-    foreach ($options as $option) {
-      CRM_Core_BAO_OptionValue::ensureOptionValueExists([
-        'option_group_id' => 'hrleaveandabsences_absence_type_category',
-        'name' => $option['name'],
-        'label' => $option['label'],
-        'is_active' => TRUE,
-      ]);
+    if ($result === 0) {
+      $file = $this->extensionDir . '/xml/option_groups/absence_type_category_install.xml';
+      $this->executeCustomDataFileByAbsPath($file);
     }
   }
 }
--- uk.co.compucorp.civicrm.hrleaveandabsences/sql/auto_install.sql
+++ uk.co.compucorp.civicrm.hrleaveandabsences/sql/auto_install.sql
@@ -31,7 +31,7 @@ CREATE TABLE `civicrm_hrleaveandabsences_absence_type` (
      `is_sick` tinyint   DEFAULT 0 COMMENT 'A flag which is used to determine if this Absence Type can be used for a Sickness Request',
      `calculation_unit` varchar(512) NOT NULL   COMMENT 'One of the values of the Absence type calculation units option group',
      `hide_label` tinyint   DEFAULT 0 COMMENT 'This controls the visibility of the Leave Type label in the calendar and feeds.',
-     `category` int unsigned NOT NULL   COMMENT 'This is used for grouping leave types.',
+     `category` varchar(10) NOT NULL   COMMENT 'This is used for grouping leave types.',
     PRIMARY KEY ( `id` ),
     UNIQUE INDEX `hrleaveandabsences_absence_type_title`(title)
 
