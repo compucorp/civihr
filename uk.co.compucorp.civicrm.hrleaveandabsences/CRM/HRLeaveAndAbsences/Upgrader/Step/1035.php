14d6022f519677b75474012f4a10c9857e7b8d4c
Samson Alajede <ajesamson@yahoo.com>
2018-10-22 15:16:34 +0100

    PCHR-4217: Schema fixes for absence type log table

--- uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader/Step/1035.php
+++ uk.co.compucorp.civicrm.hrleaveandabsences/CRM/HRLeaveAndAbsences/Upgrader/Step/1035.php
@@ -17,6 +17,10 @@ trait CRM_HRLeaveAndAbsences_Upgrader_Step_1035 {
     $absenceTypeTable = AbsenceType::getTableName();
     if (SchemaHandler::checkIfFieldExists($absenceTypeTable, 'is_default')) {
       CRM_Core_DAO::executeQuery("ALTER TABLE {$absenceTypeTable} DROP COLUMN is_default");
+      // This helps to fix issues with trigger when logging is enabled
+      // as a result of log table structure not being updated with column modifications
+      $logging = new CRM_Logging_Schema();
+      $logging->fixSchemaDifferencesFor($absenceTypeTable, [], TRUE);
     }
 
     return TRUE;
