a783632bcb61bedd1fca7a92ea8457528c522dae
Igor Pavlov <pavlov.org@gmail.com>
2018-11-27 13:47:56 +0000

    PCHR-4243: PR Feedback

--- org.civicrm.reqangular/js/src/common/models/instances/job-role-instance.js
+++ org.civicrm.reqangular/js/src/common/models/instances/job-role-instance.js
@@ -12,12 +12,12 @@ define([
     function (ModelInstance, HRSettings) {
       return ModelInstance.extend({
 
-      /**
-       * Override of parent method
-       *
-       * @param {Object} result - The accumulator object
-       * @param {String} key - The property name
-       */
+        /**
+         * Override of parent method
+         *
+         * @param {Object} result - The accumulator object
+         * @param {String} key - The property name
+         */
         fromAPIFilter: function (result, __, key) {
           var dateFormat = (HRSettings.DATE_FORMAT || 'YYYY-MM-DD').toUpperCase();
 
@@ -32,20 +32,18 @@ define([
         },
 
         /**
-       * Override of parent method
-       *
-       * @param {Object} result - The accumulator object
-       * @param {String} key - The property name
-       */
+         * Override of parent method
+         *
+         * @param {Object} result - The accumulator object
+         * @param {String} key - The property name
+         */
         toAPIFilter: function (result, __, key) {
           var blackList = ['contact_id', 'job_contract_id'];
           var dateFormat = (HRSettings.DATE_FORMAT || 'YYYY-MM-DD').toUpperCase();
 
           if (_.endsWith(key, '_date')) {
             result[key] = moment(this[key], dateFormat).format('YYYY-MM-DD HH:mm:ss');
-          } else if (_.includes(blackList, key)) {
-
-          } else {
+          } else if (!_.includes(blackList, key)) {
             result[key] = this[key];
           }
         }
