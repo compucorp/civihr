7d7f5a9355c161eaafa4c18fefca52e9766dadfd
Igor Pavlov <pavlov.org@gmail.com>
2018-12-07 10:56:38 +0000

    PCHR-4437: Allow public holidays in multiple leave types

--- hrjobcontract/js/src/job-contract/controllers/form/form-leave.controller.js
+++ hrjobcontract/js/src/job-contract/controllers/form/form-leave.controller.js
@@ -19,7 +19,6 @@ define([
      */
     (function init () {
       loadNumberOfPublicHolidays();
-      initWatchers();
     }());
 
     /**
@@ -30,37 +29,6 @@ define([
         .then(function (number) {
           vm.numberOfPublicHolidays = number;
         });
-    }
-
-    /**
-     * Attach listeners to $scope
-     */
-    function initWatchers () {
-      $scope.$watch('entity.leave', toggleAddPublicHolidayRadios, true);
-    }
-
-    /**
-     * This is a listener for when any of the leave types in the form changes.
-     *
-     * Only one leave type can have "add_public_holidays" selected. This function
-     * checks if we have a type where its value changed from false to true and, if so,
-     * set "add_public_holidays" to false for every other type.
-     *
-     * @param {Object} newValue - An object containing the new leave type values
-     * @param {Object} oldValue - An object containing the old leave type values
-     */
-    function toggleAddPublicHolidayRadios (newValue, oldValue) {
-      var newLeaveWithPublicHolidays = _.find(newValue, function (value, i) {
-        return value.add_public_holidays && !oldValue[i].add_public_holidays;
-      });
-
-      if (newLeaveWithPublicHolidays) {
-        newValue.forEach(function (value) {
-          if (value.leave_type !== newLeaveWithPublicHolidays.leave_type) {
-            value.add_public_holidays = false;
-          }
-        });
-      }
     }
 
     return vm;
