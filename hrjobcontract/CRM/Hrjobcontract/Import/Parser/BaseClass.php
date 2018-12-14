63b9decf8bbd6ca48e19495a52800cd36460ba05
Tunbola Ogunwande <tunbolawande@yahoo.com>
2018-12-14 17:10:51 +0100

    PCHR-4446: Use class property for $_mapperLocType variable

--- hrjobcontract/CRM/Hrjobcontract/Import/Parser/BaseClass.php
+++ hrjobcontract/CRM/Hrjobcontract/Import/Parser/BaseClass.php
@@ -83,7 +83,7 @@ class CRM_Hrjobcontract_Import_Parser_BaseClass extends CRM_Hrjobcontract_Import
       $this->addField($name, CRM_Utils_Array::value('title', $field, $name), $field['type'], $field['headerPattern'], $field['dataPattern']);
     }
 
-    $mapperLocType = !empty($mapperLocType) ? $mapperLocType : [];
+    $mapperLocType = !empty($this->_mapperLocType) ? $this->_mapperLocType : [];
     $this->setActiveFields($this->_mapperKeys);
     $this->setActiveFieldLocationTypes($mapperLocType);
 
